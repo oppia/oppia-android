a697532da9f01e5861aa060bc614888e15611986
Ben Henning <henning.benmax@gmail.com>
2019-07-11 19:22:08 -0700

    Introduce data specification module.
    This introduces a protolite-based data specification module that can be
    imported in other modules (both Java and Kotlin supported). This
    approach is based on various resources including
    https://medium.com/@elye.project/android-protobuf-in-kotlin-a1c33014a4e2,
    https://proandroiddev.com/how-to-setup-your-android-app-to-use-protobuf-96132340de5c,
    https://github.com/google/protobuf-gradle-plugin, and others.

--- app/src/main/java/org/oppia/app/HomeActivity.kt
+++ app/src/main/java/org/oppia/app/HomeActivity.kt
@@ -2,6 +2,8 @@ package org.oppia.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import org.oppia.app.model.UserAppHistory
 
 /** The central activity for all users entering the app. */
 class HomeActivity : AppCompatActivity() {
@@ -9,5 +11,22 @@ class HomeActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.home_activity)
+  }
+
+  override fun onStart() {
+    super.onStart()
+    val userAppHistory = getUserAppHistory()
+    if (userAppHistory.alreadyOpenedApp) {
+      getWelcomeTextView().setText(R.string.welcome_back_text)
+    }
+  }
+
+  private fun getWelcomeTextView(): TextView {
+    return findViewById(R.id.welcome_text_view)
+  }
+
+  private fun getUserAppHistory(): UserAppHistory {
+    // TODO(BenHenning): Retrieve this from a domain provider.
+    return UserAppHistory.newBuilder().setAlreadyOpenedApp(false).build()
   }
 }
