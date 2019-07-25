51176f4bd2ad3256da9a067f44f133a32f71eb95
Ben Henning <henning.benmax@gmail.com>
2019-07-25 13:07:48 -0700

    Clean up gitignore filters and fix Javadoc in AsyncDataSource interface.

--- utility/src/main/java/org/oppia/util/data/AsyncDataSource.kt
+++ utility/src/main/java/org/oppia/util/data/AsyncDataSource.kt
@@ -6,7 +6,7 @@ import com.google.common.util.concurrent.ListenableFuture
  * Represents a source of data that can be delivered and changed asynchronously.
  *
  * @param <T> The type of data being provided by this data source.
-</T> */
+ */
 interface AsyncDataSource<T> {
   // TODO(BenHenning): Finalize the interfaces for this API beyond a basic prototype for the initial project intro.
 
