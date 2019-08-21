36b6e18a3fcf811f86139e46ebbc6a1c42c6ba8e
veena <veena14cs@gmail.com>
2019-08-21 03:37:54 -0700

    created background for splashtheme
    This is to avoid black or white screen when app is launched and resources are not yet initialized

--- app/src/main/java/org/oppia/app/splash/SplashActivity.kt
+++ app/src/main/java/org/oppia/app/splash/SplashActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentTransaction
 
 import android.os.Bundle
+import android.view.WindowManager
 import com.example.myapplication.SplashFragment
 import org.oppia.app.R
 
@@ -13,6 +14,9 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         val fragment = SplashFragment()
         val transaction = supportFragmentManager.beginTransaction()
