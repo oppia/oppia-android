8f15b20398a2cdec9cf1ff0dd8b04f2c90b79458
veena <veena14cs@gmail.com>
2019-08-22 03:08:54 -0700

    Fixed issues
    - Removed unwanted spaces
    - Removed wait time during transition
    - Added Javadoc comment
    - created background image for splashActivity and it is set to theme.
    - Added animation to SplashTheme styles.

--- app/src/main/java/org/oppia/app/splash/SplashActivity.kt
+++ app/src/main/java/org/oppia/app/splash/SplashActivity.kt
@@ -1,10 +1,15 @@
 package org.oppia.app.splash
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import org.oppia.app.R
+import android.content.res.Configuration
 
+/*
+* This activity acts as a container to SplashFragment
+* There is a background theme set to this activity to display app logo until the app is initialized
+*/
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/org/oppia/app/splash/SplashFragment.kt
+++ app/src/main/java/org/oppia/app/splash/SplashFragment.kt
@@ -2,7 +2,6 @@ package org.oppia.app.splash
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +9,10 @@ import androidx.fragment.app.Fragment
 import org.oppia.app.HomeActivity
 import org.oppia.app.R
 
+/*
+* The SplashFragments navigates to Home page once
+* the app is finished loading completely
+* */
 
 class SplashFragment : Fragment() {
 
@@ -22,36 +25,19 @@ class SplashFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_splash, container, false)
 
-//        loadSplashScreen()
         routeToHomePage();
+
         return view
     }
-
-    private fun loadSplashScreen() {
-
-        // TODO(Veena): Replace below code to load profile data and then redirect to HomePage
-
-        Handler().postDelayed(// Using handler with postDelayed called runnable run method
-
-        {
-            routeToHomePage()
-        }, (3 * 1000).toLong()) // wait for 5 seconds
-
-    }
-
-
     private fun routeToHomePage() {
-        val intent = Intent(requireContext(), HomeActivity::class.java)
 
+        val intent = Intent(requireContext(), HomeActivity::class.java)
         startActivity(intent)
 
         // close this activity
-
         activity!!.finish()
-        activity!!.overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
 
     }
-
 
 }
 
