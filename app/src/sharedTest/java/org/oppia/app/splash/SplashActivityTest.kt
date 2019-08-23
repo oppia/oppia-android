5ae198cdec4c9320ea3e93ef12eb9b0a594373c7
veena <veena14cs@gmail.com>
2019-08-23 04:23:29 -0700

    working on test cases

---
+++ app/src/sharedTest/java/org/oppia/app/splash/SplashActivityTest.kt
@@ -0,0 +1,19 @@
+package org.oppia.app.splash
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.util.FragmentTestUtil
+
+
+
+/** Tests for [SplashActivity]. */
+@RunWith(AndroidJUnit4::class)
+class SplashActivityTest {
+  @get:Rule
+  val activityActivityTestRule = ActivityScenarioRule(SplashActivity::class.java)
+
+
+}
---
+++ app/src/sharedTest/java/org/oppia/app/splash/SplashFragmentTest.kt
@@ -0,0 +1,15 @@
+package org.oppia.app.splash
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.junit.After
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.Rule
+
+class SplashFragmentTest {
+
+  @get:Rule
+  val activityActivityTestRule = ActivityScenarioRule(SplashActivity::class.java)
+
+}
