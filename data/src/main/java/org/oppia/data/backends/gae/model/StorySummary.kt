3790e9953e9c4c6865f503e6ef98b5da7154129e
Rajat Talesra <talesra@talesra-macbookpro.roam.corp.google.com>
2019-08-26 17:45:57 +0530

    Linked models to python code

--- data/src/main/java/org/oppia/data/backends/gae/model/StorySummary.kt
+++ data/src/main/java/org/oppia/data/backends/gae/model/StorySummary.kt
@@ -3,8 +3,9 @@ package org.oppia.data.backends.gae.model
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
-/** Model for serialization/deserialization using Moshi with Retrofit */
-/** Oppia-Web StorySummary structure link: https://github.com/oppia/oppia/blob/b33aa9cf1aa6372e12d0b35f95cceb44efe5320f/core/domain/story_domain.py#L1118 */
+/** Model for serialization/deserialization using Moshi with Retrofit
+ * @see <a href="https://github.com/oppia/oppia/blob/b33aa9cf1aa6372e12d0b35f95cceb44efe5320f/core/domain/story_domain.py#L1118">Oppia-Web StorySummary structure</a>
+ */
 @JsonClass(generateAdapter = true)
 data class StorySummary(
 
