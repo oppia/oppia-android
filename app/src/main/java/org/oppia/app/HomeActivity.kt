b8968c5d325316fdf781c0bb8d527c25881800f9
veena <veena14cs@gmail.com>
2019-08-26 16:40:11 +0530

    Created logging system
    - Added necessary permissions
    - Create a text file to store logs

--- app/src/main/java/org/oppia/app/HomeActivity.kt
+++ app/src/main/java/org/oppia/app/HomeActivity.kt
@@ -1,57 +1,14 @@
 package org.oppia.app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.TextView
-import com.google.common.util.concurrent.FutureCallback
-import com.google.common.util.concurrent.Futures
-import com.google.common.util.concurrent.ListenableFuture
-import com.google.common.util.concurrent.MoreExecutors.directExecutor
-import org.oppia.app.model.UserAppHistory
-import org.oppia.app.utility.Logger
-import org.oppia.util.data.AsyncDataSource
+import androidx.appcompat.app.AppCompatActivity
 
 /** The central activity for all users entering the app. */
 class HomeActivity : AppCompatActivity() {
 
-  val TAG = HomeActivity::class.java.getName()
-
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.home_activity)
-    Logger.d(applicationContext,TAG,"Welcome to Oppia")
-  }
-
-  override fun onStart() {
-    super.onStart()
-    // TODO(BenHenning): Convert this to LiveData rather than risk an async operation completing outside of the
-    // activity's lifecycle. Also, a directExecutor() in this context could be a really bad idea if it isn't the main
-    // thread.
-    Futures.addCallback(getUserAppHistory().pendingOperation, object: FutureCallback<UserAppHistory> {
-      override fun onSuccess(result: UserAppHistory?) {
-        if (result != null && result.alreadyOpenedApp) {
-          getWelcomeTextView().setText(R.string.welcome_back_text)
-          Logger.d(applicationContext,TAG,"Welcome back to oppia")
-        }
-      }
-
-      override fun onFailure(t: Throwable) {
-        // TODO(BenHenning): Replace this log statement with a clearer logging API.
-        Log.e("HomeActivity", "Failed to retrieve user app history", t)
-      }
-    }, directExecutor())
-  }
-
-  private fun getWelcomeTextView(): TextView {
-    return findViewById(R.id.welcome_text_view)
-  }
-
-  private fun getUserAppHistory(): AsyncDataSource<UserAppHistory> {
-    // TODO(BenHenning): Retrieve this from a domain provider.
-    return object: AsyncDataSource<UserAppHistory> {
-      override val pendingOperation: ListenableFuture<UserAppHistory>
-        get() = Futures.immediateFuture(UserAppHistory.newBuilder().setAlreadyOpenedApp(false).build())
-    }
+    supportFragmentManager.beginTransaction().add(R.id.home_fragment_placeholder, HomeFragment()).commitNow()
   }
 }
--- app/src/main/java/org/oppia/app/application/OppiaApplication.kt
+++ app/src/main/java/org/oppia/app/application/OppiaApplication.kt
@@ -1,16 +1,72 @@
 package org.oppia.app.application
 
+import android.Manifest.permission.*
 import android.app.Application
-import android.util.Log
+import android.os.Environment
+import org.oppia.app.utility.PermissionUtils
 
-import com.hypertrack.hyperlog.HyperLog
+import java.io.File
+import java.io.IOException
 
+/** Called when the application is starting, before any activity, service, or receiver objects (excluding content providers) have been created.
+ */
 class OppiaApplication : Application() {
 
-    override fun onCreate() {
-        super.onCreate()
-
-        HyperLog.initialize(this)
-//        HyperLog.setURL("<Set URL>")    // if log has to be posted to server. Set the url here
+  /* Checks if external storage is available for read and write */
+  val isExternalStorageWritable: Boolean
+    get() {
+      val state = Environment.getExternalStorageState()
+      return if (Environment.MEDIA_MOUNTED == state) {
+        true
+      } else false
     }
+
+  /* Checks if external storage is available to at least read */
+  val isExternalStorageReadable: Boolean
+    get() {
+      val state = Environment.getExternalStorageState()
+      return if (Environment.MEDIA_MOUNTED == state || Environment.MEDIA_MOUNTED_READ_ONLY == state) {
+        true
+      } else false
+    }
+
+  override fun onCreate() {
+    super.onCreate()
+    PermissionUtils.checkPermissions(applicationContext, *permissions)
+    PermissionUtils.requestPermissions(applicationContext, 1, *permissions)
+
+    if (isExternalStorageWritable) {
+      val appDirectory = File(applicationContext.getExternalFilesDir(null), "OppiaLogFile")
+      val logDirectory = File("$appDirectory/log")
+      val logFile = File(logDirectory, "logcat" + System.currentTimeMillis() + ".txt")
+
+      // create app folder
+      if (!appDirectory.exists()) {
+        appDirectory.mkdir()
+      }
+
+      // create log folder
+      if (!logDirectory.exists()) {
+        logDirectory.mkdir()
+      }
+
+      // clear the previous logcat and then write the new one to the file
+      try {
+        var process = Runtime.getRuntime().exec("logcat -c")
+        process = Runtime.getRuntime().exec("logcat -f $logFile")
+      } catch (e: IOException) {
+        e.printStackTrace()
+      }
+
+    } else if (isExternalStorageReadable) {
+      // only readable
+    } else {
+      // not accessible
+    }
+  }
+
+  companion object {
+    internal var permissions = arrayOf(READ_LOGS, WRITE_EXTERNAL_STORAGE,READ_EXTERNAL_STORAGE)
+  }
 }
+
