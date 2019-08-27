141ae322394a3c4676475c25a0ae8448bcc64c89
veena <veena14cs@gmail.com>
2019-08-27 12:14:19 +0530

    writting unit test

--- app/src/sharedTest/java/org/oppia/app/splash/SplashActivityTest.kt
+++ app/src/sharedTest/java/org/oppia/app/splash/SplashActivityTest.kt
@@ -1,12 +1,18 @@
 package org.oppia.app.splash
 
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.util.FragmentTestUtil
-
+import org.junit.Before
+import org.oppia.app.R
 
 
 /** Tests for [SplashActivity]. */
@@ -15,5 +21,11 @@ class SplashActivityTest {
   @get:Rule
   val activityActivityTestRule = ActivityScenarioRule(SplashActivity::class.java)
 
+  @Test
+  fun tournamentsContainerIsDisplayed() {
+    launchFragmentInContainer<SplashFragment>()
 
+    onView(withId(R.id.fragment_container))
+      .check(matches(isDisplayed()))
+  }
 }
