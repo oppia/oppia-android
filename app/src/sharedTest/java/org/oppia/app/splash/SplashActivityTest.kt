e78eaaec0b8ee122367ad16044819b2a3984189e
veena <veena14cs@gmail.com>
2019-08-28 10:05:08 +0530

    Workinh on writting testcase for fragment

--- app/src/sharedTest/java/org/oppia/app/splash/SplashActivityTest.kt
+++ app/src/sharedTest/java/org/oppia/app/splash/SplashActivityTest.kt
@@ -1,19 +1,51 @@
 package org.oppia.app.splash
 
+import android.app.PendingIntent.getActivity
+import android.widget.FrameLayout
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import kotlinx.android.synthetic.main.activity_splash.view.*
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.util.FragmentTestUtil
+import org.oppia.app.HomeActivity
+import org.oppia.app.R
+import androidx.test.rule.ActivityTestRule
 
 
 
 /** Tests for [SplashActivity]. */
 @RunWith(AndroidJUnit4::class)
 class SplashActivityTest {
+
+
   @get:Rule
-  val activityActivityTestRule = ActivityScenarioRule(SplashActivity::class.java)
+  var activityTestRule: ActivityTestRule<SplashActivity> = ActivityTestRule(SplashActivity::class.java)
+
+
+  private var splashActivity: SplashActivity? = null
+  private var supportFM: FragmentTransaction? = null
+
+  @Before
+  fun setUp() {
+    splashActivity = activityTestRule.getActivity()
+    supportFM = splashActivity!!.supportFragmentManager.beginTransaction();
+
+
+  }
+
+  @Test
+  fun testEvent() {
+
+      supportFM!!.add(R.id.fragment_container, SplashFragment()).commitNow()
+
+  }
 
 
 }
+
--- app/src/sharedTest/java/org/oppia/app/splash/SplashFragmentTest.kt
+++ app/src/sharedTest/java/org/oppia/app/splash/SplashFragmentTest.kt
@@ -1,15 +1,27 @@
 package org.oppia.app.splash
 
+import android.content.Context
+import android.widget.FrameLayout
+import androidx.fragment.app.FragmentTransaction
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.After
 import org.junit.Before
 
 import org.junit.Assert.*
 import org.junit.Rule
+import org.junit.Test
+import org.oppia.app.HomeActivity
+import org.oppia.app.R
 
+import androidx.test.rule.ActivityTestRule
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
 class SplashFragmentTest {
 
-  @get:Rule
-  val activityActivityTestRule = ActivityScenarioRule(SplashActivity::class.java)
+
 
 }
