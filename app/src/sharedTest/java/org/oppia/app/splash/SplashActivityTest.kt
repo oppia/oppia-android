c48b2556c32adb70c9c4b8f27225dd33c2d695b5
veena <veena14cs@gmail.com>
2019-08-28 10:22:14 +0530

    Fixing issue with test case

--- app/src/sharedTest/java/org/oppia/app/splash/SplashActivityTest.kt
+++ app/src/sharedTest/java/org/oppia/app/splash/SplashActivityTest.kt
@@ -1,6 +1,6 @@
 package org.oppia.app.splash
 
-import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.fragment.app.testing.launchFragment
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
@@ -10,10 +10,7 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.util.FragmentTestUtil
-import org.junit.Before
 import org.oppia.app.R
-
 
 /** Tests for [SplashActivity]. */
 @RunWith(AndroidJUnit4::class)
@@ -22,8 +19,8 @@ class SplashActivityTest {
   val activityActivityTestRule = ActivityScenarioRule(SplashActivity::class.java)
 
   @Test
-  fun tournamentsContainerIsDisplayed() {
-    launchFragmentInContainer<SplashFragment>()
+  fun splashContainerIsDisplayed() {
+    launchFragment<SplashFragment>()
 
     onView(withId(R.id.fragment_container))
       .check(matches(isDisplayed()))
