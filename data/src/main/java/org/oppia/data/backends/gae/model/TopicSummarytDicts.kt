c64e0e19e1c88af0a12f0c56b5273bc7efdd10ea
veena <veena14cs@gmail.com>
2019-08-29 13:25:54 +0530

    Update TopicSummarytDicts.kt

--- data/src/main/java/org/oppia/data/backends/gae/model/TopicSummarytDicts.kt
+++ data/src/main/java/org/oppia/data/backends/gae/model/TopicSummarytDicts.kt
@@ -9,7 +9,7 @@ data class TopicSummarytDicts(
   @Json(name = "id") val id: String?,
   @Json(name = "category") val category: String?,
   @Json(name = "title") val title: String?,
-  @Json(name = "lesson_count") val num_of_lessons: String?,
+  @Json(name = "lesson_count") val lesson_count: Int?,
   @Json(name = "last_updated_msec") val last_updated_msec: Double?
 
 )
