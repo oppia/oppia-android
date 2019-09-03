0024f52af119626865652468258b5b705bae8a52
veena <veena14cs@gmail.com>
2019-09-03 12:52:57 +0530

    Update SplashFragment.kt

--- app/src/main/java/org/oppia/app/splash/SplashFragment.kt
+++ app/src/main/java/org/oppia/app/splash/SplashFragment.kt
@@ -13,16 +13,12 @@ import org.oppia.app.R
 
 class SplashFragment : Fragment() {
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-  }
-
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
     val view = inflater.inflate(R.layout.splash_fragment, container, false)
-    routeToHomePage();
+    routeToHomePage()
     return view
   }
 
