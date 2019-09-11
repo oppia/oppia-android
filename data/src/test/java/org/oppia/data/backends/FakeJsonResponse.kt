0b335e18217fa8da81e2355da7045abf8fde7aad
Rajat Talesra <talesra@talesra-macbookpro.roam.corp.google.com>
2019-09-11 13:52:30 +0530

    Classroom service test cases

--- data/src/test/java/org/oppia/data/backends/FakeJsonResponse.kt
+++ data/src/test/java/org/oppia/data/backends/FakeJsonResponse.kt
@@ -13,5 +13,8 @@ object FakeJsonResponse {
   /** Fake json response for [MockTopicService] */
   const val TOPIC_SERVICE_RESPONSE: String =
     "{\"canonical_story_dicts\":[{\"title\":\"Story 1\",\"description\":\"Story Description\",\"id\":\"4wv9hSZ4F67I\"}],\"is_moderator\":true,\"is_admin\":true,\"topic_name\":\"Topic1\",\"username\":\"rt4914\",\"skill_descriptions\":{\"A9j9taXAqqkV\":\"Skill 1\"},\"user_email\":\"test@example.com\",\"iframed\":false,\"additional_story_dicts\":[],\"additional_angular_modules\":[],\"is_topic_manager\":false,\"uncategorized_skill_ids\":[],\"topic_id\":\"baWJOUFeUAnn\",\"degrees_of_mastery\":{\"A9j9taXAqqkV\":null},\"subtopics\":[{\"skill_ids\":[\"A9j9taXAqqkV\"],\"title\":\"Subtopic 1\",\"id\":1}],\"is_super_admin\":true}"
+  /** Fake json response for [MockClassroomService] */
+  const val CLASSROOM_SERVICE_RESPONSE: String =
+    "{\"is_super_admin\":true,\"topic_summary_dicts\":[{\"version\":3,\"language_code\":\"en\",\"subtopic_count\":0,\"canonical_story_count\":1,\"uncategorized_skill_count\":0,\"topic_model_last_updated\":1567587844908.068,\"id\":\"BpslppK1Tb89\",\"additional_story_count\":0,\"total_skill_count\":0,\"topic_model_created_on\":1567587801448.179,\"name\":\"Math\"}],\"user_email\":\"test@example.com\",\"iframed\":false,\"username\":\"veena\",\"is_topic_manager\":false,\"is_moderator\":true,\"is_admin\":true}"
 
 }
