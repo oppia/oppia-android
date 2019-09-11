707626995100f0cb4a06d6aa28642095c22cfdf8
marysoloman <nikitamarysolomanpvt@gmail.com>
2019-09-11 21:35:49 +0530

    removed parentActivity,navigation  navigation function onmenuitem clicked is moved to NavigationFragmentDrawer,new directory help with HelpActivity,HelpActivityController,HelpFragmentController,EOF issues fix, review suggestions in pr #84 fixes

--- app/src/main/java/org/oppia/app/home/HomeActivity.kt
+++ app/src/main/java/org/oppia/app/home/HomeActivity.kt
@@ -1,10 +1,7 @@
 package org.oppia.app.home
 
 import android.os.Bundle
-import com.google.gson.GsonBuilder
-import com.google.gson.annotations.SerializedName
 import org.oppia.app.activity.InjectableAppCompatActivity
-import java.io.FileReader
 import javax.inject.Inject
 
 /** The central activity for all users entering the app. */
