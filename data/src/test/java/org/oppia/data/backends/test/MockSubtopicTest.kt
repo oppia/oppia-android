cbe94173ae5687f4f0f39a92fc2cf0710d41fdf5
Rajat Talesra <rajattalesra4914@gmail.com>
2019-09-17 17:19:44 +0530

    Fix part of #5: Abstraction on top of Oppia GAE for Subtopic Management System [Blocked: #78] (#102)
    * Subtopic service, model and testcases
    
    * Reformatting code
    
    * Nit changes

--- data/src/test/java/org/oppia/data/backends/test/MockSubtopicTest.kt
+++ data/src/test/java/org/oppia/data/backends/test/MockSubtopicTest.kt
@@ -6,20 +6,20 @@ import okhttp3.OkHttpClient
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.oppia.data.backends.api.MockClassroomService
+import org.oppia.data.backends.api.MockSubtopicService
 import org.oppia.data.backends.gae.NetworkInterceptor
 import org.oppia.data.backends.gae.NetworkSettings
-import org.oppia.data.backends.gae.api.ClassroomService
+import org.oppia.data.backends.gae.api.SubtopicService
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.mock.MockRetrofit
 import retrofit2.mock.NetworkBehavior
 
 /**
- * Test for [ClassroomService] retrofit instance using [MockClassroomService]
+ * Test for [SubtopicService] retrofit instance using [MockSubtopicService]
  */
 @RunWith(AndroidJUnit4::class)
-class MockClassroomTest {
+class MockSubtopicTest {
   private lateinit var mockRetrofit: MockRetrofit
   private lateinit var retrofit: Retrofit
 
@@ -41,14 +41,14 @@ class MockClassroomTest {
   }
 
   @Test
-  fun testClassroomService_usingFakeJson_deserializationSuccessful() {
-    val delegate = mockRetrofit.create(ClassroomService::class.java)
-    val mockClassroomService = MockClassroomService(delegate)
+  fun testSubtopicService_usingFakeJson_deserializationSuccessful() {
+    val delegate = mockRetrofit.create(SubtopicService::class.java)
+    val mockSubtopicService = MockSubtopicService(delegate)
 
-    val classroom = mockClassroomService.getClassroom("Math")
-    val classroomResponse = classroom.execute()
+    val subtopic = mockSubtopicService.getSubtopic("Subtopic 1", "randomId")
+    val subtopicResponse = subtopic.execute()
 
-    assertThat(classroomResponse.isSuccessful).isTrue()
-    assertThat(classroomResponse.body()!!.topicSummaryDicts?.get(0)?.name).isEqualTo("Math")
+    assertThat(subtopicResponse.isSuccessful).isTrue()
+    assertThat(subtopicResponse.body()!!.subtopicTitle).isEqualTo("Subtopic 1")
   }
 }
