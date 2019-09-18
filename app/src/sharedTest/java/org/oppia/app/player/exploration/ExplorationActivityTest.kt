209ab5cf21bae43f9b37177c863c6772237319d6
Rajat Talesra <talesra@talesra-macbookpro.roam.corp.google.com>
2019-09-18 13:30:26 +0530

    Code checking and refactoring in ExplorationActivityTest

--- app/src/sharedTest/java/org/oppia/app/player/exploration/ExplorationActivityTest.kt
+++ app/src/sharedTest/java/org/oppia/app/player/exploration/ExplorationActivityTest.kt
@@ -31,15 +31,6 @@ import javax.inject.Singleton
 /** Tests for [ExplorationActivity]. */
 @RunWith(AndroidJUnit4::class)
 class ExplorationActivityTest {
-  @Before
-  fun setUp() {
-    IdlingRegistry.getInstance().register(MainThreadExecutor.countingResource)
-  }
-
-  @After
-  fun tearDown() {
-    IdlingRegistry.getInstance().unregister(MainThreadExecutor.countingResource)
-  }
 
   @Test
   fun testExplorationActivity_loadExplorationFragment_hasDummyString() {
@@ -67,13 +58,6 @@ class ExplorationActivityTest {
     fun provideBackgroundDispatcher(@BlockingDispatcher blockingDispatcher: CoroutineDispatcher): CoroutineDispatcher {
       return blockingDispatcher
     }
-
-    @Singleton
-    @Provides
-    @BlockingDispatcher
-    fun provideBlockingDispatcher(): CoroutineDispatcher {
-      return MainThreadExecutor.asCoroutineDispatcher()
-    }
   }
 
   @Singleton
@@ -85,47 +69,6 @@ class ExplorationActivityTest {
       fun setApplication(application: Application): Builder
 
       fun build(): TestApplicationComponent
-    }
-  }
-
-  // TODO(#59): Move this to a general-purpose testing library that replaces all CoroutineExecutors with an
-  //  Espresso-enabled executor service. This service should also allow for background threads to run in both Espresso
-  //  and Robolectric to help catch potential race conditions, rather than forcing parallel execution to be sequential
-  //  and immediate.
-  //  NB: This also blocks on #59 to be able to actually create a test-only library.
-  /**
-   * An executor service that schedules all [Runnable]s to run asynchronously on the main thread. This is based on:
-   * https://android.googlesource.com/platform/packages/apps/TV/+/android-live-tv/src/com/android/tv/util/MainThreadExecutor.java.
-   */
-  private object MainThreadExecutor : AbstractExecutorService() {
-    override fun isTerminated(): Boolean = false
-
-    private val handler = Handler(Looper.getMainLooper())
-    val countingResource = CountingIdlingResource("main_thread_executor_counting_idling_resource")
-
-    override fun execute(command: Runnable?) {
-      countingResource.increment()
-      handler.post {
-        try {
-          command?.run()
-        } finally {
-          countingResource.decrement()
-        }
-      }
-    }
-
-    override fun shutdown() {
-      throw UnsupportedOperationException()
-    }
-
-    override fun shutdownNow(): MutableList<Runnable> {
-      throw UnsupportedOperationException()
-    }
-
-    override fun isShutdown(): Boolean = false
-
-    override fun awaitTermination(timeout: Long, unit: TimeUnit?): Boolean {
-      throw UnsupportedOperationException()
     }
   }
 }
