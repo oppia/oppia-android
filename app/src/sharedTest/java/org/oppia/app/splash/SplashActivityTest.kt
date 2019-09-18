5574ca72bd2b740d23dc6134d41e4ee39f127bf3
veena <veena14cs@gmail.com>
2019-09-18 16:47:54 +0530

    Fixed testcases

--- app/src/sharedTest/java/org/oppia/app/splash/SplashActivityTest.kt
+++ app/src/sharedTest/java/org/oppia/app/splash/SplashActivityTest.kt
@@ -1,47 +1,44 @@
 package org.oppia.app.splash
 
 import androidx.fragment.app.Fragment
-import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.Intents
+import androidx.test.rule.ActivityTestRule
 import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.junit.Assert
-import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
 import org.junit.runner.RunWith
 import org.oppia.app.HomeActivity
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.IdlingResource
+import org.junit.*
 
 /** Tests for [SplashActivity]. */
 @RunWith(AndroidJUnit4::class)
 class SplashActivityTest {
 
   @get:Rule
-  var activityRule: IntentsTestRule<SplashActivity> = IntentsTestRule(
-    SplashActivity::class.java
+  var activityTestRule: ActivityTestRule<SplashActivity> = ActivityTestRule(
+    SplashActivity::class.java, true,
+    false
   )
-  var splahActivity: SplashActivity? = null
-  var splashActivityFragment: SplashFragment? = null
 
   @Before
   fun setUp() {
-    splahActivity = activityRule.getActivity();
-    splashActivityFragment = SplashFragment()
+    Intents.init()
+  }
+
+  @After
+  fun tearDown() {
+    Intents.release()
   }
 
   @Test
+  @Throws(InterruptedException::class)
   fun testSplashActivity_initialOpen_routesToHomeActivity() {
-    Assert.assertNotNull(splahActivity)
-    startFragment(splashActivityFragment!!)
-  }
 
-  private fun startFragment(fragment: Fragment) {
-    val fragmentManager = splahActivity?.getSupportFragmentManager()
-    val fragmentTransaction = fragmentManager?.beginTransaction()
-    fragmentTransaction?.add(fragment, null)
-    fragmentTransaction?.commit()
+    activityTestRule.launchActivity(null)
 
     intended(hasComponent(HomeActivity::class.java.getName()))
   }
-
 }
