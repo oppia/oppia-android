f6322a31ef789725e89ba8422e659b2298f5f307
marysoloman <nikitamarysolomanpvt@gmail.com>
2019-09-19 17:17:34 +0530

    TextInputInteractionView

---
+++ app/src/main/java/org/oppia/app/view/TextInputInteractionView.java
@@ -0,0 +1,84 @@
+package org.oppia.app.view;
+
+import android.content.Context;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.method.MovementMethod;
+import android.util.AttributeSet;
+
+
+
+
+    import android.content.Context;
+    import android.text.Editable;
+    import android.text.TextUtils.TruncateAt;
+    import android.text.method.MovementMethod;
+    import android.util.AttributeSet;
+import android.widget.TextView;
+import android.widget.TextView.BufferType;
+
+public class TextInputInteractionView extends TextView {
+  public TextInputInteractionView(Context context) {
+    super((Context)null);
+    throw new RuntimeException("Stub!");
+  }
+
+  public TextInputInteractionView(Context context, AttributeSet attrs) {
+    super((Context)null);
+    throw new RuntimeException("Stub!");
+  }
+
+  public TextInputInteractionView(Context context, AttributeSet attrs, int defStyleAttr) {
+    super((Context)null);
+    throw new RuntimeException("Stub!");
+  }
+
+  public TextInputInteractionView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+    super((Context)null);
+    throw new RuntimeException("Stub!");
+  }
+
+  public boolean getFreezesText() {
+    throw new RuntimeException("Stub!");
+  }
+
+  protected boolean getDefaultEditable() {
+    throw new RuntimeException("Stub!");
+  }
+
+  protected MovementMethod getDefaultMovementMethod() {
+    throw new RuntimeException("Stub!");
+  }
+
+  public Editable getText() {
+    throw new RuntimeException("Stub!");
+  }
+
+  public void setText(CharSequence text, BufferType type) {
+    throw new RuntimeException("Stub!");
+  }
+
+  public void setSelection(int start, int stop) {
+    throw new RuntimeException("Stub!");
+  }
+
+  public void setSelection(int index) {
+    throw new RuntimeException("Stub!");
+  }
+
+  public void selectAll() {
+    throw new RuntimeException("Stub!");
+  }
+
+  public void extendSelection(int index) {
+    throw new RuntimeException("Stub!");
+  }
+
+  public void setEllipsize(TextUtils.TruncateAt ellipsis) {
+    throw new RuntimeException("Stub!");
+  }
+
+  public CharSequence getAccessibilityClassName() {
+    throw new RuntimeException("Stub!");
+  }
+}
