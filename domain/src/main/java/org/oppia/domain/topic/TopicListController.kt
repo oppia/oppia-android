2f535466c3999c6724c485186478856bb085ece8
Ben Henning <henning.benmax@gmail.com>
2019-09-22 14:14:43 -0700

    Introduce initially viable TopicListController interface with tests, and no real implementation.
    This commit includes thumbnail structures that should be split into its
    own branch/PR since other controllers will depend on it.

---
+++ domain/src/main/java/org/oppia/domain/topic/TopicListController.kt
@@ -0,0 +1,118 @@
+package org.oppia.domain.topic
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import org.oppia.app.model.LessonThumbnail
+import org.oppia.app.model.LessonThumbnailGraphic
+import org.oppia.app.model.OngoingStoryList
+import org.oppia.app.model.PromotedStory
+import org.oppia.app.model.TopicList
+import org.oppia.app.model.TopicSummary
+import org.oppia.util.data.AsyncResult
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
+import javax.inject.Singleton
+
+const val TEST_TOPIC_ID_0 = "test_topic_id_0"
+const val TEST_TOPIC_ID_1 = "test_topic_id_1"
+const val TEST_STORY_ID_1 = "test_story_id_1"
+
+private val EVICTION_TIME_MILLIS = TimeUnit.DAYS.toMillis(1)
+
+/** Controller for retrieving the list of topics available to the learner to play. */
+@Singleton
+class TopicListController @Inject constructor() {
+  /**
+   * Returns the list of [TopicSummary]s currently tracked by the app, possibly up to
+   * [EVICTION_TIME_MILLIS] old.
+   */
+  fun getTopicList(): LiveData<AsyncResult<TopicList>> {
+    return MutableLiveData(AsyncResult.success(createTopicList()))
+  }
+
+  /**
+   * Returns the list of ongoing [PromotedStory]s that can be viewed via a link on the homescreen. The total number of
+   * promoted stories should correspond to the ongoing story count within the [TopicList] returned by [getTopicList].
+   */
+  fun getOngoingStoryList(): LiveData<AsyncResult<OngoingStoryList>> {
+    return MutableLiveData(AsyncResult.success(createOngoingStoryList()))
+  }
+
+  private fun createTopicList(): TopicList {
+    return TopicList.newBuilder()
+      .setPromotedStory(createPromotedStory1())
+      .addTopicSummary(createTopicSummary0())
+      .addTopicSummary(createTopicSummary1())
+      .setOngoingStoryCount(2)
+      .build()
+  }
+
+  private fun createTopicSummary0(): TopicSummary {
+    return TopicSummary.newBuilder()
+      .setTopicId(TEST_TOPIC_ID_0)
+      .setName("First Topic")
+      .setVersion(1)
+      .setSubtopicCount(0)
+      .setCanonicalStoryCount(2)
+      .setUncategorizedSkillCount(0)
+      .setAdditionalStoryCount(0)
+      .setTotalSkillCount(2)
+      .setTotalChapterCount(4)
+      .setTopicThumbnail(createTopicThumbnail0())
+      .build()
+  }
+
+  private fun createTopicSummary1(): TopicSummary {
+    return TopicSummary.newBuilder()
+      .setTopicId(TEST_TOPIC_ID_1)
+      .setName("Second Topic")
+      .setVersion(3)
+      .setSubtopicCount(0)
+      .setCanonicalStoryCount(1)
+      .setUncategorizedSkillCount(0)
+      .setAdditionalStoryCount(0)
+      .setTotalSkillCount(1)
+      .setTotalChapterCount(1)
+      .setTopicThumbnail(createTopicThumbnail1())
+      .build()
+  }
+
+  private fun createOngoingStoryList(): OngoingStoryList {
+    return OngoingStoryList.newBuilder()
+      .addRecentStory(createPromotedStory1())
+      .build()
+  }
+
+  private fun createPromotedStory1(): PromotedStory {
+    return PromotedStory.newBuilder()
+      .setStoryId(TEST_STORY_ID_1)
+      .setStoryName("Second Story")
+      .setTopicId(TEST_TOPIC_ID_0)
+      .setTopicName("First Topic")
+      .setCompletedChapterCount(1)
+      .setTotalChapterCount(3)
+      .setLessonThumbnail(createStoryThumbnail())
+      .build()
+  }
+
+  private fun createTopicThumbnail0(): LessonThumbnail {
+    return LessonThumbnail.newBuilder()
+      .setThumbnailGraphic(LessonThumbnailGraphic.BOY_WITH_CUPCAKES)
+      .setBackgroundColorRgb(0xd5836f)
+      .build()
+  }
+
+  private fun createTopicThumbnail1(): LessonThumbnail {
+    return LessonThumbnail.newBuilder()
+      .setThumbnailGraphic(LessonThumbnailGraphic.BOY_WITH_CUPCAKES)
+      .setBackgroundColorRgb(0xf7bf73)
+      .build()
+  }
+
+  private fun createStoryThumbnail(): LessonThumbnail {
+    return LessonThumbnail.newBuilder()
+      .setThumbnailGraphic(LessonThumbnailGraphic.BOY_WITH_CUPCAKES)
+      .setBackgroundColorRgb(0xa5d3ec)
+      .build()
+  }
+}
