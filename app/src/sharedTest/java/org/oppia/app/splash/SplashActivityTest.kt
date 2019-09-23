65ad575a268d799941966b36365f2c45040acf98
veena <veena14cs@gmail.com>
2019-09-23 14:29:38 +0530

    added javadoc comments for activitytestrule

--- app/src/sharedTest/java/org/oppia/app/splash/SplashActivityTest.kt
+++ app/src/sharedTest/java/org/oppia/app/splash/SplashActivityTest.kt
@@ -16,6 +16,23 @@ import org.oppia.app.home.HomeActivity
 @RunWith(AndroidJUnit4::class)
 class SplashActivityTest {
 
+  /**
+   * Creates an {@link ActivityTestRule} for the Activity under test.
+   *
+   * @param SplashActivity    The activity under test. This must be a class in the instrumentation
+   *                         targetPackage specified in the AndroidManifest.xml
+   * @param initialTouchMode indicates whether the activity should be launched in touch mode.
+   * @param launchActivity   true if the Activity should be launched once per
+   *                         "http://junit.org/javadoc/latest/org/junit/Test.html"
+   *                         <code>Test</code></a> method. It will be launched before the first
+   *                         "http://junit.sourceforge.net/javadoc/org/junit/Before.html"
+   *                         <code>Before</code></a> method, and terminated after the last
+   *                         "http://junit.sourceforge.net/javadoc/org/junit/After.html"
+   *                         <code>After</code></a> method.
+   *                         false if the Activity should be launched explicitly within each test case
+   *                         We can either enter null to use default intent or pass a custom intent as a parameter
+   *                         https://jabknowsnothing.wordpress.com/2015/11/05/activitytestrule-espressos-test-lifecycle/
+   */
   @get:Rule
   var activityTestRule: ActivityTestRule<SplashActivity> = ActivityTestRule(
     SplashActivity::class.java, true,
@@ -24,12 +41,8 @@ class SplashActivityTest {
 
   @Before
   fun setUp() {
+    //initial setup code
     Intents.init()
-  }
-
-  @After
-  fun tearDown() {
-    Intents.release()
   }
 
   @Test
@@ -39,4 +52,11 @@ class SplashActivityTest {
 
     intended(hasComponent(HomeActivity::class.java.getName()))
   }
+
+  @After
+  fun tearDown() {
+    //clean up code
+    Intents.release()
+  }
+
 }
