04e2c6fe0d2d9f2352c98d6e536b2fa73b58ac0d
Rajat Talesra <talesra@talesra-macbookpro.roam.corp.google.com>
2019-09-24 08:57:04 +0530

    Remove StateFragment usgae from HomeActivityController

--- app/src/main/java/org/oppia/app/player/audio/AudioFragment.kt
+++ app/src/main/java/org/oppia/app/player/audio/AudioFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import org.oppia.app.fragment.InjectableFragment
 import javax.inject.Inject
 
