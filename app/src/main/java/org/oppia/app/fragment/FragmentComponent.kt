90e5484f6bbf95d2518324305e32c1a40a0ccab8
veena <veena14cs@gmail.com>
2019-09-24 11:55:42 +0530

    working on interaction

--- app/src/main/java/org/oppia/app/fragment/FragmentComponent.kt
+++ app/src/main/java/org/oppia/app/fragment/FragmentComponent.kt
@@ -4,6 +4,7 @@ import androidx.fragment.app.Fragment
 import dagger.BindsInstance
 import dagger.Subcomponent
 import org.oppia.app.home.HomeFragment
+import org.oppia.app.player.content.ContentInteractionFragment
 import org.oppia.app.player.content.ContentListFragment
 import org.oppia.app.player.exploration.ExplorationFragment
 import org.oppia.app.player.state.StateFragment
--- app/src/main/java/org/oppia/app/player/content/ContentInteractionFragmentPresenter.kt
+++ app/src/main/java/org/oppia/app/player/content/ContentInteractionFragmentPresenter.kt
@@ -6,18 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import org.oppia.app.application.ApplicationContext
+import org.oppia.data.backends.gae.model.GaeCustomizationArgs
 import org.oppia.app.databinding.ContentInteractionFragmentBinding
-import org.oppia.data.backends.gae.model.GaeSubtitledHtml
 import javax.inject.Inject
 
 /** Presenter for [ContentInteractionFragment]. */
 class ContentInteractionFragmentPresenter @Inject constructor(
   @ApplicationContext private val context: Context,
-  private val fragment: Fragment
+  private val fragment: Fragment,
+  private val interactionId:String,val customizationArgs: Map<String, GaeCustomizationArgs>
 ) {
 
   private lateinit var binding: ContentInteractionFragmentBinding
-  var contentList: MutableList<GaeSubtitledHtml> = ArrayList()
+  var contentList: MutableList<GaeCustomizationArgs> = ArrayList()
 
   fun handleCreateView(inflater: LayoutInflater, container: ViewGroup?): View? {
 
@@ -31,7 +32,7 @@ class ContentInteractionFragmentPresenter @Inject constructor(
 
   private fun fetchDummyExplorations() {
 
-   contentList.add(GaeSubtitledHtml("content","\u003cp\u003eThe number of pieces of cake I want.\u003c/p\u003e"+
+   contentList.add(GaeCustomizationArgs("content","\u003cp\u003eThe number of pieces of cake I want.\u003c/p\u003e"+
       "\u003cp\u003eThe number of pieces the whole cake is cut into.\u003c/p\u003e"+
       "\u003cp\u003eI don't remember!\u003c/p\u003e"))
 
