371d4e5d7f291d29b9856f04df64923820869da0
veena <veena14cs@gmail.com>
2019-09-24 12:47:44 +0530

    Update SplashActivityTest.kt

--- app/src/sharedTest/java/org/oppia/app/splash/SplashActivityTest.kt
+++ app/src/sharedTest/java/org/oppia/app/splash/SplashActivityTest.kt
@@ -17,7 +17,7 @@ import org.oppia.app.home.HomeActivity
 @RunWith(AndroidJUnit4::class)
 class SplashActivityTest {
 
-  // The initialTouchMode enables the activity to be launched in touch mode.The launchActivity is set to false if the Activity should be launched explicitly within each test case
+  // The initialTouchMode enables the activity to be launched in touch mode.The launchActivity is disabled to launch Activity explicitly within each test case
   @get:Rule
   var activityTestRule: ActivityTestRule<SplashActivity> = ActivityTestRule(
     SplashActivity::class.java, true,
