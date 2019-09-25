4404f715ff7589b72bc58af2f8c37f5cc8a8df6f
Rajat Talesra <rajattalesra4914@gmail.com>
2019-09-25 11:59:28 +0530

    Fix part of #130: Cellular data alert in audio player (#146)
    * Celluar data alert in audio player
    
    * Javadoc updates
    
    * Changed CellularDataInterface with simpler methods
    
    * CellularDataFragment shifted to StateFragment
    
    * Imrpovised Language and Celluar Fragments
    
    * Implementation change in CellularDataINterface
    
    * Implementation change in LanguageInterface
    
    * Complete cellular, audio, language screen flow test
    
    * Nit changes
    
    * Nit changes and removed try-catch-throw code
    
    * Removed unnecessary interface code
    
    * Remove StateFragment usgae from HomeActivityController
    
    * Nit javadoc changes
    
    * Optimise DialogFragment code and test case
    
    * Code optimization
    
    * Updated isAudioFragmemntVisible usage
    
    * Data share via Bundle in DialogFragment
    
    * Using targetFragment for interfaces
    
    * Using childFragmentManager
    
    * Test case for CellularDataDialogFragment

--- app/src/main/java/org/oppia/app/player/audio/LanguageInterface.kt
+++ app/src/main/java/org/oppia/app/player/audio/LanguageInterface.kt
@@ -1,6 +1,7 @@
 package org.oppia.app.player.audio
 
-/** Interface to receive selected language from [LanguageDialogFragment] */
+/** Interface to receive selected language from [LanguageDialogFragment]. */
 interface LanguageInterface {
+  /** Play the audio corresponding to the language-selected. */
   fun onLanguageSelected(currentLanguageCode: String)
 }
