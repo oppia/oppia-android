ac4cea0ee4b959317f30c9dd401eb1c3e3bda554
vinitamurthi <murthi.vinita@gmail.com>
2019-09-26 13:16:14 -0700

    Initial check in for the ExplorationDataController

---
+++ domain/src/main/java/org/oppia/domain/exploration/ExplorationDataController.kt
@@ -0,0 +1,67 @@
+package org.oppia.domain.exploration
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
+import javax.inject.Singleton
+import org.oppia.app.model.Exploration
+import org.oppia.util.data.AsyncResult
+import java.io.IOException
+import android.content.res.AssetManager
+import androidx.annotation.Nullable
+
+const val TEST_EXPLORATION_ID_0 = "test_exp_id_0"
+const val TEST_EXPLORATION_ID_1 = "test_exp_id_1"
+
+private val EVICTION_TIME_MILLIS = TimeUnit.DAYS.toMillis(1)
+
+/** Controller for retrieving an exploration. */
+@Singleton
+class ExplorationDataController @Inject constructor(private val context: Context) {
+  /**
+   * Returns an  [Exploration] given an ID.
+   */
+
+  fun getExplorationById(ID: String): LiveData<AsyncResult<Exploration>> {
+    return MutableLiveData(AsyncResult.success(createExploration(ID)))
+  }
+
+
+  private fun createExploration(ID: String): Exploration {
+    return if (ID == TEST_EXPLORATION_ID_0) createExploration0() else createExploration1();
+    }
+
+  private fun createExploration0(): Exploration {
+    return Exploration.newBuilder()
+     // Add fields
+      .build()
+  }
+
+  private fun createExploration1(): Exploration {
+    return Exploration.newBuilder()
+     // Add fields
+      .build()
+  }
+
+  @Nullable
+  fun loadJSONFromAsset(assetName: String): String? {
+    val am = context.getAssets()
+
+    var json: String? = null
+    try {
+      val `is` = am.open("assetName")
+      val size = `is`.available()
+      val buffer = ByteArray(size)
+      `is`.read(buffer)
+      `is`.close()
+      json = String(buffer, "UTF-8")
+    } catch (ex: IOException) {
+      ex.printStackTrace()
+      return null
+    }
+
+    return json
+  }
+}
