02328bb4ed1b1c93140688edab953d1b65105183
Rajat Talesra <talesra@talesra-macbookpro.roam.corp.google.com>
2019-09-27 16:14:04 +0530

    Javadoc and nit changes

--- app/src/main/java/org/oppia/app/topic/TopicActivity.kt
+++ app/src/main/java/org/oppia/app/topic/TopicActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import org.oppia.app.activity.InjectableAppCompatActivity
 import javax.inject.Inject
 
-/** The central activity for all users entering the app. */
+/** The activity for tabs in Topic. */
 class TopicActivity : InjectableAppCompatActivity() {
   @Inject lateinit var topicActivityController: TopicActivityController
 
