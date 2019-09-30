f6c211da47bf2e36e69d4f6de6207c2fd8f74da6
marysoloman <nikitamarysolomanpvt@gmail.com>
2019-09-30 15:10:40 +0530

    PR #84 suggestion of ben fixes

--- app/src/main/java/org/oppia/app/fragment/FragmentComponent.kt
+++ app/src/main/java/org/oppia/app/fragment/FragmentComponent.kt
@@ -4,8 +4,8 @@ import androidx.fragment.app.Fragment
 import dagger.BindsInstance
 import dagger.Subcomponent
 import org.oppia.app.home.HomeFragment
-import org.oppia.app.player.state.StateFragment
 import org.oppia.app.player.exploration.ExplorationFragment
+import org.oppia.app.player.state.StateFragment
 import org.oppia.app.player.audio.AudioFragment
 import org.oppia.app.topic.conceptcard.ConceptCardFragment
 
@@ -15,12 +15,13 @@ import org.oppia.app.topic.conceptcard.ConceptCardFragment
 interface FragmentComponent {
   @Subcomponent.Builder
   interface Builder {
-    @BindsInstance fun setFragment(fragment: Fragment): Builder
+    @BindsInstance
+    fun setFragment(fragment: Fragment): Builder
     fun build(): FragmentComponent
   }
 
-  fun inject(explorationFragment: ExplorationFragment)
   fun inject(audioFragment: AudioFragment)
+  fun inject(explorationFragment: ExplorationFragment)
   fun inject(homeFragment: HomeFragment)
   fun inject(stateFragment: StateFragment)
   fun inject(conceptCardFragment: ConceptCardFragment)
