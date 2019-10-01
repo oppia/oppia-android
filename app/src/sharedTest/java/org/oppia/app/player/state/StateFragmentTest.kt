4f20fba85ae34098b5ca2f4fb87a14f0314f0ef9
veena <veena14cs@gmail.com>
2019-10-01 15:26:18 +0530

    Update StateFragmentTest.kt

--- app/src/sharedTest/java/org/oppia/app/player/state/StateFragmentTest.kt
+++ app/src/sharedTest/java/org/oppia/app/player/state/StateFragmentTest.kt
@@ -1,33 +1,17 @@
 package org.oppia.app.player.state;
 
-import android.provider.MediaStore
-import android.view.View
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.hamcrest.Matchers.allOf
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.oppia.app.R
-import org.oppia.app.home.HomeActivity
 import org.oppia.app.player.exploration.ExplorationActivity
-import android.widget.TextView
-import android.widget.LinearLayout
-import android.widget.RadioGroup
-import androidx.recyclerview.widget.RecyclerView
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.ViewAction
-import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.contrib.RecyclerViewActions
-import androidx.test.platform.ui.UiController
-import junit.framework.TestCase.assertEquals
-import org.hamcrest.Matcher
+import junit.framework.TestCase.assertEquals as assertEquals1
 
 /** Tests for [StateFragment]. */
 @RunWith(AndroidJUnit4::class)
@@ -38,7 +22,7 @@ class StateFragmentTest {
   fun testMultipleChoiceInput_showsRadioButtons_onMultipleChoiceInputInteractionInstanceId_userSelectsDesiredOption() {
     ActivityScenario.launch(ExplorationActivity::class.java).use {
       interactionInstanceId = "MultipleChoiceInput"
-      assertEquals(interactionInstanceId, "MultipleChoiceInput")
+      assertEquals1(interactionInstanceId, "MultipleChoiceInput")
       onView(withId(R.id.rvInteractions)).perform(
         RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(2,click()))
     }
@@ -48,7 +32,7 @@ class StateFragmentTest {
   fun tesItemSelectionInput_showsCheckbox_onItemSelectionInputInteractionInstanceId_userCanSelectMoreThanOneCorrectOption() {
     ActivityScenario.launch(ExplorationActivity::class.java).use {
       interactionInstanceId = "ItemSelectionInput"
-      assertEquals(interactionInstanceId, "ItemSelectionInput")
+      assertEquals1(interactionInstanceId, "ItemSelectionInput")
       onView(withId(R.id.rvInteractions)).perform(
         RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(1,click()))
     }
