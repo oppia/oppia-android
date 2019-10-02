9ebf10782b42f51f3e1750e0dbe0670c7d84cb3e
James Xu <jamesxu@umich.edu>
2019-10-01 18:48:50 -0700

    Started getting data from exploration

--- app/src/main/java/org/oppia/app/player/audio/AudioFragment.kt
+++ app/src/main/java/org/oppia/app/player/audio/AudioFragment.kt
@@ -8,13 +8,31 @@ import android.view.ViewGroup
 import org.oppia.app.fragment.InjectableFragment
 import javax.inject.Inject
 
-private const val TAG_LANGUAGE_DIALOG = "LANGUAGE_DIALOG"
+private const val KEY_EXPLORATION_ID = "EXPLORATION_ID"
+private const val KEY_CONTENT_ID = "CONTENT_ID"
 
 /** Fragment that controls audio for a content-card. */
 class AudioFragment : InjectableFragment(), LanguageInterface {
+
+  companion object {
+    /**
+     * Creates a new instance of a AudioFragment
+     * @param explorationId: Used for ExplorationDataController to get correct exploration
+     * @param contentId: Used to get correct VoiceoverMapping
+     * @return [AudioFragment]: Fragment
+     */
+    fun newInstance(explorationId: String, contentId: String): AudioFragment {
+      val audioFragment = AudioFragment()
+      val args = Bundle()
+      args.putString(KEY_EXPLORATION_ID, explorationId)
+      args.putString(KEY_CONTENT_ID, contentId)
+      audioFragment.arguments = args
+      return audioFragment
+    }
+  }
+
   @Inject
   lateinit var audioFragmentPresenter: AudioFragmentPresenter
-  private var selectedLanguageCode: String = "en"
 
   override fun onAttach(context: Context?) {
     super.onAttach(context)
@@ -22,38 +40,21 @@ class AudioFragment : InjectableFragment(), LanguageInterface {
   }
 
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-    return audioFragmentPresenter.handleCreateView(inflater, container)
+    super.onCreateView(inflater, container, savedInstanceState)
+    val args = checkNotNull(arguments) { "Expected arguments to be passed to AudioFragment" }
+    val explorationId = checkNotNull(args.getString(KEY_EXPLORATION_ID)) { "Expected explorationId to be passed to AudioFragment" }
+    val contentId = checkNotNull(args.getString(KEY_CONTENT_ID)) { "Expected contentId to be passed to AudioFragment" }
+    return audioFragmentPresenter.handleCreateView(inflater, container, explorationId, contentId)
   }
 
   fun languageSelectionClicked() {
-    showLanguageDialogFragment()
+    audioFragmentPresenter.showLanguageDialogFragment()
   }
 
   fun getUserIsSeeking() = audioFragmentPresenter.userIsSeeking
   fun getUserPosition() = audioFragmentPresenter.userProgress
 
-  private fun showLanguageDialogFragment() {
-    val previousFragment = childFragmentManager.findFragmentByTag(TAG_LANGUAGE_DIALOG)
-    if (previousFragment != null) {
-      childFragmentManager.beginTransaction().remove(previousFragment).commitNow()
-    }
-    val dialogFragment = LanguageDialogFragment.newInstance(
-      getDummyAudioLanguageList() as ArrayList<String>,
-      selectedLanguageCode
-    )
-    dialogFragment.showNow(childFragmentManager, TAG_LANGUAGE_DIALOG)
-  }
-
-  private fun getDummyAudioLanguageList(): List<String> {
-    val languageCodeList = ArrayList<String>()
-    languageCodeList.add("en")
-    languageCodeList.add("hi")
-    languageCodeList.add("hi-en")
-    return languageCodeList
-  }
-
   override fun onLanguageSelected(currentLanguageCode: String) {
-    selectedLanguageCode = currentLanguageCode
     audioFragmentPresenter.languageSelected(currentLanguageCode)
   }
 }
--- app/src/main/java/org/oppia/app/player/audio/AudioFragmentPresenter.kt
+++ app/src/main/java/org/oppia/app/player/audio/AudioFragmentPresenter.kt
@@ -5,22 +5,36 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.SeekBar
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
 import org.oppia.app.databinding.AudioFragmentBinding
 import org.oppia.app.fragment.FragmentScope
+import org.oppia.app.model.Exploration
+import org.oppia.app.model.VoiceoverMapping
 import org.oppia.app.viewmodel.ViewModelProvider
+import org.oppia.domain.exploration.ExplorationDataController
+import org.oppia.util.data.AsyncResult
+import org.oppia.util.logging.Logger
 import javax.inject.Inject
+
+private const val TAG_LANGUAGE_DIALOG = "LANGUAGE_DIALOG"
 
 /** The presenter for [AudioFragment]. */
 @FragmentScope
 class AudioFragmentPresenter @Inject constructor(
   private val fragment: Fragment,
-  private val viewModelProvider: ViewModelProvider<AudioViewModel>
+  private val viewModelProvider: ViewModelProvider<AudioViewModel>,
+  private val logger: Logger,
+  private val explorationDataController: ExplorationDataController
 ) {
 
   var userIsSeeking = false
   var userProgress = 0
+  private var selectedLanguageCode: String = "en" // what should this default to?
 
-  fun handleCreateView(inflater: LayoutInflater, container: ViewGroup?): View? {
+  private lateinit var voiceoverMapping: VoiceoverMapping
+
+  fun handleCreateView(inflater: LayoutInflater, container: ViewGroup?, explorationId: String, contentId: String): View? {
     val binding = AudioFragmentBinding.inflate(inflater, container, /* attachToRoot= */ false)
     binding.sbAudioProgress.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
       override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
@@ -35,19 +49,58 @@ class AudioFragmentPresenter @Inject constructor(
       }
     })
 
+    val viewModel = getAudioViewModel()
+
+    val explorationResultLiveData = explorationDataController.getExplorationById(explorationId)
+    val explorationLiveData = processExplorationLiveData(explorationResultLiveData)
+    val state = explorationLiveData.value?.statesMap?.get("TODO: what is a state name")
+    voiceoverMapping = state?.recordedVoiceoversMap?.get(contentId)!!
+
     binding.let {
-      it.viewModel = getAudioViewModel()
+      it.viewModel = viewModel
       it.audioFragment = fragment as AudioFragment
       it.lifecycleOwner = fragment
     }
     return binding.root
   }
 
+  fun languageSelected(language: String) {
+    selectedLanguageCode = language
+    getAudioViewModel().setAudioLanguageCode(language)
+  }
+
+  fun showLanguageDialogFragment() {
+    val previousFragment = fragment.childFragmentManager.findFragmentByTag(TAG_LANGUAGE_DIALOG)
+    if (previousFragment != null) {
+      fragment.childFragmentManager.beginTransaction().remove(previousFragment).commitNow()
+    }
+    val dialogFragment = LanguageDialogFragment.newInstance(
+      getDummyAudioLanguageList() as ArrayList<String>,
+      selectedLanguageCode
+    )
+    dialogFragment.showNow(fragment.childFragmentManager, TAG_LANGUAGE_DIALOG)
+  }
+
+  private fun getDummyAudioLanguageList(): List<String> {
+    val languageCodeList = ArrayList<String>()
+    languageCodeList.add("en")
+    languageCodeList.add("hi")
+    languageCodeList.add("hi-en")
+    return languageCodeList
+  }
+
   private fun getAudioViewModel(): AudioViewModel {
     return viewModelProvider.getForFragment(fragment, AudioViewModel::class.java)
   }
 
-  fun languageSelected(language: String) {
-    getAudioViewModel().setAudioLanguageCode(language)
+  private fun processExplorationLiveData(explorationResultLiveData: LiveData<AsyncResult<Exploration>>): LiveData<Exploration> {
+    return Transformations.map(explorationResultLiveData, ::processExplorationResult)
+  }
+
+  private fun processExplorationResult(explorationResult: AsyncResult<Exploration>): Exploration {
+    if (explorationResult.isFailure()) {
+      logger.e("AudioFragment", "Failed to retrieve Exploration: " + explorationResult.getErrorOrNull())
+    }
+    return explorationResult.getOrDefault(Exploration.getDefaultInstance())
   }
 }
