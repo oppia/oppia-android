456d1299dd85899b28802b2c1a00192e3c8071b6
Rajat Talesra <talesra@talesra-macbookpro.roam.corp.google.com>
2019-10-03 15:54:25 +0530

    Topic Train Test Case

--- app/src/sharedTest/java/org/oppia/app/topic/train/TopicTrainFragmentTest.kt
+++ app/src/sharedTest/java/org/oppia/app/topic/train/TopicTrainFragmentTest.kt
@@ -4,7 +4,11 @@ import android.app.Application
 import android.content.Context
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.scrollTo
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isClickable
+import androidx.test.espresso.matcher.ViewMatchers.isNotChecked
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -13,10 +17,11 @@ import dagger.Component
 import dagger.Module
 import dagger.Provides
 import kotlinx.coroutines.CoroutineDispatcher
+import org.hamcrest.Matchers.not
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.oppia.app.R
-import org.oppia.app.topic.TopicActivity
+import org.oppia.app.home.HomeActivity
 import org.oppia.util.threading.BackgroundDispatcher
 import org.oppia.util.threading.BlockingDispatcher
 import javax.inject.Singleton
@@ -26,9 +31,13 @@ import javax.inject.Singleton
 class TopicTrainFragmentTest {
 
   @Test
-  fun testTopicTrainFragment_loadFragment_textIsDisplayed() {
-    ActivityScenario.launch(TopicActivity::class.java).use {
-      onView(withId(R.id.dummy_text_view)).check(matches(withText("This is dummy TextView for testing")))
+  fun testTopicTrainFragment_loadFragment_selectSkills_submitButtonIsActivated() {
+    ActivityScenario.launch(HomeActivity::class.java).use {
+      onView(withId(R.id.master_skills_text_view)).check(matches(withText(R.string.topic_train_master_these_skills)))
+      onView(withId(R.id.topic_train_start_button)).check(matches(not(isClickable())))
+      onView(withText("Writing Fractions")).check(matches(isNotChecked())).perform(scrollTo(), click())
+      onView(withText("Equivalent Fractions")).check(matches(isNotChecked())).perform(scrollTo(), click())
+      onView(withId(R.id.topic_train_start_button)).check(matches(isClickable()))
     }
   }
 
