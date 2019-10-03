41742eee6c84c6390d686307122fd8607907d9cc
Ben Henning <henning.benmax@gmail.com>
2019-10-02 23:31:37 -0700

    Introduce sample of progress controller.

--- app/src/main/java/org/oppia/app/home/HomeFragmentController.kt
+++ app/src/main/java/org/oppia/app/home/HomeFragmentController.kt
@@ -4,10 +4,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import org.oppia.app.databinding.HomeFragmentBinding
 import org.oppia.app.fragment.FragmentScope
 import org.oppia.app.viewmodel.ViewModelProvider
 import org.oppia.domain.UserAppHistoryController
+import org.oppia.domain.exploration.ExplorationDataController
+import org.oppia.domain.exploration.ExplorationProgressController
+import org.oppia.util.data.AsyncResult
+import org.oppia.util.logging.Logger
 import javax.inject.Inject
 
 /** The controller for [HomeFragment]. */
@@ -15,23 +20,56 @@ import javax.inject.Inject
 class HomeFragmentController @Inject constructor(
   private val fragment: Fragment,
   private val viewModelProvider: ViewModelProvider<UserAppHistoryViewModel>,
-  private val userAppHistoryController: UserAppHistoryController
+  private val userAppHistoryController: UserAppHistoryController,
+  private val explorationDataController: ExplorationDataController,
+  private val explorationProgressController: ExplorationProgressController,
+  private val logger: Logger
 ) {
+  private val userAppHistoryViewModel: UserAppHistoryViewModel by lazy {
+    viewModelProvider.getForFragment(fragment, UserAppHistoryViewModel::class.java)
+  }
+
   fun handleCreateView(inflater: LayoutInflater, container: ViewGroup?): View? {
     val binding = HomeFragmentBinding.inflate(inflater, container, /* attachToRoot= */ false)
     // NB: Both the view model and lifecycle owner must be set in order to correctly bind LiveData elements to
     // data-bound view models.
     binding.let {
-      it.viewModel = getUserAppHistoryViewModel()
+      it.viewModel = userAppHistoryViewModel
+      it.presenter = this
       it.lifecycleOwner = fragment
     }
 
     userAppHistoryController.markUserOpenedApp()
+    userAppHistoryViewModel.currentStateLabel = "Waiting for input"
 
     return binding.root
   }
 
-  private fun getUserAppHistoryViewModel(): UserAppHistoryViewModel {
-    return viewModelProvider.getForFragment(fragment, UserAppHistoryViewModel::class.java)
+  fun onLoadExplorationButtonClicked(v: View) {
+    explorationDataController.startPlayingExploration(
+      userAppHistoryViewModel.userProvidedExplorationId ?: "null"
+    ).observe(fragment, Observer<AsyncResult<Any?>> { result ->
+      if (result.isPending()) {
+        userAppHistoryViewModel.currentStateLabel = "Loading exploration"
+      } else if (result.isFailure()) {
+        userAppHistoryViewModel.currentStateLabel = "Failed: ${result.getErrorOrNull()}"
+        logger.e("HomeFragment", "Failed to load exploration", result.getErrorOrNull()!!)
+      } else {
+        userAppHistoryViewModel.currentStateLabel = "Successfully called startPlayingExploration()"
+      }
+    })
+  }
+
+  fun onUnloadExplorationButtonClicked(v: View) {
+    explorationDataController.stopPlayingExploration().observe(fragment, Observer<AsyncResult<Any?>> { result ->
+      if (result.isPending()) {
+        userAppHistoryViewModel.currentStateLabel = "Unloading exploration"
+      } else if (result.isFailure()) {
+        userAppHistoryViewModel.currentStateLabel = "Failed: ${result.getErrorOrNull()}"
+        logger.e("HomeFragment", "Failed to unload exploration", result.getErrorOrNull()!!)
+      } else {
+        userAppHistoryViewModel.currentStateLabel = "Successfully called stopPlayingExploration()"
+      }
+    })
   }
 }
--- app/src/main/java/org/oppia/app/home/UserAppHistoryViewModel.kt
+++ app/src/main/java/org/oppia/app/home/UserAppHistoryViewModel.kt
@@ -1,12 +1,16 @@
 package org.oppia.app.home
 
+import androidx.databinding.Bindable
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import org.oppia.app.fragment.FragmentScope
+import org.oppia.app.model.EphemeralState
 import org.oppia.app.model.UserAppHistory
+import org.oppia.app.viewmodel.ObservableViewModel
 import org.oppia.util.logging.Logger
 import org.oppia.domain.UserAppHistoryController
+import org.oppia.domain.exploration.ExplorationProgressController
 import org.oppia.util.data.AsyncResult
 import javax.inject.Inject
 
@@ -14,21 +18,48 @@ import javax.inject.Inject
 @FragmentScope
 class UserAppHistoryViewModel @Inject constructor(
   private val userAppHistoryController: UserAppHistoryController,
-  private val logger: Logger
-): ViewModel() {
+  private val logger: Logger,
+  private val explorationProgressController: ExplorationProgressController
+): ObservableViewModel() {
   val userAppHistoryLiveData: LiveData<UserAppHistory>? by lazy {
     getUserAppHistory()
   }
+
+  val ephemeralStateLiveData: LiveData<EphemeralState> by lazy {
+    getEphemeralState()
+  }
+
+  @Bindable
+  var userProvidedExplorationId: String? = null
+
+  @Bindable
+  var currentStateLabel: String? = null
+    set(value) {
+      field = value
+      notifyChange()
+    }
 
   private fun getUserAppHistory(): LiveData<UserAppHistory>? {
     // If there's an error loading the data, assume the default.
     return Transformations.map(userAppHistoryController.getUserAppHistory(), ::processUserAppHistoryResult)
   }
 
+  private fun getEphemeralState(): LiveData<EphemeralState> {
+    return Transformations.map(explorationProgressController.getCurrentState(), ::processCurrentState)
+  }
+
   private fun processUserAppHistoryResult(appHistoryResult: AsyncResult<UserAppHistory>): UserAppHistory {
     if (appHistoryResult.isFailure()) {
-      logger.e("HomeFragment", "Failed to retrieve user app history"+ appHistoryResult.getErrorOrNull())
+      logger.e("HomeFragment", "Failed to retrieve user app history", appHistoryResult.getErrorOrNull()!!)
     }
     return appHistoryResult.getOrDefault(UserAppHistory.getDefaultInstance())
+  }
+
+  private fun processCurrentState(ephemeralStateResult: AsyncResult<EphemeralState>): EphemeralState {
+    if (ephemeralStateResult.isFailure()) {
+      logger.e("HomeFragment", "Failed to retrieve ephemeral state", ephemeralStateResult.getErrorOrNull()!!)
+      currentStateLabel = "Failed to retrieve state: ${ephemeralStateResult.getErrorOrNull()}"
+    }
+    return ephemeralStateResult.getOrDefault(EphemeralState.getDefaultInstance())
   }
 }
