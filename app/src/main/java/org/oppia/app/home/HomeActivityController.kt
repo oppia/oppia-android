6c5387ffdcc9c9848b529fe0ce9ad0785dd8b188
Rajat Talesra <talesra@talesra-macbookpro.roam.corp.google.com>
2019-10-06 14:47:00 +0530

    Fixing HomeActivityController

--- app/src/main/java/org/oppia/app/home/HomeActivityController.kt
+++ app/src/main/java/org/oppia/app/home/HomeActivityController.kt
@@ -3,6 +3,7 @@ package org.oppia.app.home
 import androidx.appcompat.app.AppCompatActivity
 import org.oppia.app.R
 import org.oppia.app.activity.ActivityScope
+import org.oppia.app.topic.train.TopicTrainFragment
 import javax.inject.Inject
 
 /** The controller for [HomeActivity]. */
@@ -10,9 +11,15 @@ import javax.inject.Inject
 class HomeActivityController @Inject constructor(private val activity: AppCompatActivity) {
   fun handleOnCreate() {
     activity.setContentView(R.layout.home_activity)
-    activity.supportFragmentManager.beginTransaction().add(
-      R.id.home_fragment_placeholder,
-      HomeFragment()
-    ).commitNow()
+    if (getHomeFragment() == null) {
+      activity.supportFragmentManager.beginTransaction().add(
+        R.id.home_fragment_placeholder,
+        HomeFragment()
+      ).commitNow()
+    }
+  }
+
+  private fun getHomeFragment(): HomeFragment? {
+    return activity.supportFragmentManager.findFragmentById(R.id.home_fragment_placeholder) as HomeFragment?
   }
 }
