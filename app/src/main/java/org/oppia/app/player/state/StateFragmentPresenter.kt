267ecf6e4e342b07da90e0f0794c87775d436ab3
Rajat Talesra <talesra@talesra-macbookpro.roam.corp.google.com>
2019-10-06 12:50:01 +0530

    ExplorationDataController usage

--- app/src/main/java/org/oppia/app/player/state/StateFragmentPresenter.kt
+++ app/src/main/java/org/oppia/app/player/state/StateFragmentPresenter.kt
@@ -4,12 +4,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import org.oppia.app.databinding.StateFragmentBinding
 import org.oppia.app.fragment.FragmentScope
 import org.oppia.app.viewmodel.ViewModelProvider
 import org.oppia.domain.exploration.ExplorationDataController
+import org.oppia.domain.exploration.ExplorationProgressController
 import org.oppia.domain.exploration.TEST_EXPLORATION_ID_5
 import org.oppia.domain.exploration.TEST_EXPLORATION_ID_6
+import org.oppia.util.data.AsyncResult
+import org.oppia.util.logging.Logger
 import javax.inject.Inject
 
 /** The presenter for [StateFragment]. */
@@ -17,8 +21,13 @@ import javax.inject.Inject
 class StateFragmentPresenter @Inject constructor(
   private val fragment: Fragment,
   private val viewModelProvider: ViewModelProvider<StateViewModel>,
-  private val explorationDataController: ExplorationDataController
-) {
+  private val explorationDataController: ExplorationDataController,
+  private val explorationProgressController: ExplorationProgressController,
+  private val logger: Logger
+  ) {
+
+  var currentExplorationId: String = TEST_EXPLORATION_ID_5
+
   fun handleCreateView(inflater: LayoutInflater, container: ViewGroup?): View? {
     val binding = StateFragmentBinding.inflate(inflater, container, /* attachToRoot= */ false)
     binding.let {
@@ -33,6 +42,35 @@ class StateFragmentPresenter @Inject constructor(
 
   private fun getStateViewModel(): StateViewModel {
     return viewModelProvider.getForFragment(fragment, StateViewModel::class.java)
+  }
+
+  fun loadExploration(explorationId: String){
+    explorationDataController.startPlayingExploration(
+      explorationId
+    ).observe(fragment, Observer<AsyncResult<Any?>> { result ->
+      if (result.isPending()) {
+        // Loading exploration
+      } else if (result.isFailure()) {
+        // Failed to load exploration.
+        logger.e("StateFragmentPresenter", "Failed to load exploration", result.getErrorOrNull()!!)
+      } else {
+        // Exploration loaded successfully
+
+      }
+    })
+  }
+
+  fun unloadExploration(explorationId: String){
+    explorationDataController.stopPlayingExploration().observe(fragment, Observer<AsyncResult<Any?>> { result ->
+      if (result.isPending()) {
+        // Failed to unload exploration.
+      } else if (result.isFailure()) {
+        // Unloading exploration
+        logger.e("StateFragmentPresenter", "Failed to unload exploration", result.getErrorOrNull()!!)
+      } else {
+        // Exploration unloaded successfully
+      }
+    })
   }
 
   fun setAudioFragmentVisible(isVisible: Boolean) {
--- app/src/main/java/org/oppia/app/player/state/StateViewModel.kt
+++ app/src/main/java/org/oppia/app/player/state/StateViewModel.kt
@@ -1,25 +1,15 @@
 package org.oppia.app.player.state
 
 import androidx.databinding.ObservableField
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import org.oppia.app.fragment.FragmentScope
-import org.oppia.app.model.AnswerOutcome
-import org.oppia.app.model.EphemeralState
-import org.oppia.app.model.InteractionObject
-import org.oppia.domain.exploration.ExplorationDataController
 import org.oppia.domain.exploration.ExplorationProgressController
-import org.oppia.domain.exploration.TEST_EXPLORATION_ID_6
-import org.oppia.util.data.AsyncResult
 import org.oppia.util.logging.Logger
-import java.util.*
 import javax.inject.Inject
 
 /** [ViewModel] for StateFragment. */
 @FragmentScope
 class StateViewModel @Inject constructor(
-  private val explorationDataController: ExplorationDataController,
   private val explorationProgressController: ExplorationProgressController,
   private val logger: Logger
 ) : ViewModel() {
@@ -31,35 +21,6 @@ class StateViewModel @Inject constructor(
   var isPreviousButtonVisible = ObservableField<Boolean>(false)
   var isSubmitButtonVisible = ObservableField<Boolean>(false)
   var isSubmitButtonActive = ObservableField<Boolean>(false)
-
-  init {
-    explorationDataController.startPlayingExploration(TEST_EXPLORATION_ID_6)
-  }
-
-  val ephemeralStateLiveData: LiveData<EphemeralState> by lazy { getEphemeralState() }
-
-  private fun getEphemeralState(): LiveData<EphemeralState> {
-    return Transformations.map(explorationProgressController.getCurrentState(), ::processEphemeralStateResult)
-  }
-
-  private fun processEphemeralStateResult(ephemeralState: AsyncResult<EphemeralState>): EphemeralState {
-    if (ephemeralState.isFailure()) {
-      logger.e("StateFragment", "Failed to retrieve ephemeral state ${ephemeralState.getErrorOrNull()}")
-    }
-    return ephemeralState.getOrDefault(EphemeralState.getDefaultInstance())
-  }
-
-  private val moveToNextState by lazy {
-    explorationProgressController.moveToNextState()
-  }
-
-  private val moveToPreviousState by lazy {
-    explorationProgressController.moveToPreviousState()
-  }
-
-  private fun submitAnswer(answer: InteractionObject) {
-    var answerOutcome: LiveData<AsyncResult<AnswerOutcome>> = explorationProgressController.submitAnswer(answer)
-  }
 
   fun hideAllButtons() {
     isContinueButtonVisible.set(false)
