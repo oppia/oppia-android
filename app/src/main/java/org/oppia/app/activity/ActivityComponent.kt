a1f53fc186d376cef0c997d03426f5bd3605f206
Rajat Talesra <rajattalesra4914@gmail.com>
2019-10-08 10:18:59 +0530

    Fix part of #138: Topic train fragment Low-fi UI (Part 3) (#203)
    * All xml files for topic-train except layout folder
    
    * Skill item related files
    
    * TopicTrainFragment implementation
    
    * Domain layer code integration
    
    * Start button activated
    
    * Start button activated
    
    * Separate ViewModel for item
    
    * Managing configuration changes
    
    * Checkbox state save
    
    * Nit changes
    
    * Fixing HomeActivityController
    
    * Managing configuration change in checkboxes
    
    * Proper route to QuestionPlayer
    
    * Nit changes
    
    * Nit changes
    
    * Changes as per review

--- app/src/main/java/org/oppia/app/activity/ActivityComponent.kt
+++ app/src/main/java/org/oppia/app/activity/ActivityComponent.kt
@@ -9,6 +9,7 @@ import org.oppia.app.player.exploration.ExplorationActivity
 import org.oppia.app.player.state.testing.StateFragmentTestActivity
 import org.oppia.app.testing.BindableAdapterTestActivity
 import org.oppia.app.topic.TopicActivity
+import org.oppia.app.topic.questionplayer.QuestionPlayerActivity
 import javax.inject.Provider
 
 /** Root subcomponent for all activities. */
@@ -23,9 +24,10 @@ interface ActivityComponent {
 
   fun getFragmentComponentBuilderProvider(): Provider<FragmentComponent.Builder>
 
+  fun inject(bindableAdapterTestActivity: BindableAdapterTestActivity)
   fun inject(explorationActivity: ExplorationActivity)
   fun inject(homeActivity: HomeActivity)
-  fun inject(bindableAdapterTestActivity: BindableAdapterTestActivity)
+  fun inject(questionPlayerActivity: QuestionPlayerActivity)
   fun inject(topicActivity: TopicActivity)
   fun inject(stateFragmentTestActivity: StateFragmentTestActivity)
 }
