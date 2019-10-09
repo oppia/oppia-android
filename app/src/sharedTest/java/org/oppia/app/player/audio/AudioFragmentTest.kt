c1bf08ae84f8dab17c2fb19853b3dea5fa8749d9
James Xu <jamesxu@umich.edu>
2019-10-09 15:58:23 -0700

    Started testing setup

--- app/src/sharedTest/java/org/oppia/app/player/audio/AudioFragmentTest.kt
+++ app/src/sharedTest/java/org/oppia/app/player/audio/AudioFragmentTest.kt
@@ -7,7 +7,9 @@ import android.widget.ImageView
 import androidx.annotation.DrawableRes
 import androidx.core.graphics.drawable.toBitmap
 import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
+import kotlinx.coroutines.test.TestCoroutineDispatcher
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
@@ -19,25 +21,39 @@ import dagger.Component
 import dagger.Module
 import dagger.Provides
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.hamcrest.Description
 import org.hamcrest.TypeSafeMatcher
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.oppia.app.R
 import org.oppia.app.player.audio.testing.AudioFragmentTestActivity
+import org.oppia.domain.audio.AudioPlayerController
+import org.oppia.util.logging.EnableConsoleLog
+import org.oppia.util.logging.EnableFileLog
+import org.oppia.util.logging.GlobalLogLevel
+import org.oppia.util.logging.LogLevel
 import org.oppia.util.threading.BackgroundDispatcher
 import org.oppia.util.threading.BlockingDispatcher
 import javax.inject.Singleton
 import org.robolectric.shadows.ShadowMediaPlayer
+import javax.inject.Inject
+import javax.inject.Qualifier
 
-// TODO(#116): Write test-cases when the user enables/disables on cellular with/without saving the setting.
-// TODO: https://github.com/robolectric/robolectric/issues/5235 Need ShadowMediaPlayer to write tests, can import robolectric beccause of duplicated classes
-/** Tests for [CellularDataDialogFragment]. */
+/** Tests for [AudioFragment]. */
 @RunWith(AndroidJUnit4::class)
 class AudioFragmentTest {
 
+  @Inject
+  lateinit var audioPlayerController: AudioPlayerController
+  private lateinit var shadowMediaPlayer: ShadowMediaPlayer
 
-  private lateinit var shadow: ShadowMediaPlayer
+  @Before
+  fun setUp() {
+
+  }
+
 
   @Test
   fun testAudioFragment_openFragment_showsFragment() {
@@ -59,6 +75,17 @@ class AudioFragmentTest {
     }
   }
 
+  private fun setUpTestApplicationComponent() {
+    DaggerAudioFragmentTest_TestApplicationComponent.builder()
+      .setApplication(ApplicationProvider.getApplicationContext())
+      .build()
+      .inject(this)
+  }
+
+  @Qualifier
+  annotation class TestDispatcher
+
+  // TODO(#89): Move this to a common test application component.
   @Module
   class TestModule {
     @Provides
@@ -67,16 +94,41 @@ class AudioFragmentTest {
       return application
     }
 
-    // TODO(#89): Introduce a proper IdlingResource for background dispatchers to ensure they all complete before
-    //  proceeding in an Espresso test. This solution should also be interoperative with Robolectric contexts by using a
-    //  test coroutine dispatcher.
+    @ExperimentalCoroutinesApi
+    @Singleton
+    @Provides
+    @TestDispatcher
+    fun provideTestDispatcher(): CoroutineDispatcher {
+      return TestCoroutineDispatcher()
+    }
 
     @Singleton
     @Provides
     @BackgroundDispatcher
-    fun provideBackgroundDispatcher(@BlockingDispatcher blockingDispatcher: CoroutineDispatcher): CoroutineDispatcher {
-      return blockingDispatcher
+    fun provideBackgroundDispatcher(@TestDispatcher testDispatcher: CoroutineDispatcher): CoroutineDispatcher {
+      return testDispatcher
     }
+
+    @Singleton
+    @Provides
+    @BlockingDispatcher
+    fun provideBlockingDispatcher(@TestDispatcher testDispatcher: CoroutineDispatcher): CoroutineDispatcher {
+      return testDispatcher
+    }
+
+    // TODO(#59): Either isolate these to their own shared test module, or use the real logging
+    // module in tests to avoid needing to specify these settings for tests.
+    @EnableConsoleLog
+    @Provides
+    fun provideEnableConsoleLog(): Boolean = true
+
+    @EnableFileLog
+    @Provides
+    fun provideEnableFileLog(): Boolean = false
+
+    @GlobalLogLevel
+    @Provides
+    fun provideGlobalLogLevel(): LogLevel = LogLevel.VERBOSE
   }
 
   @Singleton
@@ -86,8 +138,8 @@ class AudioFragmentTest {
     interface Builder {
       @BindsInstance
       fun setApplication(application: Application): Builder
-
       fun build(): TestApplicationComponent
     }
+    fun inject(audioFragmentTest: AudioFragmentTest)
   }
 }
