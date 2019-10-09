3db1aaeb794d6f1ec7c9176b210abdf2f39652db
Rajat Talesra <rajattalesra4914@gmail.com>
2019-10-09 14:39:24 +0530

    Introduce load exploration part 1 (#213)
    * Introduce first pass interface for ExplorationProgressController.
    
    * Fill in the stubbed logic for ExplorationProgressController. Still no
    tests to verify correctness.
    
    Also, added a method to facilitate notifying of DataProvider changes on
    the UI thread.
    
    * Fix lateinit issue in ExplorationProgressController due to wrongly ordered
    initialization.
    
    * Fix a variaty of issues in the exp progress controller, properly hook it up to
    the data controller, and start adding tests.
    
    * Created a separate ExplorationRetriever, hooked up
    AnswerClassificationController, and attempted to make
    ExplorationProgressController thread-safe.
    
    The thread-safety led to significant interface changes in the progress
    controller, and led to discovering some issues with the mediator live data
    approach to interop coroutines and LiveData. This locking mechanism will
    need to change since the optimal solution requires resolving #90.
    
    * Change the locking mechanism for ExplorationProgressController to work
    with the current MediatorLiveData implementation (see #90 for more
    context). Fix existing progress controller tests and add a few more. All
    current progress controller tests are passing.
    
    * Finish tests for ExplorationProgressController and add test classification
    support for the second test exploration (about_oppia).
    
    * ObservableViewModel
    
    * Load exploration and open ExplorationActivity
    
    * Test case for load exploration
    
    * Connection with StateFragment
    
    * Nit changes self review
    
    * Refactor home
    
    * Resolve merge conflicts
    
    * Test case updated

--- app/src/main/java/org/oppia/app/home/HomeFragmentPresenter.kt
+++ app/src/main/java/org/oppia/app/home/HomeFragmentPresenter.kt
@@ -3,26 +3,41 @@ package org.oppia.app.home
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import org.oppia.app.databinding.HomeFragmentBinding
 import org.oppia.app.fragment.FragmentScope
 import org.oppia.app.viewmodel.ViewModelProvider
 import org.oppia.domain.UserAppHistoryController
+import org.oppia.domain.exploration.ExplorationDataController
+import org.oppia.domain.exploration.TEST_EXPLORATION_ID_5
+import org.oppia.util.data.AsyncResult
+import org.oppia.util.logging.Logger
 import javax.inject.Inject
+
+private const val EXPLORATION_ID = TEST_EXPLORATION_ID_5
 
 /** The controller for [HomeFragment]. */
 @FragmentScope
 class HomeFragmentPresenter @Inject constructor(
+  activity: AppCompatActivity,
   private val fragment: Fragment,
   private val viewModelProvider: ViewModelProvider<UserAppHistoryViewModel>,
-  private val userAppHistoryController: UserAppHistoryController
+  private val userAppHistoryController: UserAppHistoryController,
+  private val explorationDataController: ExplorationDataController,
+  private val logger: Logger
 ) {
+
+  private val routeToExplorationListener = activity as RouteToExplorationListener
+
   fun handleCreateView(inflater: LayoutInflater, container: ViewGroup?): View? {
     val binding = HomeFragmentBinding.inflate(inflater, container, /* attachToRoot= */ false)
     // NB: Both the view model and lifecycle owner must be set in order to correctly bind LiveData elements to
     // data-bound view models.
     binding.let {
       it.viewModel = getUserAppHistoryViewModel()
+      it.presenter = this
       it.lifecycleOwner = fragment
     }
 
@@ -33,5 +48,20 @@ class HomeFragmentPresenter @Inject constructor(
 
   private fun getUserAppHistoryViewModel(): UserAppHistoryViewModel {
     return viewModelProvider.getForFragment(fragment, UserAppHistoryViewModel::class.java)
+  }
+
+  fun playExplorationButton(v: View) {
+    explorationDataController.startPlayingExploration(
+      EXPLORATION_ID
+    ).observe(fragment, Observer<AsyncResult<Any?>> { result ->
+      when {
+        result.isPending() -> logger.d("HomeFragment", "Loading exploration")
+        result.isFailure() -> logger.e("HomeFragment", "Failed to load exploration", result.getErrorOrNull()!!)
+        else -> {
+          logger.d("HomeFragment", "Successfully loaded exploration")
+          routeToExplorationListener.routeToExploration(EXPLORATION_ID)
+        }
+      }
+    })
   }
 }
