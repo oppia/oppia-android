26301bb0eaa25d198aae120cb77a707fb6e99547
Rajat Talesra <talesra@talesra-macbookpro.roam.corp.google.com>
2019-10-11 00:24:50 +0530

    Test case added

--- app/src/sharedTest/java/org/oppia/app/topic/review/TopicReviewFragmentTest.kt
+++ app/src/sharedTest/java/org/oppia/app/topic/review/TopicReviewFragmentTest.kt
@@ -4,18 +4,28 @@ import android.app.Application
 import android.content.Context
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra
+import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
 import dagger.BindsInstance
 import dagger.Component
 import dagger.Module
 import dagger.Provides
 import kotlinx.coroutines.CoroutineDispatcher
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.oppia.app.R
+import org.oppia.app.recyclerview.RecyclerViewMatcher.Companion.atPosition
 import org.oppia.app.topic.TopicActivity
 import org.oppia.util.threading.BackgroundDispatcher
 import org.oppia.util.threading.BlockingDispatcher
@@ -23,13 +33,40 @@ import javax.inject.Singleton
 
 /** Tests for [TopicReviewFragment]. */
 @RunWith(AndroidJUnit4::class)
-class TopicTopicReviewFragmentTest {
+class TopicReviewFragmentTest {
+
+  private var skillId = "test_skill_id_0"
+
+  @get:Rule
+  var activityTestRule: ActivityTestRule<TopicActivity> = ActivityTestRule(
+    TopicActivity::class.java, /* initialTouchMode= */ true, /* launchActivity= */ false
+  )
+
+  @Before
+  fun setUp() {
+    Intents.init()
+  }
 
   @Test
-  fun testTopicReviewFragment_loadFragment_textIsDisplayed() {
+  fun testTopicReviewFragment_loadFragment_displayReviewSkills_startButtonIsInactive() {
     ActivityScenario.launch(TopicActivity::class.java).use {
-      onView(withId(R.id.dummy_text_view)).check(matches(withText("This is dummy TextView for testing")))
+      onView(atPosition(R.id.review_skill_recycler_view, 0))
+        .check(matches(hasDescendant(withId(R.id.skill_name))))
     }
+  }
+
+  @Test
+  fun testTopicReviewFragment_loadFragment_selectReviewSkill_opensReviewActivity() {
+    ActivityScenario.launch(TopicActivity::class.java).use {
+      onView(atPosition(R.id.review_skill_recycler_view, 0)).perform(click())
+      intended(hasComponent(ReviewActivity::class.java.name))
+      intended(hasExtra(ReviewActivity.getIntentKey(), skillId))
+    }
+  }
+
+  @After
+  fun tearDown() {
+    Intents.release()
   }
 
   @Module
