446df700a0f7818028277e895aef45352077a64c
Haresh Khanna <khanna.haresh@yahoo.co.in>
2019-10-10 07:30:36 +0530

    Adds story fragment layout

---
+++ app/src/main/java/org/oppia/app/story/StoryViewModel.kt
@@ -0,0 +1,9 @@
+package org.oppia.app.story
+
+import androidx.lifecycle.ViewModel
+import org.oppia.app.fragment.FragmentScope
+import javax.inject.Inject
+
+@FragmentScope
+class StoryViewModel @Inject constructor() : ViewModel() {
+}
