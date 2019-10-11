770fdb0a346cdf6018457d630a0de0141e62a914
Haresh Khanna <khanna.haresh@yahoo.co.in>
2019-10-11 10:17:35 +0530

    Adds code to retrieve story live data

--- app/src/main/java/org/oppia/app/story/StoryViewModel.kt
+++ app/src/main/java/org/oppia/app/story/StoryViewModel.kt
@@ -1,14 +1,43 @@
 package org.oppia.app.story
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import org.oppia.app.fragment.FragmentScope
+import org.oppia.app.model.StorySummary
+import org.oppia.domain.topic.TopicController
+import org.oppia.util.data.AsyncResult
+import org.oppia.util.logging.Logger
 import javax.inject.Inject
 
 @FragmentScope
-class StoryViewModel @Inject constructor() : ViewModel() {
+class StoryViewModel @Inject constructor(
+  private val topicController: TopicController,
+  private val logger: Logger
+) : ViewModel() {
   private lateinit var storyId: String
+
+  val storyLiveData: LiveData<StorySummary> by lazy {
+    processStoryLiveData()
+  }
+
+  private val storyResultLiveData: LiveData<AsyncResult<StorySummary>> by lazy {
+    topicController.getStory(storyId)
+  }
 
   fun setStoryId(storyId: String) {
     this.storyId = storyId
+  }
+
+  private fun processStoryLiveData(): LiveData<StorySummary> {
+    return Transformations.map(storyResultLiveData, ::processStoryResult)
+  }
+
+  private fun processStoryResult(storyResult: AsyncResult<StorySummary>): StorySummary {
+    if (storyResult.isFailure()) {
+      logger.e("StoryFragment", "Failed to retrieve Story: " + storyResult.getErrorOrNull())
+    }
+
+    return storyResult.getOrDefault(StorySummary.getDefaultInstance())
   }
 }
