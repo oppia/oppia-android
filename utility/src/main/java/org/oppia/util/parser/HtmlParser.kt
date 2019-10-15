a56e29874b34a9ba04a4d6c77061c9201bc0e2fb
veena <veena14cs@gmail.com>
2019-10-15 10:46:39 +0530

    Update HtmlParser.kt

--- utility/src/main/java/org/oppia/util/parser/HtmlParser.kt
+++ utility/src/main/java/org/oppia/util/parser/HtmlParser.kt
@@ -1,17 +1,17 @@
-package org.oppia.util.data
+package org.oppia.util.parser
 
 import android.content.Context
 import android.text.Html
 import android.text.Spannable
 import android.widget.TextView
 
-private const val CUSTOM_TAG = "oppia-noninteractive-image"
-private const val HTML_TAG = "img"
-private const val CUSTOM_ATTRIBUTE = "filepath-with-value"
-private const val HTML_ATTRIBUTE = "src"
+private const val CUSTOM_IMG_TAG = "oppia-noninteractive-image"
+private const val REPLACE_IMG_TAG = "img"
+private const val CUSTOM_IMG_FILE_PATH_ATTRIBUTE = "filepath-with-value"
+private const val REPLACE_IMG_FILE_PATH_ATTRIBUTE = "src"
 
 /** Html Parser for android TextView to parse Html tag. */
-class HtmlParser(internal var context: Context) {
+class HtmlParser(private val context: Context) {
 
   /***
    * This method is called to parse HTML from a string-content, it manipulates the HTML elements, attributes and text
@@ -22,14 +22,16 @@ class HtmlParser(internal var context: Context) {
   fun parseHtml(rawString: String?, tvContents: TextView): Spannable {
     val html: Spannable
     var htmlContent = rawString
-    if (htmlContent!!.contains(CUSTOM_TAG)) {
-      htmlContent = htmlContent.replace(CUSTOM_TAG, HTML_TAG, false);
-      htmlContent = htmlContent.replace(CUSTOM_ATTRIBUTE, HTML_ATTRIBUTE, false);
+    if (htmlContent!!.contains(CUSTOM_IMG_TAG)) {
+      htmlContent = htmlContent.replace(CUSTOM_IMG_TAG, REPLACE_IMG_TAG, false);
+      htmlContent = htmlContent.replace(
+        CUSTOM_IMG_FILE_PATH_ATTRIBUTE,
+        REPLACE_IMG_FILE_PATH_ATTRIBUTE, false);
       htmlContent = htmlContent.replace("&amp;quot;", "")
     }
-
+  // TODO(#205): Integrate UrlImageParser below once it's available.
     if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-      html = Html.fromHtml(htmlContent, Html.FROM_HTML_MODE_LEGACY, null, null) as Spannable
+      html = Html.fromHtml(htmlContent, Html.FROM_HTML_MODE_LEGACY, /* imageGetter= */null, /* tagHandler= */null) as Spannable
     } else {
       html = Html.fromHtml(htmlContent, null, null) as Spannable
     }
