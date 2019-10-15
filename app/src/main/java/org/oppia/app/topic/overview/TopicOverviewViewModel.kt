07b79503819a27628a202c4197ffe4406f284c04
Rajat Talesra <talesra@talesra-macbookpro.roam.corp.google.com>
2019-10-15 13:50:22 +0530

    ViewModel to ObservableViewModel

--- app/src/main/java/org/oppia/app/topic/overview/TopicOverviewViewModel.kt
+++ app/src/main/java/org/oppia/app/topic/overview/TopicOverviewViewModel.kt
@@ -7,11 +7,12 @@ import androidx.lifecycle.ViewModel
 import org.oppia.app.R
 import org.oppia.app.fragment.FragmentScope
 import org.oppia.app.model.Topic
+import org.oppia.app.viewmodel.ObservableViewModel
 import javax.inject.Inject
 
 /** [ViewModel] for showing topic overview details. */
 @FragmentScope
-class TopicOverviewViewModel @Inject constructor() : ViewModel() {
+class TopicOverviewViewModel @Inject constructor() : ObservableViewModel() {
   companion object {
     @JvmStatic
     @BindingAdapter("downloadDrawable")
