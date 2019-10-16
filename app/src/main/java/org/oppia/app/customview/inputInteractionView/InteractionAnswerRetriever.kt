3499ac03162b3599bed5f26a1117839baec85577
marysoloman <nikitamarysolomanpvt@gmail.com>
2019-10-16 17:22:40 +0530

    nit changes

--- app/src/main/java/org/oppia/app/customview/inputInteractionView/InteractionAnswerRetriever.kt
+++ app/src/main/java/org/oppia/app/customview/inputInteractionView/InteractionAnswerRetriever.kt
@@ -2,7 +2,7 @@ package org.oppia.app.customview.inputInteractionView
 
 import org.oppia.app.model.InteractionObject
 
-/** This interface that helps to get answer from input interaction views. */
+/** This interface helps to get answer from input interaction views. */
 interface InteractionAnswerRetriever {
   fun getPendingAnswer(): InteractionObject
 }
