d35ceda808236dc53f4ba9f30797a8e36d117357
veena <veena14cs@gmail.com>
2019-10-17 12:36:11 +0530

    Update HtmlParserTestActivty.kt

--- app/src/main/java/org/oppia/app/activity/HtmlParserTestActivty.kt
+++ app/src/main/java/org/oppia/app/activity/HtmlParserTestActivty.kt
@@ -3,22 +3,19 @@ package org.oppia.app.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Spannable
-import kotlinx.android.synthetic.main.activity_test_html_parser.*
 import org.oppia.app.R
 import org.oppia.util.parser.HtmlParser
 
-/** This is a dummy activity to test [NumericInputInteractionView]. */
+/** This is a dummy activity to test Html parsing. */
 class HtmlParserTestActivty : AppCompatActivity() {
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_test_html_parser)
 
-    val rawString: String =
-      ("\u003cp\u003e\"Let's try one last question,\" said Mr. Baker. \"Here's a pineapple cake cut into pieces.\"\u003c/p\u003e\u003coppia-noninteractive-image alt-with-value=\"\u0026amp;quot;Pineapple cake with 7/9 having cherries.\u0026amp;quot;\" caption-with-value=\"\u0026amp;quot;\u0026amp;quot;\" filepath-with-value=\"\u0026amp;quot;pineapple_cake_height_479_width_480.png\u0026amp;quot;\"\u003e\u003c/oppia-noninteractive-image\u003e\u003cp\u003e\u00a0\u003c/p\u003e\u003cp\u003e\u003cstrong\u003eQuestion 6\u003c/strong\u003e: What fraction of the cake has big red cherries in the pineapple slices?\u003c/p\u003e")
-
-    val html: Spannable = HtmlParser(applicationContext, "", "").parseHtml(rawString, test_html_content_text_view)
-
-    test_html_content_text_view.text = html
+    val rawDummyString: String = "\u003cp\u003e\"Let's try one last question,\" said Mr. Baker. \"Here's a pineapple cake cut into pieces.\"\u003c/p\u003e\u003coppia-noninteractive-image alt-with-value=\"\u0026amp;quot;Pineapple cake with 7/9 having cherries.\u0026amp;quot;\" caption-with-value=\"\u0026amp;quot;\u0026amp;quot;\" filepath-with-value=\"\u0026amp;quot;pineapple_cake_height_479_width_480.png\u0026amp;quot;\"\u003e\u003c/oppia-noninteractive-image\u003e\u003cp\u003e\u00a0\u003c/p\u003e\u003cp\u003e\u003cstrong\u003eQuestion 6\u003c/strong\u003e: What fraction of the cake has big red cherries in the pineapple slices?\u003c/p\u003e"
+    val htmlResult: Spannable = HtmlParser(applicationContext, /* entityType= */"", /* entityId= */"").parseHtml(rawDummyString, test_html_content_text_view)
+    test_html_content_text_view.text = htmlResult
 
   }
 }
