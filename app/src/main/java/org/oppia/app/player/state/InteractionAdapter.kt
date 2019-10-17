2cbf7a0e9d79ed3edd1921d464a26faf66bfe1a8
veena <veena14cs@gmail.com>
2019-10-17 13:14:26 +0530

    Update InteractionAdapter.kt

--- app/src/main/java/org/oppia/app/player/state/InteractionAdapter.kt
+++ app/src/main/java/org/oppia/app/player/state/InteractionAdapter.kt
@@ -14,7 +14,6 @@ import kotlinx.android.synthetic.main.multiple_choice_interaction_items.view.*
 import org.oppia.app.R
 import org.oppia.app.databinding.ItemSelectionInteractionItemsBinding
 import org.oppia.app.databinding.MultipleChoiceInteractionItemsBinding
-import org.oppia.util.parser.HtmlParser
 
 const val VIEW_TYPE_MULTIPLE_CHOICE = 1
 const val VIEW_TYPE_ITEM_SELECTION = 2
