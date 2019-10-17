3e46d44368ef715dfa387724b76558204917d2a6
veena <veena14cs@gmail.com>
2019-10-17 13:12:12 +0530

    added testcases.

--- app/src/sharedTest/java/org/oppia/app/activity/HtmlParserTestActivty.kt
+++ app/src/sharedTest/java/org/oppia/app/activity/HtmlParserTestActivty.kt
@@ -11,23 +11,41 @@ import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
+import org.junit.After
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.oppia.app.R
+import org.oppia.app.splash.SplashActivity
 
 /** Tests for [HtmlParserTestActivityTest]. */
 @RunWith(AndroidJUnit4::class)
 class HtmlParserTestActivityTest {
+
+  private lateinit var launchedActivity: Activity
+
   @get:Rule
   var activityTestRule: ActivityTestRule<HtmlParserTestActivty> = ActivityTestRule(
     HtmlParserTestActivty::class.java, /* initialTouchMode= */ true, /* launchActivity= */ false
   )
 
+  @Before
+  fun setUp() {
+    Intents.init()
+    val intent = Intent(Intent.ACTION_PICK)
+    launchedActivity = activityTestRule.launchActivity(intent)
+  }
+
   @Test
-  fun testHtmlContent_replace_custom_oppia_tags_displaysStyleText() {
-    ActivityScenario.launch(HtmlParserTestActivty::class.java).use {
-      onView(withId(R.id.test_html_content_text_view)).check(matches(isDisplayed()))
-    }
+  fun testHtmlContent_replaceCustomOppiaTags_displaysStyleText() {
+    activityTestRule.activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+    activityTestRule.activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+    onView(withId(R.id.test_html_content_text_view)).check(matches(isDisplayed()))
+  }
+
+  @After
+  fun tearDown() {
+    Intents.release()
   }
 }
