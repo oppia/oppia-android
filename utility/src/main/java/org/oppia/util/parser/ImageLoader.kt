7897368011707905cec35ea68f879d5edb203faf
veena <veena14cs@gmail.com>
2019-10-18 17:04:18 +0530

    Update ImageLoader.kt

--- utility/src/main/java/org/oppia/util/parser/ImageLoader.kt
+++ utility/src/main/java/org/oppia/util/parser/ImageLoader.kt
@@ -6,14 +6,12 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.SimpleTarget
 
 interface ImageLoader {
-//  companion object {
 
-    fun load(context: Context, path: String, placeholder: Int, target: SimpleTarget<Bitmap>) {
+  fun load(context: Context, path: String, placeholder: Int, target: SimpleTarget<Bitmap>) {
       Glide.with(context)
         .asBitmap()
         .load(path)
         .placeholder(placeholder)
         .into(target)
     }
-//  }
 }
