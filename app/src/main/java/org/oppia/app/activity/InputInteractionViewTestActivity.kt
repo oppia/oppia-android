d20c12e5cef45ca9cf54d69be21ab83f9651430e
marysoloman <nikitamarysolomanpvt@gmail.com>
2019-10-18 22:58:40 +0530

    TextInputInteractionView

--- app/src/main/java/org/oppia/app/activity/InputInteractionViewTestActivity.kt
+++ app/src/main/java/org/oppia/app/activity/InputInteractionViewTestActivity.kt
@@ -3,14 +3,11 @@ package org.oppia.app.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.oppia.app.R
-import org.oppia.app.customview.interaction.NumericInputInteractionView
 
-/** This is a dummy activity to test [NumericInputInteractionView]. */
+/** This is a dummy activity to test input interaction views. */
 class InputInteractionViewTestActivity : AppCompatActivity() {
-  private lateinit var contentComponent: NumericInputInteractionView
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_numeric_input_interaction_view_test)
-    contentComponent = findViewById(R.id.test_number_input_interaction_view) as NumericInputInteractionView
   }
 }
---
+++ app/src/main/java/org/oppia/app/customview/interaction/FractionInputInteractionView.kt
@@ -0,0 +1,21 @@
+package org.oppia.app.customview.interaction
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.EditText
+import org.oppia.app.model.Fraction
+import org.oppia.app.model.InteractionObject
+
+/** The custom EditText class for text input interaction view. */
+class FractionInputInteractionView @JvmOverloads constructor(
+  context: Context,
+  attrs: AttributeSet? = null,
+  defStyle: Int = android.R.attr.editTextStyle
+) : EditText(context, attrs, defStyle), InteractionAnswerRetriever {
+
+  override fun getPendingAnswer(): InteractionObject {
+    return if (text.isNullOrEmpty()) (InteractionObject.newBuilder().build()) else (InteractionObject.newBuilder().setFraction(
+      text.toString()as Fraction
+    ).build())
+  }
+}
