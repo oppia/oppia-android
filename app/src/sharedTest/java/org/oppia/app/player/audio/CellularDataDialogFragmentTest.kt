9dae69d4c237a9b60aa9c7fc96a9a519eb1e5429
marysoloman <nikitamarysolomanpvt@gmail.com>
2019-10-20 14:30:54 +0530

    nit

--- app/src/sharedTest/java/org/oppia/app/player/audio/CellularDataDialogFragmentTest.kt
+++ app/src/sharedTest/java/org/oppia/app/player/audio/CellularDataDialogFragmentTest.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
