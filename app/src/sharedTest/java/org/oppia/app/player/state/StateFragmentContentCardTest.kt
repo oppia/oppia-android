ad22bd215aad8002c3d38e46e03e6479bb2d6d65
veena <veena14cs@gmail.com>
2019-10-21 17:49:48 +0530

    Update StateFragmentContentCardTest.kt

--- app/src/sharedTest/java/org/oppia/app/player/state/StateFragmentContentCardTest.kt
+++ app/src/sharedTest/java/org/oppia/app/player/state/StateFragmentContentCardTest.kt
@@ -2,8 +2,6 @@ package org.oppia.app.player.state
 
 import android.app.Activity
 import android.content.Intent
-import android.text.Spannable
-import android.view.View
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
@@ -14,7 +12,6 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
-import com.google.common.truth.Truth.assertThat
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -22,26 +19,13 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.oppia.app.R
 import org.oppia.app.home.HomeActivity
-import org.oppia.app.player.exploration.ExplorationActivity
 import org.oppia.app.recyclerview.RecyclerViewMatcher.Companion.atPosition
-import org.oppia.util.parser.HtmlParser
-import javax.inject.Inject
-import androidx.recyclerview.widget.RecyclerView
-import android.view.ViewGroup
-import android.widget.TextView
-import androidx.test.espresso.matcher.ViewMatchers
-import org.hamcrest.CoreMatchers.not
-import org.hamcrest.Description
-import org.hamcrest.Matcher
-import org.hamcrest.TypeSafeMatcher
 
 /** Tests for [VIEW_TYPE_CONTENT]. */
 @RunWith(AndroidJUnit4::class)
 class StateFragmentContentCardTest {
 
   private lateinit var launchedActivity: Activity
-  @Inject
-  lateinit var htmlParserFactory : HtmlParser.Factory
 
   @get:Rule
   var activityTestRule: ActivityTestRule<HomeActivity> = ActivityTestRule(
@@ -57,33 +41,13 @@ class StateFragmentContentCardTest {
 
   @Test
   fun testHtmlContent_handleCustomOppiaTags_parsedHtmldisplaysStyledText() {
-
     ActivityScenario.launch(HomeActivity::class.java).use {
       onView(withId(R.id.play_exploration_button)).perform(ViewActions.click())
-//      val textView = activityTestRule.activity.findViewById(R.id.content_text_view) as? TextView
-//     val htmlResult: Spannable = htmlParserFactory.create( /* entityType= */ "",  /* entityId= */ "")
-//      .parseOppiaHtml(
-//        "<p>Hi, welcome to Oppia! <oppia-noninteractive-link text-with-value=\"&quot;Oppia&quot;\" url-with-value=\"&quot;https://oppia.github.io&quot;\"></oppia-noninteractive-link> is a tool that helps you create interactive learning activities that can be continually improved over time.<br><br>Incidentally, do you know where the name 'Oppia' comes from?<br></p>",
-//      textView
-//    )
-      val htmlResult =  "Hi, welcome to Oppia! is a tool that helps you create interactive learning activities that can be continually improved over time.\n" +
-            "\n" +
-            "    Incidentally, do you know where the name 'Oppia' comes from?"
-
-          onView(atPosition(R.id.state_recycler_view, 1)).check(matches(hasDescendant(withText(htmlResult))))
+      val htmlResult =  "Hi, welcome to Oppia! is a tool that helps you create interactive learning activities that can be continually improved over time.\n\n" +
+            "Incidentally, do you know where the name 'Oppia' comes from?\n\n"
+      onView(atPosition(R.id.state_recycler_view, 0)).check(matches(hasDescendant(withText(htmlResult))))
     }
   }
-
-//  @Test
-//  fun testHtmlContent_nonCustomOppiaTags_notParsed() {
-//    ActivityScenario.launch(HomeActivity::class.java).use {
-//      onView(withId(R.id.play_exploration_button)).perform(ViewActions.click())
-//      val htmlResult ="<p>Hi, welcome to Oppia! <oppia-noninteractive-link text-with-value=\"&quot;Oppia&quot;\" url-with-value=\"&quot;https://oppia.github.io&quot;\"></oppia-noninteractive-link> is a tool that helps you create interactive learning activities that can be continually improved over time.<br><br>Incidentally, do you know where the name 'Oppia' comes from?<br></p>"
-//     onView(atPosition(R.id.state_recycler_view, 0)).check(matches(hasDescendant(withText(htmlResult))))
-//    }
-//  }
-
-
   @After
   fun tearDown() {
     Intents.release()
