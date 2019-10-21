1bb75a987301968532198acdc2f1cdc4a8ba4425
veena <veena14cs@gmail.com>
2019-10-21 18:08:30 +0530

    Update StateFragmentContentCardTest.kt

--- app/src/sharedTest/java/org/oppia/app/player/state/StateFragmentContentCardTest.kt
+++ app/src/sharedTest/java/org/oppia/app/player/state/StateFragmentContentCardTest.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
@@ -40,9 +41,9 @@ class StateFragmentContentCardTest {
   }
 
   @Test
-  fun testHtmlContent_handleCustomOppiaTags_parsedHtmldisplaysStyledText() {
+  fun testContentCard_loadExplorationTest5_handleCustomOppiaTags_parsedHtmlDisplaysCorrectly() {
     ActivityScenario.launch(HomeActivity::class.java).use {
-      onView(withId(R.id.play_exploration_button)).perform(ViewActions.click())
+      onView(withId(R.id.play_exploration_button)).perform(click())
       val htmlResult =  "Hi, welcome to Oppia! is a tool that helps you create interactive learning activities that can be continually improved over time.\n\n" +
             "Incidentally, do you know where the name 'Oppia' comes from?\n\n"
       onView(atPosition(R.id.state_recycler_view, 0)).check(matches(hasDescendant(withText(htmlResult))))
