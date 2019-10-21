a68b3643d313a1d11e760d5c671d128f5f3a52f8
marysoloman <nikitamarysolomanpvt@gmail.com>
2019-10-21 18:01:13 +0530

    nit

--- app/src/sharedTest/java/org/oppia/app/activity/InputInteractionViewTestActivityTest.kt
+++ app/src/sharedTest/java/org/oppia/app/activity/InputInteractionViewTestActivityTest.kt
@@ -1,10 +1,6 @@
 package org.oppia.app.activity
 
-import android.app.Activity
-import android.content.Intent
-import android.content.pm.ActivityInfo
 import android.content.res.Configuration
-import android.os.SystemClock
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -15,13 +11,10 @@ import org.junit.runner.RunWith
 import org.oppia.app.R
 import org.oppia.app.model.InteractionObject
 import androidx.test.core.app.ActivityScenario
-import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.matcher.ViewMatchers
 import com.google.common.truth.Truth.assertThat
-import org.junit.After
 import org.junit.Before
 import org.oppia.app.customview.interaction.NumericInputInteractionView
 
