d5f0e12b6b1ce1b291a10a0048d5416561fc7162
Rajat Talesra <rajattalesra4914@gmail.com>
2019-10-24 11:52:18 +0530

    Fix #142: Topic review tab low fi  (#224)
    * XML files
    
    * Basic structure
    
    * Review funtional implementation
    
    * Test case added
    
    * Test case added
    
    * Final ui
    
    * Nit changes
    
    * Configuration change test case added
    
    * Removed TopicReviewViewModel
    
    * Test cases updated
    
    * Nit changes
    
    * Glide library added
    
    * Nit changes
    
    * Test-case changes
    
    * Test-case changes
    
    * Linked to ConceptCardFragment
    
    * ImageBindingAdapter changes
    
    * Updated test-cases
    
    * Updated test-cases

--- app/src/main/java/org/oppia/app/topic/TopicActivity.kt
+++ app/src/main/java/org/oppia/app/topic/TopicActivity.kt
@@ -2,12 +2,16 @@ package org.oppia.app.topic
 
 import android.os.Bundle
 import org.oppia.app.activity.InjectableAppCompatActivity
+import org.oppia.app.topic.conceptcard.ConceptCardFragment
 import org.oppia.app.topic.questionplayer.QuestionPlayerActivity
 import javax.inject.Inject
 
+//internal const val TAG_CONCEPT_CARD_DIALOG = "CONCEPT_CARD_DIALOG"
+
 /** The activity for tabs in Topic. */
-class TopicActivity : InjectableAppCompatActivity(), RouteToQuestionPlayerListener {
-  @Inject lateinit var topicActivityPresenter: TopicActivityPresenter
+class TopicActivity : InjectableAppCompatActivity(), RouteToQuestionPlayerListener, RouteToConceptCardListener {
+  @Inject
+  lateinit var topicActivityPresenter: TopicActivityPresenter
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -17,5 +21,20 @@ class TopicActivity : InjectableAppCompatActivity(), RouteToQuestionPlayerListen
 
   override fun routeToQuestionPlayer(skillIdList: ArrayList<String>) {
     startActivity(QuestionPlayerActivity.createQuestionPlayerActivityIntent(this, skillIdList))
+  }
+
+  override fun routeToConceptCard(skillId: String) {
+    if (getConceptCardFragment() == null) {
+      val conceptCardFragment: ConceptCardFragment = ConceptCardFragment.newInstance(skillId)
+      conceptCardFragment.showNow(supportFragmentManager, TAG_CONCEPT_CARD_DIALOG)
+    }
+  }
+
+  private fun getConceptCardFragment(): ConceptCardFragment? {
+    return supportFragmentManager.findFragmentByTag(TAG_CONCEPT_CARD_DIALOG) as ConceptCardFragment?
+  }
+
+  companion object {
+    internal const val TAG_CONCEPT_CARD_DIALOG = "CONCEPT_CARD_DIALOG"
   }
 }
