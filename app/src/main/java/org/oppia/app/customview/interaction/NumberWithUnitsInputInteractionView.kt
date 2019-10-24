da99dfb3fb52a236d3512719c21bfbf171e59c20
marysoloman <nikitamarysolomanpvt@gmail.com>
2019-10-25 00:06:34 +0530

    nit

--- app/src/main/java/org/oppia/app/customview/interaction/NumberWithUnitsInputInteractionView.kt
+++ app/src/main/java/org/oppia/app/customview/interaction/NumberWithUnitsInputInteractionView.kt
@@ -3,7 +3,6 @@ package org.oppia.app.customview.interaction
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.EditText
-import org.oppia.app.model.Fraction
 import org.oppia.app.model.InteractionObject
 import org.oppia.app.model.NumberUnit
 import org.oppia.app.model.NumberWithUnits
@@ -18,37 +17,30 @@ class NumberWithUnitsInputInteractionView @JvmOverloads constructor(
 ) : EditText(context, attrs, defStyle), InteractionAnswerRetriever {
 
   override fun getPendingAnswer(): InteractionObject {
-    var interactionObjectBuilder = InteractionObject.newBuilder().setNumberWithUnits(NumberWithUnits.getDefaultInstance())
     var units = ""
-    var value = ""
-    var rawInput = text.toString()
-    if (!text.isNullOrEmpty()) {
-      if (Pattern.compile("[^A-Za-z(₹$€£¥]").matcher(rawInput).find()) {
-        var m = Pattern.compile("[a-zA-Z(₹\$€£¥/^*()-?\\d]+|\\d+").matcher(rawInput)
-        while (m.find()) {
-          if (Pattern.compile("[a-zA-Z(₹\$€£¥]").matcher(m.group()).find())
-            units = m.group()
-        }
-        value = rawInput.replace(units, "")
-      } else {
-        value = rawInput
-        units = ""
-      }
-      if (value.contains("/").or(value.contains("-"))) {
-        interactionObjectBuilder.setNumberWithUnits(
-          NumberWithUnits.newBuilder().setFraction(StringToFractionParser().getFractionFromString(value)).addUnit(
-            NumberUnit.newBuilder().setUnit(units)
-          )
-        )
-      } else {
-        interactionObjectBuilder.setNumberWithUnits(
-          NumberWithUnits.newBuilder().setReal(value.toFloat()).addUnit(
-            NumberUnit.newBuilder().setUnit(units)
-          )
-        )
-      }
+    lateinit var value: String
+    val rawInput = text.toString()
+    val interactionObjectBuilder = InteractionObject.newBuilder().setNumberWithUnits(NumberWithUnits.getDefaultInstance())
+    if (text.isNullOrEmpty()) {
+      return interactionObjectBuilder.build()
     }
-    return interactionObjectBuilder.build();
+    if (Pattern.compile("[^A-Za-z(₹$€£¥]").matcher(rawInput).find()) {
+      val m = Pattern.compile("[a-zA-Z(₹\$€£¥/^*)-?\\d]+|\\d+").matcher(rawInput)
+      while (m.find()) {
+        if (Pattern.compile("[a-zA-Z(₹\$€£¥]").matcher(m.group()).find()) {
+          units = m.group()
+        }
+      }
+      value = rawInput.replace(units, "")
+    } else {
+      value = rawInput
+      units = ""
+    }
+    if (value.contains("/").or(value.contains("-"))) {
+      interactionObjectBuilder.setNumberWithUnits(
+        NumberWithUnits.newBuilder().setFraction(StringToFractionParser().getFractionFromString(value)).addUnit(NumberUnit.newBuilder().setUnit(units)))
+    } else interactionObjectBuilder.setNumberWithUnits(NumberWithUnits.newBuilder().setReal(value.toFloat()).addUnit(NumberUnit.newBuilder().setUnit(units)))
+    return interactionObjectBuilder.build()
   }
 }
 
