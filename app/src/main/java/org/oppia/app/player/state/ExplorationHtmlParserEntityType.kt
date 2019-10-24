90ee2b056f56e1026a95e742ff0b485f22f45565
veena <veena14cs@gmail.com>
2019-10-24 18:15:03 +0530

    added Kdoc

--- app/src/main/java/org/oppia/app/player/state/ExplorationHtmlParserEntityType.kt
+++ app/src/main/java/org/oppia/app/player/state/ExplorationHtmlParserEntityType.kt
@@ -2,5 +2,6 @@ package org.oppia.app.player.state
 
 import javax.inject.Qualifier
 
+/** Qualifier for injecting the entity type for exploration. */
 @Qualifier
 annotation class ExplorationHtmlParserEntityType
--- app/src/main/java/org/oppia/app/player/state/HtmlParserEntityTypeModule.kt
+++ app/src/main/java/org/oppia/app/player/state/HtmlParserEntityTypeModule.kt
@@ -3,6 +3,7 @@ package org.oppia.app.player.state
 import dagger.Module
 import dagger.Provides
 
+/** Provides Html parsing entity type dependencies. */
 @Module
 class HtmlParserEntityTypeModule {
   @Provides
