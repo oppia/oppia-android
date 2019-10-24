4555085b839ad88a5f76449642890e82dcc3e747
Rajat Talesra <rajattalesra4914@gmail.com>
2019-10-24 15:33:35 +0530

    Fix part of #137: Topic-play-tab low-fi (part-1) (Without expandable list) (#260)
    * Topic-play-tab basic implementation
    
    * Added test cases
    
    * Nit changes
    
    * Id rename
    
    * StoryActivity introduced
    
    * Nit changes
    
    * New test cases added
    
    * Nit changes

--- app/src/main/java/org/oppia/app/topic/TopicActivity.kt
+++ app/src/main/java/org/oppia/app/topic/TopicActivity.kt
@@ -2,13 +2,15 @@ package org.oppia.app.topic
 
 import android.os.Bundle
 import org.oppia.app.activity.InjectableAppCompatActivity
+import org.oppia.app.story.StoryActivity
 import org.oppia.app.topic.conceptcard.ConceptCardFragment
 import org.oppia.app.topic.questionplayer.QuestionPlayerActivity
 import javax.inject.Inject
 
 /** The activity for tabs in Topic. */
+
 class TopicActivity : InjectableAppCompatActivity(), RouteToQuestionPlayerListener, RouteToConceptCardListener,
-  RouteToTopicPlayListener {
+  RouteToTopicPlayListener, RouteToStoryListener {
   @Inject
   lateinit var topicActivityPresenter: TopicActivityPresenter
 
@@ -20,6 +22,10 @@ class TopicActivity : InjectableAppCompatActivity(), RouteToQuestionPlayerListen
 
   override fun routeToQuestionPlayer(skillIdList: ArrayList<String>) {
     startActivity(QuestionPlayerActivity.createQuestionPlayerActivityIntent(this, skillIdList))
+  }
+
+  override fun routeToStory(storyId: String) {
+    startActivity(StoryActivity.createStoryActivityIntent(this, storyId))
   }
 
   override fun routeToTopicPlayFragment() {
