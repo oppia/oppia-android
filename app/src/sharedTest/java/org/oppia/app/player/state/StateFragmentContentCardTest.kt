cb90be5e6dcc13f64e7abb3835d46d0fdc030669
veena <veena14cs@gmail.com>
2019-10-25 16:28:35 +0530

    Update StateFragmentContentCardTest.kt

--- app/src/sharedTest/java/org/oppia/app/player/state/StateFragmentContentCardTest.kt
+++ app/src/sharedTest/java/org/oppia/app/player/state/StateFragmentContentCardTest.kt
@@ -1,21 +1,13 @@
 package org.oppia.app.player.state
 
-import android.app.Activity
-import android.content.Intent
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.rule.ActivityTestRule
-import org.junit.After
-import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.oppia.app.R
