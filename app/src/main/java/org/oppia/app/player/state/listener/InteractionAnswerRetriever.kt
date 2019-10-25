76bb52c0017f7c41af0ddefcc434dcba4d78e8cf
veena <veena14cs@gmail.com>
2019-10-25 11:18:38 +0530

    Update InteractionAnswerRetriever.kt

--- app/src/main/java/org/oppia/app/player/state/listener/InteractionAnswerRetriever.kt
+++ app/src/main/java/org/oppia/app/player/state/listener/InteractionAnswerRetriever.kt
@@ -3,6 +3,6 @@ package org.oppia.app.player.state.listener
 import org.oppia.app.model.InteractionObject
 
 /** This interface helps to get pending answer of any  learner interaction. */
-interface InteractionAnswerRetriever  {
+interface InteractionAnswerRetriever {
   fun getPendingAnswer(): InteractionObject
 }
