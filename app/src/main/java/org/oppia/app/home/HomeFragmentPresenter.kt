88cd912006828ffae8c5c7e525d906ab585e8276
Ben Henning <henning.benmax@gmail.com>
2019-10-25 17:48:47 -0700

    Fix broken build after merges.

--- app/src/main/java/org/oppia/app/home/HomeFragmentPresenter.kt
+++ app/src/main/java/org/oppia/app/home/HomeFragmentPresenter.kt
@@ -12,6 +12,7 @@ import org.oppia.app.viewmodel.ViewModelProvider
 import org.oppia.domain.UserAppHistoryController
 import org.oppia.domain.exploration.ExplorationDataController
 import org.oppia.domain.exploration.TEST_EXPLORATION_ID_5
+import org.oppia.domain.exploration.TEST_EXPLORATION_ID_6
 import org.oppia.util.data.AsyncResult
 import org.oppia.util.logging.Logger
 import javax.inject.Inject
