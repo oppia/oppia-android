7a05550b0053fc04bb0a96b0629c727280c232aa
veena <veena14cs@gmail.com>
2019-10-29 18:27:13 +0530

    Update ItemClickListener.kt

--- app/src/main/java/org/oppia/app/player/state/listener/ItemClickListener.kt
+++ app/src/main/java/org/oppia/app/player/state/listener/ItemClickListener.kt
@@ -2,6 +2,7 @@ package org.oppia.app.player.state.listener
 
 import org.oppia.app.model.InteractionObject
 
+/** This interface helps to get pending answer of MultipleChoice/ItemSelection input interaction. */
 interface ItemClickListener{
   fun onItemClick(interactionObject: InteractionObject)
 }
