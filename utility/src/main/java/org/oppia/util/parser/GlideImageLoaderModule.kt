07ef755e84b159ab8620cc41026a7329d0387257
veena <veena14cs@gmail.com>
2019-10-30 12:30:20 +0530

    Update GlideImageLoaderModule.kt

--- utility/src/main/java/org/oppia/util/parser/GlideImageLoaderModule.kt
+++ utility/src/main/java/org/oppia/util/parser/GlideImageLoaderModule.kt
@@ -1,21 +1,7 @@
 package org.oppia.util.parser
 
-import android.content.Context
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
-import javax.inject.Singleton
-
-/** Provides image loading dependencies. */
-//@Module
-//class GlideImageLoaderModule {
-//
-//  @Provides
-//  @ImageLoaderAnnotation
-//  fun providesGlideImageLoader(context: Context): ImageLoader {
-//    return GlideImageLoader(context)
-//  }
-//}
 
 @Module
 abstract class GlideImageLoaderModule {
