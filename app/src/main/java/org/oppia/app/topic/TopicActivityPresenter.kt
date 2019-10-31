90c126e57dc545f9f6fb32fe5907273f30cf30b6
marysoloman <nikitamarysolomanpvt@gmail.com>
2019-11-01 02:08:59 +0530

    nit changes

--- app/src/main/java/org/oppia/app/topic/TopicActivityPresenter.kt
+++ app/src/main/java/org/oppia/app/topic/TopicActivityPresenter.kt
@@ -3,7 +3,6 @@ package org.oppia.app.topic
 import androidx.appcompat.app.AppCompatActivity
 import org.oppia.app.R
 import org.oppia.app.activity.ActivityScope
-import org.oppia.app.topic.review.TopicReviewFragment
 import javax.inject.Inject
 
 /** The presenter for [TopicActivity]. */
