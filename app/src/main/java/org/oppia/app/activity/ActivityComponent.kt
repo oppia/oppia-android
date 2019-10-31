0e82431269ee2bf98d4740a1593ddacb29b52475
veena <veena14cs@gmail.com>
2019-10-31 16:32:42 +0530

    update

--- app/src/main/java/org/oppia/app/activity/ActivityComponent.kt
+++ app/src/main/java/org/oppia/app/activity/ActivityComponent.kt
@@ -34,6 +34,7 @@ interface ActivityComponent {
   fun inject(conceptCardFragmentTestActivity: ConceptCardFragmentTestActivity)
   fun inject(explorationActivity: ExplorationActivity)
   fun inject(homeActivity: HomeActivity)
+  fun inject(htmlParserTestActivty: HtmlParserTestActivity)
   fun inject(profileActivity: ProfileActivity)
   fun inject(questionPlayerActivity: QuestionPlayerActivity)
   fun inject(stateFragmentTestActivity: StateFragmentTestActivity)
