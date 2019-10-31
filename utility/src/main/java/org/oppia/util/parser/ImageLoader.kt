344e1538038985f1365cf35599297405ead473ec
marysoloman <nikitamarysolomanpvt@gmail.com>
2019-11-01 00:21:16 +0530

    revert back to develop

--- utility/src/main/java/org/oppia/util/parser/ImageLoader.kt
+++ utility/src/main/java/org/oppia/util/parser/ImageLoader.kt
@@ -1,6 +1,11 @@
 package org.oppia.util.parser
 
+import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.SimpleTarget
+import androidx.annotation.DrawableRes
 import com.bumptech.glide.request.target.CustomTarget
 
 /** Loads an image from the provided URL into the specified target, optionally caching it. */
