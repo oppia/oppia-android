7b6704e52cd684b889730126646b7d31ce3a6987
Haresh Khanna <khanna.haresh@yahoo.co.in>
2019-11-03 14:26:46 +0530

    Adds routing to exploration activity

--- app/src/main/java/org/oppia/app/story/StoryActivity.kt
+++ app/src/main/java/org/oppia/app/story/StoryActivity.kt
@@ -4,11 +4,16 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import org.oppia.app.activity.InjectableAppCompatActivity
+import org.oppia.app.player.exploration.ExplorationActivity
 import javax.inject.Inject
 
 /** Activity for stories. */
 class StoryActivity : InjectableAppCompatActivity() {
   @Inject lateinit var storyActivityPresenter: StoryActivityPresenter
+
+  init {
+    instance = this
+  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -23,12 +28,17 @@ class StoryActivity : InjectableAppCompatActivity() {
 
   companion object {
     const val STORY_ACTIVITY_STORY_ID_ARGUMENT_KEY = "StoryActivity.story_id"
+    private var instance: StoryActivity? = null
 
     /** Returns a new [Intent] to route to [StoryActivity] for a specified story ID. */
     fun createStoryActivityIntent(context: Context, storyId: String): Intent {
       val intent = Intent(context, StoryActivity::class.java)
       intent.putExtra(STORY_ACTIVITY_STORY_ID_ARGUMENT_KEY, storyId)
       return intent
+    }
+
+    fun routeToExploration(explorationId: String) {
+      instance?.startActivity(ExplorationActivity.createExplorationActivityIntent(instance!!, explorationId))
     }
   }
 }
--- app/src/main/java/org/oppia/app/story/storyitemviewmodel/StoryChapterSummaryViewModel.kt
+++ app/src/main/java/org/oppia/app/story/storyitemviewmodel/StoryChapterSummaryViewModel.kt
@@ -1,12 +1,19 @@
 package org.oppia.app.story.storyitemviewmodel
 
+import android.view.View
 import org.oppia.app.model.ChapterSummary
 import org.oppia.app.model.LessonThumbnail
+import org.oppia.app.story.StoryActivity
 
 class StoryChapterSummaryViewModel(
   chapterSummary: ChapterSummary
-): StoryItemViewModel() {
+) : StoryItemViewModel() {
+  val id: String = chapterSummary.explorationId
   val name: String = chapterSummary.name
   val summary: String = chapterSummary.summary
   val chapterThumbnail: LessonThumbnail = chapterSummary.chapterThumbnail
+
+  fun onChapterClicked(v: View) {
+    StoryActivity.routeToExploration(id)
+  }
 }
