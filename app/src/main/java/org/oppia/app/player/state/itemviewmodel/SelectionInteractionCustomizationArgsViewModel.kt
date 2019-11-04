eef603d03d92cb0a8503ce89cf3dfc7205ab70b1
veena <veena14cs@gmail.com>
2019-11-04 18:26:19 +0530

    Update SelectionInteractionCustomizationArgsViewModel.kt

--- app/src/main/java/org/oppia/app/player/state/itemviewmodel/SelectionInteractionCustomizationArgsViewModel.kt
+++ app/src/main/java/org/oppia/app/player/state/itemviewmodel/SelectionInteractionCustomizationArgsViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.ViewModel
 
 /** [ViewModel] for multiple or item-selection input choice list. */
 class SelectionInteractionCustomizationArgsViewModel : ViewModel() {
-
   var choiceItems: MutableList<String>? = null
   var interactionId: String = ""
   var maxAllowableSelectionCount: Int = 0
