32b288226e28d52bd0856cc1651beb32cb472c63
veena <veena14cs@gmail.com>
2019-11-05 13:08:50 +0530

    Update ExplorationRetriever.kt

--- domain/src/main/java/org/oppia/domain/exploration/ExplorationRetriever.kt
+++ domain/src/main/java/org/oppia/domain/exploration/ExplorationRetriever.kt
@@ -23,7 +23,7 @@ class ExplorationRetriever @Inject constructor(
   internal suspend fun loadExploration(explorationId: String): Exploration {
     return when (explorationId) {
       TEST_EXPLORATION_ID_5 -> loadExplorationFromAsset("welcome.json")
-      TEST_EXPLORATION_ID_6 -> loadExplorationFromAsset("oppia_exploration.json")
+      TEST_EXPLORATION_ID_6 -> loadExplorationFromAsset("about_oppia.json")
       else -> throw IllegalStateException("Invalid exploration ID: $explorationId")
     }
   }
