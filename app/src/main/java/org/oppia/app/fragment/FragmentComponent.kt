45ea71a8874868308660934a1d6c8bbb46f548df
Ben Henning <henning.benmax@gmail.com>
2019-11-06 22:08:35 -0800

    Revert "Remove view scope & component."
    This reverts commit 99783d53bae9d4233ef5c2de96f17a1a2499ecd8.

--- app/src/main/java/org/oppia/app/fragment/FragmentComponent.kt
+++ app/src/main/java/org/oppia/app/fragment/FragmentComponent.kt
@@ -21,9 +21,11 @@ import org.oppia.app.topic.play.TopicPlayFragment
 import org.oppia.app.topic.questionplayer.QuestionPlayerFragment
 import org.oppia.app.topic.review.TopicReviewFragment
 import org.oppia.app.topic.train.TopicTrainFragment
+import org.oppia.app.view.ViewComponent
+import javax.inject.Provider
 
 /** Root subcomponent for all fragments. */
-@Subcomponent(modules = [InteractionViewModelModule::class])
+@Subcomponent(modules = [FragmentModule::class, InteractionViewModelModule::class])
 @FragmentScope
 interface FragmentComponent {
   @Subcomponent.Builder
@@ -33,6 +35,8 @@ interface FragmentComponent {
 
     fun build(): FragmentComponent
   }
+
+  fun getViewComponentBuilderProvider(): Provider<ViewComponent.Builder>
 
   fun inject(addProfileFragment: AddProfileFragment)
   fun inject(adminAuthFragment: AdminAuthFragment)
