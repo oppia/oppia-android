be186d3792949d7efa6c4ebb9a79eed5d0a4c7c8
James Xu <jamesxu@umich.edu>
2019-11-08 13:30:29 -0800

    Added ProfileTestHelperTest

--- domain/src/main/java/org/oppia/domain/profile/ProfileTestHelper.kt
+++ domain/src/main/java/org/oppia/domain/profile/ProfileTestHelper.kt
@@ -7,6 +7,7 @@ import javax.inject.Inject
 class ProfileTestHelper @Inject constructor(
   private val profileManagementController: ProfileManagementController
 ) {
+  /** Create one admin profile and one user profile. */
   @ExperimentalCoroutinesApi
   fun initializeProfiles() {
     profileManagementController.addProfile("Sean", "12345", null, allowDownloadAccess = true, isAdmin = true)
@@ -14,9 +15,19 @@ class ProfileTestHelper @Inject constructor(
     profileManagementController.loginToProfile(ProfileId.newBuilder().setInternalId(0).build())
   }
 
+  /** Create [numProfiles] number of user profiles. */
+  @ExperimentalCoroutinesApi
+  fun addMoreProfiles(numProfiles: Int) {
+    for (x in 0 until numProfiles) {
+      profileManagementController.addProfile((x+65).toChar().toString(), "123", null, allowDownloadAccess = false, isAdmin = false)
+    }
+  }
+
+  /** Login to Admin profile. */
   @ExperimentalCoroutinesApi
   fun loginToAdmin() = profileManagementController.loginToProfile(ProfileId.newBuilder().setInternalId(0).build())
 
+  /* Login to user profile. */
   @ExperimentalCoroutinesApi
   fun loginToUser() = profileManagementController.loginToProfile(ProfileId.newBuilder().setInternalId(1).build())
 }
