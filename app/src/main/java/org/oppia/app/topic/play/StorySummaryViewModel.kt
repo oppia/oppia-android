c499df2138448e658fb985bceae261e67352c314
Rajat Talesra <talesra@talesra-macbookpro.roam.corp.google.com>
2019-11-12 17:06:26 +0530

    Nit changes

--- app/src/main/java/org/oppia/app/topic/play/StorySummaryViewModel.kt
+++ app/src/main/java/org/oppia/app/topic/play/StorySummaryViewModel.kt
@@ -2,12 +2,9 @@ package org.oppia.app.topic.play
 
 import android.view.View
 import androidx.lifecycle.ViewModel
-import org.oppia.app.model.PromotedStory
 import org.oppia.app.model.StorySummary
 
-// TODO(#297): Add download status information to promoted-story-card.
-
-/** [ViewModel] for displaying a promoted story. */
+/** [ViewModel] for displaying a story summary. */
 class StorySummaryViewModel(
   val storySummary: StorySummary,
   private val storySummarySelector: StorySummarySelector
