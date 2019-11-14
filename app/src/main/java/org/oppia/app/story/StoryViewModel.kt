9c57d1ae8b0dd436b67d8616348259c83f30ce23
Rajat Talesra <rajattalesra4914@gmail.com>
2019-11-15 02:15:58 +0530

    Fix part of #17: Hifi audio bar basic and fix story crash (#347)
    * Nit changes
    
    * Audio UI changes
    
    * Fix off-by-one on iterated story range.

--- app/src/main/java/org/oppia/app/story/StoryViewModel.kt
+++ app/src/main/java/org/oppia/app/story/StoryViewModel.kt
@@ -1,5 +1,6 @@
 package org.oppia.app.story
 
+import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Transformations
@@ -58,7 +59,8 @@ class StoryViewModel @Inject constructor(
   private fun processStoryChapterList(storySummary: StorySummary): List<StoryItemViewModel> {
     val chapterList: List<ChapterSummary> = storySummary.chapterList
 
-    for (position in 0..storySummary.chapterList.size) {
+    Log.d("TAG", "chapterList: " + chapterList.size)
+    for (position in 0 until chapterList.size) {
       if (storySummary.chapterList[position].chapterPlayState == ChapterPlayState.NOT_STARTED) {
         (fragment as StoryFragment).smoothScrollToPosition(position + 1)
         break
