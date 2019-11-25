38819662078deb34ef881600aba2eb63dcf674ae
James Xu <jamesxu@umich.edu>
2019-11-25 15:54:31 -0800

    minor fixes

--- app/src/main/java/org/oppia/app/databinding/TextViewBindingAdapters.kt
+++ app/src/main/java/org/oppia/app/databinding/TextViewBindingAdapters.kt
@@ -1,10 +1,9 @@
 package org.oppia.app.databinding
 
 import android.text.format.DateUtils
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
-import java.util.*
+import java.util.Date
 
 /** Converts time in ms to readable relative time string. */
 @BindingAdapter("profile:date")
@@ -12,4 +11,3 @@ fun setTextWithDate(textView: TextView, timeMs: Long) {
   val dateText = "Last used " + DateUtils.getRelativeTimeSpanString(timeMs, Date().time, DateUtils.MINUTE_IN_MILLIS, DateUtils.FORMAT_ABBREV_RELATIVE)
   textView.text = dateText
 }
-
