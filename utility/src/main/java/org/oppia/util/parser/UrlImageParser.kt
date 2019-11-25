93e8e4e9b470073fd34687da2bfa92e70da28f7a
veena <veena14cs@gmail.com>
2019-11-25 12:26:11 +0530

    Update UrlImageParser.kt

--- utility/src/main/java/org/oppia/util/parser/UrlImageParser.kt
+++ utility/src/main/java/org/oppia/util/parser/UrlImageParser.kt
@@ -78,7 +78,7 @@ class UrlImageParser private constructor(
     }
   }
 
-  public fun calculateInitialMargin(drawableWidth: Int): Int {
+   fun calculateInitialMargin(drawableWidth: Int): Int {
     val availableAreaWidth = htmlContentTextView.width
     return (availableAreaWidth - drawableWidth) / 2
   }
