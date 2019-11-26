d36e0addbe7f324f888bdc3b40e901e6f1a55234
marysoloman <nikitamarysolomanpvt@gmail.com>
2019-11-26 16:47:33 +0530

    nit

--- app/src/main/java/org/oppia/app/customview/interaction/EditTextBindingAdapters.java
+++ app/src/main/java/org/oppia/app/customview/interaction/EditTextBindingAdapters.java
@@ -3,7 +3,7 @@ package org.oppia.app.customview.interaction;
 import android.text.TextWatcher;
 import android.widget.EditText;
 import androidx.databinding.BindingAdapter;
-
+/** This bindingAdapter tells the compiler how to set a TextWatcher on an EditText. */
 public class EditTextBindingAdapters {
 
   @BindingAdapter("textChangedListener")
