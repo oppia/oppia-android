04215e7b7f899d635a5ac8973b041e7b72344635
Veena <veena14cs@gmail.com>
2019-12-09 13:19:24 +0530

    Fix part of 44: High-fi Profile Selection page (#525)
    * updated specs
    
    * Hifi bug fixes
    
    * Update profile_chooser_add_view.xml
    
    * Update profile_chooser_fragment.xml
    
    * Update profile_chooser_fragment.xml

--- app/src/main/java/org/oppia/app/profile/ProfileChooserFragmentPresenter.kt
+++ app/src/main/java/org/oppia/app/profile/ProfileChooserFragmentPresenter.kt
@@ -36,6 +36,7 @@ class ProfileChooserFragmentPresenter @Inject constructor(
       viewModel = chooserViewModel
       lifecycleOwner = fragment
     }
+    binding.profileRecyclerView.isNestedScrollingEnabled = false
     binding.profileRecyclerView.apply {
       adapter = createRecyclerViewAdapter()
     }
