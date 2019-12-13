56d1e56c89444754751f56b3512eea3b54647e63
marysoloman <nikitamarysolomanpvt@gmail.com>
2019-12-14 01:11:45 +0530

    added test cases for verifying opening and closing of navigation drawer

--- app/src/sharedTest/java/org/oppia/app/testing/NavigationDrawerTestActivityTest.kt
+++ app/src/sharedTest/java/org/oppia/app/testing/NavigationDrawerTestActivityTest.kt
@@ -5,6 +5,8 @@ import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.DrawerActions
+import androidx.test.espresso.contrib.DrawerMatchers
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
@@ -27,48 +29,125 @@ class NavigationDrawerTestActivityTest {
       ).perform(click())
       onView(withId(R.id.home_fragment_placeholder))
         .check(matches(isDisplayed()))
+      onView(withId(R.id.drawer_layout)).check(matches(DrawerMatchers.isOpen()))
     }
   }
 
   @Test
-  fun testNavigationDrawerTestActivity_clickNavigationDrawerHandBurger_clickHelpMenuInNavigationDrawer_showsHelpFragment() {
+  fun testNavigationDrawerTestActivity_clickNavigationDrawerHandBurger_closingOfNavigationDrawerIsVerifiedSucessfully() {
     ActivityScenario.launch(NavigationDrawerTestActivity::class.java).use {
-      onView(withContentDescription(R.string.drawer_open_content_description))
-        .perform(click())
-      onView(ViewMatchers.withText("Help")).check(matches(isDisplayed())).perform(click())
-      onView(
-        Matchers.allOf(
-          Matchers.instanceOf(TextView::class.java),
-          ViewMatchers.withParent(withId(R.id.toolbar))
-        )
-      )
-        .check(matches(ViewMatchers.withText("Help")))
+      onView(withContentDescription(R.string.drawer_open_content_description)).check(
+        matches(isDisplayed())
+      ).perform(click())
+      onView(withId(R.id.drawer_layout)).perform(DrawerActions.close())
+      onView(withId(R.id.drawer_layout)).check(matches(DrawerMatchers.isClosed()))
     }
-  }
+}
 
-  @Test
-  fun testNavigationDrawerTestActivity_clickNavigationDrawerHandBurger_clickHelpMenuInNavigationDrawer_clickHomeMenuInNavigationDrawer_showsHomeFragment() {
-    ActivityScenario.launch(NavigationDrawerTestActivity::class.java).use {
-      onView(withContentDescription(R.string.drawer_open_content_description))
-        .perform(click())
-      onView(ViewMatchers.withText("Help")).check(matches(isDisplayed())).perform(click())
-      onView(withContentDescription(R.string.drawer_open_content_description))
-        .perform(click())
-      onView(ViewMatchers.withText("Home")).check(matches(isDisplayed())).perform(click())
-      onView(
-        Matchers.allOf(
-          Matchers.instanceOf(TextView::class.java),
-          ViewMatchers.withParent(withId(R.id.toolbar))
-        )
+@Test
+fun testNavigationDrawerTestActivity_clickNavigationDrawerHandBurger_clickHelpMenuInNavigationDrawer_showsHelpFragment() {
+  ActivityScenario.launch(NavigationDrawerTestActivity::class.java).use {
+    onView(withContentDescription(R.string.drawer_open_content_description))
+      .perform(click())
+    onView(ViewMatchers.withText("Help")).check(matches(isDisplayed())).perform(click())
+    onView(
+      Matchers.allOf(
+        Matchers.instanceOf(TextView::class.java),
+        ViewMatchers.withParent(withId(R.id.toolbar))
       )
-        .check(matches(ViewMatchers.withText("Home")))
-      onView(
-        RecyclerViewMatcher.atPositionOnView(
-          R.id.home_recycler_view,
-          0,
-          R.id.welcome_text_view
-        )
-      ).check(matches(ViewMatchers.withText("Welcome to Oppia!")))
-    }
+    )
+      .check(matches(ViewMatchers.withText("Help")))
   }
+}
+
+@Test
+fun testNavigationDrawerTestActivity_clickNavigationDrawerHandBurger_clickHelpMenuInNavigationDrawer_clickNavigationDrawerHandBurger_navigationDrawerIsOpenedAndVerified() {
+  ActivityScenario.launch(NavigationDrawerTestActivity::class.java).use {
+    onView(withContentDescription(R.string.drawer_open_content_description))
+      .perform(click())
+    onView(ViewMatchers.withText("Help")).check(matches(isDisplayed())).perform(click())
+    onView(
+      Matchers.allOf(
+        Matchers.instanceOf(TextView::class.java),
+        ViewMatchers.withParent(withId(R.id.toolbar))
+      )
+    )
+      .check(matches(ViewMatchers.withText("Help")))
+    onView(withContentDescription(R.string.drawer_open_content_description)).check(
+      matches(isDisplayed())
+    ).perform(click())
+    onView(withId(R.id.home_fragment_placeholder))
+      .check(matches(isDisplayed()))
+  }
+}
+
+@Test
+fun testNavigationDrawerTestActivity_clickNavigationDrawerHandBurger_clickHelpMenuInNavigationDrawer_openingAndClosingOfDrawerIsVerified_navigationDrawerIsOpenAndVerified() {
+  ActivityScenario.launch(NavigationDrawerTestActivity::class.java).use {
+    onView(withContentDescription(R.string.drawer_open_content_description))
+      .perform(click())
+    onView(ViewMatchers.withText("Help")).check(matches(isDisplayed())).perform(click())
+    onView(withContentDescription(R.string.drawer_open_content_description))
+      .perform(click())
+    onView(withId(R.id.drawer_layout)).perform(DrawerActions.close())
+    onView(withId(R.id.drawer_layout)).check(matches(DrawerMatchers.isClosed()))
+    onView(
+      Matchers.allOf(
+        Matchers.instanceOf(TextView::class.java),
+        ViewMatchers.withParent(withId(R.id.toolbar))
+      )
+    )
+      .check(matches(ViewMatchers.withText("Help")))
+    onView(withContentDescription(R.string.drawer_open_content_description)).check(
+      matches(isDisplayed())
+    ).perform(click())
+    onView(withId(R.id.drawer_layout)).perform(DrawerActions.open())
+    onView(withId(R.id.drawer_layout)).check(matches(DrawerMatchers.isOpen()))
+  }
+}@Test
+fun testNavigationDrawerTestActivity_clickNavigationDrawerHandBurger_clickHelpMenuInNavigationDrawer_navigationDrawerClosingIsVerified() {
+  ActivityScenario.launch(NavigationDrawerTestActivity::class.java).use {
+    onView(withContentDescription(R.string.drawer_open_content_description))
+      .perform(click())
+    onView(ViewMatchers.withText("Help")).check(matches(isDisplayed())).perform(click())
+    onView(withContentDescription(R.string.drawer_open_content_description))
+      .perform(click())
+    onView(
+      Matchers.allOf(
+        Matchers.instanceOf(TextView::class.java),
+        ViewMatchers.withParent(withId(R.id.toolbar))
+      )
+    )
+      .check(matches(ViewMatchers.withText("Help")))
+    onView(withId(R.id.drawer_layout)).perform(DrawerActions.open())
+    onView(withId(R.id.drawer_layout)).perform(DrawerActions.close())
+    onView(withId(R.id.drawer_layout)).check(matches(DrawerMatchers.isClosed()))
+  }
+}
+
+@Test
+fun testNavigationDrawerTestActivity_clickNavigationDrawerHandBurger_clickHelpMenuInNavigationDrawer_clickHomeMenuInNavigationDrawer_showsHomeFragment() {
+  ActivityScenario.launch(NavigationDrawerTestActivity::class.java).use {
+    onView(withContentDescription(R.string.drawer_open_content_description))
+      .perform(click())
+    onView(ViewMatchers.withText("Help")).check(matches(isDisplayed())).perform(click())
+    onView(withContentDescription(R.string.drawer_open_content_description))
+      .perform(click())
+    onView(ViewMatchers.withText("Home")).check(matches(isDisplayed())).perform(click())
+    onView(
+      Matchers.allOf(
+        Matchers.instanceOf(TextView::class.java),
+        ViewMatchers.withParent(withId(R.id.toolbar))
+      )
+    )
+      .check(matches(ViewMatchers.withText("Home")))
+    onView(
+      RecyclerViewMatcher.atPositionOnView(
+        R.id.home_recycler_view,
+        0,
+        R.id.welcome_text_view
+      )
+    ).check(matches(ViewMatchers.withText("Welcome to Oppia!")))
+  }
+}
 }
