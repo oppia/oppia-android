b5f219f14316da289488e109c4c99915a7eac5d3
veena <veena14cs@gmail.com>
2019-12-31 14:57:02 +0530

    added last visited time

--- app/src/main/java/org/oppia/app/databinding/TextViewBindingAdapters.kt
+++ app/src/main/java/org/oppia/app/databinding/TextViewBindingAdapters.kt
@@ -1,10 +1,13 @@
 package org.oppia.app.databinding
 
+import android.util.Log
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import org.oppia.app.R
 import java.text.SimpleDateFormat
 import java.util.*
+import java.text.ParseException
+import java.util.concurrent.TimeUnit
 
 /** Binds date text with relative time. */
 @BindingAdapter("profile:created")
@@ -13,4 +16,50 @@ fun setProfileDataText(textView: TextView, timestamp: Long) {
   val sdf = SimpleDateFormat("dd MMM yyyy", Locale.getDefault())
   val time = sdf.format(Date(timestamp))
   textView.text = String.format(textView.context.getString(R.string.profile_edit_created, time))
+}
+
+@BindingAdapter("profile:lastVisited")
+fun setProfileLastVisitedText(textView: TextView, timestamp: Long) {
+  // TODO(#555): Create one central utility file from where we should access date format or even convert date timestamp to string from that file.
+  textView.text =
+    String.format(textView.context.getString(R.string.profile_last_used) + " " + getTimeAgo(Date(timestamp)))
+}
+
+private val SECOND_MILLIS = 1000
+private val MINUTE_MILLIS = 60 * SECOND_MILLIS
+private val HOUR_MILLIS = 60 * MINUTE_MILLIS
+private val DAY_MILLIS = 24 * HOUR_MILLIS
+
+private fun currentDate(): Date {
+  val calendar = Calendar.getInstance()
+  return calendar.time
+}
+
+fun getTimeAgo(date: Date): String {
+  var time = date.time
+  if (time < 1000000000000L) {
+    time *= 1000
+  }
+
+  val now = currentDate().time
+  if (time > now || time <= 0) {
+    return "in the future"
+  }
+
+  val diff = now - time
+  return if (diff < MINUTE_MILLIS) {
+    "moments ago"
+  } else if (diff < 2 * MINUTE_MILLIS) {
+    "a minute ago"
+  } else if (diff < 60 * MINUTE_MILLIS) {
+    (diff / MINUTE_MILLIS).toString() + " minutes ago"
+  } else if (diff < 2 * HOUR_MILLIS) {
+    "an hour ago"
+  } else if (diff < 24 * HOUR_MILLIS) {
+    (diff / HOUR_MILLIS).toString() + " hours ago"
+  } else if (diff < 48 * HOUR_MILLIS) {
+    "yesterday"
+  } else {
+    (diff / DAY_MILLIS).toString() + " days ago"
+  }
 }
