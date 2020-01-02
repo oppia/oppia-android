3f5bdbd96867a3bef9a1478e2149009ac4edf17b
veena <veena14cs@gmail.com>
2020-01-02 13:27:54 +0530

    added string in xml

--- app/src/main/java/org/oppia/app/databinding/TextViewBindingAdapters.kt
+++ app/src/main/java/org/oppia/app/databinding/TextViewBindingAdapters.kt
@@ -1,5 +1,6 @@
 package org.oppia.app.databinding
 
+import android.content.Context
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import org.oppia.app.R
@@ -19,44 +20,44 @@ fun setProfileDataText(textView: TextView, timestamp: Long) {
 fun setProfileLastVisitedText(textView: TextView, timestamp: Long) {
   // TODO(#555): Create one central utility file from where we should access date format or even convert date timestamp to string from that file.
   textView.text =
-    String.format(textView.context.getString(R.string.profile_last_used) + " " + getTimeAgo(Date(timestamp)))
+    String.format(textView.context.getString(R.string.profile_last_used) + " " + getTimeAgo(timestamp,textView.context))
 }
 
-private val SECOND_MILLIS = 1000
-private val MINUTE_MILLIS = 60 * SECOND_MILLIS
-private val HOUR_MILLIS = 60 * MINUTE_MILLIS
-private val DAY_MILLIS = 24 * HOUR_MILLIS
+private const val SECOND_MILLIS = 1000
+private const val MINUTE_MILLIS = 60 * SECOND_MILLIS
+private const val HOUR_MILLIS = 60 * MINUTE_MILLIS
+private const val DAY_MILLIS = 24 * HOUR_MILLIS
 
 private fun currentDate(): Date {
   val calendar = Calendar.getInstance()
   return calendar.time
 }
 
-fun getTimeAgo(date: Date): String {
-  var time = date.time
-  if (time < 1000000000000L) {
+fun getTimeAgo(time: Long, context: Context): String {
+  var time = time
+  if (time < 1000000000000L)
+  // if timestamp given in seconds, convert to millis
     time *= 1000
-  }
 
   val now = currentDate().time
-  if (time > now || time <= 0) {
-    return "in the future"
-  }
+  if (time > now || time <= 0) return ""
 
-  val diff = now - time
-  return if (diff < MINUTE_MILLIS) {
-    "moments ago"
-  } else if (diff < 2 * MINUTE_MILLIS) {
-    "a minute ago"
-  } else if (diff < 60 * MINUTE_MILLIS) {
-    (diff / MINUTE_MILLIS).toString() + " minutes ago"
-  } else if (diff < 2 * HOUR_MILLIS) {
-    "an hour ago"
-  } else if (diff < 24 * HOUR_MILLIS) {
-    (diff / HOUR_MILLIS).toString() + " hours ago"
-  } else if (diff < 48 * HOUR_MILLIS) {
-    "yesterday"
-  } else {
-    (diff / DAY_MILLIS).toString() + " days ago"
+  val res = context.resources
+  val timeDifference = now - time
+  return when {
+    timeDifference < MINUTE_MILLIS -> context.getString(R.string.just_now)
+    timeDifference < 50 * MINUTE_MILLIS -> context.getString(
+      R.string.time_ago,
+      res.getQuantityString(R.plurals.minutes, timeDifference.toInt() / MINUTE_MILLIS, timeDifference / MINUTE_MILLIS)
+    )
+    timeDifference < 24 * HOUR_MILLIS -> context.getString(
+      R.string.time_ago,
+      res.getQuantityString(R.plurals.hours, timeDifference.toInt() / HOUR_MILLIS, timeDifference / HOUR_MILLIS)
+    )
+    timeDifference < 48 * HOUR_MILLIS -> context.getString(R.string.yesterday)
+    else -> context.getString(
+      R.string.time_ago,
+      res.getQuantityString(R.plurals.days, timeDifference.toInt() / DAY_MILLIS, timeDifference / DAY_MILLIS)
+    )
   }
 }
