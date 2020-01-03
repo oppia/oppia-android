3d32893f52ad021f1ca4b69edf5e302cdbaf2b7b
marysoloman <nikitamarysolomanpvt@gmail.com>
2020-01-03 14:09:51 +0530

    Admin Auth hi-fi

--- app/src/main/java/org/oppia/app/profile/AdminAuthActivityPresenter.kt
+++ app/src/main/java/org/oppia/app/profile/AdminAuthActivityPresenter.kt
@@ -24,11 +24,14 @@ class AdminAuthActivityPresenter @Inject constructor(
 
   /** Binds ViewModel and sets up text and button listeners. */
   fun handleOnCreate() {
-    activity.title = activity.getString(R.string.add_profile_title)
-    activity.supportActionBar?.setDisplayHomeAsUpEnabled(true)
-    activity.supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_close_white_24dp)
-
     val binding = DataBindingUtil.setContentView<AdminAuthActivityBinding>(activity, R.layout.admin_auth_activity)
+
+    binding.adminAuthToolbar.setNavigationIcon(R.drawable.ic_close_white_24dp)
+    binding.adminAuthToolbar.setNavigationOnClickListener {
+      (activity as AdminAuthActivity).finish()
+    }
+    binding.adminAuthToolbar.title = activity.getString(R.string.add_profile_title)
+
     val adminPin = activity.intent.getStringExtra(KEY_ADMIN_AUTH_ADMIN_PIN)
     binding.apply {
       lifecycleOwner = activity
--- app/src/main/java/org/oppia/app/profile/AdminAuthViewModel.kt
+++ app/src/main/java/org/oppia/app/profile/AdminAuthViewModel.kt
@@ -1,5 +1,8 @@
 package org.oppia.app.profile
 
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.databinding.Bindable
 import androidx.databinding.ObservableField
 import org.oppia.app.activity.ActivityScope
 import org.oppia.app.viewmodel.ObservableViewModel
@@ -9,4 +12,21 @@ import javax.inject.Inject
 @ActivityScope
 class AdminAuthViewModel @Inject constructor() : ObservableViewModel() {
   val errorMessage = ObservableField("")
+
+  var isSubmitButtonActive = ObservableField<Boolean>(false)
+
+  @Bindable
+  fun getInputPinTextWatcher(): TextWatcher {
+    return object : TextWatcher {
+      override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+      }
+
+      override fun onTextChanged(answer: CharSequence, start: Int, before: Int, count: Int) {
+        isSubmitButtonActive.set(answer.toString().trim().length==5)
+      }
+
+      override fun afterTextChanged(s: Editable) {
+      }
+    }
+  }
 }
