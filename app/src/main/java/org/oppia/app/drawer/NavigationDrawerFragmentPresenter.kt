34c7feb585d733e56ad7a4a5779dc904f9cced50
Veena <veena14cs@gmail.com>
2020-02-03 11:55:22 +0530

    Fix #577:  Display profile name on navigation drawer. (#578)
    * display profile name in navigation drawer
    
    * Updated code implementation
    
    * adding tests
    
    * added testcases
    
    * Update NavigationDrawerFragmentPresenter.kt
    
    * updated nit changes.

--- app/src/main/java/org/oppia/app/drawer/NavigationDrawerFragmentPresenter.kt
+++ app/src/main/java/org/oppia/app/drawer/NavigationDrawerFragmentPresenter.kt
@@ -6,32 +6,80 @@ import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.Transformations
 import com.google.android.material.navigation.NavigationView
 import org.oppia.app.R
+import org.oppia.app.databinding.DrawerFragmentBinding
+import org.oppia.app.databinding.NavHeaderNavigationDrawerBinding
 import org.oppia.app.fragment.FragmentScope
 import org.oppia.app.help.HelpActivity
 import org.oppia.app.home.HomeActivity
+import org.oppia.app.home.KEY_HOME_PROFILE_ID
+import org.oppia.app.model.Profile
+import org.oppia.app.model.ProfileId
+import org.oppia.domain.profile.ProfileManagementController
+import org.oppia.util.data.AsyncResult
+import org.oppia.util.logging.Logger
 import javax.inject.Inject
 
 /** The presenter for [NavigationDrawerFragment]. */
 @FragmentScope
 class NavigationDrawerFragmentPresenter @Inject constructor(
-  private val fragment: Fragment
+  private val activity: AppCompatActivity,
+  private val fragment: Fragment,
+  private val profileManagementController: ProfileManagementController,
+  private val logger: Logger
 ) : NavigationView.OnNavigationItemSelectedListener {
-  private lateinit var navView: NavigationView
   private lateinit var drawerToggle: ActionBarDrawerToggle
   private lateinit var drawerLayout: DrawerLayout
   private var previousMenuItemId: Int? = null
+  private lateinit var binding: DrawerFragmentBinding
+  private var internalProfileId: Int = -1
+  private lateinit var profileId: ProfileId
+  private lateinit var navigationDrawerHeaderViewModel: NavigationDrawerHeaderViewModel
 
   fun handleCreateView(inflater: LayoutInflater, container: ViewGroup?): View? {
-    val view: View? = inflater.inflate(R.layout.fragment_drawer, container, false)
-    navView = view!!.findViewById(R.id.fragment_drawer_nav_view)
-    navView.setNavigationItemSelectedListener(this)
+    binding = DrawerFragmentBinding.inflate(inflater, container, /* attachToRoot= */ false)
+    binding.fragmentDrawerNavView.setNavigationItemSelectedListener(this)
+
     fragment.setHasOptionsMenu(true)
-    return view
+
+    internalProfileId = activity.intent.getIntExtra(KEY_HOME_PROFILE_ID, -1)
+    profileId = ProfileId.newBuilder().setInternalId(internalProfileId).build()
+
+    navigationDrawerHeaderViewModel = NavigationDrawerHeaderViewModel()
+
+    val headerBinding = NavHeaderNavigationDrawerBinding.inflate(inflater, container, /* attachToRoot= */ false)
+    headerBinding.viewModel = navigationDrawerHeaderViewModel
+    subscribeToProfileLiveData()
+
+    binding.fragmentDrawerNavView.addHeaderView(headerBinding.root)
+    binding.executePendingBindings()
+
+    return binding.root
+  }
+
+  private fun getProfileData(): LiveData<Profile> {
+    return Transformations.map(profileManagementController.getProfile(profileId), ::processGetProfileResult)
+  }
+
+  private fun subscribeToProfileLiveData() {
+    getProfileData().observe(fragment, Observer<Profile> {
+      navigationDrawerHeaderViewModel.profileName.set(it.name)
+    })
+  }
+
+  private fun processGetProfileResult(profileResult: AsyncResult<Profile>): Profile {
+    if (profileResult.isFailure()) {
+      logger.e("NavigationDrawerFragmentPresenter", "Failed to retrieve profile", profileResult.getErrorOrNull()!!)
+    }
+    return profileResult.getOrDefault(Profile.getDefaultInstance())
   }
 
   private fun openActivityByMenuItemId(menuItemId: Int) {
@@ -58,10 +106,10 @@ class NavigationDrawerFragmentPresenter @Inject constructor(
   fun setUpDrawer(drawerLayout: DrawerLayout, toolbar: Toolbar, menuItemId: Int) {
     when (NavigationDrawerItem.valueFromNavId(menuItemId)) {
       NavigationDrawerItem.HOME -> {
-        navView.menu.getItem(NavigationDrawerItem.HOME.ordinal).isChecked = true
+        binding.fragmentDrawerNavView.menu.getItem(NavigationDrawerItem.HOME.ordinal).isChecked = true
       }
       NavigationDrawerItem.HELP -> {
-        navView.menu.getItem(NavigationDrawerItem.HELP.ordinal).isChecked = true
+        binding.fragmentDrawerNavView.menu.getItem(NavigationDrawerItem.HELP.ordinal).isChecked = true
       }
     }
     this.drawerLayout = drawerLayout
