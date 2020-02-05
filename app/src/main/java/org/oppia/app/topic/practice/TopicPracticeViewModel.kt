5801eebf6c520c8dde78e158b5a4913ba93ee552
veena <veena14cs@gmail.com>
2020-02-05 16:51:41 +0530

    working on topic practice.

--- app/src/main/java/org/oppia/app/topic/practice/TopicPracticeViewModel.kt
+++ app/src/main/java/org/oppia/app/topic/practice/TopicPracticeViewModel.kt
@@ -1,16 +1,27 @@
 package org.oppia.app.topic.practice
 
 import android.view.View
+import androidx.databinding.ObservableArrayList
 import androidx.databinding.ObservableField
+import androidx.databinding.ObservableList
 import androidx.lifecycle.ViewModel
 import org.oppia.app.fragment.FragmentScope
+import org.oppia.app.model.SkillSummary
+import org.oppia.app.viewmodel.ObservableViewModel
+import org.oppia.util.logging.Logger
 import javax.inject.Inject
 
 /** [ViewModel] for showing skills in practice fragment. */
 @FragmentScope
 class TopicPracticeViewModel @Inject constructor(
-  private val topicPracticeFragmentPresenter: TopicPracticeFragmentPresenter
-) : ViewModel() {
+  private val topicPracticeFragmentPresenter: TopicPracticeFragmentPresenter,
+  private val logger: Logger
+  ) : ObservableViewModel() {
+
+  val skillListLiveData : ObservableList<SkillSummary> = ObservableArrayList()
+  fun setSkillList(skillList: List<SkillSummary>) {
+    skillListLiveData.addAll(skillList)
+  }
 
   var isSubmitButtonActive = ObservableField<Boolean>(false)
 
