98715df8e132ad1a2007d46aed566028ff6ce3d0
veena <veena14cs@gmail.com>
2020-02-06 13:47:37 +0530

    Update ContinuePlayingItemViewModel.kt

--- app/src/main/java/org/oppia/app/home/continueplaying/ContinuePlayingItemViewModel.kt
+++ app/src/main/java/org/oppia/app/home/continueplaying/ContinuePlayingItemViewModel.kt
@@ -3,4 +3,4 @@ package org.oppia.app.home.continueplaying
 import org.oppia.app.viewmodel.ObservableViewModel
 
 /** The root [ViewModel] for all individual items that may be displayed in continue-playing fragment recycler view. */
-abstract class ContinuePlayingItemViewModel(): ObservableViewModel()
+abstract class ContinuePlayingItemViewModel: ObservableViewModel()
