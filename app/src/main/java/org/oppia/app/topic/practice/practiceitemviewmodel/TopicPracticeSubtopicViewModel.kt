601fcb507f586605555b7e6b17b1130c73c4495f
veena <veena14cs@gmail.com>
2020-02-17 16:38:25 +0530

    updated practice

--- app/src/main/java/org/oppia/app/topic/practice/practiceitemviewmodel/TopicPracticeSubtopicViewModel.kt
+++ app/src/main/java/org/oppia/app/topic/practice/practiceitemviewmodel/TopicPracticeSubtopicViewModel.kt
@@ -1,7 +1,7 @@
 package org.oppia.app.topic.practice.practiceitemviewmodel
 
-import org.oppia.app.model.SkillSummary
+import org.oppia.app.model.Subtopic
 import org.oppia.app.topic.practice.TopicPracticeFragment
 
 /** Skill summary view model for the recycler view in [TopicPracticeFragment]. */
-class TopicPracticeSkillSummaryViewModel(val skillSummary: SkillSummary) : TopicPracticeItemViewModel()
+class TopicPracticeSubtopicViewModel(val subtopic: Subtopic) : TopicPracticeItemViewModel()
