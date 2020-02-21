7ac70b200e0651518fd69b36dd989ae3838fff3b
abhinavraj23 <abhinav.23.april@gmail.com>
2020-02-21 15:24:56 +0530

    Changed comment

--- app/src/main/java/org/oppia/app/home/RouteToRecentlyPlayedListener.kt
+++ app/src/main/java/org/oppia/app/home/RouteToRecentlyPlayedListener.kt
@@ -1,6 +1,6 @@
 package org.oppia.app.home
 
-/** Listener for when an activity should route to [ContinuePlayingActivity]. */
+/** Listener for when an activity should route to [RecentlyPlayedActivity]. */
 interface RouteToRecentlyPlayedListener {
   fun routeToRecentlyPlayed()
 }
