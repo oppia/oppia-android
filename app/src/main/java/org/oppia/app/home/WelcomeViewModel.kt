616c9a0707276a31ffe05b4f4fb738794e7438d4
Sarthak2601 <agarwal.sarthak262012@gmail.com>
2020-02-22 16:54:10 +0530

    Nit changes

--- app/src/main/java/org/oppia/app/home/WelcomeViewModel.kt
+++ app/src/main/java/org/oppia/app/home/WelcomeViewModel.kt
@@ -3,7 +3,7 @@ package org.oppia.app.home
 import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
 
-/** [ViewModel] for user app usage history. */
+/** [ViewModel] for welcome text in home screen. */
 class WelcomeViewModel : HomeItemViewModel() {
   var profileName : String = ""
 }
