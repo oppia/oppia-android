47d34f87d4e580562f7dd46214d139b70ac42d65
Prarabdh Garg <prarabdhgrg@gmail.com>
2020-02-22 17:30:28 +0530

    Move toolbar from fragment to activity

--- app/src/main/java/org/oppia/app/topic/TopicActivityPresenter.kt
+++ app/src/main/java/org/oppia/app/topic/TopicActivityPresenter.kt
@@ -1,9 +1,13 @@
 package org.oppia.app.topic
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import org.oppia.app.R
 import org.oppia.app.activity.ActivityScope
+import org.oppia.app.drawer.NavigationDrawerFragment
 import javax.inject.Inject
 
 const val TOPIC_FRAGMENT_TAG = "TopicFragment"
@@ -13,9 +17,11 @@ const val STORY_ID_ARGUMENT_KEY = "story_id"
 /** The presenter for [TopicActivity]. */
 @ActivityScope
 class TopicActivityPresenter @Inject constructor(private val activity: AppCompatActivity) {
+  private var navigationDrawerFragment: NavigationDrawerFragment? = null
 
   fun handleOnCreate(topicId: String, storyId: String?) {
     activity.setContentView(R.layout.topic_activity)
+    setUpNavigationDrawer()
     if (getTopicFragment() == null) {
       val topicFragment = TopicFragment()
       val args = Bundle()
@@ -29,6 +35,19 @@ class TopicActivityPresenter @Inject constructor(private val activity: AppCompat
         topicFragment, TOPIC_FRAGMENT_TAG
       ).commitNow()
     }
+  }
+
+  private fun setUpNavigationDrawer() {
+    val toolbar = activity.findViewById<View>(R.id.topic_activity_toolbar) as Toolbar
+    toolbar.title = activity.getString(R.string.topic_prefix) + " "
+    activity.setSupportActionBar(toolbar)
+    activity.supportActionBar!!.setDisplayShowHomeEnabled(true)
+    navigationDrawerFragment =
+      activity.supportFragmentManager.findFragmentById(R.id.topic_activity_fragment_navigation_drawer) as NavigationDrawerFragment
+    navigationDrawerFragment!!.setUpDrawer(
+      activity.findViewById<View>(R.id.topic_activity_drawer_layout) as DrawerLayout,
+      toolbar, R.id.nav_home
+    )
   }
 
   private fun getTopicFragment(): TopicFragment? {
