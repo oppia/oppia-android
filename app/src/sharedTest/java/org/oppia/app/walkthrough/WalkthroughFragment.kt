278d8d5e501c46e0321840c381913b74ebc65d1e
pulkit-mac <aggarwalpulkit596@Gmail.com>
2020-02-28 15:48:00 +0530

    remove wildcard imports

--- app/src/sharedTest/java/org/oppia/app/walkthrough/WalkthroughFragment.kt
+++ app/src/sharedTest/java/org/oppia/app/walkthrough/WalkthroughFragment.kt
@@ -2,9 +2,10 @@ package org.oppia.app.walkthrough
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.*
-import androidx.test.espresso.assertion.ViewAssertions.*
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -23,6 +24,7 @@ class OnboardingFragmentTest {
       onView(withId(R.id.walkthrough_progress_bar)).check(matches(isDisplayed()))
     }
   }
+
   @Test
   fun testWalkthroughFragment_increaseProgress_worksCorrectly() {
     ActivityScenario.launch(WalkthroughActivity::class.java).use {
