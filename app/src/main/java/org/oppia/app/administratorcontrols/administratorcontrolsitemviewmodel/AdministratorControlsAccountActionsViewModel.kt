3ade5194c3757735a3d268bff9614c22c8797f84
Luffy18346 <akashkoradia1@gmail.com>
2020-03-03 07:39:47 +0530

    suggested changes are done

---
+++ app/src/main/java/org/oppia/app/administratorcontrols/administratorcontrolsitemviewmodel/AdministratorControlsAccountActionsViewModel.kt
@@ -0,0 +1,4 @@
+package org.oppia.app.administratorcontrols.administratorcontrolsitemviewmodel
+
+/** Application settings view model for the recycler view in [AdministratorControlsFragment]. */
+class AdministratorControlsAppInformationViewModel : AdministratorControlsItemViewModel()
---
+++ app/src/main/java/org/oppia/app/administratorcontrols/administratorcontrolsitemviewmodel/AdministratorControlsProfileViewModel.kt
@@ -0,0 +1,4 @@
+package org.oppia.app.administratorcontrols.administratorcontrolsitemviewmodel
+
+/** General details and profile management view model for the recycler view in [AdministratorControlsFragment]. */
+class AdministratorControlsGeneralProfileViewModel : AdministratorControlsItemViewModel()
