f1790cb61021484a399b93b60665605dbdd3c4ed
marysoloman <nikitamarysolomanpvt@gmail.com>
2020-03-04 15:00:25 +0530

    Unit parsing

--- app/src/main/java/org/oppia/app/parser/StringToNumberWithUnitsParser.kt
+++ app/src/main/java/org/oppia/app/parser/StringToNumberWithUnitsParser.kt
@@ -9,6 +9,7 @@ import org.oppia.app.model.NumberWithUnits
 import org.oppia.app.model.Unit
 import org.oppia.domain.util.normalizeWhitespace
 import java.lang.Double
+import java.lang.Integer.parseInt
 import java.util.regex.Pattern
 
 /**
@@ -61,7 +62,7 @@ class StringToNumberWithUnitsParser {
         units = rawInput.substring(ind).trim()
       }
     } else return null
-    if (value.contains('/')) {
+    if (value.contains("/")) {
       type = "fraction"
       fractionObj = StringToFractionParser().parseFractionFromString(value)
     } else {
@@ -97,19 +98,248 @@ class StringToNumberWithUnitsParser {
         units = units.trim()
       }
     }
-    if (value.contains('/')) {
+    if (value.contains("/")) {
       type = "fraction"
       fractionObj = StringToFractionParser().parseFractionFromString(value)
     } else {
       type = "real"
       real = Double.parseDouble(value)
     }
+    var unitsObj = UnitsObjectFactory().fromRawInputString(units)
     if (type.equals("fraction"))
       return NumberWithUnits.newBuilder().setFraction(fractionObj).addUnit(NumberUnit.newBuilder().setUnit(units))
         .build()
     else
       return NumberWithUnits.newBuilder().setReal(real).addUnit(NumberUnit.newBuilder().setUnit(units)).build()
     return NumberWithUnits.getDefaultInstance()
+  }
+
+
+  private interface IUnitsDict {
+    // TODO(#7165): Replace "ArrayList<String>" with the exact type. This has been kept as
+    // "ArrayList<String>" because "units" is a list with varying element types. An exact
+    // type needs to be found for it.
+    var units: ArrayList<String>
+  }
+
+  private class Units {
+    // TODO(#7165): Replace "ArrayList<String>" with the exact type. This has been kept as
+    // "ArrayList<String>" because "units" is a list with varying element types. An exact
+    // type needs to be found for it.
+    var units: ArrayList<NumberUnit>
+
+    constructor(unitsList: ArrayList<NumberUnit>) {
+      this.units = unitsList
+    }
+
+    fun toDict(): IUnitsDict {
+      units = this.units
+      return units as IUnitsDict
+    }
+
+    fun unitToString(): String {
+      var unit = ""
+      for (i in 0 until this.units.size) {
+        var d = this.units[i]
+        if (d.exponent == 1) {
+          unit += d.unit + " ";
+        } else {
+          unit += d.unit + "^" + d.exponent.toString() + " ";
+        }
+      }
+      return unit.trim();
+    }
+  }
+
+  private class UnitsObjectFactory {
+    fun isunit(unit: String): Boolean {
+      return !("/*() ".contains(unit))
+    }
+
+    fun stringToLexical(unitsString: String): ArrayList<String> {
+      var units = unitsString
+      units += "#"
+      var unitList: ArrayList<String> = ArrayList()
+      var unit = " "
+      for (i in 0 until units.length) {
+        if ("*/()# ".contains(units[i]) && unit !== "per") {
+          if (unit.length > 0) {
+            if (unitList.size > 0 && this.isunit(unitList.removeAt(unitList.lastIndex))) {
+              unitList.add("*");
+            }
+            unitList.add(unit)
+            unit = ""
+          }
+          if (!("# ".contains(units[i]))) {
+            unitList.add(units[i].toString())
+          }
+        } else if (units[i].toString() == " " && unit == "per") {
+          unitList.add("/");
+          unit = ""
+        } else {
+          unit += units[i];
+        }
+      }
+      return unitList
+    }
+
+    // TODO(#7165): Replace "ArrayList<String>" with the exact type. This has been kept as
+    // "ArrayList<String>" because the return type is a list with varying element types. An
+    // exact type needs to be found for it.
+    fun unitWithMultiplier(unitList: ArrayList<String>): MutableMap<String, Int> {
+      var multiplier = 1
+      var unitsWithMultiplier: MutableMap<String, Int> = HashMap()
+      var parenthesisStack: MutableMap<String, Int> = HashMap()
+
+      for (ind in 0 until unitList.size) {
+        if (unitList[ind] == "/") {
+          multiplier = -multiplier;
+        } else if (unitList[ind] == "(") {
+          if (unitList[ind - 1] == "/") {
+            // If previous element was division then we need to inverse
+            // multiplier when we find its corresponsing closing parenthesis.
+            // Second element of pushed element is used for this purpose.
+            parenthesisStack.put("(", -1)
+          } else {
+            // If previous element was not division then we don"t need to
+            // invert the multiplier.
+            parenthesisStack.put("(", 1)
+          }
+        } else if (unitList[ind] == ")") {
+          var elem = parenthesisStack.values.last()
+          parenthesisStack.remove(parenthesisStack.keys.last())
+          multiplier = elem * multiplier
+        } else if (this.isunit(unitList[ind])) {
+          unitsWithMultiplier.put(unitList[ind], multiplier)
+          // If previous element was division then we need to invert
+          // multiplier.
+          if (unitList[ind - 1] == "/") {
+            multiplier = -multiplier;
+          }
+        }
+      }
+      return unitsWithMultiplier
+    }
+
+    // TODO(#7165): Replace "ArrayList<String>" with the exact type. This has been kept as
+    // "ArrayList<String>" because "unitDict" is a dict with varying element types. An
+    // exact type needs to be found for it, Once that is found the return type
+    // can also be typed.
+    fun convertUnitDictToList(unitDict: MutableMap<String, Int>): ArrayList<NumberUnit> {
+      var unitList: ArrayList<NumberUnit> = ArrayList()
+      for ((key, value) in unitDict) {
+        unitList.add(NumberUnit.newBuilder().setUnit(key).setExponent(value).build())
+      }
+      return unitList
+    }
+
+    // TODO(#7165): Replace "ArrayList<String>" with the exact type. This has been kept as
+    // "ArrayList<String>" because "unitsWithMultiplier" is a dict with varying element types.
+    // An exact type needs to be found for it, Once that is found the return type
+    // can also be typed.
+    fun unitToList(unitsWithMultiplier: MutableMap<String, Int>): ArrayList<NumberUnit> {
+      var unitDict: MutableMap<String, Int> = HashMap()
+      for ((key, value) in unitsWithMultiplier) {
+        var unit = key
+        var multiplier = value
+        var ind = unit.indexOf("^")
+        var s: String
+        var power: Int
+        if (ind > -1) {
+          s = unit.substring(0, ind)
+          power = parseInt(unit.substring(ind + 1))
+        } else {
+          s = unit
+          power = 1
+        }
+        if (!(s in unitDict)) {
+          unitDict.put(s, 0)
+        }
+        unitDict.set(s, multiplier * power)
+      }
+      return this.convertUnitDictToList(unitDict)
+    }
+
+    // TODO(#7165): Replace "ArrayList<String>" with the exact type. This has been kept as
+    // "ArrayList<String>" because "units" is a list with varying element types. An exact
+    // type needs to be found for it.
+    fun fromList(unitsList: ArrayList<NumberUnit>): Units {
+      return Units(unitsList)
+    }
+
+    // TODO(#7165): Replace "ArrayList<String>" with the exact type. This has been kept as
+    // "ArrayList<String>" because the return type is to be determined once "unitToList" has
+    // determined return type.
+    fun fromStringToList(unitsString: String): ArrayList<NumberUnit> {
+      return this.unitToList(
+        this.unitWithMultiplier(this.stringToLexical(unitsString))
+      )
+    }
+
+   fun createCurrencyUnits()
+    {
+      var keys = CURRENCY_UNITS.keys
+      for ( i in 0 until  keys.size) {
+      if (CURRENCY_UNITS[keys.elementAt(i)]!!.baseUnit == null) {
+        // Base unit (like: rupees, dollar etc.).
+        createUnit(CURRENCY_UNITS[keys.elementAt(i)]!!.name, {
+          aliases: CURRENCY_UNITS[keys.elementAt(i)]!!.aliases
+        });
+      } else {
+        // Sub unit (like: paise, cents etc.).
+        createUnit(CURRENCY_UNITS[keys.elementAt(i)].name, {
+          definition: CURRENCY_UNITS[keys.elementAt(i)].base_unit,
+          aliases: CURRENCY_UNITS[keys.elementAt(i)].aliases
+        });
+      }
+    }
+    }
+
+    // TODO(#7165): Replace "ArrayList<String>" with the exact type. This has been kept as
+    // "ArrayList<String>" because "units" is a list with varying element types. An exact
+    // type needs to be found for it.
+    fun toMathjsCompatibleString(unitsString: String): String {
+      var units = unitsString
+      // Makes the units compatible with the math.js allowed format.
+      units = units.replace("per", "/");
+
+      // Special symbols need to be replaced as math.js doesn"t support custom
+      // units starting with special symbols. Also, it doesn"t allow units
+      // followed by a number as in the case of currency units.
+      var keys = (CURRENCY_UNITS).keys
+      for (i in 0 until keys.size) {
+        for (j in 0 until CURRENCY_UNITS[keys.elementAt(i)]!!.frontUnits.size) {
+          if (units.contains(CURRENCY_UNITS[keys.elementAt(i)]!!.frontUnits[j])) {
+            units = units.replace(CURRENCY_UNITS[keys.elementAt(i)]!!.frontUnits[j], "")
+            units = CURRENCY_UNITS[keys.elementAt(i)]!!.name + units
+          }
+        }
+
+        for (j in 0 until CURRENCY_UNITS[keys.elementAt(i)]!!.aliases.size) {
+          if (units.contains(CURRENCY_UNITS[keys.elementAt(i)]!!.aliases[j])) {
+            units = units.replace(
+              CURRENCY_UNITS[keys.elementAt(i)]!!.aliases[j],
+              CURRENCY_UNITS[keys.elementAt(i)]!!.name
+            )
+          }
+        }
+      }
+      return units.trim()
+    }
+
+    // TODO(#7165): Replace "ArrayList<String>" with the exact type. This has been kept as
+    // "ArrayList<String>" because "units" is a list with varying element types. An exact
+    // type needs to be found for it.
+    fun fromRawInputString(unitsString: String): Units {
+      var units = unitsString
+      try {
+        this.createCurrencyUnits()
+      } catch (e:Exception) {}
+
+      var compatibleUnits = this.toMathjsCompatibleString(units)
+
+      return Units(this.fromStringToList(units));
+    }
   }
 
   fun getNumberWithUnitsRealTimeError(rawInput: String, context: Context): String? {
@@ -151,7 +381,7 @@ class StringToNumberWithUnitsParser {
         return NumberWithUnitsParsingError.INVALID_UNIT_CHARS.getErrorMessageFromStringRes(context)
       }
     }
-    if (value.contains('/')) {
+    if (value.contains("/")) {
       type = "fraction"
     } else {
       type = "real"
@@ -162,9 +392,7 @@ class StringToNumberWithUnitsParser {
           context
         )
         units.contains("/") -> NumberWithUnitsParsingError.INVALID_FORMAT.getErrorMessageFromStringRes(context)
-        units.count { it == '-' } > 1 || units.count { it == '.' } > 1 -> NumberWithUnitsParsingError.INVALID_UNIT.getErrorMessageFromStringRes(
-          context
-        )
+        units.count { it == '-' } > 1 || units.count { it == '.' } > 1-> NumberWithUnitsParsingError.INVALID_UNIT.getErrorMessageFromStringRes(context)
         else -> NumberWithUnitsParsingError.VALID.getErrorMessageFromStringRes(context)
       }
     } else
@@ -274,7 +502,7 @@ class StringToNumberWithUnitsParser {
     INVALID_VALUE(error = R.string.number_with_units_error_invalid_value),
     STARTING_WITH_FLOATING_POINT(error = R.string.number_error_starting_with_floating_point);
 
-    /** Returns the string corresponding to this error's string resources, or null if there is none. */
+    /** Returns the string corresponding to this error"s string resources, or null if there is none. */
     fun getErrorMessageFromStringRes(context: Context): String? {
       return error?.let(context::getString)
     }
