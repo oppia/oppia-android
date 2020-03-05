63c99f65f0bdcb9a11dcaae56ce66e25e8e1274a
Rajat Talesra <rajattalesra4914@gmail.com>
2020-03-05 14:48:18 +0530

    Nit changes (#740)

--- app/src/main/java/org/oppia/app/administratorcontrols/administratorcontrolsitemviewmodel/AdministratorControlsAccountActionsViewModel.kt
+++ app/src/main/java/org/oppia/app/administratorcontrols/administratorcontrolsitemviewmodel/AdministratorControlsAccountActionsViewModel.kt
@@ -1,4 +1,4 @@
 package org.oppia.app.administratorcontrols.administratorcontrolsitemviewmodel
 
-/**  [ViewModel] for the recycler view in [AdministratorControlsFragment]. */
+/** [ViewModel] for the recycler view in [AdministratorControlsFragment]. */
 class AdministratorControlsAccountActionsViewModel : AdministratorControlsItemViewModel()
