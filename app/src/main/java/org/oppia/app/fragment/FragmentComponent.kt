0e319b35c298a43f6689e63c7963d9279d55f93f
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2020-03-07 06:08:19 +0530

    Fix #692: Lowfi: HelpActivity (Part 1) (#724)
    * Single item layout done
    
    * Recycler view implemented using MVVM
    
    * Refractoring done and minor fix
    
    * FAQ fragment, activity and its presenter created
    
    * Interface created
    
    * Help FAQ test implemented
    
    * Refractoring done
    
    * Item click implemented
    
    * Test bug fixed
    
    * Refractoring done
    
    * Nit changes and refractor
    
    * Refractor in fetching ViewModel
    
    * Tests modified
    
    * HelpItems enum class implemented
    
    * Refractoring and NIT changes
    
    * Interface and OnClick listener done
    
    * NIT changes
    
    * refractoring done and bug fix
    
    * Indentation fixed
    
    * RouteToFAQ interface implemented
    
    * Item click done through enum of HelpItem
    
    * Constructor redefined in HelpViewModel
    
    * Undoing addition of auto-generated lines
    
    * Manifest files re-arrangement
    
    * Nit changes
    
    * Nit changes and refactoring
    
    * Item click done inside Adapter and corresponding View Model itemClick deleted
    
    * Comment modified
    
    * Secondary constructor omitted in HelpViewModel
    
    * Indentation fix
    
    * HelpActivity Test added and NIT fix
    
    * NIT fix

--- app/src/main/java/org/oppia/app/fragment/FragmentComponent.kt
+++ app/src/main/java/org/oppia/app/fragment/FragmentComponent.kt
@@ -19,6 +19,7 @@ import org.oppia.app.profile.AdminSettingsDialogFragment
 import org.oppia.app.profile.ProfileChooserFragment
 import org.oppia.app.profile.ResetPinDialogFragment
 import org.oppia.app.administratorcontrols.AdministratorControlsFragment
+import org.oppia.app.help.faq.FAQFragment
 import org.oppia.app.story.StoryFragment
 import org.oppia.app.testing.BindableAdapterTestFragment
 import org.oppia.app.topic.TopicFragment
@@ -54,6 +55,7 @@ interface FragmentComponent {
   fun inject(conceptCardFragment: ConceptCardFragment)
   fun inject(downloadsTabFragment: DownloadsTabFragment)
   fun inject(explorationFragment: ExplorationFragment)
+  fun inject(faqFragment: FAQFragment)
   fun inject(helpFragment: HelpFragment)
   fun inject(homeFragment: HomeFragment)
   fun inject(myDownloadsFragment: MyDownloadsFragment)
