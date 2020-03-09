3b260ef884afa093b95efa6a6b9039da61096ac3
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2020-03-09 19:22:53 +0530

    Recycler view attachment with multiple views done and related test written

--- app/src/main/java/org/oppia/app/help/faq/FAQCategoryAdapter.kt
+++ app/src/main/java/org/oppia/app/help/faq/FAQCategoryAdapter.kt
@@ -1,36 +1,26 @@
 package org.oppia.app.help.faq
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.NonNull
 import androidx.recyclerview.widget.RecyclerView
+import org.oppia.app.R
 import org.oppia.app.databinding.FAQItemBinding
 
 /** The Recycler View adapter in the [FAQFragment]. */
 class FAQCategoryAdapter(
   private val arrayList: ArrayList<FAQViewModel>
-) :
-  RecyclerView.Adapter<FAQCategoryAdapter.FAQItemView>() {
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-  override fun onCreateViewHolder(
-    parent: ViewGroup,
-    viewType: Int
-  ): FAQItemView {
-    val layoutInflater = LayoutInflater.from(parent.context)
-    val faqItemBinding = FAQItemBinding.inflate(
-      layoutInflater,
-      parent,
-      /* attachToParent= */ false
-    )
-    return FAQItemView(faqItemBinding)
-  }
+  private val header = 1
+  private val content = 2
 
   override fun getItemCount(): Int {
     return arrayList.size
   }
 
-  override fun onBindViewHolder(holder: FAQItemView, position: Int) {
-    holder.bind(arrayList[position])
-  }
+  class HeaderViewHolder(@NonNull itemView: View) : RecyclerView.ViewHolder(itemView)
 
   class FAQItemView(private val faqItemBinding: FAQItemBinding) :
     RecyclerView.ViewHolder(faqItemBinding.root) {
@@ -39,4 +29,40 @@ class FAQCategoryAdapter(
       faqItemBinding.executePendingBindings()
     }
   }
+
+  override fun getItemViewType(position: Int): Int {
+    return if (position == 0) {
+      header;
+    } else {
+      content;
+    }
+  }
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+    return if (viewType == header) {
+      val view: View = LayoutInflater.from(parent.context).inflate(
+        R.layout.faq_item_header,
+        parent,
+        /* attachToParent= */false
+      )
+      HeaderViewHolder(view)
+    } else {
+      val layoutInflater = LayoutInflater.from(parent.context)
+      val faqItemBinding = FAQItemBinding.inflate(
+        layoutInflater,
+        parent,
+        /* attachToParent= */ false
+      )
+      FAQItemView(faqItemBinding)
+    }
+  }
+
+  override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    if (getItemViewType(position) == header) {
+      (holder as HeaderViewHolder)
+    } else {
+      (holder as FAQItemView).bind(arrayList[position])
+    }
+  }
 }
+
--- app/src/sharedTest/java/org/oppia/app/faq/FAQFragmentTest.kt
+++ app/src/sharedTest/java/org/oppia/app/faq/FAQFragmentTest.kt
@@ -1,8 +1,10 @@
 package org.oppia.app.faq
 
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ActivityScenario.launch
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -10,14 +12,30 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.oppia.app.R
 import org.oppia.app.help.faq.FAQActivity
+import org.oppia.app.recyclerview.RecyclerViewMatcher
 
 @RunWith(AndroidJUnit4::class)
 class FAQFragmentTest {
 
   @Test
-  fun openFAQActivity_checkFeaturedQuestionsDisplayedSuccessfully() {
+  fun openFAQActivity_scrollRecyclerViewToZeroPosition_checkFeaturedQuestionsDisplayedSuccessfully() {
     launch(FAQActivity::class.java).use {
-      onView(withId(R.id.featured_questions)).check(matches(withText(R.string.featured_questions)))
+      onView(withId(R.id.faq_fragment_recycler_view)).perform(
+        RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
+          0
+        )
+      )
+      onView(
+        RecyclerViewMatcher.atPositionOnView(
+          R.id.faq_fragment_recycler_view,
+          0,
+          R.id.recycler_item_text_view
+        )
+      ).check(
+        matches(
+          withText(R.string.featured_questions)
+        )
+      )
     }
   }
 }
