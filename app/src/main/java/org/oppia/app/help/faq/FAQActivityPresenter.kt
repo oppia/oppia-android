82cdae9191b84f5f5be1532bc45e2ed88ada8382
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2020-03-10 11:51:15 +0530

    FAQ Activity back button and title set inside presenter

--- app/src/main/java/org/oppia/app/help/faq/FAQActivityPresenter.kt
+++ app/src/main/java/org/oppia/app/help/faq/FAQActivityPresenter.kt
@@ -1,6 +1,5 @@
 package org.oppia.app.help.faq
 
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil
@@ -31,7 +30,9 @@ class FAQActivityPresenter @Inject constructor(
 
     faqActivityToolbar = binding.faqActivityToolbar
     activity.setSupportActionBar(faqActivityToolbar)
-    this.faqActivityToolbar.title = activity.getString(R.string.FAQs)
+    activity.supportActionBar!!.title = activity.getString(R.string.FAQs)
+    activity.supportActionBar!!.setDisplayShowHomeEnabled(true)
+    activity.supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
     if (getFAQFragment() == null) {
       activity.supportFragmentManager.beginTransaction().add(
@@ -39,13 +40,6 @@ class FAQActivityPresenter @Inject constructor(
         FAQFragment()
       ).commitNow()
     }
-  }
-
-  private fun setUpToolbar() {
-    val toolbar = activity.findViewById<View>(R.id.faq_activity_toolbar) as Toolbar
-    activity.setSupportActionBar(toolbar)
-    activity.supportActionBar!!.setDisplayShowHomeEnabled(true)
-    activity.supportActionBar!!.setDisplayHomeAsUpEnabled(true)
   }
 
   private fun getFAQFragment(): FAQFragment? {
