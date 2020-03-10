54b36ab0041299677c2a78bc58a1b37ce832672c
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2020-03-11 01:54:56 +0530

    Tests implemented

--- app/src/sharedTest/java/org/oppia/app/faq/FAQFragmentTest.kt
+++ app/src/sharedTest/java/org/oppia/app/faq/FAQFragmentTest.kt
@@ -5,9 +5,11 @@ import androidx.test.core.app.ActivityScenario.launch
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.Matchers.not
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.oppia.app.R
@@ -16,6 +18,8 @@ import org.oppia.app.recyclerview.RecyclerViewMatcher
 
 @RunWith(AndroidJUnit4::class)
 class FAQFragmentTest {
+
+  var itemCount: Int = 9
 
   @Test
   fun openFAQActivity_scrollRecyclerViewToZeroPosition_checkFeaturedQuestionsDisplayedSuccessfully() {
@@ -34,6 +38,28 @@ class FAQFragmentTest {
       ).check(
         matches(
           withText(R.string.featured_questions)
+        )
+      )
+    }
+  }
+
+  @Test
+  fun openFAQActivity_scrollRecyclerViewToLastPosition_checkDividerLineIsNotDisplayed() {
+    launch(FAQActivity::class.java).use {
+      onView(withId(R.id.faq_fragment_recycler_view)).perform(
+        RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
+          itemCount - 1
+        )
+      )
+      onView(
+        RecyclerViewMatcher.atPositionOnView(
+          R.id.faq_fragment_recycler_view,
+          itemCount - 1,
+          R.id.recycler_item_divider_view
+        )
+      ).check(
+        matches(
+          not(isDisplayed())
         )
       )
     }
