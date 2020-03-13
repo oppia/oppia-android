f99690c9dce28612f4a5f760451054a384fd3364
Pulkit Aggarwal <aggarwalpulkit596@gmail.com>
2020-03-13 14:20:40 +0530

    Fix #729: Add ViewPager to Walkthrough (#730)
    * Add PagerAdapter
    
    * Add Sub Fragments and Presenter
    
    * Add Sub Fragments and Presenters [skip CI]
    
    * Use ViewPager instead of ViewPager2
    
    * Add PageChangeListener
    
    * Add buttons to fragments
    
    * remove viewpager from walkthrough
    
    * use enum for Changing Fragments[skip CI]
    
    * walkthrough completed[skip CI]
    
    * Formatting changes[skip CI]
    
    * update test for walkthrough
    
    * nit changes
    
    * add one more test
    
    * nit changes
    
    * some more changes
    
    * Add Landscape for HelpActivity
    
    * use replace instead of add for fragment
    
    * nit changes
    
    * nit changes
    
    * remove unused imports

--- app/src/main/java/org/oppia/app/fragment/FragmentComponent.kt
+++ app/src/main/java/org/oppia/app/fragment/FragmentComponent.kt
@@ -36,7 +36,9 @@ import org.oppia.app.topic.questionplayer.QuestionPlayerFragment
 import org.oppia.app.topic.review.TopicReviewFragment
 import org.oppia.app.topic.reviewcard.ReviewCardFragment
 import org.oppia.app.view.ViewComponent
-import org.oppia.app.walkthrough.WalkthroughFragment
+import org.oppia.app.walkthrough.end.WalkthroughFinalFragment
+import org.oppia.app.walkthrough.topiclist.WalkthroughTopicListFragment
+import org.oppia.app.walkthrough.welcome.WalkthroughWelcomeFragment
 import javax.inject.Provider
 
 /** Root subcomponent for all fragments. */
@@ -84,5 +86,7 @@ interface FragmentComponent {
   fun inject(topicReviewFragment: TopicReviewFragment)
   fun inject(topicPracticeFragment: TopicPracticeFragment)
   fun inject(updatesTabFragment: UpdatesTabFragment)
-  fun inject(walkthroughFragment: WalkthroughFragment)
+  fun inject(walkthroughFinalFragment: WalkthroughFinalFragment)
+  fun inject(walkthroughTopicListFragment: WalkthroughTopicListFragment)
+  fun inject(walkthroughWelcomeFragment: WalkthroughWelcomeFragment)
 }
