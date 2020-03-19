1f585470e2d2a341f0a2bfe821df3414bb2679ec
pulkit-mac <aggarwalpulkit596@Gmail.com>
2020-03-20 00:00:21 +0530

    Add WalkthroughTopicSummaryViewModel

--- app/src/main/java/org/oppia/app/walkthrough/topiclist/WalkthroughTopicListFragmentPresenter.kt
+++ app/src/main/java/org/oppia/app/walkthrough/topiclist/WalkthroughTopicListFragmentPresenter.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import org.oppia.app.databinding.WalkthroughTopicHeaderViewBinding
 import org.oppia.app.databinding.WalkthroughTopicListFragmentBinding
+import org.oppia.app.databinding.WalkthroughTopicSummaryViewBinding
 import org.oppia.app.fragment.FragmentScope
 import org.oppia.app.recyclerview.BindableAdapter
 import org.oppia.app.viewmodel.ViewModelProvider
@@ -70,12 +71,12 @@ class WalkthroughTopicListFragmentPresenter @Inject constructor(
         setViewModel = WalkthroughTopicHeaderViewBinding::setViewModel,
         transformViewModel = { it as WalkthroughTopicHeaderViewModel }
       )
-//      .registerViewDataBinder(
-//        viewType = ViewType.VIEW_TYPE_TOPIC,
-//        inflateDataBinding = StoryChapterViewBinding::inflate,
-//        setViewModel = StoryChapterViewBinding::setViewModel,
-//        transformViewModel = { it as StoryChapterSummaryViewModel }
-//      )
+      .registerViewDataBinder(
+        viewType = ViewType.VIEW_TYPE_TOPIC,
+        inflateDataBinding = WalkthroughTopicSummaryViewBinding::inflate,
+        setViewModel = WalkthroughTopicSummaryViewBinding::setViewModel,
+        transformViewModel = { it as WalkthroughTopicSummaryViewModel }
+      )
       .build()
   }
 
