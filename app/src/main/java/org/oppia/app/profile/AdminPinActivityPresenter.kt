13ce331414357e01210ad7d24d967f63bf2d337b
Cheenu Soni <43966615+NullByte08@users.noreply.github.com>
2020-03-20 15:07:00 +0530

    Update AdminPinActivityPresenter.kt

--- app/src/main/java/org/oppia/app/profile/AdminPinActivityPresenter.kt
+++ app/src/main/java/org/oppia/app/profile/AdminPinActivityPresenter.kt
@@ -98,11 +98,9 @@ class AdminPinActivityPresenter @Inject constructor(
   ) {
     profileInputView.addTextChangedListener(object : TextWatcher {
       override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-
       override fun afterTextChanged(p0: Editable?) {
         onTextChanged(p0)
       }
-
       override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
     })
   }
