098916dee17010c420d01077a7e941496d845d5b
marysoloman <nikitamarysolomanpvt@gmail.com>
2020-03-20 12:23:03 +0530

    Merge branches 'develop' and 'low-fi-landscape-profile-selection-approach-1' of https://github.com/oppia/oppia-android into landscape
    # Conflicts:
    #	app/src/Test/java/org/oppia/app/administratorcontrols/AdministratorControlsActivityTest.kt
    #	app/src/Test/java/org/oppia/app/administratorcontrols/AppVersionActivityTest.kt
    #	app/src/Test/java/org/oppia/app/completedstorylist/CompletedStoryListActivityTest.kt
    #	app/src/Test/java/org/oppia/app/faq/FAQFragmentTest.kt
    #	app/src/Test/java/org/oppia/app/help/HelpFragmentTest.kt
    #	app/src/Test/java/org/oppia/app/home/ContinuePlayingFragmentTest.kt
    #	app/src/Test/java/org/oppia/app/home/RecentlyPlayedFragmentTest.kt
    #	app/src/Test/java/org/oppia/app/onboarding/OnboardingFragmentTest.kt
    #	app/src/Test/java/org/oppia/app/ongoingtopiclist/OngoingTopicListActivityTest.kt
    #	app/src/Test/java/org/oppia/app/options/OptionsFragmentTest.kt
    #	app/src/Test/java/org/oppia/app/profileprogress/ProfilePictureActivityTest.kt
    #	app/src/Test/java/org/oppia/app/profileprogress/ProfileProgressFragmentTest.kt
    #	app/src/Test/java/org/oppia/app/topic/lessons/TopicLessonsFragmentTest.kt
    #	app/src/Test/java/org/oppia/app/topic/play/TopicPlayFragmentTest.kt
    #	app/src/Test/java/org/oppia/app/topic/practice/TopicPracticeFragmentTest.kt
    #	app/src/Test/java/org/oppia/app/topic/reviewcard/ReviewCardFragmentTest.kt
    #	app/src/Test/java/org/oppia/app/topic/train/TopicTrainFragmentTest.kt
    #	app/src/Test/java/org/oppia/app/utility/ProgressMatcher.kt
    #	app/src/Test/java/org/oppia/app/walkthrough/WalkthroughActivityTest.kt
    #	app/src/Test/java/org/oppia/app/walkthrough/WalkthroughWelcomeFragmentTest.kt
    #	app/src/main/res/values-xhdpi/dimens.xml
    #	app/src/sharedTest/java/org/oppia/app/topic/play/TopicPlayFragmentTest.kt

---
+++ app/src/sharedTest/java/org/oppia/app/topic/TopicFragmentTest.kt
@@ -0,0 +1,380 @@
+package org.oppia.app.topic
+
+import android.app.Application
+import android.content.Context
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ActivityScenario.launch
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.swipeLeft
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions.scrollToPosition
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.isRoot
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withParent
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import dagger.BindsInstance
+import dagger.Component
+import dagger.Module
+import dagger.Provides
+import kotlinx.coroutines.CoroutineDispatcher
+import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.CoreMatchers.containsString
+import org.hamcrest.CoreMatchers.instanceOf
+import org.hamcrest.Matchers
+import org.junit.Ignore
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.oppia.app.R
+import org.oppia.app.recyclerview.RecyclerViewMatcher
+import org.oppia.app.recyclerview.RecyclerViewMatcher.Companion.atPositionOnView
+import org.oppia.app.utility.EspressoTestsMatchers.matchCurrentTabTitle
+import org.oppia.app.utility.OrientationChangeAction.Companion.orientationLandscape
+import org.oppia.domain.topic.FRACTIONS_TOPIC_ID
+import org.oppia.util.threading.BackgroundDispatcher
+import org.oppia.util.threading.BlockingDispatcher
+import javax.inject.Singleton
+
+private const val TOPIC_NAME = "Fractions"
+
+/** Tests for [TopicFragment]. */
+@RunWith(AndroidJUnit4::class)
+class TopicFragmentTest {
+
+  private val internalProfileId = 0
+
+  @Test
+  fun testTopicFragment_showsTopicFragmentWithMultipleTabs() {
+    launchTopicActivityIntent(internalProfileId, FRACTIONS_TOPIC_ID).use {
+      onView(withId(R.id.topic_tabs_container)).perform(click()).check(matches(isDisplayed()))
+    }
+  }
+
+  @Test
+  fun testTopicFragment_swipePage_hasSwipedPage() {
+    launchTopicActivityIntent(internalProfileId, FRACTIONS_TOPIC_ID).use {
+      onView(withId(R.id.topic_tabs_viewpager)).check(matches(isDisplayed()))
+      onView(withId(R.id.topic_tabs_viewpager)).perform(swipeLeft())
+      onView(withId(R.id.topic_tabs_container)).check(matches(matchCurrentTabTitle(TopicTab.getTabForPosition(1).name)))
+    }
+  }
+
+  @Test
+  fun testTopicFragment_infoTopicTab_isDisplayedInTabLayout() {
+    launchTopicActivityIntent(internalProfileId, FRACTIONS_TOPIC_ID).use {
+      onView(withText(TopicTab.getTabForPosition(0).name)).check(matches(isDescendantOfA(withId(R.id.topic_tabs_container))))
+    }
+  }
+
+  @Test
+  fun testTopicFragment_defaultTabIsInfo_isSuccessful() {
+    launchTopicActivityIntent(internalProfileId, FRACTIONS_TOPIC_ID).use {
+      onView(withId(R.id.topic_tabs_container)).check(matches(matchCurrentTabTitle(TopicTab.getTabForPosition(0).name)))
+    }
+  }
+
+  @Test
+  fun testTopicFragment_defaultTabIsInfo_showsMatchingContent() {
+    launchTopicActivityIntent(internalProfileId, FRACTIONS_TOPIC_ID).use {
+      onView(withId(R.id.topic_name_text_view)).check(
+        matches(
+          withText(
+            containsString(TOPIC_NAME)
+          )
+        )
+      )
+    }
+  }
+
+  @Test
+  fun testTopicFragment_clickOnPlayTab_showsPlayTabSelected() {
+    launchTopicActivityIntent(internalProfileId, FRACTIONS_TOPIC_ID).use {
+      onView(
+        allOf(
+          withText(TopicTab.getTabForPosition(1).name),
+          isDescendantOfA(withId(R.id.topic_tabs_container))
+        )
+      ).perform(click())
+      onView(withId(R.id.topic_tabs_container)).check(matches(matchCurrentTabTitle(TopicTab.getTabForPosition(1).name)))
+    }
+  }
+
+  @Test
+  fun testTopicFragment_clickOnPlayTab_showsPlayTabWithContentMatched() {
+    launchTopicActivityIntent(internalProfileId, FRACTIONS_TOPIC_ID).use {
+      onView(
+        allOf(
+          withText(TopicTab.getTabForPosition(1).name),
+          isDescendantOfA(withId(R.id.topic_tabs_container))
+        )
+      ).perform(click())
+      onView(withId(R.id.story_summary_recycler_view)).perform(
+        scrollToPosition<RecyclerView.ViewHolder>(
+          1
+        )
+      )
+      onView(
+        RecyclerViewMatcher.atPosition(
+          R.id.story_summary_recycler_view,
+          1
+        )
+      ).check(matches(ViewMatchers.hasDescendant(withText(containsString("Matthew Goes to the Bakery")))))
+    }
+  }
+
+  @Test
+  fun testTopicFragment_clickOnPracticeTab_showsPracticeTabSelected() {
+    launchTopicActivityIntent(internalProfileId, FRACTIONS_TOPIC_ID).use {
+      onView(
+        allOf(
+          withText(TopicTab.getTabForPosition(2).name),
+          isDescendantOfA(withId(R.id.topic_tabs_container))
+        )
+      ).perform(click())
+      onView(withId(R.id.topic_tabs_container)).check(matches(matchCurrentTabTitle(TopicTab.getTabForPosition(2).name)))
+    }
+  }
+
+  @Test
+  fun testTopicFragment_clickOnPracticeTab_showsPracticeTabWithContentMatched() {
+    launchTopicActivityIntent(internalProfileId, FRACTIONS_TOPIC_ID).use {
+      onView(
+        allOf(
+          withText(TopicTab.getTabForPosition(2).name),
+          isDescendantOfA(withId(R.id.topic_tabs_container))
+        )
+      ).perform(click())
+      onView(withText("Master These Skills")).check(matches(isDisplayed()))
+    }
+  }
+
+  @Test
+  fun testTopicFragment_clickOnReviewTab_showsReviewTabSelected() {
+    launchTopicActivityIntent(internalProfileId, FRACTIONS_TOPIC_ID).use {
+      onView(
+        allOf(
+          withText(TopicTab.getTabForPosition(3).name),
+          isDescendantOfA(withId(R.id.topic_tabs_container))
+        )
+      ).perform(click())
+      onView(withId(R.id.topic_tabs_container)).check(matches(matchCurrentTabTitle(TopicTab.getTabForPosition(3).name)))
+    }
+  }
+
+  @Test
+  fun testTopicFragment_clickOnReviewTab_showsReviewTabWithContentMatched() {
+    launchTopicActivityIntent(internalProfileId, FRACTIONS_TOPIC_ID).use {
+      onView(
+        allOf(
+          withText(TopicTab.getTabForPosition(3).name),
+          isDescendantOfA(withId(R.id.topic_tabs_container))
+        )
+      ).perform(click())
+      onView(atPositionOnView(R.id.review_recycler_view, 0, R.id.subtopic_title)).check(
+        matches(
+          withText(
+            containsString(
+              "What is a Fraction?"
+            )
+          )
+        )
+      )
+    }
+  }
+
+  @Test
+  fun testTopicFragment_clickOnReviewTab_thenInfoTab_showsInfoTab() {
+    launchTopicActivityIntent(internalProfileId, FRACTIONS_TOPIC_ID).use {
+      onView(
+        allOf(
+          withText(TopicTab.getTabForPosition(3).name),
+          isDescendantOfA(withId(R.id.topic_tabs_container))
+        )
+      ).perform(click())
+      onView(
+        allOf(
+          withText(TopicTab.getTabForPosition(0).name),
+          isDescendantOfA(withId(R.id.topic_tabs_container))
+        )
+      ).perform(click())
+      onView(withId(R.id.topic_tabs_container)).check(
+        matches(
+          matchCurrentTabTitle(
+            TopicTab.getTabForPosition(0).name
+          )
+        )
+      )
+    }
+  }
+
+  @Test
+  fun testTopicFragment_clickOnReviewTab_thenInfoTab_showsInfoTabWithContentMatched() {
+    launchTopicActivityIntent(internalProfileId, FRACTIONS_TOPIC_ID).use {
+      onView(
+        allOf(
+          withText(TopicTab.getTabForPosition(3).name),
+          isDescendantOfA(withId(R.id.topic_tabs_container))
+        )
+      ).perform(click())
+      onView(
+        allOf(
+          withText(TopicTab.getTabForPosition(0).name),
+          isDescendantOfA(withId(R.id.topic_tabs_container))
+        )
+      ).perform(click())
+      onView(withId(R.id.topic_name_text_view)).check(
+        matches(
+          withText(
+            containsString(TOPIC_NAME)
+          )
+        )
+      )
+    }
+  }
+
+  @Test
+  @Ignore("Landscape not properly supported") // TODO(#56): Reenable once landscape is supported.
+  fun testTopicFragment_clickOnPlayTab_configurationChange_showsSameTabAndItsContent() {
+    launchTopicActivityIntent(internalProfileId, FRACTIONS_TOPIC_ID).use {
+      onView(
+        allOf(
+          withText(TopicTab.getTabForPosition(1).name),
+          isDescendantOfA(withId(R.id.topic_tabs_container))
+        )
+      ).perform(click())
+      onView(isRoot()).perform(orientationLandscape())
+      onView(withId(R.id.topic_tabs_container)).check(
+        matches(
+          matchCurrentTabTitle(
+            TopicTab.getTabForPosition(1).name
+          )
+        )
+      )
+      onView(withId(R.id.story_summary_recycler_view)).perform(
+        scrollToPosition<RecyclerView.ViewHolder>(
+          1
+        )
+      )
+      onView(
+        RecyclerViewMatcher.atPosition(
+          R.id.story_summary_recycler_view,
+          1
+        )
+      ).check(matches(ViewMatchers.hasDescendant(withText(Matchers.containsString("First Story")))))
+    }
+  }
+
+  @Test
+  @Ignore("Landscape not properly supported") // TODO(#56): Reenable once landscape is supported.
+  fun testTopicFragment_clickOnPracticeTab_configurationChange_showsSameTabAndItsContent() {
+    launchTopicActivityIntent(internalProfileId, FRACTIONS_TOPIC_ID).use {
+      onView(
+        allOf(
+          withText(TopicTab.getTabForPosition(2).name),
+          isDescendantOfA(withId(R.id.topic_tabs_container))
+        )
+      ).perform(click())
+      onView(withText("Master These Skills")).check(matches(isDisplayed()))
+      onView(isRoot()).perform(orientationLandscape())
+      onView(withId(R.id.topic_tabs_container)).check(
+        matches(
+          matchCurrentTabTitle(
+            TopicTab.getTabForPosition(2).name
+          )
+        )
+      )
+      onView(withText("Master These Skills")).check(matches(isDisplayed()))
+    }
+  }
+
+  @Test
+  @Ignore("Landscape not properly supported") // TODO(#56): Reenable once landscape is supported.
+  fun testTopicFragment_clickOnReviewTab_configurationChange_showsSameTabAndItsContent() {
+    launchTopicActivityIntent(internalProfileId, FRACTIONS_TOPIC_ID).use {
+      onView(
+        allOf(
+          withText(TopicTab.getTabForPosition(3).name),
+          isDescendantOfA(withId(R.id.topic_tabs_container))
+        )
+      ).perform(click())
+      onView(isRoot()).perform(orientationLandscape())
+      onView(withId(R.id.topic_tabs_container)).check(
+        matches(
+          matchCurrentTabTitle(
+            TopicTab.getTabForPosition(3).name
+          )
+        )
+      )
+      onView(
+        atPositionOnView(R.id.review_recycler_view, 0, R.id.subtopic_title)
+      ).check(matches(withText(containsString("What is a Fraction?"))))
+    }
+  }
+
+  @Test
+  @Ignore("Landscape not properly supported") // TODO(#56): Reenable once landscape is supported.
+  fun testTopicFragment_configurationChange_showsDefaultTabAndItsContent() {
+    launchTopicActivityIntent(internalProfileId, FRACTIONS_TOPIC_ID).use {
+      onView(isRoot()).perform(orientationLandscape())
+      onView(withId(R.id.topic_tabs_container)).check(
+        matches(
+          matchCurrentTabTitle(
+            TopicTab.getTabForPosition(0).name
+          )
+        )
+      )
+      onView(withId(R.id.topic_name_text_view)).check(
+        matches(
+          withText(
+            containsString(TOPIC_NAME)
+          )
+        )
+      )
+    }
+  }
+
+  private fun launchTopicActivityIntent(internalProfileId: Int, topicId: String): ActivityScenario<TopicActivity> {
+    val intent =
+      TopicActivity.createTopicActivityIntent(ApplicationProvider.getApplicationContext(), internalProfileId, topicId)
+    return launch(intent)
+  }
+
+  @Module
+  class TestModule {
+    @Provides
+    @Singleton
+    fun provideContext(application: Application): Context {
+      return application
+    }
+
+    // TODO(#89): Introduce a proper IdlingResource for background dispatchers to ensure they all complete before
+    //  proceeding in an Espresso test. This solution should also be interoperative with Robolectric contexts by using a
+    //  test coroutine dispatcher.
+
+    @Singleton
+    @Provides
+    @BackgroundDispatcher
+    fun provideBackgroundDispatcher(@BlockingDispatcher blockingDispatcher: CoroutineDispatcher): CoroutineDispatcher {
+      return blockingDispatcher
+    }
+  }
+
+  @Singleton
+  @Component(modules = [TestModule::class])
+  interface TestApplicationComponent {
+    @Component.Builder
+    interface Builder {
+      @BindsInstance
+      fun setApplication(application: Application): Builder
+
+      fun build(): TestApplicationComponent
+    }
+  }
+}
