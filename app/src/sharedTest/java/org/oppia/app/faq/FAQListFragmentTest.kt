d2dea5814018b71dca16b281d42e2077a78d44f5
Rajat Talesra <rajattalesra4914@gmail.com>
2020-03-30 10:33:19 +0530

    Highfi FAQList (#906)
    Co-authored-by: Rajat Talesra <talesra@talesra-macbookpro.roam.corp.google.com>

--- app/src/sharedTest/java/org/oppia/app/faq/FAQListFragmentTest.kt
+++ app/src/sharedTest/java/org/oppia/app/faq/FAQListFragmentTest.kt
@@ -13,12 +13,11 @@ import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.isRoot
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.hamcrest.Matchers.allOf
-import org.hamcrest.Matchers.not
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -28,12 +27,11 @@ import org.oppia.app.help.faq.FAQListActivity
 import org.oppia.app.help.faq.faqsingle.FAQSingleActivity
 import org.oppia.app.recyclerview.RecyclerViewMatcher.Companion.atPosition
 import org.oppia.app.recyclerview.RecyclerViewMatcher.Companion.atPositionOnView
+import org.oppia.app.utility.OrientationChangeAction.Companion.orientationLandscape
 
 /** Tests for [FAQListFragment]. */
 @RunWith(AndroidJUnit4::class)
 class FAQListFragmentTest {
-
-  private val itemCount: Int = 9
 
   @Before
   fun setUp() {
@@ -72,14 +70,17 @@ class FAQListFragmentTest {
   }
 
   @Test
-  fun openFAQListActivity_scrollRecyclerViewToLastPosition_checkDividerLineIsNotDisplayed() {
+  fun openFAQListActivity_changeConfiguration_selectFAQQuestion_opensFAQSingleActivity() {
     launch(FAQListActivity::class.java).use {
-      onView(withId(R.id.faq_fragment_recycler_view)).perform(scrollToPosition<RecyclerView.ViewHolder>(itemCount - 1))
-      onView(
-        atPositionOnView(
-          R.id.faq_fragment_recycler_view, itemCount - 1, R.id.faq_question_divider_view
+      onView(isRoot()).perform(orientationLandscape())
+      onView(atPosition(R.id.faq_fragment_recycler_view, 1)).perform(click())
+      intended(
+        allOf(
+          hasExtra(FAQSingleActivity.FAQ_SINGLE_ACTIVITY_QUESTION, getResources().getString(R.string.faq_question_1)),
+          hasExtra(FAQSingleActivity.FAQ_SINGLE_ACTIVITY_ANSWER, getResources().getString(R.string.faq_answer_1)),
+          hasComponent(FAQSingleActivity::class.java.name)
         )
-      ).check(matches(not(isDisplayed())))
+      )
     }
   }
 
