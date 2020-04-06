3479c1f9993ed48aa1f3df4128a49d6ef761a029
veena <veena14cs@gmail.com>
2020-04-06 13:21:55 +0530

    Update DateTimeUtilTest.kt

--- utility/src/test/java/org/oppia/util/datetime/DateTimeUtilTest.kt
+++ utility/src/test/java/org/oppia/util/datetime/DateTimeUtilTest.kt
@@ -33,8 +33,6 @@ import javax.inject.Inject
 import javax.inject.Qualifier
 import javax.inject.Singleton
 
-
-
 private const val MORNING_TIMESTAMP = 1585888380000//1579666500000- 19800000
 private const val AFTERNOON_TIMESTAMP = 1585902780000//1579774500000 -19800000
 private const val EVENING_TIMESTAMP = 1585917180000//1579792500000 - 19800000
@@ -44,9 +42,12 @@ private const val EVENING_TIMESTAMP = 1585917180000//1579792500000 - 19800000
 @Config(manifest = Config.NONE)
 class DateTimeUtilTest {
 
-  @Inject lateinit var dateTimeUtil: DateTimeUtil
-  @Inject lateinit var context: Context
-  @Inject lateinit var oppiaClock: OppiaClock
+  @Inject
+  lateinit var dateTimeUtil: DateTimeUtil
+  @Inject
+  lateinit var context: Context
+  @Inject
+  lateinit var oppiaClock: OppiaClock
 
   // https://kotlin.github.io/kotlinx.coroutines/kotlinx-coroutines-test/
   @ObsoleteCoroutinesApi
