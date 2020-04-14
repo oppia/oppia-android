bdadc588c7c905e22611b054056d7129c8257b6a
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2020-04-14 10:59:26 +0530

    NIT update (#977)

--- app/src/main/java/org/oppia/app/help/HelpItems.kt
+++ app/src/main/java/org/oppia/app/help/HelpItems.kt
@@ -1,15 +1,6 @@
 package org.oppia.app.help
 
 /** Enum class containing the items for the Recycler view of [HelpActivity]. */
-enum class HelpItems(private var position: Int) {
-  FAQ(position = 0);
-
-  companion object {
-    fun getHelpItemForPosition(position: Int): HelpItems {
-      val ordinal = checkNotNull(values().map(HelpItems::position)[position]) {
-        "No tab corresponding to position: $position"
-      }
-      return values()[ordinal]
-    }
-  }
+enum class HelpItems {
+  FAQ;
 }
