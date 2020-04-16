3abecc695e51f364cec14cc628da097cbb996cbd
Sarthak Agarwal <agarwal.sarthak262012@gmail.com>
2020-04-16 12:33:13 +0530

    Fix #966 : Domain Remove unnecessary thumbnail code (#982)
    * removal of SkillThumbnail and SubtopicThumbnail code
    
    * modification
    
    * nit

--- app/src/main/java/org/oppia/app/databinding/ImageViewBindingAdapters.kt
+++ app/src/main/java/org/oppia/app/databinding/ImageViewBindingAdapters.kt
@@ -9,8 +9,6 @@ import com.bumptech.glide.request.RequestOptions
 import org.oppia.app.R
 import org.oppia.app.model.LessonThumbnailGraphic
 import org.oppia.app.model.ProfileAvatar
-import org.oppia.app.model.SkillThumbnailGraphic
-import org.oppia.app.model.SubtopicThumbnailGraphic
 
 /**
  * Allows binding drawables to an [ImageView] via "android:src". Source: https://stackoverflow.com/a/35809319/3689782.
@@ -48,45 +46,19 @@ fun setImageDrawable(imageView: ImageView, thumbnailGraphic: LessonThumbnailGrap
         R.drawable.lesson_thumbnail_graphic_child_with_fractions_homework
       LessonThumbnailGraphic.DUCK_AND_CHICKEN -> R.drawable.lesson_thumbnail_graphic_duck_and_chicken
       LessonThumbnailGraphic.PERSON_WITH_PIE_CHART -> R.drawable.lesson_thumbnail_graphic_person_with_pie_chart
-      else -> R.drawable.lesson_thumbnail_default
-    }
-  )
-}
-
-/**
- * Binds the specified [SkillThumbnailGraphic] as the source for the [ImageView]. The view should be specified to have
- * no width/height (when sized in a constraint layout), and use centerCrop for the image to appear correctly.
- */
-@BindingAdapter("android:src")
-fun setImageDrawable(imageView: ImageView, thumbnailGraphic: SkillThumbnailGraphic) {
-  setImageDrawable(
-    imageView, when (thumbnailGraphic) {
-      SkillThumbnailGraphic.IDENTIFYING_THE_PARTS_OF_A_FRACTION -> R.drawable.topic_fractions_01
-      SkillThumbnailGraphic.WRITING_FRACTIONS -> R.drawable.topic_fractions_02
-      SkillThumbnailGraphic.EQUIVALENT_FRACTIONS -> R.drawable.topic_fractions_03
-      SkillThumbnailGraphic.MIXED_NUMBERS_AND_IMPROPER_FRACTIONS -> R.drawable.topic_fractions_04
-      SkillThumbnailGraphic.COMPARING_FRACTIONS -> R.drawable.topic_fractions_05
-      SkillThumbnailGraphic.ADDING_AND_SUBTRACTING_FRACTIONS -> R.drawable.topic_fractions_06
-      SkillThumbnailGraphic.MULTIPLYING_FRACTIONS -> R.drawable.topic_fractions_07
-      SkillThumbnailGraphic.DIVIDING_FRACTIONS -> R.drawable.topic_fractions_08
-      SkillThumbnailGraphic.DERIVE_A_RATIO -> R.drawable.topic_ratios_01
-      else -> R.drawable.topic_fractions_01
-    }
-  )
-}
-
-/**
- * Binds the specified [SkillThumbnailGraphic] as the source for the [ImageView]. The view should be specified to have
- * no width/height (when sized in a constraint layout), and use centerCrop for the image to appear correctly.
- */
-@BindingAdapter("android:src")
-fun setImageDrawable(imageView: ImageView, thumbnailGraphic: SubtopicThumbnailGraphic) {
-  setImageDrawable(
-    imageView, when (thumbnailGraphic) {
-      SubtopicThumbnailGraphic.WHAT_IS_A_FRACTION -> R.drawable.topic_fractions_01
-      SubtopicThumbnailGraphic.FRACTION_OF_A_GROUP -> R.drawable.topic_fractions_02
-      SubtopicThumbnailGraphic.ADDING_FRACTIONS -> R.drawable.topic_fractions_03
-      SubtopicThumbnailGraphic.MIXED_NUMBERS -> R.drawable.topic_fractions_04
+      LessonThumbnailGraphic.IDENTIFYING_THE_PARTS_OF_A_FRACTION -> R.drawable.topic_fractions_01
+      LessonThumbnailGraphic.WRITING_FRACTIONS -> R.drawable.topic_fractions_02
+      LessonThumbnailGraphic.EQUIVALENT_FRACTIONS -> R.drawable.topic_fractions_03
+      LessonThumbnailGraphic.MIXED_NUMBERS_AND_IMPROPER_FRACTIONS -> R.drawable.topic_fractions_04
+      LessonThumbnailGraphic.COMPARING_FRACTIONS -> R.drawable.topic_fractions_05
+      LessonThumbnailGraphic.ADDING_AND_SUBTRACTING_FRACTIONS -> R.drawable.topic_fractions_06
+      LessonThumbnailGraphic.MULTIPLYING_FRACTIONS -> R.drawable.topic_fractions_07
+      LessonThumbnailGraphic.DIVIDING_FRACTIONS -> R.drawable.topic_fractions_08
+      LessonThumbnailGraphic.DERIVE_A_RATIO -> R.drawable.topic_ratios_01
+      LessonThumbnailGraphic.WHAT_IS_A_FRACTION -> R.drawable.topic_fractions_01
+      LessonThumbnailGraphic.FRACTION_OF_A_GROUP -> R.drawable.topic_fractions_02
+      LessonThumbnailGraphic.ADDING_FRACTIONS -> R.drawable.topic_fractions_03
+      LessonThumbnailGraphic.MIXED_NUMBERS -> R.drawable.topic_fractions_04
       else -> R.drawable.topic_fractions_01
     }
   )
