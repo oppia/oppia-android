37728dee6e1d980728e05bb592e407cda0d4e549
Rajat Talesra <talesra@talesra-macbookpro.roam.corp.google.com>
2020-04-24 15:18:58 +0530

    Scripts added

---
+++ scripts/copy_dummy_assets.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# INSTRUCTIONS:
+# This script copies all dummy-assets to domain/src/main/assets folder and delete
+# the original oppia assets.
+#
+# Run the script from the oppia-android root folder:
+#
+#   bash scripts/copy_dummy_assets.sh
+#
+# NOTE: This script should only be run after running the scripts/copy_oppia_assets.sh
+
+destination_assets_path="domain/src/main/assets"
+
+# Remove current assets from domain/src/main/assets
+rm -r domain/src/main/assets/*
+
+# Copy old assets files to domain/src/main/assets
+rsync -r ../old-assets/ domain/src/main/assets/
+
+# Remove the assets folder available outside the repo.
+rm -rf ../old-assets
+
+# Remove .gitignore for the assets folder
+grep -v $destination_assets_path ".gitignore" > temp && mv temp ".gitignore"
---
+++ scripts/copy_oppia_assets.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# INSTRUCTIONS:
+# This script copies all oppia-assets to domain/src/main/assets folder in
+# oppia-android project and also makes sure that they are not getting committed to github.
+#
+# Run the script from the oppia-android root folder:
+#
+#   bash scripts/copy_oppia_assets.sh <local_path_to_oppia_assets>
+#
+# NOTE: Once this script is run, we can build a signed APK and after that run scripts/copy_dummy_assets.sh
+
+source_assets_path="$1"
+destination_assets_path="domain/src/main/assets"
+
+# Copy current assets files outside local repo
+rsync -r "$destination_assets_path/" ../old-assets
+
+# Remove current assets from domain/src/main/assets
+rm -r domain/src/main/assets/*
+
+# Add .gitignore for the assets folder
+if [ "${source_assets_path: -1}" != "/" ]; then
+    source_assets_path="$source_assets_path/"
+fi
+
+if ! grep -q $destination_assets_path ".gitignore"; then
+    touch .gitignore
+    echo $destination_assets_path >> .gitignore
+fi
+
+# Populate new data inside assets folder
+rsync -r $source_assets_path domain/src/main/assets/
