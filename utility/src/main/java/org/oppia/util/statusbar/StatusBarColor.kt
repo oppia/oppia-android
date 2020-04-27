e9744907748c07264a80a9f376fa889cad8b958d
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2020-04-28 01:27:24 +0530

    Linter fix

--- utility/src/main/java/org/oppia/util/statusbar/StatusBarColor.kt
+++ utility/src/main/java/org/oppia/util/statusbar/StatusBarColor.kt
@@ -5,11 +5,11 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 
-class StatusBarColor{
+class StatusBarColor {
   companion object {
     fun statusBarColorUpdate(colorId: Int, activity: AppCompatActivity, statusBarLight: Boolean) {
       if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-        if(statusBarLight){
+        if (statusBarLight) {
           activity.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
         }
         activity.window.statusBarColor = ContextCompat.getColor(activity, colorId)
