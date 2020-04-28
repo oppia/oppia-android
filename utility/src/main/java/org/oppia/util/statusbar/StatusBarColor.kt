759308dd2bf67b71b3e583c7cab7d15bdf780885
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2020-04-28 21:27:59 +0530

    status bar color introduced in kitkat

--- utility/src/main/java/org/oppia/util/statusbar/StatusBarColor.kt
+++ utility/src/main/java/org/oppia/util/statusbar/StatusBarColor.kt
@@ -2,8 +2,10 @@ package org.oppia.util.statusbar
 
 import android.os.Build
 import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.readystatesoftware.systembartint.SystemBarTintManager
 
 /** Utility to change the color of Status Bar. */
 class StatusBarColor {
@@ -15,11 +17,16 @@ class StatusBarColor {
      * @param statusBarLight passed Boolean true if the status bar theme is light, else passed Boolean false
      */
     fun statusBarColorUpdate(colorId: Int, activity: AppCompatActivity, statusBarLight: Boolean) {
-      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-        if (statusBarLight) {
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        if (statusBarLight && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
           activity.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
         }
         activity.window.statusBarColor = ContextCompat.getColor(activity, colorId)
+      } else if (Build.VERSION.SDK_INT == Build.VERSION_CODES.KITKAT) {
+        activity.window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        val tintManager = SystemBarTintManager(activity)
+        tintManager.isStatusBarTintEnabled = true
+        tintManager.setTintColor(ContextCompat.getColor(activity, colorId))
       }
     }
   }
