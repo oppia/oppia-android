53e49114be744c06a17427bed1e8de78987f3164
veena <veena14cs@gmail.com>
2020-04-29 13:31:15 +0530

    Update SvgSoftwareLayerSetter.kt

--- utility/src/main/java/org/oppia/util/parser/SvgSoftwareLayerSetter.kt
+++ utility/src/main/java/org/oppia/util/parser/SvgSoftwareLayerSetter.kt
@@ -1,15 +1,15 @@
 package org.oppia.util.parser
 
 import android.graphics.drawable.PictureDrawable
-import android.widget.ImageView
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
 
 /**
- * Listener which updates the [ImageView] to be software rendered, because [ ]/[Picture][android.graphics.Picture] can't render on a
- * hardware backed [Canvas][android.graphics.Canvas].
+ * Listener which updates the {@link ImageView} to be software rendered, because {@link
+ * com.caverock.androidsvg.SVG SVG}/{@link android.graphics.Picture Picture} can't render on a
+ * hardware backed {@link android.graphics.Canvas Canvas}.
  */
 class SvgSoftwareLayerSetter : RequestListener<PictureDrawable> {
   override fun onLoadFailed(
