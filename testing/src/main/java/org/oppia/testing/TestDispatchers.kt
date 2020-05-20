fba02ce154908d5c136aadf945e17a9c43faf99e
Ben Henning <henning.benmax@gmail.com>
2020-05-20 01:33:21 -0700

    Remove infeasible testing structures, add documentation, and clean up implementation to prepare for code review.

--- testing/src/main/java/org/oppia/testing/TestDispatchers.kt
+++ testing/src/main/java/org/oppia/testing/TestDispatchers.kt
@@ -2,6 +2,8 @@ package org.oppia.testing
 
 import javax.inject.Qualifier
 
+/** Corresponds to the [TestCoroutineDispatcher] that's used for background task execution. */
 @Qualifier annotation class BackgroundTestDispatcher
 
+/** Corresponds to the [TestCoroutineDispatcher] that's used for blocking task execution. */
 @Qualifier annotation class BlockingTestDispatcher
