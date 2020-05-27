ce4d9f705f1a67be51e4f492f9e39569aa046c65
Veena <veena14cs@gmail.com>
2020-05-27 16:54:39 +0530

    Fix part of #1028: Add Story app tests to the non-flaky queue  (#1133)
    * Update config.yml
    
    * Update config.yml
    
    * Update config.yml
    
    * Update WalkthroughFinalFragmentTest.kt
    
    * Update WalkthroughFinalFragmentTest.kt

--- app/src/sharedTest/java/org/oppia/app/walkthrough/WalkthroughFinalFragmentTest.kt
+++ app/src/sharedTest/java/org/oppia/app/walkthrough/WalkthroughFinalFragmentTest.kt
@@ -10,7 +10,10 @@ import androidx.test.espresso.action.ViewActions.scrollTo
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions.scrollToPosition
 import androidx.test.espresso.intent.Intents
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.isRoot
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.hamcrest.CoreMatchers.containsString
 import org.junit.After
