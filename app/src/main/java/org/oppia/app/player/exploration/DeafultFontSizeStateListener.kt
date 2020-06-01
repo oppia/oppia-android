f975d65029bfd238d78bad6f196d9a9f08085ad8
marysoloman <nikitamarysolomanpvt@gmail.com>
2020-06-01 21:52:20 +0530

    nit

--- app/src/main/java/org/oppia/app/player/exploration/DeafultFontSizeStateListener.kt
+++ app/src/main/java/org/oppia/app/player/exploration/DeafultFontSizeStateListener.kt
@@ -2,7 +2,7 @@ package org.oppia.app.player.exploration
 
 import org.oppia.app.model.StoryTextSize
 
-/** Listener for default font size fetch and for when activity load exploration fragment. */
+/** Listener for default font size fetch. */
 interface DeafultFontSizeStateListener {
   fun onDeafultFontSizeLoaded(result: StoryTextSize)
 }
