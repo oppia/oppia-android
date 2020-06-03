1e50e5eeba351ce3480f934e55dc9ae0d5ec59fb
Sarthak2601 <agarwal.sarthak262012@gmail.com>
2020-06-03 16:23:36 +0530

    nits + interface

--- utility/src/main/java/org/oppia/util/firebase/CrashLogger.kt
+++ utility/src/main/java/org/oppia/util/firebase/CrashLogger.kt
@@ -8,20 +8,20 @@ import javax.inject.Singleton
 @Singleton
 open class CrashLogger @Inject constructor(
   private var firebaseCrashlytics: FirebaseCrashlytics
-) {
+): CrashLoggerInterface {
 
   /** Logs a custom non-fatal exception to Firebase Crashlytics. */
-  open fun logException(exception: Exception) {
+  override fun logException(exception: Exception) {
       firebaseCrashlytics.recordException(exception)
   }
 
   /** Logs a custom log message which can be put alongside a crash report to Firebase Crashlytics. */
-  open fun logMessage(message: String) {
+  override fun logMessage(message: String) {
     firebaseCrashlytics.log(message)
   }
 
   /** Sets up a user identifier which is attached in every crash report to Firebase Crashlytics. */
-  open fun setUserIdentifier(identifier: String) {
+  override fun setUserIdentifier(identifier: String) {
     firebaseCrashlytics.setUserId(identifier)
   }
 }
