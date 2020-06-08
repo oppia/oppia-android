bd39412f2e7b1ade89624688b545a87401c55135
Pulkit Aggarwal <aggarwalpulkit596@gmail.com>
2020-06-08 17:56:55 +0530

    remove wildcard import

--- domain/src/test/java/org/oppia/domain/classify/rules/dragAndDropSortInput/DragDropSortInputIsEqualToOrderingClassifierProviderTest.kt
+++ domain/src/test/java/org/oppia/domain/classify/rules/dragAndDropSortInput/DragDropSortInputIsEqualToOrderingClassifierProviderTest.kt
@@ -3,7 +3,7 @@ package org.oppia.domain.classify.rules.dragAndDropSortInput
 import android.app.Application
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.google.common.truth.Truth.*
+import com.google.common.truth.Truth.assertThat
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Inject
