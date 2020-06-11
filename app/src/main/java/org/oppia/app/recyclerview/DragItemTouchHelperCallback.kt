a04e54cc6186d407e6b72ff290b6c4b6d0e9ebab
Pulkit Aggarwal <aggarwalpulkit596@gmail.com>
2020-06-11 13:39:20 +0530

    finalise the drag drop helper class

---
+++ app/src/main/java/org/oppia/app/recyclerview/DragItemTouchHelperCallback.kt
@@ -0,0 +1,68 @@
+package org.oppia.app.recyclerview
+
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * This is used to enable functionality like  drag up and down to the RecyclerView.
+ */
+class DragItemTouchHelperCallback private constructor(dragDirs: Int, swipeDirs: Int) :
+  ItemTouchHelper.SimpleCallback(dragDirs, swipeDirs) {
+  private var dragEnabled = false
+  private var onItemDragListener: OnItemDragListener? = null
+
+  private constructor(builder: Builder) : this(builder.dragDirs, builder.swipeDirs) {
+    dragEnabled = builder.dragEnabled
+    onItemDragListener = builder.onItemDragListener
+  }
+
+  override fun isLongPressDragEnabled(): Boolean {
+    return dragEnabled
+  }
+
+  override fun onMove(
+    recyclerView: RecyclerView,
+    source: RecyclerView.ViewHolder,
+    target: RecyclerView.ViewHolder
+  ): Boolean {
+    if (source.itemViewType != target.itemViewType) {
+      return false
+    }
+    onItemDragListener!!.onItemDragged(
+      source.adapterPosition,
+      target.adapterPosition,
+      recyclerView.adapter!!
+    )
+    return true
+  }
+
+  override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {}
+  override fun onSelectedChanged(viewHolder: RecyclerView.ViewHolder?, actionState: Int) {
+    if (actionState != ItemTouchHelper.ACTION_STATE_IDLE) {
+      viewHolder!!.itemView.alpha = ALPHA_FULL / 2
+    }
+    super.onSelectedChanged(viewHolder, actionState)
+  }
+
+  override fun clearView(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder) {
+    viewHolder.itemView.alpha = ALPHA_FULL
+    super.clearView(recyclerView, viewHolder)
+  }
+
+  class Builder(internal val dragDirs: Int, internal val swipeDirs: Int) {
+    internal var onItemDragListener: OnItemDragListener? = null
+    internal var dragEnabled = true
+    fun onItemDragListener(value: OnItemDragListener?): Builder {
+      onItemDragListener = value
+      return this
+    }
+
+    fun build(): DragItemTouchHelperCallback {
+      return DragItemTouchHelperCallback(this)
+    }
+  }
+
+  companion object {
+    const val ALPHA_FULL = 1.0f
+  }
+}
--- app/src/main/java/org/oppia/app/recyclerview/RvItemTouchHelperListener.kt
+++
@@ -1,6 +0,0 @@
-package org.oppia.app.recyclerview
-
-/** Listener for handling action of  [RvItemTouchHelperCallback]. */
-interface RvItemTouchHelperListener {
-  fun onMove(fromPosition: Int, toPosition: Int)
-}
