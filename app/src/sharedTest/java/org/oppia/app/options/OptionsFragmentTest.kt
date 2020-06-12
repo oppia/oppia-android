84977f6ae424bfed6c68b1bd7bcfc9b43c4bf88c
Akshay Nandwana <akshaynandwana001@gmail.com>
2020-06-13 01:06:29 +0530

    Fix part of #1287: Fix lint issues in app module in the kotlin files (options) (#1304)
    * fin ktlint issue in options package
    
    * fix max length check to one line

--- app/src/sharedTest/java/org/oppia/app/options/OptionsFragmentTest.kt
+++ app/src/sharedTest/java/org/oppia/app/options/OptionsFragmentTest.kt
@@ -56,8 +56,11 @@ import javax.inject.Singleton
 @RunWith(AndroidJUnit4::class)
 class OptionsFragmentTest {
 
-  @Inject lateinit var profileTestHelper: ProfileTestHelper
-  @Inject lateinit var context: Context
+  @Inject
+  lateinit var profileTestHelper: ProfileTestHelper
+
+  @Inject
+  lateinit var context: Context
 
   @Before
   @ExperimentalCoroutinesApi
@@ -81,7 +84,9 @@ class OptionsFragmentTest {
 
   @get:Rule
   var optionActivityTestRule: ActivityTestRule<OptionsActivity> = ActivityTestRule(
-    OptionsActivity::class.java, /* initialTouchMode= */ true, /* launchActivity= */ false
+    OptionsActivity::class.java,
+    /* initialTouchMode= */ true,
+    /* launchActivity= */ false
   )
 
   private fun createOptionActivityIntent(profileId: Int): Intent {
@@ -117,22 +122,38 @@ class OptionsFragmentTest {
       )
       onView(withId(R.id.story_text_size_seekBar)).perform(clickSeekBar(10))
       onView(withContentDescription(R.string.go_to_previous_page)).perform(click())
-      onView(atPositionOnView(R.id.options_recyclerview, 0, R.id.story_text_size_text_view)).check(
+      onView(
+        atPositionOnView(
+          R.id.options_recyclerview,
+          0, R.id.story_text_size_text_view
+        )
+      ).check(
         matches(withText("Large"))
       )
     }
   }
 
   @Test
-  fun testOptionFragment_clickStoryTextSize_changeTextSizeToLarge_changeConfiguration_checkTextSizeLargeIsSelected() {
+  fun testOptionFragment_clickStoryTextSize_changeTextSizeToLarge_changeConfiguration_checkTextSizeLargeIsSelected() { // ktlint-disable max-length-line
     launch<OptionsActivity>(createOptionActivityIntent(0)).use {
-      onView(atPositionOnView(R.id.options_recyclerview, 0, R.id.story_text_size_item_layout)).perform(
+      onView(
+        atPositionOnView(
+          R.id.options_recyclerview,
+          0,
+          R.id.story_text_size_item_layout
+        )
+      ).perform(
         click()
       )
       onView(withId(R.id.story_text_size_seekBar)).perform(clickSeekBar(10))
       onView(isRoot()).perform(orientationLandscape())
       onView(withContentDescription(R.string.go_to_previous_page)).perform(click())
-      onView(atPositionOnView(R.id.options_recyclerview, 0, R.id.story_text_size_text_view)).check(
+      onView(
+        atPositionOnView(
+          R.id.options_recyclerview,
+          0, R.id.story_text_size_text_view
+        )
+      ).check(
         matches(withText("Large"))
       )
     }
@@ -152,22 +173,38 @@ class OptionsFragmentTest {
       )
       onView(withId(R.id.story_text_size_seekBar)).perform(clickSeekBar(5))
       onView(withContentDescription(R.string.go_to_previous_page)).perform(click())
-      onView(atPositionOnView(R.id.options_recyclerview, 0, R.id.story_text_size_text_view)).check(
+      onView(
+        atPositionOnView(
+          R.id.options_recyclerview,
+          0, R.id.story_text_size_text_view
+        )
+      ).check(
         matches(withText("Medium"))
       )
     }
   }
 
   @Test
-  fun testOptionFragment_clickStoryTextSize_changeTextSizeToMedium_changeConfiguration_checkTextSizeMediumIsSelected() {
+  fun testOptionFragment_clickStoryTextSize_changeTextSizeToMedium_changeConfiguration_checkTextSizeMediumIsSelected() { // ktlint-disable max-length-line
     launch<OptionsActivity>(createOptionActivityIntent(0)).use {
-      onView(atPositionOnView(R.id.options_recyclerview, 0, R.id.story_text_size_item_layout)).perform(
+      onView(
+        atPositionOnView(
+          R.id.options_recyclerview,
+          0,
+          R.id.story_text_size_item_layout
+        )
+      ).perform(
         click()
       )
       onView(withId(R.id.story_text_size_seekBar)).perform(clickSeekBar(5))
       onView(isRoot()).perform(orientationLandscape())
       onView(withContentDescription(R.string.go_to_previous_page)).perform(click())
-      onView(atPositionOnView(R.id.options_recyclerview, 0, R.id.story_text_size_text_view)).check(
+      onView(
+        atPositionOnView(
+          R.id.options_recyclerview,
+          0, R.id.story_text_size_text_view
+        )
+      ).check(
         matches(withText("Medium"))
       )
     }
@@ -187,22 +224,38 @@ class OptionsFragmentTest {
       )
       onView(withId(R.id.story_text_size_seekBar)).perform(clickSeekBar(15))
       onView(withContentDescription(R.string.go_to_previous_page)).perform(click())
-      onView(atPositionOnView(R.id.options_recyclerview, 0, R.id.story_text_size_text_view)).check(
+      onView(
+        atPositionOnView(
+          R.id.options_recyclerview,
+          0, R.id.story_text_size_text_view
+        )
+      ).check(
         matches(withText("Extra Large"))
       )
     }
   }
 
   @Test
-  fun testOptionFragment_clickStoryTextSize_changeTextSizeToExtraLarge_changeConfiguration_checkTextSizeExtraLargeIsSelected() {
+  fun testOptionFragment_clickStoryTextSize_changeTextSizeToExtraLarge_changeConfiguration_checkTextSizeExtraLargeIsSelected() { // ktlint-disable max-length-line
     launch<OptionsActivity>(createOptionActivityIntent(0)).use {
-      onView(atPositionOnView(R.id.options_recyclerview, 0, R.id.story_text_size_item_layout)).perform(
+      onView(
+        atPositionOnView(
+          R.id.options_recyclerview,
+          0,
+          R.id.story_text_size_item_layout
+        )
+      ).perform(
         click()
       )
       onView(withId(R.id.story_text_size_seekBar)).perform(clickSeekBar(15))
       onView(isRoot()).perform(orientationLandscape())
       onView(withContentDescription(R.string.go_to_previous_page)).perform(click())
-      onView(atPositionOnView(R.id.options_recyclerview, 0, R.id.story_text_size_text_view)).check(
+      onView(
+        atPositionOnView(
+          R.id.options_recyclerview,
+          0, R.id.story_text_size_text_view
+        )
+      ).check(
         matches(withText("Extra Large"))
       )
     }
@@ -211,31 +264,61 @@ class OptionsFragmentTest {
   @Test
   fun testOptionFragment_clickAppLanguage_changeAppLanguageToFrenchSuccessfully() {
     launch<OptionsActivity>(createOptionActivityIntent(0)).use {
-      onView(atPositionOnView(R.id.options_recyclerview, 1, R.id.app_language_item_layout)).perform(
+      onView(
+        atPositionOnView(
+          R.id.options_recyclerview,
+          1, R.id.app_language_item_layout
+        )
+      ).perform(
         click()
       )
-      onView(atPositionOnView(R.id.language_recycler_view, 1, R.id.language_radio_button)).perform(
+      onView(
+        atPositionOnView(
+          R.id.language_recycler_view,
+          1, R.id.language_radio_button
+        )
+      ).perform(
         click()
       )
       onView(withContentDescription(R.string.go_to_previous_page)).perform(click())
-      onView(atPositionOnView(R.id.options_recyclerview, 1, R.id.app_language_text_view)).check(
+      onView(
+        atPositionOnView(
+          R.id.options_recyclerview,
+          1, R.id.app_language_text_view
+        )
+      ).check(
         matches(withText("French"))
       )
     }
   }
 
   @Test
-  fun testOptionFragment_clickAppLanguage_changeAppLanguageToFrench_changeConfiguration_selectedLanguageIsFrench() {
+  fun testOptionFragment_clickAppLanguage_changeAppLanguageToFrench_changeConfiguration_selectedLanguageIsFrench() { // ktlint-disable max-length-line
     launch<OptionsActivity>(createOptionActivityIntent(0)).use {
-      onView(atPositionOnView(R.id.options_recyclerview, 1, R.id.app_language_item_layout)).perform(
+      onView(
+        atPositionOnView(
+          R.id.options_recyclerview,
+          1, R.id.app_language_item_layout
+        )
+      ).perform(
         click()
       )
-      onView(atPositionOnView(R.id.language_recycler_view, 1, R.id.language_radio_button)).perform(
+      onView(
+        atPositionOnView(
+          R.id.language_recycler_view,
+          1, R.id.language_radio_button
+        )
+      ).perform(
         click()
       )
       onView(isRoot()).perform(orientationLandscape())
       onView(withContentDescription(R.string.go_to_previous_page)).perform(click())
-      onView(atPositionOnView(R.id.options_recyclerview, 1, R.id.app_language_text_view)).check(
+      onView(
+        atPositionOnView(
+          R.id.options_recyclerview,
+          1, R.id.app_language_text_view
+        )
+      ).check(
         matches(withText("French"))
       )
     }
@@ -244,7 +327,12 @@ class OptionsFragmentTest {
   @Test
   fun testOptionFragment_clickAppLanguage_changeAppLanguageHindiSuccessfully() {
     launch<OptionsActivity>(createOptionActivityIntent(0)).use {
-      onView(atPositionOnView(R.id.options_recyclerview, 1, R.id.app_language_item_layout)).perform(
+      onView(
+        atPositionOnView(
+          R.id.options_recyclerview,
+          1, R.id.app_language_item_layout
+        )
+      ).perform(
         click()
       )
       onView(withId(R.id.language_recycler_view))
@@ -255,14 +343,19 @@ class OptionsFragmentTest {
           )
         )
       onView(withContentDescription(R.string.go_to_previous_page)).perform(click())
-      onView(atPositionOnView(R.id.options_recyclerview, 1, R.id.app_language_text_view)).check(
+      onView(
+        atPositionOnView(
+          R.id.options_recyclerview,
+          1, R.id.app_language_text_view
+        )
+      ).check(
         matches(withText("Hindi"))
       )
     }
   }
 
   @Test
-  fun testOptionFragment_clickDefaultAudioLanguage_changeDefaultAudioLanguageToEnglishSuccessfully() {
+  fun testOptionFragment_clickDefaultAudioLanguage_changeDefaultAudioLanguageToEnglishSuccessfully() { // ktlint-disable max-length-line
     launch<OptionsActivity>(createOptionActivityIntent(0)).use {
       onView(
         atPositionOnView(
@@ -282,14 +375,19 @@ class OptionsFragmentTest {
           )
         )
       onView(withContentDescription(R.string.go_to_previous_page)).perform(click())
-      onView(atPositionOnView(R.id.options_recyclerview, 2, R.id.audio_language_text_view)).check(
+      onView(
+        atPositionOnView(
+          R.id.options_recyclerview,
+          2, R.id.audio_language_text_view
+        )
+      ).check(
         matches(withText("English"))
       )
     }
   }
 
   @Test
-  fun testOptionFragment_checkDefaultAudioLanguage_changeLanguageToEnglish_changeConfiguration_checkEnglishLanguageIsSelected() {
+  fun testOptionFragment_checkDefaultAudioLanguage_changeLanguageToEnglish_changeConfiguration_checkEnglishLanguageIsSelected() { // ktlint-disable max-length-line
     launch<OptionsActivity>(createOptionActivityIntent(0)).use {
       onView(
         atPositionOnView(
@@ -309,14 +407,19 @@ class OptionsFragmentTest {
         )
       onView(isRoot()).perform(orientationLandscape())
       onView(withContentDescription(R.string.go_to_previous_page)).perform(click())
-      onView(atPositionOnView(R.id.options_recyclerview, 2, R.id.audio_language_text_view)).check(
+      onView(
+        atPositionOnView(
+          R.id.options_recyclerview,
+          2, R.id.audio_language_text_view
+        )
+      ).check(
         matches(withText("English"))
       )
     }
   }
 
   @Test
-  fun testOptionFragment_checkDefaultAudioLanguage_changeLanguageToChinese_changeConfiguration_checkChineseLanguageIsSelected() {
+  fun testOptionFragment_checkDefaultAudioLanguage_changeLanguageToChinese_changeConfiguration_checkChineseLanguageIsSelected() { // ktlint-disable max-length-line
     launch<OptionsActivity>(createOptionActivityIntent(0)).use {
       onView(
         atPositionOnView(
@@ -337,14 +440,19 @@ class OptionsFragmentTest {
       onView(isRoot()).perform(orientationLandscape())
       onView(withContentDescription(R.string.go_to_previous_page)).perform(click())
       swipeUp()
-      onView(atPositionOnView(R.id.options_recyclerview, 2, R.id.audio_language_text_view)).check(
+      onView(
+        atPositionOnView(
+          R.id.options_recyclerview,
+          2, R.id.audio_language_text_view
+        )
+      ).check(
         matches(withText("Chinese"))
       )
     }
   }
 
   @Test
-  fun testOptionFragment_checkDefaultAudioLanguage_changeLanguageToHindi_changeConfiguration_checkHindiLanguageIsSelected() {
+  fun testOptionFragment_checkDefaultAudioLanguage_changeLanguageToHindi_changeConfiguration_checkHindiLanguageIsSelected() { // ktlint-disable max-length-line
     launch<OptionsActivity>(createOptionActivityIntent(0)).use {
       onView(
         atPositionOnView(
@@ -364,14 +472,19 @@ class OptionsFragmentTest {
         )
       onView(isRoot()).perform(orientationLandscape())
       onView(withContentDescription(R.string.go_to_previous_page)).perform(click())
-      onView(atPositionOnView(R.id.options_recyclerview, 2, R.id.audio_language_text_view)).check(
+      onView(
+        atPositionOnView(
+          R.id.options_recyclerview,
+          2, R.id.audio_language_text_view
+        )
+      ).check(
         matches(withText("Hindi"))
       )
     }
   }
 
   @Test
-  fun testOptionFragment_clickDefaultAudioLanguage_changeDefaultAudioLanguageToChineseSuccessfully() {
+  fun testOptionFragment_clickDefaultAudioLanguage_changeDefaultAudioLanguageToChineseSuccessfully() { // ktlint-disable max-length-line
     launch<OptionsActivity>(createOptionActivityIntent(0)).use {
       onView(
         atPositionOnView(
@@ -390,7 +503,12 @@ class OptionsFragmentTest {
           )
         )
       onView(withContentDescription(R.string.go_to_previous_page)).perform(click())
-      onView(atPositionOnView(R.id.options_recyclerview, 2, R.id.audio_language_text_view)).check(
+      onView(
+        atPositionOnView(
+          R.id.options_recyclerview,
+          2, R.id.audio_language_text_view
+        )
+      ).check(
         matches(withText("Chinese"))
       )
     }
@@ -400,7 +518,12 @@ class OptionsFragmentTest {
   fun testOptionFragment_changeConfiguration_checkTextSizeLargeIsSmall() {
     launch<OptionsActivity>(createOptionActivityIntent(0)).use {
       onView(isRoot()).perform(orientationLandscape())
-      onView(atPositionOnView(R.id.options_recyclerview, 0, R.id.story_text_size_text_view)).check(
+      onView(
+        atPositionOnView(
+          R.id.options_recyclerview,
+          0, R.id.story_text_size_text_view
+        )
+      ).check(
         matches(withText("Small"))
       )
     }
@@ -410,7 +533,12 @@ class OptionsFragmentTest {
   fun testOptionFragment_changeConfiguration_checkAppLanguageIsEnglish() {
     launch<OptionsActivity>(createOptionActivityIntent(0)).use {
       onView(isRoot()).perform(orientationLandscape())
-      onView(atPositionOnView(R.id.options_recyclerview, 1, R.id.app_language_text_view)).check(
+      onView(
+        atPositionOnView(
+          R.id.options_recyclerview,
+          1, R.id.app_language_text_view
+        )
+      ).check(
         matches(withText("English"))
       )
     }
@@ -420,30 +548,40 @@ class OptionsFragmentTest {
   fun testOptionFragment_changeConfiguration_checkAudioLanguageIsHindi() {
     launch<OptionsActivity>(createOptionActivityIntent(0)).use {
       onView(isRoot()).perform(orientationLandscape())
-      onView(atPositionOnView(R.id.options_recyclerview, 2, R.id.audio_language_text_view)).check(
+      onView(
+        atPositionOnView(
+          R.id.options_recyclerview,
+          2, R.id.audio_language_text_view
+        )
+      ).check(
         matches(withText("Hindi"))
       )
     }
   }
 
   private fun clickSeekBar(position: Int): ViewAction {
-    return GeneralClickAction(Tap.SINGLE, CoordinatesProvider { view ->
-      val seekBar = view as SeekBar
-      val screenPos = IntArray(2)
-      seekBar.getLocationInWindow(screenPos)
-      val trueWith = seekBar.width - seekBar.paddingLeft - seekBar.paddingRight
+    return GeneralClickAction(
+      Tap.SINGLE,
+      CoordinatesProvider { view ->
+        val seekBar = view as SeekBar
+        val screenPos = IntArray(2)
+        seekBar.getLocationInWindow(screenPos)
+        val trueWith = seekBar.width - seekBar.paddingLeft - seekBar.paddingRight
 
-      val percentagePos = (position.toFloat() / seekBar.max)
-      val screenX = trueWith * percentagePos + screenPos[0] + seekBar.paddingLeft
-      val screenY = seekBar.height / 2f + screenPos[1]
-      val coordinates = FloatArray(2)
-      coordinates[0] = screenX
-      coordinates[1] = screenY
-      coordinates
-    }, Press.FINGER, /* inputDevice= */ 0, /* deviceState= */ 0)
+        val percentagePos = (position.toFloat() / seekBar.max)
+        val screenX = trueWith * percentagePos + screenPos[0] + seekBar.paddingLeft
+        val screenY = seekBar.height / 2f + screenPos[1]
+        val coordinates = FloatArray(2)
+        coordinates[0] = screenX
+        coordinates[1] = screenY
+        coordinates
+      },
+      Press.FINGER, /* inputDevice= */ 0, /* deviceState= */ 0
+    )
   }
 
-  @Qualifier annotation class TestDispatcher
+  @Qualifier
+  annotation class TestDispatcher
 
   @Module
   class TestModule {
@@ -464,14 +602,18 @@ class OptionsFragmentTest {
     @Singleton
     @Provides
     @BackgroundDispatcher
-    fun provideBackgroundDispatcher(@TestDispatcher testDispatcher: CoroutineDispatcher): CoroutineDispatcher {
+    fun provideBackgroundDispatcher(
+      @TestDispatcher testDispatcher: CoroutineDispatcher
+    ): CoroutineDispatcher {
       return testDispatcher
     }
 
     @Singleton
     @Provides
     @BlockingDispatcher
-    fun provideBlockingDispatcher(@TestDispatcher testDispatcher: CoroutineDispatcher): CoroutineDispatcher {
+    fun provideBlockingDispatcher(
+      @TestDispatcher testDispatcher: CoroutineDispatcher
+    ): CoroutineDispatcher {
       return testDispatcher
     }
 
@@ -504,4 +646,3 @@ class OptionsFragmentTest {
     fun inject(optionsFragmentTest: OptionsFragmentTest)
   }
 }
-
