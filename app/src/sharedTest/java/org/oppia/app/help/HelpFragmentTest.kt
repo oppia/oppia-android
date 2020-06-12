619d450d0c6b8e83113c040489b14bdf5765fd89
Akshay Nandwana <akshaynandwana001@gmail.com>
2020-06-13 01:04:32 +0530

    Fix part of #1287: Fix lint issues in app module in the kotlin files (help) (#1299)
    * fix ktlint issue on help package
    
    * [skip ci] nit changes with clean way in single line char limit
    
    * nit changes

--- app/src/sharedTest/java/org/oppia/app/help/HelpFragmentTest.kt
+++ app/src/sharedTest/java/org/oppia/app/help/HelpFragmentTest.kt
@@ -50,7 +50,7 @@ class HelpFragmentTest {
   }
 
   @Test
-  fun openHelpActivity_configurationChanged_scrollRecyclerViewToZeroPosition_showsFAQSuccessfully() {
+  fun openHelpActivity_configurationChanged_scrollRecyclerViewToZeroPosition_showsFAQSuccessfully() { // ktlint-disable max-line-length
     launch(HelpActivity::class.java).use {
       onView(isRoot()).perform(orientationLandscape())
       onView(withId(R.id.help_fragment_recycler_view)).perform(
@@ -85,7 +85,7 @@ class HelpFragmentTest {
   }
 
   @Test
-  fun openHelpActivity_openNavigationDrawerAndClose_closingOfNavigationDrawerIsVerifiedSuccessfully() {
+  fun openHelpActivity_openNavigationDrawerAndClose_closingOfNavigationDrawerIsVerifiedSuccessfully() { // ktlint-disable max-line-length
     launch(HelpActivity::class.java).use {
       onView(withContentDescription(R.string.drawer_open_content_description)).perform(click())
       onView(withId(R.id.help_activity_drawer_layout)).perform(close())
