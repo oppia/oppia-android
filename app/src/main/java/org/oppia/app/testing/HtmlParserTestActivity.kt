1573cfbcefe3116702a00140805428514a553aeb
anandwana001 <akshaynandwana001@gmail.com>
2020-06-12 14:39:59 +0530

    [skip ci] nit changes

--- app/src/main/java/org/oppia/app/testing/HtmlParserTestActivity.kt
+++ app/src/main/java/org/oppia/app/testing/HtmlParserTestActivity.kt
@@ -28,10 +28,10 @@ class HtmlParserTestActivity : InjectableAppCompatActivity() {
       "\u003cp\u003e\"Let's try one last question,\" said Mr. Baker. \"Here's a pineapple cake cut into pieces.\"\u003c/p\u003e\u003coppia-noninteractive-image alt-with-value=\"\u0026amp;quot;Pineapple cake with 7/9 having cherries.\u0026amp;quot;\" caption-with-value=\"\u0026amp;quot;\u0026amp;quot;\" filepath-with-value=\"\u0026amp;quot;pineapple_cake_height_479_width_480.png\u0026amp;quot;\"\u003e\u003c/oppia-noninteractive-image\u003e\u003cp\u003e\u00a0\u003c/p\u003e\u003cp\u003e\u003cstrong\u003eQuestion 6\u003c/strong\u003e: What fraction of the cake has big red cherries in the pineapple slices?\u003c/p\u003e" // ktlint-disable max-line-length
     val htmlResult: Spannable =
       htmlParserFactory.create(
-        resourceBucketName, /* entityType= */
-        "exploration", /* entityId= */
-        "oppia", /* imageCenterAlign= */
-        false
+        resourceBucketName,
+        /* entityType= */ "exploration",
+        /* entityId= */ "oppia",
+        /* imageCenterAlign= */ false
       ).parseOppiaHtml(
         rawDummyString,
         testHtmlContentTextView
