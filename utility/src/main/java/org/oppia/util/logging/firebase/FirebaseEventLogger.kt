a3045e01563a27ee77accee0f276424ea837721d
Sarthak2601 <agarwal.sarthak262012@gmail.com>
2020-06-16 02:33:14 +0530

    topic and story contexts | tests | data module tests.

--- utility/src/main/java/org/oppia/util/logging/firebase/FirebaseEventLogger.kt
+++ utility/src/main/java/org/oppia/util/logging/firebase/FirebaseEventLogger.kt
@@ -6,6 +6,8 @@ import com.google.firebase.analytics.FirebaseAnalytics
 import org.oppia.app.model.EventLog
 import org.oppia.app.model.EventLog.Context.ActivityContextCase.EXPLORATION_CONTEXT
 import org.oppia.app.model.EventLog.Context.ActivityContextCase.QUESTION_CONTEXT
+import org.oppia.app.model.EventLog.Context.ActivityContextCase.STORY_CONTEXT
+import org.oppia.app.model.EventLog.Context.ActivityContextCase.TOPIC_CONTEXT
 import org.oppia.util.logging.EventBundleCreator
 import org.oppia.util.logging.EventLogger
 import javax.inject.Singleton
@@ -23,6 +25,8 @@ class FirebaseEventLogger(
       when (eventLog.context.activityContextCase) {
         EXPLORATION_CONTEXT -> EventBundleCreator().createExplorationContextBundle(eventLog)
         QUESTION_CONTEXT -> EventBundleCreator().createQuestionContextBundle(eventLog)
+        STORY_CONTEXT -> EventBundleCreator().createStoryContextBundle(eventLog)
+        TOPIC_CONTEXT -> EventBundleCreator().createTopicContextBundle(eventLog)
         else -> EventBundleCreator().defaultBundle(eventLog)
       }
     firebaseAnalytics.logEvent(eventLog.actionName.toString(), bundle)
