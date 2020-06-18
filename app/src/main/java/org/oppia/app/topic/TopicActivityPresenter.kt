7a1788d757693b7056d59afbc361ccaaee3d77d7
Rajat Talesra <talesra@google.com>
2020-06-18 06:29:10 +0530

    Back navigation button added

--- app/src/main/java/org/oppia/app/topic/TopicActivityPresenter.kt
+++ app/src/main/java/org/oppia/app/topic/TopicActivityPresenter.kt
@@ -1,21 +1,10 @@
 package org.oppia.app.topic
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Observer
-import androidx.lifecycle.Transformations
 import org.oppia.app.R
 import org.oppia.app.activity.ActivityScope
-import org.oppia.app.drawer.NavigationDrawerFragment
 import org.oppia.app.model.ProfileId
-import org.oppia.app.model.Topic
-import org.oppia.domain.topic.TopicController
-import org.oppia.util.data.AsyncResult
-import org.oppia.util.logging.Logger
 import javax.inject.Inject
 
 const val TOPIC_FRAGMENT_TAG = "TopicFragment"
@@ -26,11 +15,8 @@ const val STORY_ID_ARGUMENT_KEY = "story_id"
 /** The presenter for [TopicActivity]. */
 @ActivityScope
 class TopicActivityPresenter @Inject constructor(
-  private val activity: AppCompatActivity,
-  private val logger: Logger,
-  private val topicController: TopicController
+  private val activity: AppCompatActivity
 ) {
-  private var navigationDrawerFragment: NavigationDrawerFragment? = null
   private lateinit var topicId: String
 
   private lateinit var profileId: ProfileId
@@ -39,8 +25,6 @@ class TopicActivityPresenter @Inject constructor(
     this.topicId = topicId
     activity.setContentView(R.layout.topic_activity)
     profileId = ProfileId.newBuilder().setInternalId(internalProfileId).build()
-    subscribeToTopicLiveData()
-    setUpNavigationDrawer()
     if (getTopicFragment() == null) {
       val topicFragment = TopicFragment()
       val args = Bundle()
@@ -55,49 +39,6 @@ class TopicActivityPresenter @Inject constructor(
         topicFragment, TOPIC_FRAGMENT_TAG
       ).commitNow()
     }
-  }
-
-  private val topicLiveData: LiveData<Topic> by lazy { getTopic() }
-
-  private fun subscribeToTopicLiveData() {
-    topicLiveData.observe(
-      activity,
-      Observer<Topic> { result ->
-        val topicName = result.name
-        val toolbar = activity.findViewById<View>(R.id.topic_activity_toolbar) as Toolbar
-        toolbar.title = activity.getString(R.string.topic_prefix, topicName)
-      }
-    )
-  }
-
-  private val topicResultLiveData: LiveData<AsyncResult<Topic>> by lazy {
-    topicController.getTopic(profileId, topicId = topicId)
-  }
-
-  private fun getTopic(): LiveData<Topic> {
-    return Transformations.map(topicResultLiveData, ::processTopicResult)
-  }
-
-  private fun processTopicResult(topic: AsyncResult<Topic>): Topic {
-    if (topic.isFailure()) {
-      logger.e("TopicActivity", "Failed to retrieve topic", topic.getErrorOrNull()!!)
-    }
-    return topic.getOrDefault(Topic.getDefaultInstance())
-  }
-
-  private fun setUpNavigationDrawer() {
-    val toolbar = activity.findViewById<View>(R.id.topic_activity_toolbar) as Toolbar
-    activity.setSupportActionBar(toolbar)
-    activity.supportActionBar!!.setDisplayShowHomeEnabled(true)
-    navigationDrawerFragment = activity
-      .supportFragmentManager
-      .findFragmentById(
-        R.id.topic_activity_fragment_navigation_drawer
-      ) as NavigationDrawerFragment
-    navigationDrawerFragment!!.setUpDrawer(
-      activity.findViewById<View>(R.id.topic_activity_drawer_layout) as DrawerLayout,
-      toolbar, R.id.nav_home
-    )
   }
 
   private fun getTopicFragment(): TopicFragment? {
