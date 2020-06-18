89baa276490cbabc3af4e2e4dd33e52f44442330
Rajat Talesra <talesra@google.com>
2020-06-19 01:04:27 +0530

    Nit issue fix

--- data/src/test/java/org/oppia/data/persistence/PersistentCacheStoreTest.kt
+++ data/src/test/java/org/oppia/data/persistence/PersistentCacheStoreTest.kt
@@ -670,9 +670,11 @@ class PersistentCacheStoreTest {
   // TODO(#89): Move this to a common test application component.
   @Singleton
   @Component(
-    modules = [TestDispatcherModule::class,
+    modules = [
+      TestDispatcherModule::class,
       TestModule::class,
-      TestLogReportingModule::class]
+      TestLogReportingModule::class
+    ]
   )
   interface TestApplicationComponent {
     @Component.Builder
