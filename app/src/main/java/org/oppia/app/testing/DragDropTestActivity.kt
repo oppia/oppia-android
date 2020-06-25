c725e4f5011b678ead0afc182def43fec6ce1e38
Pulkit Aggarwal <aggarwalpulkit596@gmail.com>
2020-06-25 23:18:15 +0530

    Fixes #1222 : Add helper class and corresponding listener for DragDropSort (#1192)
    * Add helper class and corresponding listener for DragDropSort

---
+++ app/src/main/java/org/oppia/app/testing/DragDropTestActivity.kt
@@ -0,0 +1,28 @@
+package org.oppia.app.testing
+
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import org.oppia.app.activity.InjectableAppCompatActivity
+import org.oppia.app.recyclerview.OnItemDragListener
+import javax.inject.Inject
+
+/** Test Activity used for testing [DragAndDropItemFacilitator] functionality */
+class DragDropTestActivity : InjectableAppCompatActivity(), OnItemDragListener {
+
+  @Inject
+  lateinit var dragDropTestActivityPresenter: DragDropTestActivityPresenter
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    activityComponent.inject(this)
+    dragDropTestActivityPresenter.handleOnCreate()
+  }
+
+  override fun onItemDragged(
+    indexFrom: Int,
+    indexTo: Int,
+    adapter: RecyclerView.Adapter<RecyclerView.ViewHolder>
+  ) {
+    dragDropTestActivityPresenter.onItemDragged(indexFrom, indexTo, adapter)
+  }
+}
