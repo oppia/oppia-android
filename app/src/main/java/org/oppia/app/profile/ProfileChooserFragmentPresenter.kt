b58b88a2238fac58c1bda3e27028edb1156fdc15
anandwana001 <akshaynandwana001@gmail.com>
2020-06-25 08:41:43 +0530

    modified profile chooser click target and remove unused imports

--- app/src/main/java/org/oppia/app/profile/ProfileChooserFragmentPresenter.kt
+++ app/src/main/java/org/oppia/app/profile/ProfileChooserFragmentPresenter.kt
@@ -1,7 +1,6 @@
 package org.oppia.app.profile
 
 import android.content.Context
-import android.content.res.Configuration
 import android.content.res.Resources
 import android.view.LayoutInflater
 import android.view.View
@@ -14,7 +13,8 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.Transformations
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.profile_chooser_add_view.view.add_profile_item
+import kotlinx.android.synthetic.main.profile_chooser_profile_view.view.profile_item
 import org.oppia.app.R
 import org.oppia.app.databinding.ProfileChooserAddViewBinding
 import org.oppia.app.databinding.ProfileChooserFragmentBinding
@@ -168,7 +168,7 @@ class ProfileChooserFragmentPresenter @Inject constructor(
   ) {
     binding.viewModel = model
     binding.presenter = this
-    binding.root.setOnClickListener {
+    binding.root.profile_item.setOnClickListener {
       if (model.profile.pin.isEmpty()) {
         profileManagementController.loginToProfile(model.profile.id).observe(
           fragment,
@@ -201,7 +201,7 @@ class ProfileChooserFragmentPresenter @Inject constructor(
     @Suppress("UNUSED_PARAMETER") model: ProfileChooserUiModel
   ) {
     binding.presenter = this
-    binding.root.setOnClickListener {
+    binding.root.add_profile_item.setOnClickListener {
       if (chooserViewModel.adminPin.isEmpty()) {
         activity.startActivity(
           AdminPinActivity.createAdminPinActivityIntent(
