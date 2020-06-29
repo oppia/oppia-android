927905a4cc5c9bc44816cb60838ef5c7d9e10a39
Pulkit Aggarwal <aggarwalpulkit596@gmail.com>
2020-06-29 18:01:08 +0530

    add todo again

--- app/src/test/java/org/oppia/app/testing/player/state/StateFragmentAccessibilityTest.kt
+++ app/src/test/java/org/oppia/app/testing/player/state/StateFragmentAccessibilityTest.kt
@@ -151,6 +151,7 @@ class StateFragmentAccessibilityTest {
     fun provideCacheAssetsLocally(): Boolean = false
   }
 
+  // TODO(#59): Figure out a way to reuse modules instead of needing to re-declare them.
   @Singleton
   @Component(
     modules = [
