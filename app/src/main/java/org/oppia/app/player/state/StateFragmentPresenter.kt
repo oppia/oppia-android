30c7114aa88bd1b0f418e4ee42c1e76e9b7651f7
marysoloman <nikitamarysolomanpvt@gmail.com>
2020-06-30 19:47:07 +0530

    nit

--- app/src/main/java/org/oppia/app/player/state/StateFragmentPresenter.kt
+++ app/src/main/java/org/oppia/app/player/state/StateFragmentPresenter.kt
@@ -38,7 +38,7 @@ import org.oppia.util.data.AsyncResult
 import org.oppia.util.gcsresource.DefaultResourceBucketName
 import org.oppia.util.logging.Logger
 import org.oppia.util.parser.ExplorationHtmlParserEntityType
-import java.util.*
+import java.util.Date
 import javax.inject.Inject
 
 const val STATE_FRAGMENT_PROFILE_ID_ARGUMENT_KEY = "STATE_FRAGMENT_PROFILE_ID_ARGUMENT_KEY"
