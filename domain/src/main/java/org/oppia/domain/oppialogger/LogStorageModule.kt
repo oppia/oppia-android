dde24e6992ee7be87756fec9a9866931ec9dc820
Sarthak2601 <agarwal.sarthak262012@gmail.com>
2020-07-03 09:37:10 +0530

    records comment.

--- domain/src/main/java/org/oppia/domain/oppialogger/LogStorageModule.kt
+++ domain/src/main/java/org/oppia/domain/oppialogger/LogStorageModule.kt
@@ -13,10 +13,13 @@ annotation class ExceptionLogStorageCacheSize
 /** Provider to return any constants required during the storage of log reports. */
 @Module
 class LogStorageModule {
+
+  /** Provides the number of records that can be stored in EventLog's cache storage.*/
   @Provides
   @EventLogStorageCacheSize
   fun provideEventLogStorageCacheSize(): Int = 10
 
+  /** Provides the number of records that can be stored in ExceptionLog's cache storage.*/
   @Provides
   @ExceptionLogStorageCacheSize
   fun provideExceptionLogStorageCacheSize(): Int = 10
