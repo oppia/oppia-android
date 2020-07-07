1be2db0fac1112984efe3254662c032eabde1fbb
Sarthak2601 <agarwal.sarthak262012@gmail.com>
2020-07-07 21:07:12 +0530

    limit increase to 25

--- domain/src/main/java/org/oppia/domain/oppialogger/LogStorageModule.kt
+++ domain/src/main/java/org/oppia/domain/oppialogger/LogStorageModule.kt
@@ -17,10 +17,10 @@ class LogStorageModule {
   /** Provides the number of records that can be stored in EventLog's cache storage.*/
   @Provides
   @EventLogStorageCacheSize
-  fun provideEventLogStorageCacheSize(): Int = 10
+  fun provideEventLogStorageCacheSize(): Int = 25
 
   /** Provides the number of records that can be stored in ExceptionLog's cache storage.*/
   @Provides
   @ExceptionLogStorageCacheSize
-  fun provideExceptionLogStorageCacheSize(): Int = 10
+  fun provideExceptionLogStorageCacheSize(): Int = 25
 }
