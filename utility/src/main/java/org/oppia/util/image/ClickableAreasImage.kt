a0c5e8255168fcc164067c2f02ac0d57e43bc6c5
Pulkit Aggarwal <aggarwalpulkit596@gmail.com>
2020-07-08 13:05:35 +0530

    finalize the clickable areas image class

--- utility/src/main/java/org/oppia/util/image/ClickableAreasImage.kt
+++ utility/src/main/java/org/oppia/util/image/ClickableAreasImage.kt
@@ -1,14 +1,17 @@
 package org.oppia.util.image
 
+import android.graphics.RectF
+import android.view.View
+import android.widget.FrameLayout
 import android.widget.ImageView
 import com.github.chrisbanes.photoview.OnPhotoTapListener
 import com.github.chrisbanes.photoview.PhotoViewAttacher
 import org.oppia.app.model.ImageWithRegions.LabeledRegion
-import javax.inject.Singleton
+import kotlin.math.roundToInt
 
-@Singleton
 class ClickableAreasImage(
-  attacher: PhotoViewAttacher,
+  private val attacher: PhotoViewAttacher,
+  private val overlayView: View,
   private val listener: OnClickableAreaClickedListener
 ) : OnPhotoTapListener {
   private var clickableAreas: List<LabeledRegion> = emptyList()
@@ -17,25 +20,64 @@ class ClickableAreasImage(
     attacher.setOnPhotoTapListener(this)
   }
 
+  /**
+   * Called when an image is clicked.
+   *
+   * @param view the original view on which the tap/click occurs.
+   * @param x the relative x coordinate according to image
+   * @param y the relative y coordinate according to image
+   */
   override fun onPhotoTap(view: ImageView, x: Float, y: Float) {
-    val clickableAreas = getClickAbleAreas(x, y)
-    listener.onClickableAreaTouched(clickableAreas.map { it.label })
+    val clickableArea = getClickAbleAreas(x, y)
+    if (clickableArea.hasRegion()) {
+      val imageRect = RectF(
+        getXCoordinate(clickableArea.region.area.lowerRight.x),
+        getYCoordinate(clickableArea.region.area.lowerRight.y),
+        getXCoordinate(clickableArea.region.area.upperLeft.x),
+        getYCoordinate(clickableArea.region.area.upperLeft.y)
+      )
+      val overlayViewParams = FrameLayout.LayoutParams(
+        imageRect.width().roundToInt(),
+        imageRect.height().roundToInt()
+      )
+      overlayView.apply {
+        this.x = imageRect.left
+        this.y = imageRect.top
+        layoutParams = overlayViewParams
+        visibility = View.GONE
+      }
+      listener.onClickableAreaTouched(clickableArea.label)
+    } else {
+      overlayView.visibility = View.GONE
+    }
   }
 
-  private fun getClickAbleAreas(x: Float, y: Float): List<LabeledRegion> {
-    val myClickArea: MutableList<LabeledRegion> = ArrayList()
+  private fun getClickAbleAreas(x: Float, y: Float): LabeledRegion {
     for (ca in clickableAreas) {
       if (isBetween(ca.region.area.lowerRight.x, ca.region.area.upperLeft.x, x)) {
         if (isBetween(ca.region.area.lowerRight.y, ca.region.area.upperLeft.y, y)) {
-          myClickArea.add(ca)
+          return ca
         }
       }
     }
-    return myClickArea
+    return LabeledRegion.getDefaultInstance()
   }
 
+  /* Return whether a point lies between two points.*/
   private fun isBetween(start: Float, end: Float, actual: Float): Boolean {
     return actual in start..end
+  }
+
+  /* Get X co-ordinate scaled according to image.*/
+  private fun getXCoordinate(x: Float): Float {
+    val rect = attacher.displayRect
+    return (x * rect.width()) + rect.left
+  }
+
+  /* Get Y co-ordinate scaled according to image.*/
+  private fun getYCoordinate(x: Float): Float {
+    val rect = attacher.displayRect
+    return (x * rect.height()) + rect.top
   }
 
   fun setClickableAreas(clickableAreas: List<LabeledRegion>) {
--- utility/src/main/java/org/oppia/util/image/OnClickableAreaClickedListener.kt
+++ utility/src/main/java/org/oppia/util/image/OnClickableAreaClickedListener.kt
@@ -1,5 +1,9 @@
 package org.oppia.util.image
 
+/** Listener for an image when it is clicked which have a [ClickableAreasImage] attached to the view. */
 interface OnClickableAreaClickedListener {
-  fun onClickableAreaTouched(regions: List<String>)
+  /**
+   * @param region the name of the region which is selected
+   */
+  fun onClickableAreaTouched(region: String)
 }
