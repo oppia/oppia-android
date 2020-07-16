1b494e5bacf62736fbee0b2bdbfd9a4f52c57cfa
Sarthak2601 <agarwal.sarthak262012@gmail.com>
2020-07-17 04:32:13 +0530

    test fixes.

--- app/src/test/java/org/oppia/app/topic/lessons/TopicLessonsFragmentLocalTest.kt
+++ app/src/test/java/org/oppia/app/topic/lessons/TopicLessonsFragmentLocalTest.kt
@@ -29,6 +29,7 @@ import org.oppia.domain.classify.rules.multiplechoiceinput.MultipleChoiceInputMo
 import org.oppia.domain.classify.rules.numberwithunits.NumberWithUnitsRuleModule
 import org.oppia.domain.classify.rules.numericinput.NumericInputRuleModule
 import org.oppia.domain.classify.rules.textinput.TextInputRuleModule
+import org.oppia.domain.oppialogger.LogStorageModule
 import org.oppia.domain.question.QuestionModule
 import org.oppia.testing.FakeEventLogger
 import org.oppia.testing.TestAccessibilityModule
@@ -116,7 +117,7 @@ class TopicLessonsFragmentLocalTest {
       DragDropSortInputModule::class, InteractionsModule::class, GcsResourceModule::class,
       GlideImageLoaderModule::class, ImageParsingModule::class, HtmlParserEntityTypeModule::class,
       QuestionModule::class, TestLogReportingModule::class, TestAccessibilityModule::class,
-      ImageClickInputModule::class
+      ImageClickInputModule::class, LogStorageModule::class
     ]
   )
   interface TestApplicationComponent : ApplicationComponent {
