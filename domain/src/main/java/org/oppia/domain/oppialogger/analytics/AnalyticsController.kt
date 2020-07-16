51c7848f64f2fa7e15d4efd839a7143bf92dcb14
Sarthak2601 <agarwal.sarthak262012@gmail.com>
2020-07-17 04:08:39 +0530

    tests|event log storage|removal of exception proto

--- domain/src/main/java/org/oppia/domain/oppialogger/analytics/AnalyticsController.kt
+++ domain/src/main/java/org/oppia/domain/oppialogger/analytics/AnalyticsController.kt
@@ -1,16 +1,38 @@
 package org.oppia.domain.oppialogger.analytics
 
 import android.content.Context
+import androidx.lifecycle.LiveData
+import kotlinx.coroutines.CoroutineDispatcher
 import org.oppia.app.model.EventLog
 import org.oppia.app.model.EventLog.EventAction
 import org.oppia.app.model.EventLog.Priority
+import org.oppia.app.model.OppiaEventLogs
+import org.oppia.data.persistence.PersistentCacheStore
+import org.oppia.domain.oppialogger.EventLogStorageCacheSize
+import org.oppia.util.data.AsyncResult
+import org.oppia.util.data.DataProviders
+import org.oppia.util.logging.ConsoleLogger
 import org.oppia.util.logging.EventLogger
+import org.oppia.util.logging.ExceptionLogger
+import org.oppia.util.networking.NetworkConnectionUtil
+import org.oppia.util.networking.NetworkConnectionUtil.ConnectionStatus.NONE
+import org.oppia.util.threading.BackgroundDispatcher
 import javax.inject.Inject
 
 /** Controller for handling analytics event logging. */
 class AnalyticsController @Inject constructor(
-  private val eventLogger: EventLogger
+  private val eventLogger: EventLogger,
+  cacheStoreFactory: PersistentCacheStore.Factory,
+  private val dataProviders: DataProviders,
+  private val consoleLogger: ConsoleLogger,
+  private val networkConnectionUtil: NetworkConnectionUtil,
+  private val exceptionLogger: ExceptionLogger,
+  @BackgroundDispatcher private val backgroundCoroutineDispatcher: CoroutineDispatcher,
+  @EventLogStorageCacheSize private val eventLogStorageCacheSize: Int
 ) {
+  private val eventLogStore =
+    cacheStoreFactory.create("event_logs", OppiaEventLogs.getDefaultInstance())
+
   /**
    * Logs transition events.
    * These events are given HIGH priority.
@@ -21,7 +43,7 @@ class AnalyticsController @Inject constructor(
     eventAction: EventAction,
     eventContext: EventLog.Context?
   ) {
-    eventLogger.logEvent(
+    checkNetworkAndManageEventLog(
       context,
       createEventLog(
         timestamp,
@@ -42,7 +64,7 @@ class AnalyticsController @Inject constructor(
     eventAction: EventAction,
     eventContext: EventLog.Context?
   ) {
-    eventLogger.logEvent(
+    checkNetworkAndManageEventLog(
       context,
       createEventLog(
         timestamp,
@@ -157,5 +179,80 @@ class AnalyticsController @Inject constructor(
           .build()
       )
       .build()
+  }
+
+  /**
+   * Checks network connectivity of the device.
+   * Saves the [eventLog] to the [eventLogStore] in the absence of it.
+   * Uploads to remote service in the presence of it.
+   */
+  private fun checkNetworkAndManageEventLog(context: Context, eventLog: EventLog) {
+    when (networkConnectionUtil.getCurrentConnectionStatus()) {
+      NONE -> addEventLog(eventLog)
+      else -> eventLogger.logEvent(context, eventLog)
+    }
+  }
+
+  /**
+   * Adds an event to the storage.
+   * At first, it checks if the size of the store isn't exceeding [eventLogStorageCacheSize]
+   * If the limit is exceeded then the least recent event is removed from the [eventLogStore]
+   * After this, the [eventLog] is added to the store.
+   * */
+  private fun addEventLog(eventLog: EventLog) {
+    eventLogStore.storeDataAsync(updateInMemoryCache = true) { oppiaEventLogs ->
+      val storeSize = oppiaEventLogs.eventLogList.size
+      if (storeSize + 1 > eventLogStorageCacheSize) {
+        val eventLogRemovalIndex = getLeastRecentEventIndex(oppiaEventLogs)
+        if (eventLogRemovalIndex != null) {
+          return@storeDataAsync oppiaEventLogs.toBuilder()
+            .removeEventLog(eventLogRemovalIndex)
+            .addEventLog(eventLog)
+            .build()
+        } else {
+          exceptionLogger.logException(
+            NullPointerException(
+              "Least Recent Event index absent -- EventLogCacheStoreSize is 0"
+            )
+          )
+          consoleLogger.e(
+            "Analytics Controller",
+            "Least Recent Event index absent -- EventLogCacheStoreSize is 0"
+          )
+        }
+      }
+      return@storeDataAsync oppiaEventLogs.toBuilder().addEventLog(eventLog).build()
+    }.invokeOnCompletion {
+      it?.let {
+        consoleLogger.e(
+          "DOMAIN",
+          "Failed to store event log",
+          it
+        )
+      }
+    }
+  }
+
+  /**
+   * Returns the index of the least recent event from the existing store on the basis of recency and priority.
+   * At first, it checks the index of the least recent event which has OPTIONAL priority.
+   * If that returns null, then the index of the least recent event regardless of the priority is returned.
+   */
+  private fun getLeastRecentEventIndex(oppiaEventLogs: OppiaEventLogs): Int? =
+    oppiaEventLogs.eventLogList.withIndex()
+      .filter { it.value.priority == Priority.OPTIONAL }
+      .minBy { it.value.timestamp }?.index ?: getLeastRecentGeneralEventIndex(oppiaEventLogs)
+
+  /** Returns the index of the least recent event regardless of their priority. */
+  private fun getLeastRecentGeneralEventIndex(oppiaEventLogs: OppiaEventLogs): Int? =
+    oppiaEventLogs.eventLogList.withIndex()
+      .minBy { it.value.timestamp }?.index
+
+  /**
+   * Returns a [LiveData] result which can be used to get [OppiaEventLogs]
+   * for the purpose of uploading in the presence of network connectivity.
+   */
+  fun getEventLogs(): LiveData<AsyncResult<OppiaEventLogs>> {
+    return dataProviders.convertToLiveData(eventLogStore)
   }
 }
--- domain/src/test/java/org/oppia/domain/oppialogger/analytics/AnalyticsControllerTest.kt
+++ domain/src/test/java/org/oppia/domain/oppialogger/analytics/AnalyticsControllerTest.kt
@@ -2,6 +2,7 @@ package org.oppia.domain.oppialogger.analytics
 
 import android.app.Application
 import android.content.Context
+import androidx.lifecycle.Observer
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
@@ -9,9 +10,17 @@ import dagger.BindsInstance
 import dagger.Component
 import dagger.Module
 import dagger.Provides
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.InternalCoroutinesApi
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.ArgumentCaptor
+import org.mockito.Captor
+import org.mockito.Mock
+import org.mockito.Mockito.atLeastOnce
+import org.mockito.Mockito.verify
+import org.mockito.MockitoAnnotations
 import org.oppia.app.model.EventLog.Context.ActivityContextCase.ACTIVITYCONTEXT_NOT_SET
 import org.oppia.app.model.EventLog.Context.ActivityContextCase.CONCEPT_CARD_CONTEXT
 import org.oppia.app.model.EventLog.Context.ActivityContextCase.EXPLORATION_CONTEXT
@@ -21,10 +30,22 @@ import org.oppia.app.model.EventLog.Context.ActivityContextCase.STORY_CONTEXT
 import org.oppia.app.model.EventLog.Context.ActivityContextCase.TOPIC_CONTEXT
 import org.oppia.app.model.EventLog.EventAction
 import org.oppia.app.model.EventLog.Priority
+import org.oppia.app.model.OppiaEventLogs
+import org.oppia.domain.oppialogger.LogStorageModule
 import org.oppia.testing.FakeEventLogger
+import org.oppia.testing.TestCoroutineDispatchers
+import org.oppia.testing.TestDispatcherModule
 import org.oppia.testing.TestLogReportingModule
+import org.oppia.util.data.AsyncResult
+import org.oppia.util.logging.EnableConsoleLog
+import org.oppia.util.logging.EnableFileLog
+import org.oppia.util.logging.GlobalLogLevel
+import org.oppia.util.logging.LogLevel
+import org.oppia.util.networking.NetworkConnectionUtil
+import org.oppia.util.networking.NetworkConnectionUtil.ConnectionStatus.NONE
 import org.robolectric.annotation.Config
 import javax.inject.Inject
+import javax.inject.Qualifier
 import javax.inject.Singleton
 
 const val TEST_TIMESTAMP = 1556094120000
@@ -44,10 +65,25 @@ class AnalyticsControllerTest {
   lateinit var analyticsController: AnalyticsController
 
   @Inject
+  lateinit var networkConnectionUtil: NetworkConnectionUtil
+
+  @Inject
   lateinit var fakeEventLogger: FakeEventLogger
+
+  @InternalCoroutinesApi
+  @Inject
+  lateinit var testCoroutineDispatchers: TestCoroutineDispatchers
+
+  @Mock
+  lateinit var mockOppiaEventLogsObserver: Observer<AsyncResult<OppiaEventLogs>>
+
+  @Captor
+  lateinit var oppiaEventLogsResultCaptor: ArgumentCaptor<AsyncResult<OppiaEventLogs>>
 
   @Before
   fun setUp() {
+    MockitoAnnotations.initMocks(this)
+    networkConnectionUtil = NetworkConnectionUtil(ApplicationProvider.getApplicationContext())
     setUpTestApplicationComponent()
   }
 
@@ -372,12 +408,110 @@ class AnalyticsControllerTest {
     assertThat(eventContext.revisionCardContext.subTopicId).matches(TEST_SUB_TOPIC_ID)
   }
 
+  @ExperimentalCoroutinesApi
+  @InternalCoroutinesApi
+  @Test
+  fun testController_logTransitionEvent_withNoneNetwork_checkLogsEventToStore() {
+    networkConnectionUtil.setCurrentConnectionStatus(NONE)
+    analyticsController.logTransitionEvent(
+      ApplicationProvider.getApplicationContext(),
+      TEST_TIMESTAMP,
+      EventAction.EVENT_ACTION_UNSPECIFIED,
+      analyticsController.createQuestionContext(
+        TEST_QUESTION_ID,
+        listOf(
+          TEST_SKILL_LIST_ID, TEST_SKILL_LIST_ID
+        )
+      )
+    )
+
+    val eventLogs = analyticsController.getEventLogs()
+    eventLogs.observeForever(this.mockOppiaEventLogsObserver)
+    testCoroutineDispatchers.advanceUntilIdle()
+    verify(
+      this.mockOppiaEventLogsObserver,
+      atLeastOnce()
+    ).onChanged(oppiaEventLogsResultCaptor.capture())
+
+    val eventLog = oppiaEventLogsResultCaptor.value.getOrThrow().getEventLog(0)
+    assertThat(eventLog.priority).isEqualTo(Priority.ESSENTIAL)
+    assertThat(eventLog.context.activityContextCase).isEqualTo(QUESTION_CONTEXT)
+    assertThat(eventLog.timestamp).isEqualTo(TEST_TIMESTAMP)
+    assertThat(eventLog.actionName).isEqualTo(EventAction.EVENT_ACTION_UNSPECIFIED)
+  }
+
+  @ExperimentalCoroutinesApi
+  @InternalCoroutinesApi
+  @Test
+  fun testController_logClickEvent_withNoneNetwork_checkLogsEventToStore() {
+    networkConnectionUtil.setCurrentConnectionStatus(NONE)
+    analyticsController.logClickEvent(
+      ApplicationProvider.getApplicationContext(),
+      TEST_TIMESTAMP,
+      EventAction.EVENT_ACTION_UNSPECIFIED,
+      analyticsController.createQuestionContext(
+        TEST_QUESTION_ID,
+        listOf(
+          TEST_SKILL_LIST_ID, TEST_SKILL_LIST_ID
+        )
+      )
+    )
+
+    val eventLogs = analyticsController.getEventLogs()
+    eventLogs.observeForever(this.mockOppiaEventLogsObserver)
+    testCoroutineDispatchers.advanceUntilIdle()
+    verify(
+      this.mockOppiaEventLogsObserver,
+      atLeastOnce()
+    ).onChanged(oppiaEventLogsResultCaptor.capture())
+
+    val eventLog = oppiaEventLogsResultCaptor.value.getOrThrow().getEventLog(0)
+    assertThat(eventLog.priority).isEqualTo(Priority.OPTIONAL)
+    assertThat(eventLog.context.activityContextCase).isEqualTo(QUESTION_CONTEXT)
+    assertThat(eventLog.timestamp).isEqualTo(TEST_TIMESTAMP)
+    assertThat(eventLog.actionName).isEqualTo(EventAction.EVENT_ACTION_UNSPECIFIED)
+  }
+
+  @ExperimentalCoroutinesApi
+  @InternalCoroutinesApi
+  @Test
+  fun testController_logTransitionEvent_withNoneNetwork_exceedLimit_checkEventLogStoreSize() {
+    networkConnectionUtil.setCurrentConnectionStatus(NONE)
+    for (i in 1..5005) {
+      analyticsController.logTransitionEvent(
+        ApplicationProvider.getApplicationContext(),
+        TEST_TIMESTAMP,
+        EventAction.EVENT_ACTION_UNSPECIFIED,
+        analyticsController.createQuestionContext(
+          TEST_QUESTION_ID,
+          listOf(
+            TEST_SKILL_LIST_ID, TEST_SKILL_LIST_ID
+          )
+        )
+      )
+    }
+
+    val eventLogs = analyticsController.getEventLogs()
+    eventLogs.observeForever(this.mockOppiaEventLogsObserver)
+    testCoroutineDispatchers.advanceUntilIdle()
+    verify(
+      this.mockOppiaEventLogsObserver,
+      atLeastOnce()
+    ).onChanged(oppiaEventLogsResultCaptor.capture())
+
+    val eventLogStoreSize = oppiaEventLogsResultCaptor.value.getOrThrow().eventLogList.size
+    assertThat(eventLogStoreSize).isEqualTo(5000)
+  }
+
   private fun setUpTestApplicationComponent() {
     DaggerAnalyticsControllerTest_TestApplicationComponent.builder()
       .setApplication(ApplicationProvider.getApplicationContext())
       .build()
       .inject(this)
   }
+
+  @Qualifier
+  annotation class TestDispatcher
 
   // TODO(#89): Move this to a common test application component.
   @Module
@@ -387,11 +521,31 @@ class AnalyticsControllerTest {
     fun provideContext(application: Application): Context {
       return application
     }
+
+    // TODO(#59): Either isolate these to their own shared test module, or use the real logging
+    // module in tests to avoid needing to specify these settings for tests.
+    @EnableConsoleLog
+    @Provides
+    fun provideEnableConsoleLog(): Boolean = true
+
+    @EnableFileLog
+    @Provides
+    fun provideEnableFileLog(): Boolean = false
+
+    @GlobalLogLevel
+    @Provides
+    fun provideGlobalLogLevel(): LogLevel = LogLevel.VERBOSE
   }
 
   // TODO(#89): Move this to a common test application component.
   @Singleton
-  @Component(modules = [TestModule::class, TestLogReportingModule::class])
+  @Component(
+    modules = [
+      TestModule::class, TestLogReportingModule::class,
+      TestDispatcherModule::class, LogStorageModule::class
+
+    ]
+  )
   interface TestApplicationComponent {
     @Component.Builder
     interface Builder {
