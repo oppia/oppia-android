9041adcd54c64594d1c84d5e12d752b46d83a97f
Rajat Talesra <talesra@google.com>
2020-07-17 17:47:49 +0530

    Updated app module test cases.

--- app/src/test/java/org/oppia/app/testing/player/state/StateFragmentAccessibilityTest.kt
+++ app/src/test/java/org/oppia/app/testing/player/state/StateFragmentAccessibilityTest.kt
@@ -40,9 +40,9 @@ import org.oppia.domain.classify.rules.multiplechoiceinput.MultipleChoiceInputMo
 import org.oppia.domain.classify.rules.numberwithunits.NumberWithUnitsRuleModule
 import org.oppia.domain.classify.rules.numericinput.NumericInputRuleModule
 import org.oppia.domain.classify.rules.textinput.TextInputRuleModule
-import org.oppia.domain.exploration.TEST_EXPLORATION_ID_8
 import org.oppia.domain.profile.ProfileTestHelper
 import org.oppia.domain.question.QuestionModule
+import org.oppia.domain.topic.TEST_EXPLORATION_ID_4
 import org.oppia.domain.topic.TEST_STORY_ID_0
 import org.oppia.domain.topic.TEST_TOPIC_ID_0
 import org.oppia.testing.TestAccessibilityModule
@@ -88,7 +88,7 @@ class StateFragmentAccessibilityTest {
   @InternalCoroutinesApi
   @ExperimentalCoroutinesApi
   fun testStateFragment_loadDragDropExp_moveDownWithAccessibility() {
-    launchForExploration(TEST_EXPLORATION_ID_8).use {
+    launchForExploration(TEST_EXPLORATION_ID_4).use {
       startPlayingExploration()
       onView(
         RecyclerViewMatcher.atPositionOnView(
@@ -111,7 +111,7 @@ class StateFragmentAccessibilityTest {
   @InternalCoroutinesApi
   @ExperimentalCoroutinesApi
   fun testStateFragment_loadDragDropExp_moveUpWithAccessibility() {
-    launchForExploration(TEST_EXPLORATION_ID_8).use {
+    launchForExploration(TEST_EXPLORATION_ID_4).use {
       startPlayingExploration()
       onView(
         RecyclerViewMatcher.atPositionOnView(
