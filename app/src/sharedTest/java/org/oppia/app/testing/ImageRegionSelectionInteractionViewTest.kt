533ceb8d974ab08740daa1c9593d3bd45b74fe9c
Pulkit Aggarwal <aggarwalpulkit596@gmail.com>
2020-07-24 01:16:43 +0530

    move tests to espresso tests

--- app/src/sharedTest/java/org/oppia/app/testing/ImageRegionSelectionInteractionViewTest.kt
+++ app/src/sharedTest/java/org/oppia/app/testing/ImageRegionSelectionInteractionViewTest.kt
@@ -2,7 +2,6 @@ package org.oppia.app.testing
 
 import android.app.Application
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import androidx.test.core.app.ActivityScenario.launch
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
@@ -12,15 +11,20 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withTagValue
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.firebase.FirebaseApp
+import dagger.BindsInstance
 import dagger.Component
 import dagger.Module
 import dagger.Provides
 import kotlinx.android.synthetic.main.image_region_selection_test_fragment.clickable_image_view
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.CoreMatchers.not
 import org.hamcrest.Matchers.allOf
 import org.junit.Assert.assertEquals
 import org.junit.Before
+import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.ArgumentCaptor
@@ -30,11 +34,6 @@ import org.mockito.Mockito.times
 import org.mockito.Mockito.verify
 import org.mockito.MockitoAnnotations
 import org.oppia.app.R
-import org.oppia.app.activity.ActivityComponent
-import org.oppia.app.application.ActivityComponentFactory
-import org.oppia.app.application.ApplicationComponent
-import org.oppia.app.application.ApplicationContext
-import org.oppia.app.application.ApplicationModule
 import org.oppia.app.player.state.StateFragment
 import org.oppia.app.utility.DefaultRegionClickedEvent
 import org.oppia.app.utility.NamedRegionClickedEvent
@@ -42,44 +41,23 @@ import org.oppia.app.utility.OnClickableAreaClickedListener
 import org.oppia.app.utility.RegionClickedEvent
 import org.oppia.app.utility.capture
 import org.oppia.app.utility.clickPoint
-import org.oppia.data.backends.gae.NetworkModule
-import org.oppia.domain.classify.InteractionsModule
-import org.oppia.domain.classify.rules.continueinteraction.ContinueModule
-import org.oppia.domain.classify.rules.dragAndDropSortInput.DragDropSortInputModule
-import org.oppia.domain.classify.rules.fractioninput.FractionInputModule
-import org.oppia.domain.classify.rules.imageClickInput.ImageClickInputModule
-import org.oppia.domain.classify.rules.itemselectioninput.ItemSelectionInputModule
-import org.oppia.domain.classify.rules.multiplechoiceinput.MultipleChoiceInputModule
-import org.oppia.domain.classify.rules.numberwithunits.NumberWithUnitsRuleModule
-import org.oppia.domain.classify.rules.numericinput.NumericInputRuleModule
-import org.oppia.domain.classify.rules.textinput.TextInputRuleModule
-import org.oppia.domain.oppialogger.LogStorageModule
-import org.oppia.domain.question.QuestionModule
-import org.oppia.testing.TestAccessibilityModule
-import org.oppia.testing.TestDispatcherModule
 import org.oppia.testing.TestLogReportingModule
-import org.oppia.util.accessibility.FakeAccessibilityManager
-import org.oppia.util.caching.CacheAssetsLocally
-import org.oppia.util.gcsresource.GcsResourceModule
-import org.oppia.util.logging.LoggerModule
-import org.oppia.util.parser.GlideImageLoaderModule
-import org.oppia.util.parser.HtmlParserEntityTypeModule
-import org.oppia.util.parser.ImageParsingModule
-import org.robolectric.annotation.Config
+import org.oppia.util.logging.EnableConsoleLog
+import org.oppia.util.logging.EnableFileLog
+import org.oppia.util.logging.GlobalLogLevel
+import org.oppia.util.logging.LogLevel
+import org.oppia.util.threading.BackgroundDispatcher
+import org.oppia.util.threading.BlockingDispatcher
 import javax.inject.Inject
+import javax.inject.Qualifier
 import javax.inject.Singleton
 
 /** Tests for [StateFragment]. */
 @RunWith(AndroidJUnit4::class)
-@Config(application = ImageRegionSelectionInteractionViewTest.TestApplication::class)
 class ImageRegionSelectionInteractionViewTest {
 
   @Inject
-  @field:ApplicationContext
   lateinit var context: Context
-
-  @Inject
-  lateinit var fakeAccessibilityManager: FakeAccessibilityManager
 
   @Mock
   lateinit var onClickableAreaClickedListener: OnClickableAreaClickedListener
@@ -95,11 +73,14 @@ class ImageRegionSelectionInteractionViewTest {
   }
 
   private fun setUpTestApplicationComponent() {
-    ApplicationProvider.getApplicationContext<TestApplication>().inject(this)
+    DaggerImageRegionSelectionInteractionViewTest_TestApplicationComponent.builder()
+      .setApplication(ApplicationProvider.getApplicationContext())
+      .build()
+      .inject(this)
   }
 
   @Test
-  fun testImageRegionSelectionInteractionView_clickRegion3_Region3Clicked() {
+  fun testImageRegionSelectionInteractionView_clickRegion3_region3Clicked() {
     launch(ImageRegionSelectionTestActivity::class.java).use {
       it.onActivity {
         it.clickable_image_view.setListener(onClickableAreaClickedListener)
@@ -172,8 +153,8 @@ class ImageRegionSelectionInteractionViewTest {
   }
 
   @Test
-  fun testView_withTalkbackEnabled_clickRegion3_clickRegion2_Region2Clicked() {
-    fakeAccessibilityManager.setTalkbackEnabled(true)
+  @Ignore("Move to Roboelectric")
+  fun testView_withTalkbackEnabled_clickRegion3_clickRegion2_region2Clicked() {
     launch(ImageRegionSelectionTestActivity::class.java).use {
       it.onActivity {
         it.clickable_image_view.setListener(onClickableAreaClickedListener)
@@ -207,8 +188,8 @@ class ImageRegionSelectionInteractionViewTest {
   }
 
   @Test
-  fun testImageRegionSelectionInteractionView_withTalkbackEnabled_clickRegion3_Region3Clicked() {
-    fakeAccessibilityManager.setTalkbackEnabled(true)
+  @Ignore("Move to Roboelectric")
+  fun testImageRegionSelectionInteractionView_withTalkbackEnabled_clickRegion3_region3Clicked() {
     launch(ImageRegionSelectionTestActivity::class.java).use {
       it.onActivity {
         it.clickable_image_view.setListener(onClickableAreaClickedListener)
@@ -230,8 +211,8 @@ class ImageRegionSelectionInteractionViewTest {
   }
 
   @Test
+  @Ignore("Move to Roboelectric")
   fun testView_withTalkbackEnabled_clickOnDefaultRegion_defaultRegionNotClicked() {
-    fakeAccessibilityManager.setTalkbackEnabled(true)
     launch(ImageRegionSelectionTestActivity::class.java).use { scenario ->
       scenario.onActivity {
         it.clickable_image_view.setListener(onClickableAreaClickedListener)
@@ -247,48 +228,69 @@ class ImageRegionSelectionInteractionViewTest {
     }
   }
 
+  @Qualifier
+  annotation class TestDispatcher
+
   @Module
   class TestModule {
-    // Do not use caching to ensure URLs are always used as the main data source when loading audio.
     @Provides
-    @CacheAssetsLocally
-    fun provideCacheAssetsLocally(): Boolean = false
+    @Singleton
+    fun provideContext(application: Application): Context {
+      return application
+    }
+
+    @ExperimentalCoroutinesApi
+    @Singleton
+    @Provides
+    @TestDispatcher
+    fun provideTestDispatcher(): CoroutineDispatcher {
+      return TestCoroutineDispatcher()
+    }
+
+    @Singleton
+    @Provides
+    @BackgroundDispatcher
+    fun provideBackgroundDispatcher(
+      @TestDispatcher testDispatcher: CoroutineDispatcher
+    ): CoroutineDispatcher {
+      return testDispatcher
+    }
+
+    @Singleton
+    @Provides
+    @BlockingDispatcher
+    fun provideBlockingDispatcher(
+      @TestDispatcher testDispatcher: CoroutineDispatcher
+    ): CoroutineDispatcher {
+      return testDispatcher
+    }
+
+    // TODO(#59): Either isolate these to their own shared test module, or use the real logging
+    // module in tests to avoid needing to specify these settings for tests.
+    @EnableConsoleLog
+    @Provides
+    fun provideEnableConsoleLog(): Boolean = true
+
+    @EnableFileLog
+    @Provides
+    fun provideEnableFileLog(): Boolean = false
+
+    @GlobalLogLevel
+    @Provides
+    fun provideGlobalLogLevel(): LogLevel = LogLevel.VERBOSE
   }
 
-  // TODO(#59): Figure out a way to reuse modules instead of needing to re-declare them.
   @Singleton
-  @Component(
-    modules = [
-      TestModule::class, TestDispatcherModule::class, ApplicationModule::class,
-      NetworkModule::class, LoggerModule::class, ContinueModule::class, FractionInputModule::class,
-      ItemSelectionInputModule::class, MultipleChoiceInputModule::class,
-      NumberWithUnitsRuleModule::class, NumericInputRuleModule::class, TextInputRuleModule::class,
-      DragDropSortInputModule::class, InteractionsModule::class, GcsResourceModule::class,
-      GlideImageLoaderModule::class, ImageParsingModule::class, HtmlParserEntityTypeModule::class,
-      QuestionModule::class, TestLogReportingModule::class, TestAccessibilityModule::class,
-      LogStorageModule::class, ImageClickInputModule::class
-    ]
-  )
-  interface TestApplicationComponent : ApplicationComponent {
+  @Component(modules = [TestModule::class, TestLogReportingModule::class])
+  interface TestApplicationComponent {
     @Component.Builder
-    interface Builder : ApplicationComponent.Builder
+    interface Builder {
+      @BindsInstance
+      fun setApplication(application: Application): Builder
 
-    fun inject(imageRegionSelectionAccessibilityTest: ImageRegionSelectionInteractionViewTest)
-  }
-
-  class TestApplication : Application(), ActivityComponentFactory {
-    private val component: TestApplicationComponent by lazy {
-      DaggerImageRegionSelectionInteractionViewTest_TestApplicationComponent.builder()
-        .setApplication(this)
-        .build() as TestApplicationComponent
+      fun build(): TestApplicationComponent
     }
 
-    fun inject(imageRegionSelectionAccessibilityTest: ImageRegionSelectionInteractionViewTest) {
-      component.inject(imageRegionSelectionAccessibilityTest)
-    }
-
-    override fun createActivityComponent(activity: AppCompatActivity): ActivityComponent {
-      return component.getActivityComponentBuilderProvider().get().setActivity(activity).build()
-    }
+    fun inject(imageRegionSelectionInteractionViewTest: ImageRegionSelectionInteractionViewTest)
   }
 }
