ffb7e94b2de5eb8797a3aac6702965fad2079a13
Pulkit Aggarwal <aggarwalpulkit596@gmail.com>
2020-07-24 06:34:08 +0530

    Fixes #1321,1320 : Add image helper with photoview library (#1457)
    * add ClickableAreaImage class
    
    * finalize the clickable areas image class
    
    * add test for image region selection
    
    * nit changes
    
    * nit changes
    
    * structural changes
    
    * add support for accessibility
    
    * remove background resource from other views
    
    * add view on layout change
    
    * fix ktlint
    
    * fix all ktlint issues
    
    * use same code for both cases
    
    * finalize the approach
    
    * add accessibility tests
    
    * combine accessibility tests
    
    * add end to end tests
    
    * fix lint
    
    * nit changes
    
    * add mockito tests
    
    * add mockito tests
    
    * nit changes
    
    * move tests to espresso tests
    
    * use truth
    
    Co-authored-by: Ben Henning <henning.benmax@gmail.com>

---
+++ app/src/main/java/org/oppia/app/testing/ImageRegionSelectionTestActivity.kt
@@ -0,0 +1,23 @@
+package org.oppia.app.testing
+
+import android.os.Bundle
+import org.oppia.app.R
+import org.oppia.app.activity.InjectableAppCompatActivity
+import org.oppia.app.utility.ClickableAreasImage
+
+/** Test Activity used for testing [ClickableAreasImage] functionality */
+class ImageRegionSelectionTestActivity : InjectableAppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    activityComponent.inject(this)
+    setContentView(R.layout.test_activity)
+    supportFragmentManager.beginTransaction()
+      .add(
+        R.id.test_fragment_placeholder,
+        ImageRegionSelectionTestFragment(),
+        IMAGE_REGION_SELECTION_TEST_FRAGMENT_TAG
+      )
+      .commitNow()
+  }
+}
