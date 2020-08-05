e0b83f88abfaa57c732d954d9d7c4ebf15f1ea17
miaboloix <miaboloix@google.com>
2020-08-05 12:09:22 -0400

    Traslated EditTextBindingAdapter

--- app/src/main/java/org/oppia/app/databinding/EditTextBindingAdapters.java
+++ app/src/main/java/org/oppia/app/databinding/EditTextBindingAdapters.java
@@ -1,4 +1,16 @@
 package org.oppia.app.databinding;
 
-public class EditTextBindingAdapters {
+import android.text.TextWatcher;
+import android.widget.EditText;
+import androidx.databinding.BindingAdapter;
+import org.jetbrains.annotations.NotNull;
+
+public final class EditTextBindingAdapters {
+  /**
+   * Binding adapter for setting a [TextWatcher] as a change listener for an [EditText].
+   */
+  @BindingAdapter("app:textChangedListener")
+  public static void bindTextWatcher(@NotNull EditText editText, TextWatcher textWatcher) {
+    editText.addTextChangedListener(textWatcher);
+  }
 }
