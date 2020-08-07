f9ad4d50d0d18d1211976bc0734bf63b20b63b78
miaboloix <miaboloix@google.com>
2020-08-07 11:17:18 -0400

    Trying spaces

--- app/src/main/java/org/oppia/app/databinding/DrawableBindingAdapters.java
+++ app/src/main/java/org/oppia/app/databinding/DrawableBindingAdapters.java
@@ -8,36 +8,36 @@ import androidx.annotation.DrawableRes;
 import androidx.databinding.BindingAdapter;
 import org.oppia.app.R;
 
-/** Used to set a rounded-rect background drawable with a data-bound color. */
-@BindingAdapter("app:roundedRectDrawableWithColor")
-fun setBackgroundDrawable(view: View, @ColorInt colorRgb: Int) {
-  view.setBackgroundResource(R.drawable.rounded_rect_background)
-  // The input color needs to have alpha channel prepended to it.
-  (view.background as GradientDrawable).setColor((0xff000000 or colorRgb.toLong()).toInt())
-}
+  /** Used to set a rounded-rect background drawable with a data-bound color. */
+  @BindingAdapter("app:roundedRectDrawableWithColor")
+  fun setBackgroundDrawable(view: View, @ColorInt colorRgb: Int) {
+    view.setBackgroundResource(R.drawable.rounded_rect_background)
+    // The input color needs to have alpha channel prepended to it.
+    (view.background as GradientDrawable).setColor((0xff000000 or colorRgb.toLong()).toInt())
+  }
 
-@BindingAdapter("app:topRoundedRectDrawableWithColor")
-fun setTopBackgroundDrawable(view: View, @ColorInt colorRgb: Int) {
-  view.setBackgroundResource(R.drawable.top_rounded_rect_background)
-  // The input color needs to have alpha channel prepended to it.
-  (view.background as GradientDrawable).setColor((0xff000000 or colorRgb.toLong()).toInt())
-}
+  @BindingAdapter("app:topRoundedRectDrawableWithColor")
+  fun setTopBackgroundDrawable(view: View, @ColorInt colorRgb: Int) {
+    view.setBackgroundResource(R.drawable.top_rounded_rect_background)
+    // The input color needs to have alpha channel prepended to it.
+    (view.background as GradientDrawable).setColor((0xff000000 or colorRgb.toLong()).toInt())
+  }
 
-@BindingAdapter("app:bottomRoundedRectDrawableWithColor")
-fun setBottomBackgroundDrawable(view: View, @ColorInt colorRgb: Int) {
-  view.setBackgroundResource(R.drawable.bottom_rounded_rect_background)
-  // The input color needs to have alpha channel prepended to it.
-  (view.background as GradientDrawable).setColor((0xff000000 or colorRgb.toLong()).toInt())
-}
+  @BindingAdapter("app:bottomRoundedRectDrawableWithColor")
+  fun setBottomBackgroundDrawable(view: View, @ColorInt colorRgb: Int) {
+    view.setBackgroundResource(R.drawable.bottom_rounded_rect_background)
+    // The input color needs to have alpha channel prepended to it.
+    (view.background as GradientDrawable).setColor((0xff000000 or colorRgb.toLong()).toInt())
+  }
 
-@BindingAdapter("app:rectangleDrawableWithColor")
-fun setRectangleBackgroundDrawable(view: View, @ColorInt colorRgb: Int) {
-  view.setBackgroundResource(R.drawable.rectangle_background)
-  // The input color needs to have alpha channel prepended to it.
-  (view.background as GradientDrawable).setColor((0xff000000 or colorRgb.toLong()).toInt())
-}
+  @BindingAdapter("app:rectangleDrawableWithColor")
+  fun setRectangleBackgroundDrawable(view: View, @ColorInt colorRgb: Int) {
+    view.setBackgroundResource(R.drawable.rectangle_background)
+    // The input color needs to have alpha channel prepended to it.
+    (view.background as GradientDrawable).setColor((0xff000000 or colorRgb.toLong()).toInt())
+  }
 
-@BindingAdapter("android:button")
-fun setBackgroundResource(button: Button, @DrawableRes resource: Int) {
-  button.setBackgroundResource(resource)
-}
+  @BindingAdapter("android:button")
+  fun setBackgroundResource(button: Button, @DrawableRes resource: Int) {
+    button.setBackgroundResource(resource)
+  }
