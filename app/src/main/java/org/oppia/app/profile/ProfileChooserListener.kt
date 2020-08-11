5f7637ec3cd11d1738e23e0c9c42f0ec45ae3ec5
miaboloix <miaboloix@google.com>
2020-08-11 15:45:21 -0400

    Fixed profile_chooser_add_view

---
+++ app/src/main/java/org/oppia/app/profile/ProfileChooserListener.kt
@@ -0,0 +1,9 @@
+package org.oppia.app.profile
+
+import androidx.databinding.ObservableField
+
+interface ProfileChooserListener {
+
+  val wasProfileEverBeenAddedValue: ObservableField<Boolean>
+
+}
