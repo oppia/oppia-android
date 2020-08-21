404fdf63ae2402da9285f698b8b5fa87de8371fa
prayutsu <gargabhay2000@gmail.com>
2020-08-22 05:03:51 +0530

    remove build errors

--- app/src/main/java/org/oppia/app/drawer/NavigationDrawerFragmentPresenter.kt
+++ app/src/main/java/org/oppia/app/drawer/NavigationDrawerFragmentPresenter.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.forEach
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
