ae1de8ca73daef62adaa381700bc61c01e931772
anandwana001 <akshaynandwana001@gmail.com>
2020-08-23 16:38:48 +0530

    remove unused import

--- app/src/main/java/org/oppia/app/story/StoryViewModel.kt
+++ app/src/main/java/org/oppia/app/story/StoryViewModel.kt
@@ -3,7 +3,6 @@ package org.oppia.app.story
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Transformations
-import androidx.lifecycle.ViewModel
 import org.oppia.app.fragment.FragmentScope
 import org.oppia.app.model.ChapterPlayState
 import org.oppia.app.model.ChapterSummary
