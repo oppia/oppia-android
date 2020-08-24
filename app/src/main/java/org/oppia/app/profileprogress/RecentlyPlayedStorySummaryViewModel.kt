d8c6b9c6a32d24ef7eca0f83bef7d5d82f87813e
Rajat Talesra <talesra@google.com>
2020-08-24 14:30:11 +0530

    Initial implementation

--- app/src/main/java/org/oppia/app/profileprogress/RecentlyPlayedStorySummaryViewModel.kt
+++ app/src/main/java/org/oppia/app/profileprogress/RecentlyPlayedStorySummaryViewModel.kt
@@ -1,9 +1,31 @@
 package org.oppia.app.profileprogress
 
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import org.oppia.app.home.RouteToTopicPlayStoryListener
 import org.oppia.app.model.PromotedStory
+import org.oppia.app.shim.IntentFactoryShim
 
 /** Recently played item [ViewModel] for the recycler view in [ProfileProgressFragment]. */
 class RecentlyPlayedStorySummaryViewModel(
+  val activity: AppCompatActivity,
+  val internalProfileId: Int,
   val promotedStory: PromotedStory,
-  val entityType: String
-) : ProfileProgressItemViewModel()
+  val entityType: String,
+  private val IntentFactoryShim: IntentFactoryShim
+) : ProfileProgressItemViewModel(), RouteToTopicPlayStoryListener {
+  fun onStoryItemClicked() {
+    Log.d("TAG","onStoryItemClicked")
+    routeToTopicPlayStory(internalProfileId, promotedStory.topicId, promotedStory.storyId)
+  }
+
+  override fun routeToTopicPlayStory(internalProfileId: Int, topicId: String, storyId: String) {
+    val intent = IntentFactoryShim.createTopicPlayStoryActivityIntent(
+      activity.applicationContext,
+      internalProfileId,
+      topicId,
+      storyId
+    )
+    activity.startActivity(intent)
+  }
+}
