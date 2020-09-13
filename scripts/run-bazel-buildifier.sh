71d184b82c3bf66595e9eb0bd858cf3c75a33bc7
anandwana001 <akshaynandwana001@gmail.com>
2020-09-13 15:06:57 +0530

    removed unnecessary comments

--- scripts/run-bazel-buildifier.sh
+++ scripts/run-bazel-buildifier.sh
@@ -5,13 +5,6 @@
 # WORKSPACE
 # BUILD
 # *.bzl
-#
-# Run the script from the oppia-android root folder:
-#
-#       bash scripts/run-bazel-buildifier.sh
-#
-# NOTE: Once this script is run, we can safely push inorder to have successfully
-# bazel lint check on GitHub Action, until this is shifted to pre-commit hook
 
 exec ./buildifier --lint=warn --mode=check --warnings=all -r app data domain model testing utility BUILD.bazel WORKSPACE oppia_android_test.bzl
 echo "Bazel lint check completed successfully"
