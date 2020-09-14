e6d9ccc1801726afcd043eb37f797291a00ef467
anandwana001 <akshaynandwana001@gmail.com>
2020-09-14 15:58:10 +0530

    remove unused import

--- app/src/sharedTest/java/org/oppia/app/administratorcontrols/AppVersionActivityTest.kt
+++ app/src/sharedTest/java/org/oppia/app/administratorcontrols/AppVersionActivityTest.kt
@@ -23,7 +23,6 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import dagger.Component
 import org.junit.After
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.oppia.app.R
