e8a5c536dc1fa32044db5b28c83f85c129e9d7a9
Ben Henning <henning.benmax@gmail.com>
2020-09-16 13:02:03 -0700

    Introduce run_emulator_tests script
    Introduce script to simplify the workflow setup & avoid pitfalls from quote escaping.

---
+++ scripts/run_emulator_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Java Home:"
+echo $JAVA_HOME
+echo "Android Home:"
+echo $ANDROID_HOME
+echo "Android SDK Home:"
+echo $ANDROID_SDK_HOME
+export ANDROID_HOME=/tmp
+../gradlew --info --full-stacktrace connectedCheck
