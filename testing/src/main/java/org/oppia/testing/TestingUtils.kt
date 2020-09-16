0f6d0c2e006fb42765ca730088f978db77b13dc3
prayutsu <gargabhay2000@gmail.com>
2020-09-16 10:50:49 +0530

    created testUtilFile

---
+++ testing/src/main/java/org/oppia/testing/TestingUtils.kt
@@ -0,0 +1,26 @@
+package org.oppia.testing
+
+import android.view.View
+import android.widget.EditText
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.matcher.ViewMatchers
+import org.hamcrest.CoreMatchers
+import org.hamcrest.Matcher
+
+fun appendText(text: String, testCoroutineDispatchers: TestCoroutineDispatchers): ViewAction {
+  return object : ViewAction {
+    override fun getDescription(): String {
+      return "appendText($text)"
+    }
+
+    override fun getConstraints(): Matcher<View> {
+      return CoreMatchers.allOf(ViewMatchers.isEnabled())
+    }
+
+    override fun perform(uiController: UiController?, view: View?) {
+      (view as? EditText)?.append(text)
+      testCoroutineDispatchers.runCurrent()
+    }
+  }
+}
