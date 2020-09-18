266aa3ad13d2c4ad6dffa51a73a4ff26a245db28
Ben Henning <bhenning@google.com>
2020-09-18 16:39:34 -0700

    Debug arguments.

--- scripts/run_emulator_tests.sh
+++ scripts/run_emulator_tests.sh
@@ -18,6 +18,10 @@
 test_whitelist="'$*'"
 
 echo "Using Android Home: $ANDROID_HOME"
+echo "Argument 0: $0"
+echo "Argument 1: $1"
+echo "Argument 2: $2"
+echo "Argument 3: $3"
 echo "Running emulator tests using test whitelist: $test_whitelist"
 
 echo "Building all instrumentation tests (debug)"
