ebba4a9fb790d4a83878704549918ef7e3cfc22d
prayutsu <gargabhay2000@gmail.com>
2020-09-26 23:15:03 +0530

    update fork

--- app/src/main/java/org/oppia/android/app/customview/VerticalDashedLine.kt
+++ app/src/main/java/org/oppia/android/app/customview/VerticalDashedLine.kt
@@ -6,11 +6,9 @@ import android.graphics.Color
 import android.graphics.DashPathEffect
 import android.graphics.Paint
 import android.graphics.Path
-import android.os.Build
 import android.util.AttributeSet
 import android.view.View
 import androidx.annotation.Nullable
-import androidx.annotation.RequiresApi
 
 class VerticalDashedLine : View {
 
@@ -33,15 +31,15 @@ class VerticalDashedLine : View {
     initialize()
   }
 
-  @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
-  constructor(
-    context: Context?,
-    @Nullable attrs: AttributeSet?,
-    defStyleAttr: Int,
-    defStyleRes: Int
-  ) : super(context, attrs, defStyleAttr, defStyleRes) {
-    initialize()
-  }
+//  @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+//  constructor(
+//    context: Context?,
+//    @Nullable attrs: AttributeSet?,
+//    defStyleAttr: Int,
+//    defStyleRes: Int
+//  ) : super(context, attrs, defStyleAttr, defStyleRes) {
+//    initialize()
+//  }
 
   private fun initialize() {
     paint.style = Paint.Style.STROKE
@@ -58,47 +56,52 @@ class VerticalDashedLine : View {
     path.quadTo((width / 2).toFloat(), height.toFloat() / 2,
       (width / 2).toFloat(), height.toFloat()
     )
+    canvas.drawPath(path, paint)
   }
 
   override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
     val desiredWidth = 10
     val desiredHeight = 100
-
-    val widthMode = View.MeasureSpec.getMode(heightMeasureSpec)
-    val widthSize = View.MeasureSpec.getSize(widthMeasureSpec)
-    val heightMode = View.MeasureSpec.getMode(heightMeasureSpec)
-    val heightSize = View.MeasureSpec.getSize(heightMeasureSpec)
+    // Get Modes And Size
+    val widthMode = MeasureSpec.getMode(heightMeasureSpec)
+    val widthSize = MeasureSpec.getSize(widthMeasureSpec)
+    val heightMode = MeasureSpec.getMode(heightMeasureSpec)
+    val heightSize = MeasureSpec.getSize(heightMeasureSpec)
 
     val width: Int
     val height: Int
 
     //Measure Width
-
-
-    width = if (widthMode == View.MeasureSpec.EXACTLY) {
-      //Must be this size
-      widthSize
-    } else if (widthMode == View.MeasureSpec.AT_MOST) {
-      //Can't be bigger than...
-      Math.min(desiredWidth, widthSize)
-    } else {
-      //Be whatever you want
-      desiredWidth
+    width = when (widthMode) {
+      MeasureSpec.EXACTLY -> {
+        //Must be this size
+        widthSize
+      }
+      MeasureSpec.AT_MOST -> {
+        //Can't be bigger than...
+        desiredWidth.coerceAtMost(widthSize)
+      }
+      else -> {
+        //Be whatever you want
+        desiredWidth
+      }
     }
 
     //Measure Height
-
-    height = if (heightMode == View.MeasureSpec.EXACTLY) {
-      //Must be this size
-      heightSize
-    } else if (heightMode == View.MeasureSpec.AT_MOST) {
-      //Can't be bigger than...
-      heightSize
-    } else {
-      //Be whatever you want
-      desiredHeight
+    height = when (heightMode) {
+      MeasureSpec.EXACTLY -> {
+        //Must be this size
+        heightSize
+      }
+      MeasureSpec.AT_MOST -> {
+        //Can't be bigger than...
+        desiredHeight.coerceAtMost(heightSize)
+      }
+      else -> {
+        //Be whatever you want
+        desiredHeight
+      }
     }
-
     setMeasuredDimension(width, height)
   }
 
