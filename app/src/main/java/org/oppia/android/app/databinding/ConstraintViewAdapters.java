5ee2b204e74df906dd8586d346e6db594f91ac47
prayutsu <gargabhay2000@gmail.com>
2020-10-06 10:37:35 +0530

    fixed suggested changes

--- app/src/main/java/org/oppia/android/app/databinding/ConstraintViewAdapters.java
+++ app/src/main/java/org/oppia/android/app/databinding/ConstraintViewAdapters.java
@@ -8,7 +8,7 @@ import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.databinding.BindingAdapter;
 
 /** Holds all custom binding adapters that set constraints to views. */
-public class ConstraintViewAdapters {
+public final class ConstraintViewAdapters {
 
   /** Binding adapter for setting a [layout_constraintEndToEndOf] to a view */
   @BindingAdapter("app:layout_constraintEnd_toEndOf")
