187a8bc9301524ef79d5ac67400f61b2d2f3be52
Victor-Titan <vrajgandhi@hotmail.com>
2020-10-24 17:20:55 +0530

    Changed password hiding technique from using kotlin method to using pinView attribute and data binding instead

--- app/src/main/java/org/oppia/android/app/profile/PinPasswordActivityPresenter.kt
+++ app/src/main/java/org/oppia/android/app/profile/PinPasswordActivityPresenter.kt
@@ -51,9 +51,8 @@ class PinPasswordActivityPresenter @Inject constructor(
       lifecycleOwner = activity
       viewModel = pinViewModel
     }
-    binding.inputPin.setPasswordHidden(!binding.viewModel!!.showPassword.get()!!)
+
     binding.showPin.setOnClickListener {
-      binding.inputPin.setPasswordHidden(binding.viewModel!!.showPassword.get()!!)
       pinViewModel.showPassword.set(!pinViewModel.showPassword.get()!!)
     }
     binding.inputPin.requestFocus()
