e028aa9d294d2557f07a7755f7a7921ad38f218e
prayutsu <gargabhay2000@gmail.com>
2020-11-03 12:24:25 +0530

    tried implementing new methods

--- app/src/main/java/org/oppia/android/app/onboarding/OnboardingPagerAdapter.kt
+++ app/src/main/java/org/oppia/android/app/onboarding/OnboardingPagerAdapter.kt
@@ -1,8 +1,14 @@
 package org.oppia.android.app.onboarding
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import org.oppia.android.R
+
+const val onBoardingSlide = 0
+const val onBoardingFinalSlide = 1
 
 /** Adapter to control the slide details in onboarding flow. */
 class OnboardingPagerAdapter(
@@ -10,7 +16,15 @@ class OnboardingPagerAdapter(
   val onboardingSlideFinalViewModel: OnboardingSlideFinalViewModel
 ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-    TODO("Not yet implemented")
+    return if (viewType == onBoardingSlide) {
+      val onBoardingSlideView = LayoutInflater.from(context)
+        .inflate(R.layout.onboarding_slide, parent, false)
+      SlideViewHolder(onBoardingSlideView)
+    } else {
+      val onBoardingFinalSlideView = LayoutInflater.from(context)
+        .inflate(R.layout.onboarding_slide_final, parent, false)
+      FinalSlideViewHolder(onBoardingFinalSlideView)
+    }
   }
 
   override fun getItemCount(): Int {
@@ -18,6 +32,17 @@ class OnboardingPagerAdapter(
   }
 
   override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-    TODO("Not yet implemented")
   }
+
+  override fun getItemViewType(position: Int): Int {
+    return if (position == TOTAL_NUMBER_OF_SLIDES - 1) {
+      onBoardingFinalSlide
+    } else {
+      onBoardingSlide
+    }
+  }
+
+  inner class SlideViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+  inner class FinalSlideViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
 }
