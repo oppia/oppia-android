e99b5c10517276684a6fd7ba1c61fce054311fbf
Rajat Talesra <talesra@google.com>
2020-11-17 14:45:46 +0530

    Draft working code

--- app/src/main/java/org/oppia/android/app/splash/SplashActivity.kt
+++ app/src/main/java/org/oppia/android/app/splash/SplashActivity.kt
@@ -1,8 +1,10 @@
 package org.oppia.android.app.splash
 
+import android.content.res.Configuration
 import android.os.Bundle
 import org.oppia.android.app.activity.InjectableAppCompatActivity
 import org.oppia.android.app.deprecation.DeprecationNoticeExitAppListener
+import java.util.*
 import javax.inject.Inject
 
 /** An activity that shows a temporary loading page until the app is fully loaded then navigates to [ProfileActivity]. */
@@ -12,6 +14,12 @@ class SplashActivity : InjectableAppCompatActivity(), DeprecationNoticeExitAppLi
   lateinit var splashActivityPresenter: SplashActivityPresenter
 
   override fun onCreate(savedInstanceState: Bundle?) {
+    // Reference: https://stackoverflow.com/a/41101742
+    val locale = Locale("hie")
+    val conf: Configuration = resources.configuration
+    conf.setLocale(locale)
+    val metrics = resources.displayMetrics
+    resources.updateConfiguration(conf, metrics)
     super.onCreate(savedInstanceState)
     activityComponent.inject(this)
     splashActivityPresenter.handleOnCreate()
