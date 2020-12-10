5c0772634be6478a8411007e7bf92d64f1b16427
Shridhar Goel <shridhar.goel@gmail.com>
2020-12-10 10:04:23 +0530

    Update docs in AssertionHelpers

--- testing/src/main/java/org/oppia/android/testing/AssertionHelpers.kt
+++ testing/src/main/java/org/oppia/android/testing/AssertionHelpers.kt
@@ -4,9 +4,18 @@ import org.junit.Assert.fail
 import kotlin.reflect.KClass
 import kotlin.reflect.full.cast
 
+/* This file contains generic assertion helpers which are meant to be usable in any test.
+   Not for methods which are specific to a certain test or part of the codebase. */
+
 /**
- * Assertion helpers which are generic methods to be used at multiple places
- * Not for methods which are specific to a certain test or part of the codebase
+ * A replacement to JUnit5's assertThrows().
+ *
+ * Takes a parameter 'type' which defines the expected exception type and another parameter
+ * 'operation' which defines the function to be performed.
+ *
+ * Asserts that execution of the supplied operation throws an exception of the supplied type.
+ *
+ * Returns the exception.
  */
 fun <T : Throwable> assertThrows(type: KClass<T>, operation: () -> Unit): T {
   try {
