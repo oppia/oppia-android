49f8a0abb186fa2cd6a2100a84963207807f182a
Farees Hussain <fareezzhussain@gmail.com>
2020-12-21 22:20:46 +0530

    Kdocs for RobolectricModule.kt

--- testing/src/main/java/org/oppia/android/testing/RobolectricModule.kt
+++ testing/src/main/java/org/oppia/android/testing/RobolectricModule.kt
@@ -6,7 +6,8 @@ import dagger.Provides
 import javax.inject.Singleton
 
 /**
- * Dagger [Module] that provides [IsOnRobolectric]
+ * Dagger [Module] that provides [IsOnRobolectric] boolean which is used to check if
+ * a test is running on Robolectric or not
  */
 @Module
 class RobolectricModule {
