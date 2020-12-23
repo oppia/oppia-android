9566c629d4381714cf4eb44a93313e8ac6cffb54
jacqueli <jacqueli@google.com>
2020-12-23 10:08:47 -0800

    Remove unused import

--- app/src/main/java/org/oppia/android/app/home/HomeFragmentPresenter.kt
+++ app/src/main/java/org/oppia/android/app/home/HomeFragmentPresenter.kt
@@ -5,7 +5,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Transformations
 import androidx.recyclerview.widget.GridLayoutManager
 import org.oppia.android.R
 import org.oppia.android.app.drawer.KEY_NAVIGATION_PROFILE_ID
