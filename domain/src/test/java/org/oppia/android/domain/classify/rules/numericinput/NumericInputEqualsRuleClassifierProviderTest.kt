99b0db80010c8ef32a8064373f509eacfac23752
Shridhar Goel <shridhar.goel@gmail.com>
2020-12-23 15:08:05 +0530

    Fix part of #2234: Shift assertThrows function from domain module to testing module (#2284)
    * Fix part of #2234: Shift assertThrows function in the domain module to testing module
    
    * Fix lint issues
    
    * Remove dependency on kotlin-reflect
    
    * Update according to changes in develop

--- domain/src/test/java/org/oppia/android/domain/classify/rules/numericinput/NumericInputEqualsRuleClassifierProviderTest.kt
+++ domain/src/test/java/org/oppia/android/domain/classify/rules/numericinput/NumericInputEqualsRuleClassifierProviderTest.kt
@@ -11,13 +11,11 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.oppia.android.app.model.InteractionObject
 import org.oppia.android.domain.util.FLOAT_EQUALITY_INTERVAL
+import org.oppia.android.testing.assertThrows
 import org.robolectric.annotation.Config
 import org.robolectric.annotation.LooperMode
 import javax.inject.Inject
 import javax.inject.Singleton
-import kotlin.reflect.KClass
-import kotlin.reflect.full.cast
-import kotlin.test.fail
 
 /** Tests for [NumericInputEqualsRuleClassifierProvider]. */
 @RunWith(AndroidJUnit4::class)
@@ -165,20 +163,6 @@ class NumericInputEqualsRuleClassifierProviderTest {
       .setApplication(ApplicationProvider.getApplicationContext())
       .build()
       .inject(this)
-  }
-
-  // TODO(#89): Move to a common test library.
-  private fun <T : Throwable> assertThrows(type: KClass<T>, operation: () -> Unit): T {
-    try {
-      operation()
-      fail("Expected to encounter exception of $type")
-    } catch (t: Throwable) {
-      if (type.isInstance(t)) {
-        return type.cast(t)
-      }
-      // Unexpected exception; throw it.
-      throw t
-    }
   }
 
   // TODO(#89): Move this to a common test application component.
