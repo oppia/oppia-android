123e4188a724001ca731eaa3ea62ae00d0500a88
lakshaygarg128 <lakshaygarg128@gmail.com>
2021-01-07 00:45:46 +0530

    ktlint max line resolved

--- app/src/sharedTest/java/org/oppia/android/app/help/HelpFragmentTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/help/HelpFragmentTest.kt
@@ -134,7 +134,7 @@ class HelpFragmentTest {
   }
 
   @Test
-  fun openHelpActivity_configurationChanged_scrollRecyclerViewToZeroPosition_showsFAQSuccessfully() { // ktlint-disable max-line-length
+  fun openHelp_configChanged_scrollRecyclerViewToZeroPosition_showsFAQSuccess() {
     launch<HelpActivity>(createHelpActivityIntent(0, true)).use {
       onView(isRoot()).perform(orientationLandscape())
       onView(withId(R.id.help_fragment_recycler_view)).perform(
@@ -169,7 +169,7 @@ class HelpFragmentTest {
   }
 
   @Test
-  fun openHelpActivity_openNavigationDrawerAndClose_closingOfNavigationDrawerIsVerifiedSuccessfully() { // ktlint-disable max-line-length
+  fun openHelp_openNavDrawerAndClose_closingOfNavDrawerIsVerifiedSuccess() {
     launch<HelpActivity>(createHelpActivityIntent(0, true)).use {
       onView(withContentDescription(R.string.drawer_open_content_description)).perform(click())
       onView(withId(R.id.help_activity_drawer_layout)).perform(close())
