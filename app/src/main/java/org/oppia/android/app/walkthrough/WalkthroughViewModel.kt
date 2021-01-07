b26af4766c0ddc5403ca39735a756cb29ffb4ba7
prayutsu <gargabhay2000@gmail.com>
2021-01-07 21:15:00 +0530

    Restore WalkthroughViewModel.kt and remove changes in WalkthroughTopicHeaderViewModel.kt

--- app/src/main/java/org/oppia/android/app/walkthrough/WalkthroughViewModel.kt
+++ app/src/main/java/org/oppia/android/app/walkthrough/WalkthroughViewModel.kt
@@ -1,8 +1,19 @@
 package org.oppia.android.app.walkthrough
 
+import androidx.databinding.ObservableField
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import org.oppia.android.app.viewmodel.ObservableViewModel
 import javax.inject.Inject
 
 /** [ViewModel] for [WalkthroughActivity]. */
-class WalkthroughViewModel @Inject constructor() : ObservableViewModel()
+class WalkthroughViewModel @Inject constructor() : ObservableViewModel() {
+  val currentProgress = ObservableField(0)
+  private val isProgressBarVisibleMutableLiveData = MutableLiveData<Boolean>(true)
+  val isProgressBarVisible: LiveData<Boolean> = isProgressBarVisibleMutableLiveData
+
+  fun changeProgressBarVisibility(visibility: Boolean) {
+    isProgressBarVisibleMutableLiveData.value = visibility
+  }
+}
--- app/src/main/java/org/oppia/android/app/walkthrough/topiclist/topiclistviewmodel/WalkthroughTopicHeaderViewModel.kt
+++ app/src/main/java/org/oppia/android/app/walkthrough/topiclist/topiclistviewmodel/WalkthroughTopicHeaderViewModel.kt
@@ -1,16 +1,7 @@
 package org.oppia.android.app.walkthrough.topiclist.topiclistviewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import org.oppia.android.app.walkthrough.topiclist.WalkthroughTopicItemViewModel
 
 /** [ViewModel] What do want to learn text in [WalkthroughTopicListFragment]. */
-class WalkthroughTopicHeaderViewModel : WalkthroughTopicItemViewModel() {
-  private val isHeaderTextVisibleMutableLiveData = MutableLiveData<Boolean>(true)
-  val isHeaderTextVisible: LiveData<Boolean> = isHeaderTextVisibleMutableLiveData
-
-  fun changeHeaderTextVisibility(visibility: Boolean) {
-    isHeaderTextVisibleMutableLiveData.value = visibility
-  }
-}
+class WalkthroughTopicHeaderViewModel : WalkthroughTopicItemViewModel()
