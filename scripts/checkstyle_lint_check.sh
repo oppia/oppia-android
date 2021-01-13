29d505b1b0e287dcca3ec4aff6f902648c2a8e53
anandwana001 <akshaynandwana001@gmail.com>
2021-01-13 12:00:22 +0530

    nit fix

--- scripts/checkstyle_lint_check.sh
+++ scripts/checkstyle_lint_check.sh
@@ -7,7 +7,7 @@ echo "********************************"
 lint_results=$(java -jar ../oppia-android-tools/checkstyle-8.37-all.jar -c /google_checks.xml app/src/ data/src/ domain/src/ utility/src/ testing/src/ 2>&1)
 lint_command_result=$?
 echo $lint_results
-if [ "$lint_command_result" -ne 0 ] || [ -z "$lint_results" ]; then
+if [ "$lint_command_result" -ne 0 ] || [ -z "$lint_results" ] || [[ ${lint_results} == *"[WARN]"* ]]; then
   # Assume any lint output or non-zero exit code is a failure.
   echo "********************************"
   echo "Checkstyle issue found."
