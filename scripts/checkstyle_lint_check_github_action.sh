e6e27e548f8be2d2e0f3518efbf6ab0047b1b563
anandwana001 <akshaynandwana001@gmail.com>
2021-01-13 11:33:06 +0530

    separate file for action, non prepush

---
+++ scripts/checkstyle_lint_check_github_action.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "********************************"
+echo "Checking Java file formatting"
+echo "********************************"
+
+lint_results=$(java -jar checkstyle-8.37-all.jar -c /google_checks.xml app/src/ data/src/ domain/src/ utility/src/ testing/src/ 2>&1)
+lint_command_result=$?
+echo $lint_results
+if [ "$lint_command_result" -ne 0 ] || [ -z "$lint_results" ]; then
+  # Assume any lint output or non-zero exit code is a failure.
+  echo "********************************"
+  echo "Checkstyle issue found."
+  echo "Please fix the above issues."
+  echo "********************************"
+  exit 1
+else
+  echo "Checkstyle lint check completed successfully"
+  exit 0
+fi
