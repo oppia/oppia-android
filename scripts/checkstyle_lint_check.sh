a349a9d066c58c8157a8c70d1bda2f55f0003f14
anandwana001 <akshaynandwana001@gmail.com>
2021-01-13 11:29:49 +0530

    try without config file

--- scripts/checkstyle_lint_check.sh
+++ scripts/checkstyle_lint_check.sh
@@ -4,17 +4,17 @@ echo "********************************"
 echo "Checking Java file formatting"
 echo "********************************"
 
-java -jar ../oppia-android-tools/checkstyle-8.37-all.jar -c=checkstyle.xml app/src/ data/src/ domain/src/ utility/src/ testing/src/
-
-status=$?
-
-if [ "$status" = 0 ] ; then
-  echo "Checkstyle lint check completed successfully"
-  exit 0
-else
+lint_results=$(java -jar ../oppia-android-tools/checkstyle-8.37-all.jar -c /google_checks.xml app/src/ data/src/ domain/src/ utility/src/ testing/src/ 2>&1)
+lint_command_result=$?
+echo $lint_results
+if [ "$lint_command_result" -ne 0 ] || [ -z "$lint_results" ]; then
+  # Assume any lint output or non-zero exit code is a failure.
   echo "********************************"
   echo "Checkstyle issue found."
   echo "Please fix the above issues."
   echo "********************************"
   exit 1
+else
+  echo "Checkstyle lint check completed successfully"
+  exit 0
 fi
