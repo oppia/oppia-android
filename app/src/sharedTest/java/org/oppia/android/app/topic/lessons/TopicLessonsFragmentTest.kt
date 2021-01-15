a915148574cda6d72312700605cfd9dea8473845
Srikanth Kb <srikanth.kadaba@gmail.com>
2021-01-15 20:30:25 +0530

    Created helper class for scrolling actions
    * Created a new helper class including methods for all of required
      RecyclerView actions for the codebase
    * Added required gradle dependencies in the testing module

--- app/src/sharedTest/java/org/oppia/android/app/topic/lessons/TopicLessonsFragmentTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/topic/lessons/TopicLessonsFragmentTest.kt
@@ -72,6 +72,7 @@ import org.oppia.android.domain.topic.RATIOS_EXPLORATION_ID_0
 import org.oppia.android.domain.topic.RATIOS_STORY_ID_0
 import org.oppia.android.domain.topic.RATIOS_TOPIC_ID
 import org.oppia.android.domain.topic.StoryProgressTestHelper
+import org.oppia.android.testing.RecyclerViewScrollingActions
 import org.oppia.android.testing.RobolectricModule
 import org.oppia.android.testing.TestAccessibilityModule
 import org.oppia.android.testing.TestCoroutineDispatchers
@@ -103,6 +104,9 @@ class TopicLessonsFragmentTest {
 
   @Inject
   lateinit var storyProgressTestHelper: StoryProgressTestHelper
+
+  @Inject
+  lateinit var recyclerViewScrollingActions: RecyclerViewScrollingActions
 
   private val internalProfileId = 0
 
@@ -281,8 +285,9 @@ class TopicLessonsFragmentTest {
       clickLessonTab()
       scrollToPosition(position = 1)
       clickStoryItem(position = 1, targetViewId = R.id.chapter_list_drop_down_icon)
-      onView(withId(R.id.story_summary_recycler_view)).perform(
-        actionOnItemAtPosition<RecyclerView.ViewHolder>(2, scrollTo())
+      recyclerViewScrollingActions.scrollToPosition(
+        R.id.story_summary_recycler_view,
+        2
       )
       clickStoryItem(position = 2, targetViewId = R.id.chapter_list_drop_down_icon)
       scrollToPosition(position = 1)
