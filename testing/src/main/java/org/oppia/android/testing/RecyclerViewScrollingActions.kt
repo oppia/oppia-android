68e102ef5087096e03fcd40eeb0d08879eb8f6b2
Srikanth Kb <srikanth.kadaba@gmail.com>
2021-01-19 20:04:32 +0530

    Update RecyclerViewScrollingActions
    * Updated KDocs with line wrapping, better context.
    * Updated annotation @LayoutRes for recyclerViewId

--- testing/src/main/java/org/oppia/android/testing/RecyclerViewScrollingActions.kt
+++ testing/src/main/java/org/oppia/android/testing/RecyclerViewScrollingActions.kt
@@ -1,5 +1,6 @@
 package org.oppia.android.testing
 
+import androidx.annotation.LayoutRes
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.scrollTo
@@ -10,23 +11,20 @@ import org.hamcrest.BaseMatcher
 import javax.inject.Inject
 
 /**
- * Provides different scrolling actions for [RecyclerView]s.
- * This is needed because different testing scenarios potentially require different scrolling
- * actions for the tests to be successful.
+ * Provides different scrolling actions for [RecyclerView]s. This is needed because different
+ * testing scenarios potentially require different scrolling actions for the tests to be successful.
  * Use the methods in this class for all scrolling actions for [RecyclerView]s in Roboelectric and
- * Espresso test scenarios.
- * See #2209 for more information.
- * */
+ * Espresso test scenarios. See #2209 for more information.
+ */
 class RecyclerViewScrollingActions @Inject constructor(
   val testCoroutineDispatchers: TestCoroutineDispatchers
 ) {
 
   /**
-   * Performs scrolling action to particular RecyclerView at a required position
-   * Use this method for all testing scenarios when scrolling to a RecyclerView at a particular
-   * position
+   * Performs scrolling action to particular RecyclerView at a required position. Use this method
+   * for all testing scenarios when scrolling to a RecyclerView at a particular position.
    */
-  fun scrollToPosition(recyclerViewId: Int, position: Int) {
+  fun scrollToPosition(@LayoutRes recyclerViewId: Int, position: Int) {
     onView(withId(recyclerViewId)).perform(
       actionOnItemAtPosition<RecyclerView.ViewHolder>(
         position,
@@ -37,11 +35,14 @@ class RecyclerViewScrollingActions @Inject constructor(
   }
 
   /**
-   * Performs scrolling action to [RecyclerView] of a particular ViewType
-   * Use this method for all testing scenarios when the [View] type of the destination [RecyclerView]
-   * is known.
+   * Performs scrolling action to [RecyclerView] of a particular ViewType designed by a view holder
+   * via [baseMatcher]. Use this method for all testing scenarios when the [View] type of the
+   * destination [RecyclerView] is known.
    */
-  fun scrollToViewType(recyclerViewId: Int, baseMatcher: BaseMatcher<RecyclerView.ViewHolder>) {
+  fun scrollToViewType(
+    @LayoutRes recyclerViewId: Int,
+    baseMatcher: BaseMatcher<RecyclerView.ViewHolder>
+  ) {
     onView(withId(recyclerViewId)).perform(
       scrollToHolder(baseMatcher)
     )
