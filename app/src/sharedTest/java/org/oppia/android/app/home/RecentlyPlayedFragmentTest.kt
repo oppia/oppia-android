3388ab1671d819397b522b66c7e13e2482ebfc84
Souhit Dey <suprememightyzeus@gmail.com>
2021-01-27 23:50:54 +0530

    Resolved errors

--- app/src/sharedTest/java/org/oppia/android/app/home/RecentlyPlayedFragmentTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/home/RecentlyPlayedFragmentTest.kt
@@ -121,11 +121,11 @@ class RecentlyPlayedFragmentTest {
     profileId = ProfileId.newBuilder().setInternalId(internalProfileId).build()
     storyProgressTestHelper.markRecentlyPlayedForFractionsStory0Exploration0(
       profileId,
-      timestampOlderThanAWeek = false
+      timestampOlderThanOneWeek = false
     )
     storyProgressTestHelper.markRecentlyPlayedForRatiosStory0Exploration0(
       profileId,
-      timestampOlderThanAWeek = true
+      timestampOlderThanOneWeek = true
     )
   }
 
