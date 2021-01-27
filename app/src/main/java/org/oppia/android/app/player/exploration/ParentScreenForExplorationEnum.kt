dbefe42e971d25a359a91860173bd071778c5536
Rajat Talesra <talesra@google.com>
2021-01-27 18:53:53 +0530

    Remove int value

--- app/src/main/java/org/oppia/android/app/player/exploration/ParentScreenForExplorationEnum.kt
+++ app/src/main/java/org/oppia/android/app/player/exploration/ParentScreenForExplorationEnum.kt
@@ -1,8 +1,8 @@
 package org.oppia.android.app.player.exploration
 
 /** Enum to decide when an exploration is closed which activity should be displayed. */
-enum class ParentScreenForExplorationEnum(val value: Int) {
-  TOPIC_INFO(0),
-  TOPIC_LESSONS(1),
-  STORY(2);
+enum class ParentScreenForExplorationEnum() {
+  TOPIC_INFO,
+  TOPIC_LESSONS,
+  STORY;
 }
