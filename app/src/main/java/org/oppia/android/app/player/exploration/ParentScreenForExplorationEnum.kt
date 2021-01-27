cfab6c6c5f0515278206cd7a677ba168f22ada92
Rajat Talesra <talesra@google.com>
2021-01-27 18:55:26 +0530

    Removed brackets

--- app/src/main/java/org/oppia/android/app/player/exploration/ParentScreenForExplorationEnum.kt
+++ app/src/main/java/org/oppia/android/app/player/exploration/ParentScreenForExplorationEnum.kt
@@ -1,7 +1,7 @@
 package org.oppia.android.app.player.exploration
 
 /** Enum to decide when an exploration is closed which activity should be displayed. */
-enum class ParentScreenForExplorationEnum() {
+enum class ParentScreenForExplorationEnum {
   TOPIC_INFO,
   TOPIC_LESSONS,
   STORY;
