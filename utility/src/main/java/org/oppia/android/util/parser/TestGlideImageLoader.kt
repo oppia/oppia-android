a400eb72af606ac0088df452f986f963ebbe8dfd
Viktoriia Schwartz <viktoriia@google.com>
2021-01-28 19:06:15 -0800

    Nits

--- utility/src/main/java/org/oppia/android/util/parser/TestGlideImageLoader.kt
+++ utility/src/main/java/org/oppia/android/util/parser/TestGlideImageLoader.kt
@@ -1,44 +1,33 @@
 package org.oppia.android.util.parser
 
-import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.graphics.drawable.PictureDrawable
-import org.oppia.android.util.caching.AssetRepository
-import org.oppia.android.util.caching.CacheAssetsLocally
 import javax.inject.Inject
 
-/** [TestGlideImageLoader] is designed to be used in tests. It uses real [GlideImageLoader]
+/**
+ * [TestGlideImageLoader] is designed to be used in tests. It uses real [GlideImageLoader]
  * except for [loadDrawable]. [loadDrawable] function is overridden to work with drawable matchers
  * in unit tests.
  */
 class TestGlideImageLoader @Inject constructor(
-  context: Context,
-  @CacheAssetsLocally private val cacheAssetsLocally: Boolean,
-  assetRepository: AssetRepository
+  private val glideImageLoader: GlideImageLoader
 ) : ImageLoader {
-
-  private val glideImageLoader by lazy {
-    GlideImageLoader(context, cacheAssetsLocally, assetRepository)
-  }
 
   override fun loadBitmap(
     imageUrl: String,
     target: ImageTarget<Bitmap>,
     transformations: List<ImageTransformation>
-  ) {
-    glideImageLoader.loadBitmap(imageUrl, target, transformations)
-  }
+  ) = glideImageLoader.loadBitmap(imageUrl, target, transformations)
 
   override fun loadSvg(
     imageUrl: String,
     target: ImageTarget<PictureDrawable>,
     transformations: List<ImageTransformation>
-  ) {
-    glideImageLoader.loadSvg(imageUrl, target, transformations)
-  }
+  ) = glideImageLoader.loadSvg(imageUrl, target, transformations)
 
-  /** [loadDrawable] can be used in tests to match drawable ids:
+  /**
+   * [loadDrawable] can be used in tests to match drawable ids:
    * `matches(withDrawable([imageDrawableResId]))`.
    *
    * Real [loadDrawable] in [GlideImageLoader] cannot be tested using such drawable matchers.
