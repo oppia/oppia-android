6341f28085647a089f8fdd777e8b2a942b214ac8
anandwana001 <akshaynandwana001@gmail.com>
2021-02-04 14:11:36 +0530

    make actions use scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -25,8 +25,4 @@ chmod a+x ktlint
 echo Ktlint file downloaded
 
 # Download checkstyle
-CHECKSTYLE="8.37"
-echo Using Checkstyle version $CHECKSTYLE
-curl -sSLOC - https://github.com/checkstyle/checkstyle/releases/download/checkstyle-$CHECKSTYLE/checkstyle-$CHECKSTYLE-all.jar
-chmod a+x checkstyle-$CHECKSTYLE-all.jar
-echo Checkstyle file downloaded
+bash scripts/download_checkstyle.sh
