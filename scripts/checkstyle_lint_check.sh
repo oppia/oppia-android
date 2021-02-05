f7a351b27b0f97e3766de4f7f446946ba9e4fd3c
anandwana001 <akshaynandwana001@gmail.com>
2021-02-05 10:55:03 +0530

    fix for ktlint

--- scripts/checkstyle_lint_check.sh
+++ scripts/checkstyle_lint_check.sh
@@ -5,15 +5,21 @@ echo "Checking Java file formatting"
 echo "********************************"
 
 github_actions_path=$1
+
 jar_file_path=$?
+
 if [ $# -eq 0 ]; then
     jar_file_path="../oppia-android-tools/checkstyle-8.37-all.jar"
 else
     jar_file_path="$github_actions_path/oppia-android-tools/checkstyle-8.37-all.jar"
 fi
+
 lint_results=$(java -jar $jar_file_path -c /google_checks.xml app/src/ data/src/ domain/src/ utility/src/ testing/src/ 2>&1)
+
 lint_command_result=$?
+
 echo $lint_results
+
 if [ "$lint_command_result" -ne 0 ] || [ -z "$lint_results" ] || [[ ${lint_results} == *"[WARN]"* ]]; then
   # Assume any lint output or non-zero exit code is a failure.
   echo "********************************"
