a0eb27eb16ad0549e5c3959729ab630807949c9c
prayutsu <gargabhay2000@gmail.com>
2021-02-09 22:33:13 +0530

    Fix lint.

--- app/src/main/java/org/oppia/android/app/drawer/NavigationDrawerFragmentPresenter.kt
+++ app/src/main/java/org/oppia/android/app/drawer/NavigationDrawerFragmentPresenter.kt
@@ -21,7 +21,6 @@ import org.oppia.android.app.help.HelpActivity
 import org.oppia.android.app.home.HomeActivity
 import org.oppia.android.app.model.CompletedStoryList
 import org.oppia.android.app.model.OngoingTopicList
-import org.oppia.android.app.model.OppiaLogger
 import org.oppia.android.app.model.Profile
 import org.oppia.android.app.model.ProfileId
 import org.oppia.android.app.mydownloads.MyDownloadsActivity
