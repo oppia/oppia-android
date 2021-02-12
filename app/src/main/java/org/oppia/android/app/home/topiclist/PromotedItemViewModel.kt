aeed1b0fd7e38be49531a82c647dae7fe0478610
veena14cs <veena14cs@gmail.com>
2021-02-13 00:35:41 +0530

    fixed unrelated file changes

--- app/src/main/java/org/oppia/android/app/home/topiclist/PromotedItemViewModel.kt
+++
@@ -1,6 +0,0 @@
-package org.oppia.android.app.home.topiclist
-
-import org.oppia.android.app.viewmodel.ObservableViewModel
-
-/** The root [ViewModel] for all individual items that may be displayed in home fragment recycler view. */
-abstract class PromotedItemViewModel : ObservableViewModel()
