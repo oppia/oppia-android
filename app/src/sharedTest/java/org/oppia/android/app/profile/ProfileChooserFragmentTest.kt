a29fbcd675b90ff23a3b788c0f9dc8b4a552e69d
Rajat Talesra <talesra@google.com>
2021-02-19 12:58:16 +0530

    Renamed string

--- app/src/sharedTest/java/org/oppia/android/app/profile/ProfileChooserFragmentTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/profile/ProfileChooserFragmentTest.kt
@@ -128,7 +128,7 @@ class ProfileChooserFragmentTest {
     val title = activityTestRule.activity.title
     // Verify that the activity label is correct as a proxy to verify TalkBack will announce the
     // correct string when it's read out.
-    assertThat(title).isEqualTo(context.getString(R.string.profile_chooser_label))
+    assertThat(title).isEqualTo(context.getString(R.string.profile_chooser_activity_label))
   }
 
   @Test
