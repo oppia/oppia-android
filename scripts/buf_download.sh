0d10a4bdbd5e669a98c62c65708511df858a7024
anandwana001 <akshaynandwana001@gmail.com>
2021-02-24 14:51:53 +0530

    os check while downloading buf

--- scripts/buf_download.sh
+++ scripts/buf_download.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
 # Download buf
-BUFVERSION="v0.37.1"
-echo Using Bufbuild version $BUFVERSION
-curl -sSLOC - https://github.com/bufbuild/buf/releases/download/$BUFVERSION/buf-$(uname -s)-$(uname -m)
-chmod a+x buf-$(uname -s)-$(uname -m)
-echo Buf file downloaded
+download_buf() {
+  BUFVERSION="v0.37.1"
+  echo Using Bufbuild version $BUFVERSION
+  curl -sSLOC - https://github.com/bufbuild/buf/releases/download/$BUFVERSION/buf-$(uname -s)-$(uname -m)
+  chmod a+x buf-$(uname -s)-$(uname -m)
+  echo Buf file downloaded
+}
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  download_buf
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  download_buf
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+  download_buf
+else
+  echo "Protobuf lint check not available on Windows"
+fi
