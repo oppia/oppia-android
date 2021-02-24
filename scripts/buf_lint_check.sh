50ee433cf8285b7e06d0282dcb2cf4102f71a234
anandwana001 <akshaynandwana001@gmail.com>
2021-02-24 11:37:00 +0530

    nit fix

--- scripts/buf_lint_check.sh
+++ scripts/buf_lint_check.sh
@@ -1,34 +1,14 @@
 #!/bin/bash
 
+jar_file_path=$?
+config_file_path=$?
+os_type=$?
+github_actions_path=$1
+
 check_protobuf() {
   echo "********************************"
   echo "Checking Protobuf file formatting"
   echo "********************************"
-
-  github_actions_path=$1
-
-  os_type=$2
-
-  buf_file_name=$?
-
-  if [ "$os_type" = "Linux" ]; then
-    buf_file_name="buf-Linux-x86_64"
-  else
-    buf_file_name="buf-Darwin-x86_64"
-  fi
-
-  jar_file_path=$?
-
-  config_file_path=$?
-
-  # $0 means the script runs without any params
-  if [ $0 ]; then
-      jar_file_path="../oppia-android-tools/$buf_file_name"
-      config_file_path="buf.yaml"
-  else
-      jar_file_path="$github_actions_path/oppia-android-tools/$buf_file_name"
-      config_file_path="$github_actions_path/oppia-android/buf.yml"
-  fi
 
   $jar_file_path lint --config $config_file_path
 
@@ -43,7 +23,7 @@ check_protobuf() {
     echo "Please fix the above issues."
     echo "********************************"
     exit 1
-fi
+  fi
 }
 
 windows_warning() {
@@ -52,15 +32,46 @@ windows_warning() {
   echo "********************************"
 }
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]
-then
-  check_protobuf $1 Linux
-elif [[ "$OSTYPE" == "darwin"* ]]
-then
-  check_protobuf $1 Darwin
-elif [[ "$OSTYPE" == "freebsd"* ]]
-then
-  check_protobuf $1 Linux
-else
-  windows_warning
-fi
+create_jar_file_path() {
+  buf_file_name=$?
+
+  if [ "$1" = "Linux" ]; then
+    buf_file_name="buf-Linux-x86_64"
+  else
+    buf_file_name="buf-Darwin-x86_64"
+  fi
+
+  if [ $github_actions_path ]; then
+    jar_file_path="$github_actions_path/oppia-android-tools/$buf_file_name"
+    config_file_path="$github_actions_path/oppia-android/buf.yaml"
+  else
+    jar_file_path="../oppia-android-tools/$buf_file_name"
+    config_file_path="buf.yaml"
+  fi
+}
+
+check_github_action_path_exists() {
+  if [ $github_actions_path ]; then
+    create_jar_file_path $os_type $github_actions_path
+  else
+    create_jar_file_path $os_type
+  fi
+}
+
+check_os_type() {
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    os_type="Linux"
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    os_type="Darwin"
+  elif [[ "$OSTYPE" == "freebsd"* ]]; then
+    os_type="Linux"
+  else
+    windows_warning
+  fi
+}
+
+check_os_type
+
+check_github_action_path_exists $github_actions_path
+
+check_protobuf
