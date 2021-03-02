d344be22e28999844c46d13a8b3e648c294a2d29
Jacque Li <jacqueli@google.com>
2021-03-01 18:19:58 -0800

    Finish adding all data class objects

--- data/src/main/java/org/oppia/android/data/backends/gae/model/GaeFeedbackReportingAppInfo.kt
+++ data/src/main/java/org/oppia/android/data/backends/gae/model/GaeFeedbackReportingAppInfo.kt
@@ -11,11 +11,12 @@ import com.squareup.moshi.JsonClass
 data class GaeFeedbackReportingAppInfo(
 
   @Json(name = "entry_point") val entry_point: String?,
-  @Json(name = "topic_progress") val topic_progress: Map<String, GaeTopic>?,
+  // Maps the topic ID to a string representing the progress status of that topic.
+  @Json(name = "topic_progress") val topic_progress: Map<String, String>?,
   @Json(name = "text_size") val text_size: String?,
   @Json(name = "text_lang") val text_lang: String?,
   @Json(name = "audio_lang") val audio_lang: String?,
-  @Json(name = "download_and_update_only_on_wifi") val download_and_update_only_on_wifi: Boolean?,,
+  @Json(name = "download_and_update_only_on_wifi") val download_and_update_only_on_wifi: Boolean?,
   @Json(name = "automatically_update_topics") val automatically_update_topics: Boolean?,
   @Json(name = "is_admin") val is_admin: Boolean?,
 
