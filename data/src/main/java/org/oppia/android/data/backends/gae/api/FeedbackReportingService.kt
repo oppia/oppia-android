8f674b5e6a2bbc8b4ae2e01085c286b9ed37b693
Jacque Li <jacqueli@google.com>
2021-03-02 14:39:22 -0800

    Create service mock and fake JSON data for testing

--- data/src/main/java/org/oppia/android/data/backends/gae/api/FeedbackReportingService.kt
+++ data/src/main/java/org/oppia/android/data/backends/gae/api/FeedbackReportingService.kt
@@ -1,13 +1,16 @@
 package org.oppia.android.data.backends.gae.api
 
+import org.oppia.android.app.model.FeedbackReport
 import org.oppia.android.data.backends.gae.model.GaeFeedbackReport
 import retrofit2.Call
 import retrofit2.http.POST
 import retrofit2.http.Body
 
-/* Service that pushes data to access to feedback-reporting remote storage. */
+/**
+ * Service that posts data to feedback-reporting remote storage, receiving the data class
+ * representing the report on success. */
 interface FeedbackReportingService {
   @POST("feedbackreporting/incomingreport")
-  fun postFeedbackReport(@Body report: GaeFeedbackReport)
+  fun postFeedbackReport(@Body report: FeedbackReport)
   : Call<GaeFeedbackReport>
 }
