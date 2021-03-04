7ec875bbcc6832c47278a42230cddb39b40567e2
akshat <akshatgour17@gmail.com>
2021-03-04 15:22:25 +0530

    Added kdoc for the functions and class

--- testing/src/main/java/org/oppia/android/testing/TextInputAction.kt
+++ testing/src/main/java/org/oppia/android/testing/TextInputAction.kt
@@ -10,16 +10,17 @@ import org.hamcrest.TypeSafeMatcher
 import javax.inject.Inject
 
 /**
- * Action for accessing HasErrorTest and HasNoErrorTest Functions.
- *
- * This is needed because Robolectric doesn't seem to properly input digits for text views using
- * 'android:digits' or other filters. See https://github.com/robolectric/robolectric/issues/5110
- * for specifics.
+ * Action for matching error text and performing other actions on TextInputLayout in a test infrastructure-specific way.
  */
 class TextInputAction @Inject constructor(
   val context: Context
 ) {
   // TODO(#1720): Move this to a companion object & use a test-only singleton injector to retrieve
+  /**
+   * @param expectedErrorTextId is the string id which needs to match.
+   *
+   * @returns a [Matcher] that matches the specified string [expectedErrorTextId] with the view's error text.
+   */
   fun hasErrorText(@StringRes expectedErrorTextId: Int): Matcher<View> {
     return object : TypeSafeMatcher<View>() {
       override fun matchesSafely(view: View): Boolean {
@@ -33,6 +34,9 @@ class TextInputAction @Inject constructor(
     }
   }
 
+  /**
+   * @returns a [Matcher] that matches the view has no error text available.
+   */
   fun hasNoErrorText(): Matcher<View> {
     return object : TypeSafeMatcher<View>() {
       override fun matchesSafely(view: View): Boolean {
