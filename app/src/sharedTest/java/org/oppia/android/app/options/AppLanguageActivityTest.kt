bf18d5a9fe8237f656912995094b60600bfe13ab
Abhinav <abhi2000.rtk@gmail.com>
2021-03-09 20:21:09 +0530

    Fixed formatting issues

--- app/src/sharedTest/java/org/oppia/android/app/options/AppLanguageActivityTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/options/AppLanguageActivityTest.kt
@@ -44,10 +44,8 @@ import org.oppia.android.domain.question.QuestionModule
 import org.oppia.android.domain.topic.PrimeTopicAssetsControllerModule
 import org.oppia.android.testing.RobolectricModule
 import org.oppia.android.testing.TestAccessibilityModule
-import org.oppia.android.testing.TestCoroutineDispatchers
 import org.oppia.android.testing.TestDispatcherModule
 import org.oppia.android.testing.TestLogReportingModule
-import org.oppia.android.testing.profile.ProfileTestHelper
 import org.oppia.android.testing.time.FakeOppiaClockModule
 import org.oppia.android.util.caching.testing.CachingTestModule
 import org.oppia.android.util.gcsresource.GcsResourceModule
