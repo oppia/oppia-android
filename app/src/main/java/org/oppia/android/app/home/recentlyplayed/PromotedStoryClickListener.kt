2c476169cc275f992f57f008d62a5bbdf0be453b
priyanka0906 <priyanka02amb@rediffmail.com>
2021-03-09 09:15:12 +0530

    Renamed from ongoingStory to promotedStory

--- app/src/main/java/org/oppia/android/app/home/recentlyplayed/PromotedStoryClickListener.kt
+++ app/src/main/java/org/oppia/android/app/home/recentlyplayed/PromotedStoryClickListener.kt
@@ -2,7 +2,7 @@ package org.oppia.android.app.home.recentlyplayed
 
 import org.oppia.android.app.model.PromotedStory
 
-/** Listener interface for when promoted story is clicked in the UI. */
+/** Listener interface for when a promoted story in the [RecentlyPlayedFragment] is clicked in the UI. */
 interface PromotedStoryClickListener {
   fun onPromotedStoryClicked(promotedStory: PromotedStory)
 }
