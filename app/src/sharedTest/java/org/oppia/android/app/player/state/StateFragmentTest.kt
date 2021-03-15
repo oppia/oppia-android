8383b0946cc237154f1247895cff3161365c9baf
Khushal Uttam <khushaluttam@gmail.com>
2021-03-15 14:05:01 +0530

    Renamed test in StateFragmentTest

--- app/src/sharedTest/java/org/oppia/android/app/player/state/StateFragmentTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/player/state/StateFragmentTest.kt
@@ -210,7 +210,7 @@ class StateFragmentTest {
 
   //  3. Testing providing the wrong answer and showing feedback and the same question again.
   @Test
-  fun testStateFragment_loadExp_secondState_submitWrongAnswer_feedbackAndQuestionVisible() {
+  fun testStateFragment_loadExp_secondState_submitWrongAnswer_feedbackAndQuestionIsVisible() {
     launchForExploration(TEST_EXPLORATION_ID_2).use {
       startPlayingExploration()
       clickContinueInteractionButton()
@@ -227,7 +227,7 @@ class StateFragmentTest {
   }
 
   @Test
-  fun testStateFragment_loadExp_secondState_submitWrongAnswerTwice_feedbackAndQuestionVisible() {
+  fun testStateFragment_loadExp_secondState_submitWrongAnswerTwice_feedbackAndQuestionIsVisible() {
     launchForExploration(TEST_EXPLORATION_ID_2).use {
       startPlayingExploration()
       clickContinueInteractionButton()
@@ -252,7 +252,7 @@ class StateFragmentTest {
 
   @Test
   @Ignore("Work on issue regarding this test is in progress")
-  fun testStateFragment_loadExp_submitAnswer_changeConfiguration_explorationContinues() {
+  fun testStateFragment_loadExp_submitAnswer_configChange_explorationContinues() {
     launchForExploration(TEST_EXPLORATION_ID_2).use {
       startPlayingExploration()
       clickContinueInteractionButton()
@@ -270,7 +270,7 @@ class StateFragmentTest {
   //  5. Configuration change after submitting the wrong answer to show that the old answer & re-ask
   //     of the question stay the same.
   @Test
-  fun testStateFragment_loadExp_submitWrongAnswer_changeConfiguration_feedbackAndQuestionVisible() {
+  fun testStateFragment_loadExp_submitWrongAnswer_configChange_feedbackAndQuestionVisible() {
     launchForExploration(TEST_EXPLORATION_ID_2).use {
       startPlayingExploration()
       clickContinueInteractionButton()
@@ -290,7 +290,7 @@ class StateFragmentTest {
   //  6. Backward/forward navigation along with configuration changes to verify that you stay on the
   //     navigated state.
   @Test
-  fun testStateFragment_loadExp_moveForward_changeConfiguration_stayOnTheNavigatedState() {
+  fun testStateFragment_loadExp_moveForward_configChange_stayOnTheNavigatedState() {
     launchForExploration(TEST_EXPLORATION_ID_2).use {
       startPlayingExploration()
       clickContinueInteractionButton()
