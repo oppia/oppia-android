dcd590e53da40031ba06c6f6cd16ea86b8b96f6d
mrwhoknows55 <avdhuttanugade55@gmail.com>
2021-03-16 00:07:15 +0530

    Fix lint issue

--- app/src/sharedTest/java/org/oppia/android/app/profile/AdminAuthActivityTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/profile/AdminAuthActivityTest.kt
@@ -633,7 +633,9 @@ class AdminAuthActivityTest {
 
     // Verify that the activity label is correct as a proxy to verify TalkBack will announce the
     // correct string when it's read out.
-    assertThat(title).isEqualTo(context.getString(R.string.admin_auth_activity_access_controls_title))
+    assertThat(title).isEqualTo(
+      context.getString(R.string.admin_auth_activity_access_controls_title)
+    )
   }
 
   @Test
