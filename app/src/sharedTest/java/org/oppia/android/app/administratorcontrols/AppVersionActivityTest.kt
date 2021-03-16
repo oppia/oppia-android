0acfa72e2e7e01be93fb8472db2f254484237523
Amarnath K <amarnathk5674@gmail.com>
2021-03-16 23:12:24 +0530

    correction in test

--- app/src/sharedTest/java/org/oppia/android/app/administratorcontrols/AppVersionActivityTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/administratorcontrols/AppVersionActivityTest.kt
@@ -116,7 +116,7 @@ class AppVersionActivityTest {
 
     // Verify that the activity label is correct as a proxy to verify TalkBack will announce the
     // correct string when it's read out.
-    assertThat(title).isEqualTo(context.getString(R.string.faq_activity_label))
+    assertThat(title).isEqualTo(context.getString(R.string.app_version_activity_label))
   }
 
   @After
