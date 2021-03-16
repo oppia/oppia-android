2b40d742656f2357cb5a05083114625d1fad8598
justdvnsh <justdvnsh2208@gmail.com>
2021-03-16 15:58:59 +0530

    added label for profile rename activity

--- app/src/sharedTest/java/org/oppia/android/app/settings/profile/ProfileRenameActivityTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/settings/profile/ProfileRenameActivityTest.kt
@@ -22,7 +22,9 @@ import androidx.test.espresso.matcher.ViewMatchers.isRoot
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
 import com.google.android.material.textfield.TextInputLayout
+import com.google.common.truth.Truth.assertThat
 import dagger.Component
 import org.hamcrest.CoreMatchers.allOf
 import org.hamcrest.CoreMatchers.not
@@ -31,6 +33,7 @@ import org.hamcrest.Matcher
 import org.hamcrest.TypeSafeMatcher
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.oppia.android.R
@@ -102,6 +105,11 @@ class ProfileRenameActivityTest {
   @Inject
   lateinit var editTextInputAction: EditTextInputAction
 
+  @get:Rule
+  val activityTestRule: ActivityTestRule<ProfileRenameActivity> = ActivityTestRule(
+    ProfileRenameActivity::class.java, true, false
+  )
+
   @Before
   fun setUp() {
     Intents.init()
@@ -116,6 +124,21 @@ class ProfileRenameActivityTest {
 
   private fun setUpTestApplicationComponent() {
     ApplicationProvider.getApplicationContext<TestApplication>().inject(this)
+  }
+
+  @Test
+  fun testProfileRenameActivity_hasCorrectActivityLabel() {
+    activityTestRule.launchActivity(
+      ProfileRenameActivity.createProfileRenameActivity(
+        context = context,
+        profileId = 1
+      )
+    )
+
+    val title = activityTestRule.activity.title
+    assertThat(title).isEqualTo(
+      context.getString(R.string.profile_rename_activity_label)
+    )
   }
 
   @Test
