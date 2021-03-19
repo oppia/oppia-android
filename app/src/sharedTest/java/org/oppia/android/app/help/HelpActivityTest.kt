9aa64d59c42460e5ea3c8a7970ff2664b84b9275
Saksham Pruthi <saksham.0804@gmail.com>
2021-03-19 19:29:32 +0530

    created help_activity_title for label

--- app/src/sharedTest/java/org/oppia/android/app/help/HelpActivityTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/help/HelpActivityTest.kt
@@ -83,7 +83,7 @@ class HelpActivityTest {
 
     // Verify that the activity label is correct as a proxy to verify TalkBack will announce the
     // correct string when it's read out.
-    assertThat(title).isEqualTo(context.getString(R.string.menu_help))
+    assertThat(title).isEqualTo(context.getString(R.string.help_activity_title))
   }
 
   private fun setUpTestApplicationComponent() {
