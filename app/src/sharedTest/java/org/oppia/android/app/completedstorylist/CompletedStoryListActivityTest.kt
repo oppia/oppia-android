fa10ebb69252fb1b6b6fc40855c1f9bf1c03cdf7
Chirag Kalra <arlakgarihc@gmail.com>
2021-03-19 11:12:40 +0530

    Applied Suggested Changes

--- app/src/sharedTest/java/org/oppia/android/app/completedstorylist/CompletedStoryListActivityTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/completedstorylist/CompletedStoryListActivityTest.kt
@@ -20,10 +20,13 @@ import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import com.google.common.truth.Truth.assertThat
 import dagger.Component
 import org.hamcrest.CoreMatchers.containsString
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.oppia.android.R
@@ -91,6 +94,11 @@ class CompletedStoryListActivityTest {
   private val internalProfileId = 0
   private val profileId = ProfileId.newBuilder().setInternalId(internalProfileId).build()
 
+  @get:Rule
+  val activityTestRule: ActivityTestRule<CompletedStoryListActivity> = ActivityTestRule(
+    CompletedStoryListActivity::class.java
+  )
+
   @Inject
   lateinit var testCoroutineDispatchers: TestCoroutineDispatchers
 
@@ -128,6 +136,23 @@ class CompletedStoryListActivityTest {
 
   private fun setUpTestApplicationComponent() {
     ApplicationProvider.getApplicationContext<TestApplication>().inject(this)
+  }
+
+  private fun createFAQListActivityIntent(): Intent {
+    return CompletedStoryListActivity.createCompletedStoryListActivityIntent(
+      ApplicationProvider.getApplicationContext(),
+      internalProfileId
+    )
+  }
+
+  @Test
+  fun testFAQListActivity_hasCorrectActivityLabel() {
+    activityTestRule.launchActivity(createFAQListActivityIntent())
+    val title = activityTestRule.activity.title
+
+    // Verify that the activity label is correct as a proxy to verify TalkBack will announce the
+    // correct string when it's read out.
+    assertThat(title).isEqualTo(context.getString(R.string.completed_story_list_activity_title))
   }
 
   @Test
