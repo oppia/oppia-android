32903ec2472ed630e65c1de1396ec5e65904e4a5
Ben Henning <henning.benmax@gmail.com>
2021-03-22 13:29:15 -0700

    Lint fixes.

--- utility/src/test/java/org/oppia/android/util/parser/CustomHtmlContentHandlerTest.kt
+++ utility/src/test/java/org/oppia/android/util/parser/CustomHtmlContentHandlerTest.kt
@@ -218,5 +218,5 @@ class CustomHtmlContentHandlerTest {
    * A fake image retriever that satisifies both the contracts of [Html.ImageGetter] and
    * [CustomHtmlContentHandler.ImageRetriever].
    */
-  interface FakeImageRetriever: Html.ImageGetter, CustomHtmlContentHandler.ImageRetriever
+  interface FakeImageRetriever : Html.ImageGetter, CustomHtmlContentHandler.ImageRetriever
 }
