63e03534a47c292b31c37583b34b3ea7eaa8a4d8
Ben Henning <henning.benmax@gmail.com>
2021-03-23 00:31:02 -0700

    Fix blurring for SVGs.
    SVGs did not previous support blurring, & this change introduces a way
    to leverage the existing bitmap blurrer to introduce blurring
    functionality in a way that's consistent with bitmap thumbnails.

--- utility/src/main/java/org/oppia/android/util/parser/SvgPictureDrawable.kt
+++ utility/src/main/java/org/oppia/android/util/parser/SvgPictureDrawable.kt
@@ -1,7 +1,9 @@
 package org.oppia.android.util.parser
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Paint
 import android.graphics.Picture
 import android.text.TextPaint
 
@@ -10,7 +12,16 @@ open class SvgPictureDrawable(
   context: Context,
   private val oppiaSvg: OppiaSvg
 ) : TransformablePictureDrawable() {
+  private val bitmapBlurrer by lazy { BitmapBlurrer(context) }
+
+  /**
+   * The [Paint] that should be used when rendering this drawable. Note that the flags are based
+   * on the defaults set by Android's BitmapDrawable.
+   */
+  private val bitmapPaint by lazy { Paint(Paint.FILTER_BITMAP_FLAG or Paint.DITHER_FLAG) }
+
   private var picture: Picture? = null
+  private var bitmap: Bitmap? = null
   private var intrinsicSize = IntrinsicSize(width = -1f, height = -1f)
 
   override fun draw(canvas: Canvas) {
@@ -19,14 +30,20 @@ open class SvgPictureDrawable(
       // Save current transformation state.
       save()
 
-      picture?.let { picture ->
-        // Apply the picture's bounds so that it's positioned/clipped correctly.
-        bounds.apply {
-          clipRect(this)
-          translate(left.toFloat(), top.toFloat())
+      if (oppiaSvg.hasTransformations()) {
+        bitmap?.let { bitmap ->
+          drawBitmap(bitmap, /* src= */ null, bounds, bitmapPaint)
         }
+      } else {
+        picture?.let { picture ->
+          // Apply the picture's bounds so that it's positioned/clipped correctly.
+          bounds.apply {
+            clipRect(this)
+            translate(left.toFloat(), top.toFloat())
+          }
 
-        drawPicture(picture)
+          drawPicture(picture)
+        }
       }
 
       // Restore previous transformation state.
@@ -40,11 +57,13 @@ open class SvgPictureDrawable(
   override fun computeBlockPicture() {
     picture = oppiaSvg.renderToBlockPicture()
     recomputeIntrinsicSize { oppiaSvg.computeSizeSpecs(textPaint = null) }
+    if (oppiaSvg.hasTransformations()) recomputeBitmap()
   }
 
   override fun computeTextPicture(textPaint: TextPaint) {
     picture = oppiaSvg.renderToTextPicture(textPaint)
     recomputeIntrinsicSize { oppiaSvg.computeSizeSpecs(textPaint) }
+    if (oppiaSvg.hasTransformations()) recomputeBitmap()
   }
 
   override fun getIntrinsicSize(): IntrinsicSize = intrinsicSize
@@ -53,4 +72,30 @@ open class SvgPictureDrawable(
     val (width, height, verticalAlignment) = computeSizeSpecs()
     intrinsicSize = IntrinsicSize(width, height, verticalAlignment)
   }
+
+  private fun recomputeBitmap() {
+    bitmap = picture?.let { picture ->
+      val renderedPictureBitmap =
+        Bitmap.createBitmap(intrinsicWidth, intrinsicHeight, Bitmap.Config.ARGB_8888)
+      val canvas = Canvas(renderedPictureBitmap)
+      canvas.drawPicture(picture)
+      return@let transformBitmap(renderedPictureBitmap)
+    }
+  }
+
+  private fun transformBitmap(bitmap: Bitmap): Bitmap {
+    var transformedBitmap = bitmap
+    for (imageTransformation in oppiaSvg.transformations) {
+      transformedBitmap = transformBitmap(transformedBitmap, imageTransformation)
+    }
+    return transformedBitmap
+  }
+
+  private fun transformBitmap(bitmap: Bitmap, imageTransformation: ImageTransformation): Bitmap {
+    return when (imageTransformation) {
+      ImageTransformation.BLUR -> bitmapBlurrer.blur(bitmap)
+    }
+  }
 }
+
+private fun OppiaSvg.hasTransformations() = transformations.isNotEmpty()
