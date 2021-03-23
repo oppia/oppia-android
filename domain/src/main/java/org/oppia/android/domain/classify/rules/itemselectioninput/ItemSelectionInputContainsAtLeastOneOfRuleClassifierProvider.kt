cf02eda758f1d5da517525ea862bfbf4fcbce58c
Ben Henning <henning.benmax@gmail.com>
2021-03-23 00:24:06 -0700

    Fix interactions to support new localization.
    This fixes answer classification, submission, and viewing for drag &
    drop, item selection, multiple choice, and text input interactions per
    changes in the states structure to support lesson localization.

--- domain/src/main/java/org/oppia/android/domain/classify/rules/itemselectioninput/ItemSelectionInputContainsAtLeastOneOfRuleClassifierProvider.kt
+++ domain/src/main/java/org/oppia/android/domain/classify/rules/itemselectioninput/ItemSelectionInputContainsAtLeastOneOfRuleClassifierProvider.kt
@@ -1,32 +1,35 @@
 package org.oppia.android.domain.classify.rules.itemselectioninput
 
 import org.oppia.android.app.model.InteractionObject
-import org.oppia.android.app.model.StringList
+import org.oppia.android.app.model.SetOfTranslatableHtmlContentIds
 import org.oppia.android.domain.classify.RuleClassifier
 import org.oppia.android.domain.classify.rules.GenericRuleClassifier
 import org.oppia.android.domain.classify.rules.RuleClassifierProvider
+import org.oppia.android.domain.util.getContentIdSet
 import javax.inject.Inject
 
 /**
- * Provider for a classifier that determines whether an item selection answer contains at least one of a set of options
- * per the item selection input interaction.
+ * Provider for a classifier that determines whether an item selection answer contains at least one
+ * of a set of options per the item selection input interaction.
  *
  * https://github.com/oppia/oppia/blob/37285a/extensions/interactions/ItemSelectionInput/directives/item-selection-input-rules.service.ts#L32
  */
 // TODO(#1580): Re-restrict access using Bazel visibilities
 class ItemSelectionInputContainsAtLeastOneOfRuleClassifierProvider @Inject constructor(
   private val classifierFactory: GenericRuleClassifier.Factory
-) : RuleClassifierProvider, GenericRuleClassifier.SingleInputMatcher<StringList> {
+) : RuleClassifierProvider,
+  GenericRuleClassifier.SingleInputMatcher<SetOfTranslatableHtmlContentIds> {
 
   override fun createRuleClassifier(): RuleClassifier {
     return classifierFactory.createSingleInputClassifier(
-      InteractionObject.ObjectTypeCase.SET_OF_HTML_STRING,
+      InteractionObject.ObjectTypeCase.SET_OF_TRANSLATABLE_HTML_CONTENT_IDS,
       "x",
       this
     )
   }
 
-  override fun matches(answer: StringList, input: StringList): Boolean {
-    return answer.htmlList.toSet().intersect(input.htmlList).isNotEmpty()
-  }
+  override fun matches(
+    answer: SetOfTranslatableHtmlContentIds,
+    input: SetOfTranslatableHtmlContentIds
+  ): Boolean = answer.getContentIdSet().intersect(input.getContentIdSet()).isNotEmpty()
 }
--- domain/src/main/java/org/oppia/android/domain/classify/rules/textinput/TextInputContainsRuleClassifierProvider.kt
+++ domain/src/main/java/org/oppia/android/domain/classify/rules/textinput/TextInputContainsRuleClassifierProvider.kt
@@ -1,6 +1,7 @@
 package org.oppia.android.domain.classify.rules.textinput
 
 import org.oppia.android.app.model.InteractionObject
+import org.oppia.android.app.model.TranslatableSetOfNormalizedString
 import org.oppia.android.domain.classify.RuleClassifier
 import org.oppia.android.domain.classify.rules.GenericRuleClassifier
 import org.oppia.android.domain.classify.rules.RuleClassifierProvider
@@ -8,24 +9,28 @@ import org.oppia.android.domain.util.normalizeWhitespace
 import javax.inject.Inject
 
 /**
- * Provider for a classifier that determines whether an answer contains the rule's input per the text input interaction.
+ * Provider for a classifier that determines whether an answer contains the rule's input per the
+ * text input interaction.
  *
  * https://github.com/oppia/oppia/blob/37285a/extensions/interactions/TextInput/directives/text-input-rules.service.ts#L70
  */
 // TODO(#1580): Re-restrict access using Bazel visibilities
 class TextInputContainsRuleClassifierProvider @Inject constructor(
   private val classifierFactory: GenericRuleClassifier.Factory
-) : RuleClassifierProvider, GenericRuleClassifier.SingleInputMatcher<String> {
+) : RuleClassifierProvider,
+  GenericRuleClassifier.MultiTypeSingleInputMatcher<String, TranslatableSetOfNormalizedString> {
 
   override fun createRuleClassifier(): RuleClassifier {
-    return classifierFactory.createSingleInputClassifier(
+    return classifierFactory.createMultiTypeSingleInputClassifier(
       InteractionObject.ObjectTypeCase.NORMALIZED_STRING,
+      InteractionObject.ObjectTypeCase.TRANSLATABLE_SET_OF_NORMALIZED_STRING,
       "x",
       this
     )
   }
 
-  override fun matches(answer: String, input: String): Boolean {
-    return answer.normalizeWhitespace().contains(input.normalizeWhitespace())
+  override fun matches(answer: String, input: TranslatableSetOfNormalizedString): Boolean {
+    val normalizedAnswer = answer.normalizeWhitespace()
+    return input.normalizedStringsList.any { it.normalizeWhitespace().contains(normalizedAnswer) }
   }
 }
