1d9b29ac551fd859703d59acd8140e9c49fdb521
jkugsiya <jkugsiya11@gmail.com>
2021-03-24 17:29:14 +0530

    added label for ongoingTopicListActivity

--- app/src/sharedTest/java/org/oppia/android/app/ongoingtopiclist/OngoingTopicListActivityTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/ongoingtopiclist/OngoingTopicListActivityTest.kt
@@ -19,10 +19,13 @@ import androidx.test.espresso.matcher.ViewMatchers.isRoot
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import com.google.common.truth.Truth.assertThat
 import dagger.Component
 import org.hamcrest.CoreMatchers.containsString
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.oppia.android.R
@@ -94,8 +97,25 @@ class OngoingTopicListActivityTest {
   @Inject
   lateinit var storyProfileTestHelper: StoryProgressTestHelper
 
+
+  @get:Rule
+  val activityTestRule = ActivityTestRule(
+    OngoingTopicListActivity::class.java,
+    /*initialTouchMode= */ true,
+    /* launchActivity= */ false
+  )
+
   @Inject
   lateinit var testCoroutineDispatchers: TestCoroutineDispatchers
+
+  fun testCompletedOngoingTopicList_hasCorrectActivityLabel() {
+    activityTestRule.launchActivity(createOngoingTopicListActivityIntent(internalProfileId))
+    val title = activityTestRule.activity.title
+    // Verify that the activity label is correct as a proxy to verify TalkBack will announce the
+    // correct string when it's read out.
+    assertThat(title).isEqualTo(context.getString(R.string.ongoing_topic_activity_label))
+  }
+  }
 
   @Inject
   lateinit var fakeOppiaClock: FakeOppiaClock
