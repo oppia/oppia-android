16e7aba7b07a23b0ed6c41526c9f5c01cac78723
Jacque Li <jacqueli@google.com>
2021-03-25 09:14:00 -0700

    Use spinner for dropdown

--- app/src/main/java/org/oppia/android/app/feedbackreporting/FeedbackReportingEntryFragmentPresenter.kt
+++ app/src/main/java/org/oppia/android/app/feedbackreporting/FeedbackReportingEntryFragmentPresenter.kt
@@ -1,12 +1,13 @@
 package org.oppia.android.app.feedbackreporting
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.PopupMenu
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import org.oppia.android.R
 import org.oppia.android.app.viewmodel.ViewModelProvider
 import org.oppia.android.databinding.FeedbackReportingEntryFragmentBinding
 import javax.inject.Inject
@@ -14,22 +15,25 @@ import javax.inject.Inject
 /** Presenter for the [FeedbackReportingEntryFragment] */
 class FeedbackReportingEntryFragmentPresenter @Inject constructor(
   private val activity: AppCompatActivity,
+  private val context: Context,
   private val fragment: Fragment,
   private val viewModelProvider: ViewModelProvider<FeedbackReportingEntryViewModel>
 ) {
   private lateinit var binding: FeedbackReportingEntryFragmentBinding
-  private lateinit var menu: PopupMenu
+  private lateinit var spinner: Spinner
 
   fun handleCreateView(inflater: LayoutInflater, container: ViewGroup?): View? {
+    val viewModel = getFeedbackReportingEntryViewModel()
+
     binding = FeedbackReportingEntryFragmentBinding.inflate(
       inflater,
       container,
       /* attachToRoot = */ false
     )
 
-    menu = PopupMenu(activity, binding.feedbackReportTypeDropdown)
-    menu.inflate(R.menu.feedback_report_type_dropdown_menu)
-    val viewModel = getFeedbackReportingEntryViewModel()
+    binding.feedbackReportTypeDropdown.apply {
+      adapter = createSpinnerAdapter()
+    }
 
     binding.let {
       it.lifecycleOwner = fragment
@@ -39,7 +43,12 @@ class FeedbackReportingEntryFragmentPresenter @Inject constructor(
   }
 
   fun showPopup() {
-    menu.show()
+  }
+
+  private fun createSpinnerAdapter(): ArrayAdapter<String> {
+    return ArrayAdapter(
+      context, android.R.layout.simple_spinner_dropdown_item, listOf("Suggestion", "Issue")
+    )
   }
 
   private fun getFeedbackReportingEntryViewModel(): FeedbackReportingEntryViewModel {
