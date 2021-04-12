8e75c7f6a24db92bfa183a3c00a1c205da17bce4
Rajat Talesra <talesra@google.com>
2021-04-12 08:46:23 +0530

    Nit fix

--- app/src/main/java/org/oppia/android/app/home/recentlyplayed/RecentlyPlayedTitleEnum.kt
+++ app/src/main/java/org/oppia/android/app/home/recentlyplayed/RecentlyPlayedTitleEnum.kt
@@ -6,11 +6,9 @@ import org.oppia.android.R
 
 /** Represents different titles for RecentlyPlayedActivity used by screen readers. */
 enum class RecentlyPlayedTitleEnum(@StringRes private var title: Int) {
-  RECENTLY_PLAYED_STORIES(R.string.recently_played_stories),
+  RECENTLY_PLAYED_STORIES(R.string.recently_played_activity_title),
   STORIES_FOR_YOU(R.string.stories_for_you);
 
   /** Returns the string corresponding to this error's string resources, or null if there is none. */
-  fun getTitleFromStringRes(context: Context): String? {
-    return title.let(context::getString)
-  }
+  fun getTitleFromStringRes(context: Context) = title.let(context::getString)
 }
