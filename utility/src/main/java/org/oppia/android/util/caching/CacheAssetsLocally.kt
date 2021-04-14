5d61f6d3fb7e01105a7750a5404e941b0c83dfa9
Ben Henning <henning.benmax@gmail.com>
2021-04-13 17:27:48 -0700

    Address pre-merge TODO.

--- utility/src/main/java/org/oppia/android/util/caching/CacheAssetsLocally.kt
+++ utility/src/main/java/org/oppia/android/util/caching/CacheAssetsLocally.kt
@@ -6,9 +6,3 @@ import javax.inject.Qualifier
  * Corresponds to an injectable boolean indicating whether lesson assets should be cached locally.
  */
 @Qualifier annotation class CacheAssetsLocally
-
-/**
- * Corresponds to an injectable boolean indicating whether images (including thumbnails) should be
- * retrieved from the app's local assets.
- */
-@Qualifier annotation class LoadImagesFromAssets
