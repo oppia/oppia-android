714010e012655753cd4a5870b69970ea9d9cdc7e
Yash Raj <yash_r@ec.iitr.ac.in>
2021-04-21 00:49:09 +0530

    bottom dots read progress

--- app/src/main/java/org/oppia/android/app/onboarding/OnboardingViewModel.kt
+++ app/src/main/java/org/oppia/android/app/onboarding/OnboardingViewModel.kt
@@ -9,8 +9,14 @@ import javax.inject.Inject
 class OnboardingViewModel @Inject constructor() : ObservableViewModel() {
   val slideNumber = ObservableField<Int>(0)
   val totalNumberOfSlides = TOTAL_NUMBER_OF_SLIDES
+  val slideDotsContentDescription = ObservableField<String>("Slide 1 of 4")
 
   fun slideChanged(slideIndex: Int) {
     slideNumber.set(slideIndex)
+    setSlideDotsContentDescription(slideIndex)
+  }
+
+  private fun setSlideDotsContentDescription(slideIndex: Int) {
+    slideDotsContentDescription.set("Slide ${slideIndex + 1} of 4")
   }
 }
