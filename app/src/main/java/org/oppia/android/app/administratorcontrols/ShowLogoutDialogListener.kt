38acea0fa0143fb3cb8ebe58af8f8b498f630471
arpit <arpitshukla62@gmail.com>
2021-05-01 08:37:58 +0530

    nit

--- app/src/main/java/org/oppia/android/app/administratorcontrols/ShowLogoutDialogListener.kt
+++ app/src/main/java/org/oppia/android/app/administratorcontrols/ShowLogoutDialogListener.kt
@@ -1,6 +1,8 @@
 package org.oppia.android.app.administratorcontrols
 
-/** Listener for when an activity should show [LogoutDialogFragment]. */
+/** Listener for when the logout dialog should be shown. */
 interface ShowLogoutDialogListener {
+
+  /** Shows the logout dialog. This cannot be called at times when there's an ongoing fragment transaction. */
   fun showLogoutDialog()
 }
