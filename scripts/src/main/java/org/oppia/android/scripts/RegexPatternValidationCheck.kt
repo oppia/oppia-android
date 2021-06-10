225a4fc070efb18dda2f4e08d76ac94b2b0321aa
Sparsh1212 <sparshagrawal1212@gmail.com>
2021-06-10 20:42:34 +0530

    Fix #3290: Add support for generic regex pattern matching

---
+++ scripts/src/main/java/org/oppia/android/scripts/RegexPatternValidationCheck.kt
@@ -0,0 +1,42 @@
+package org.oppia.android.scripts
+
+import java.io.File
+import java.io.FileInputStream
+import org.oppia.android.app.model.FilenameChecks
+import org.oppia.android.app.model.FileContentChecks
+
+class RegexPatternValidationCheck {
+  companion object {
+    @JvmStatic
+    fun main(vararg args: String) {
+      val path = System.getProperty("user.dir")
+      val fileNamePatternsBinaryFile =
+        File("$path/scripts/src/main/assets/filename_pattern_validation_checks.pb")
+      val fileContentsBinaryFile =
+        File("$path/scripts/src/main/assets/file_content_validation_checks.pb")
+      val filenameCheckBuilder = FilenameChecks.newBuilder()
+      val fileContentCheckBuilder = FileContentChecks.newBuilder()
+      val namePatternsObj: FilenameChecks =
+        FileInputStream(fileNamePatternsBinaryFile).use {
+          filenameCheckBuilder.mergeFrom(it)
+      }.build() as FilenameChecks
+      val fileContentsObj: FileContentChecks =
+        FileInputStream(fileContentsBinaryFile).use {
+          fileContentCheckBuilder.mergeFrom(it)
+        }.build() as FileContentChecks
+
+      namePatternsObj.getFilenameChecksList().forEach {
+        println(it.getProhibitedFilenameRegex())
+        println(it.getFailureMessage())
+        println("------------------------------------")
+      }
+
+      fileContentsObj.getFileContentChecksList().forEach {
+        println(it.getFilenameRegex())
+        println(it.getFailureMessage())
+        println(it.getProhibitedContentRegex())
+        println("------------------------------------")
+      }
+    }
+  }
+}
