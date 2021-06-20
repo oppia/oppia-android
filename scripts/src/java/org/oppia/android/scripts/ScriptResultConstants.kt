163310e09894c653122d2e0512dd47b9b6f087e1
Sparsh1212 <sparshagrawal1212@gmail.com>
2021-06-21 01:17:27 +0530

    Make nit suggestions

---
+++ scripts/src/java/org/oppia/android/scripts/ScriptResultConstants.kt
@@ -0,0 +1,7 @@
+package org.oppia.android.scripts
+
+object ScriptResultConstants {
+  /** RegexPatternValidationCheck output message constants **/
+  val REGEX_CHECKS_PASSED: String = "REGEX PATTERN CHECKS PASSED"
+  val REGEX_CHECKS_FAILED: String = "REGEX PATTERN CHECKS FAILED"
+}
