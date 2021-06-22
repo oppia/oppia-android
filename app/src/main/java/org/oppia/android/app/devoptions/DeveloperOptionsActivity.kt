dd8cf75a514f8f4a7e5dbd91eea706846a26f258
yashraj-01 <yashrajprime@gmail.com>
2021-06-22 20:18:30 +0530

    Added tests and Kdocs + nit fixes

--- app/src/main/java/org/oppia/android/app/devoptions/DeveloperOptionsActivity.kt
+++ app/src/main/java/org/oppia/android/app/devoptions/DeveloperOptionsActivity.kt
@@ -3,26 +3,24 @@ package org.oppia.android.app.devoptions
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import org.oppia.android.R
 import org.oppia.android.app.activity.InjectableAppCompatActivity
+import org.oppia.android.app.drawer.KEY_NAVIGATION_PROFILE_ID
 
 /** Activity for Developer Options. */
 class DeveloperOptionsActivity : InjectableAppCompatActivity() {
-  private var internalProfileId = -1
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     activityComponent.inject(this)
-    internalProfileId = intent.getIntExtra(DEVELOPER_OPTIONS_ACTIVITY_PROFILE_ID_KEY, -1)
+    title = getString(R.string.developer_options_activity_title)
   }
 
   companion object {
-    const val DEVELOPER_OPTIONS_ACTIVITY_PROFILE_ID_KEY =
-      "DeveloperOptionsActivity.internal_profile_id"
-
     /** Function to create intent for DeveloperOptionsActivity */
     fun createDeveloperOptionsActivityIntent(context: Context, internalProfileId: Int): Intent {
       val intent = Intent(context, DeveloperOptionsActivity::class.java)
-      intent.putExtra(DEVELOPER_OPTIONS_ACTIVITY_PROFILE_ID_KEY, internalProfileId)
+      intent.putExtra(KEY_NAVIGATION_PROFILE_ID, internalProfileId)
       return intent
     }
   }
