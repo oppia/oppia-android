b93e42104241845df835145d2dba96fd7e411669
prayutsu <gargabhay2000@gmail.com>
2021-06-22 15:28:36 +0530

    Convert .kts to .kt

---
+++ app/src/main/java/org/oppia/android/app/maven/maveninstall/Dependencies.kt
@@ -0,0 +1,10 @@
+package org.oppia.android.app.maven.maveninstall
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+/** Data class that stores the list of dependencies present in maven_install.json. */
+@JsonClass(generateAdapter = true)
+data class Dependencies(
+  @Json(name = "dependencies") val dependencyList: MutableList<Dependency>
+)
---
+++ scripts/src/java/org/oppia/android/scripts/maven/GenerateMavenDepsList.kt
@@ -0,0 +1,9 @@
+package org.oppia.android.scripts
+
+class GenerateMavenDepsList {
+  companion object {
+    @JvmStatic fun main(args: Array<String>) {
+
+    }
+  }
+}
