96c0bfdcbce2a52ca0ccc128207acb19ff7fb37f
Sparsh1212 <sparshagrawal1212@gmail.com>
2021-06-24 20:46:45 +0530

    Implement review suggestions

--- scripts/src/test/org/oppia/android/scripts/RegexPatternValidationCheckTest.kt
+++ scripts/src/test/org/oppia/android/scripts/RegexPatternValidationCheckTest.kt
@@ -12,7 +12,6 @@ import java.io.PrintStream
 
 /** Tests for [RegexPatternValidationCheck]. */
 class RegexPatternValidationCheckTest {
-
   private val outContent: ByteArrayOutputStream = ByteArrayOutputStream()
   private val originalOut: PrintStream = java.lang.System.out
 
