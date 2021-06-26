3b784525daf4a3e3d71572bfc7d7badef9c874f9
Sparsh1212 <sparshagrawal1212@gmail.com>
2021-06-26 23:41:00 +0530

    Implement feedback suggestions as per #3340

--- scripts/src/java/org/oppia/android/scripts/AccessibilityLabelCheck.kt
+++ scripts/src/java/org/oppia/android/scripts/AccessibilityLabelCheck.kt
@@ -1,50 +1,108 @@
 package org.oppia.android.scripts
 
-import javax.xml.parsers.DocumentBuilderFactory
+import org.w3c.dom.Node
+import org.w3c.dom.NodeList
 import java.io.File
-import org.oppia.android.scripts.ScriptResultConstants
-import org.oppia.android.scripts.ExemptionsList
+import java.util.stream.Collectors
+import java.util.stream.IntStream
+import javax.xml.parsers.DocumentBuilderFactory
 
 class AccessibilityLabelCheck {
   companion object {
     @JvmStatic
     fun main(vararg args: String) {
+      // path of the repo to be analyzed.
       val repoPath = args[0] + "/"
 
+      // path of the manifest file relative to the repo.
       val manifesFilePath = args[1]
 
+      // full path of the manifest file.
       val fullPathToManifestFile = repoPath + manifesFilePath
 
+      // class object which is needed to acess the helper methods.
+      val accessibilityLabelCheck: AccessibilityLabelCheck = AccessibilityLabelCheck()
+
+      // builder factory which provides the builder to parse the XMl.
       val builderFactory = DocumentBuilderFactory.newInstance()
 
+      // document builder which parses the XMl.
       val docBuilder = builderFactory.newDocumentBuilder()
 
-      var scriptFailedFlag = false
-
+      // parsed DOM of manifest file.
       val doc = docBuilder.parse(File(fullPathToManifestFile))
       doc.getDocumentElement().normalize()
 
-      val nList = doc.getElementsByTagName("activity")
+      // list of all the activity elements.
+      val nodeList = accessibilityLabelCheck.convertNodeListToListOfNode(
+        doc.getElementsByTagName("activity")
+      )
 
-      for (i in 1 until nList.length) {
-        val attributesList = nList.item(i).getAttributes()
-        val activityPath = attributesList.getNamedItem("android:name").getNodeValue()
-
-        if (activityPath !in ExemptionsList.ACCESSIBILITY_LABEL_CHECK_EXEMPTIONS_LIST) {
-
-          if (attributesList.getNamedItem("android:label") == null) {
-            println("No accessibility label found for: $activityPath\n")
-            scriptFailedFlag = true
-          }
-        }
+      // list of activity elements which lacks the label.
+      val matchedNodes = nodeList.filter { node ->
+        accessibilityLabelCheck.checkIfActivityHasMissingLabel(node)
       }
 
-      if (scriptFailedFlag) {
+      accessibilityLabelCheck.logFailures(matchedNodes)
+
+      if (matchedNodes.size != 0) {
         throw Exception(ScriptResultConstants.ACCESSIBILITY_LABEL_CHECK_FAILED)
       } else {
         println(ScriptResultConstants.ACCESSIBILITY_LABEL_CHECK_PASSED)
       }
     }
   }
-}
 
+  /**
+   * Checks whether a node has a missing label.
+   *
+   * @param node instance of Node
+   * @return label is present or not
+   */
+  private fun checkIfActivityHasMissingLabel(node: Node): Boolean {
+    val attributesList = node.getAttributes()
+    val activityPath = attributesList.getNamedItem("android:name").getNodeValue()
+    if (
+      activityPath !in ExemptionsList.ACCESSIBILITY_LABEL_CHECK_EXEMPTIONS_LIST &&
+      attributesList.getNamedItem("android:label") == null
+    ) {
+      return true
+    }
+    return false
+  }
+
+  /**
+   * The [NodeList] is not iterable. This helper
+   * function converts it to List<Node>.
+   *
+   * @param nodeList instance of NodeList
+   * @return a list of nodes
+   */
+  private fun convertNodeListToListOfNode(nodeList: NodeList): List<Node> {
+    return IntStream.range(
+      0, nodeList.getLength()
+    ).mapToObj(
+      nodeList::item
+    ).collect(
+      Collectors.toList()
+    )
+  }
+
+  /**
+   * Logs the failures for accessibility label check.
+   *
+   * @param matchedNodes a list of nodes having missing label
+   * @return log the failures
+   */
+  private fun logFailures(
+    matchedNodes: List<Node>
+  ) {
+    if (matchedNodes.size != 0) {
+      println("Accessiblity labels missing for Activities:")
+      matchedNodes.forEach { node ->
+        println(node.getAttributes().getNamedItem("android:name").getNodeValue())
+      }
+      println()
+    }
+  }
+}
--- scripts/src/test/org/oppia/android/scripts/AccessibilityLabelCheckTest.kt
+++ scripts/src/test/org/oppia/android/scripts/AccessibilityLabelCheckTest.kt
@@ -1,31 +1,36 @@
 package org.oppia.android.scripts
 
-import java.io.File
-import kotlin.test.assertEquals
-import org.oppia.android.scripts.AccessibilityLabelCheck
-import org.junit.Test
-import org.junit.Rule
+import com.google.common.truth.Truth.assertThat
+import org.junit.After
 import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.rules.TemporaryFolder
-import org.junit.rules.ExpectedException
-import org.oppia.android.scripts.ScriptResultConstants
+import org.oppia.android.testing.assertThrows
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
 
 class AccessibilityLabelCheckTest {
+  private val outContent: ByteArrayOutputStream = ByteArrayOutputStream()
+  private val originalOut: PrintStream = java.lang.System.out
+
   @Rule
   @JvmField
   public var tempFolder: TemporaryFolder = TemporaryFolder()
 
-  @Rule
-  @JvmField
-  var thrown: ExpectedException = ExpectedException.none()
-
   @Before
-  fun initTestFilesDirectory() {
-    val testFilesDirectory = tempFolder.newFolder("testfiles")
+  fun setUpTests() {
+    tempFolder.newFolder("testfiles")
+    java.lang.System.setOut(PrintStream(outContent))
+  }
+
+  @After
+  fun restoreStreams() {
+    java.lang.System.setOut(originalOut)
   }
 
   @Test
-  fun testAccessibilityLabel_labelPresent_ActivitiesAreDefinedWithAccessibilityLabels(){
+  fun testAccessibilityLabel_labelPresent_activitiesAreDefinedWithAccessibilityLabels() {
     val manifestContent = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
       "<manifest xmlns:android=\"http://schemas.android.com/apk/res/android\"\n" +
       "  xmlns:tools=\"http://schemas.android.com/tools\"\n" +
@@ -64,15 +69,18 @@ class AccessibilityLabelCheckTest {
       "      android:theme=\"@style/OppiaThemeWithoutActionBar\" />\n" +
       "  </application>\n" +
       "</manifest>\n"
-
     val manifestFile = tempFolder.newFile("testfiles/AndroidManifest.xml")
     manifestFile.writeText(manifestContent)
 
     runScript("AndroidManifest.xml")
+
+    assertThat(outContent.toString().trim()).isEqualTo(
+      ScriptResultConstants.ACCESSIBILITY_LABEL_CHECK_PASSED
+    )
   }
 
   @Test
-  fun testAccessibilityLabel_labelNotPresent_ActivityAreNotDefinedWithAccessibilityLabels(){
+  fun testAccessibilityLabel_labelNotPresent_activityAreNotDefinedWithAccessibilityLabels() {
     val manifestContent = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
       "<manifest xmlns:android=\"http://schemas.android.com/apk/res/android\"\n" +
       "  xmlns:tools=\"http://schemas.android.com/tools\"\n" +
@@ -110,22 +118,78 @@ class AccessibilityLabelCheckTest {
       "      android:theme=\"@style/OppiaThemeWithoutActionBar\" />\n" +
       "  </application>\n" +
       "</manifest>\n"
-
-    expectScriptFailure()
-
     val manifestFile = tempFolder.newFile("testfiles/AndroidManifest.xml")
     manifestFile.writeText(manifestContent)
 
-    runScript("AndroidManifest.xml")
+    val exception = assertThrows(Exception::class) {
+      runScript("AndroidManifest.xml")
+    }
+
+    assertThat(exception).hasMessageThat().contains(
+      ScriptResultConstants.ACCESSIBILITY_LABEL_CHECK_FAILED
+    )
+    assertThat(outContent.toString().trim()).isEqualTo(
+      "Accessiblity labels missing for Activities:\nTempActivity2"
+    )
   }
 
-  fun runScript(manifestFile: String) {
-    AccessibilityLabelCheck.main(tempFolder.getRoot().toString()+"/testfiles", manifestFile)
+  @Test
+  fun testAccessibilityLabel_labelsNotPresent_activitiesAreNotDefinedWithAccessibilityLabel() {
+    val manifestContent = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+      "<manifest xmlns:android=\"http://schemas.android.com/apk/res/android\"\n" +
+      "  xmlns:tools=\"http://schemas.android.com/tools\"\n" +
+      "  package=\"org.oppia.android\">\n" +
+      "\n" +
+      "  <uses-permission android:name=\"android.permission.ACCESS_NETWORK_STATE\" />\n" +
+      "  <uses-permission android:name=\"android.permission.INTERNET\" />\n" +
+      "  <application\n" +
+      "    android:name=\".app.application.OppiaApplication\"\n" +
+      "    android:allowBackup=\"true\"\n" +
+      "    android:icon=\"@mipmap/ic_launcher\"\n" +
+      "    android:label=\"@string/app_name\"\n" +
+      "    android:roundIcon=\"@mipmap/ic_launcher_round\"\n" +
+      "    android:supportsRtl=\"true\"\n" +
+      "    android:theme=\"@style/OppiaTheme\">\n" +
+      "    <meta-data\n" +
+      "      android:name=\"firebase_analytics_collection_deactivated\"\n" +
+      "      android:value=\"true\" />\n" +
+      "    <meta-data\n" +
+      "      android:name=\"firebase_crashlytics_collection_enabled\"\n" +
+      "      android:value=\"false\" />\n" +
+      "    <meta-data\n" +
+      "      android:name=\"automatic_app_expiration_enabled\"\n" +
+      "      android:value=\"false\" />\n" +
+      "    <meta-data\n" +
+      "      android:name=\"expiration_date\"\n" +
+      "      android:value=\"2020-09-01\" />\n" +
+      "\n" +
+      "    <activity\n" +
+      "      android:name=\"TempActivity1\"\n" +
+      "      android:theme=\"@style/OppiaThemeWithoutActionBar\" />\n" +
+      "    <activity\n" +
+      "      android:name=\"TempActivity2\"\n" +
+      "      android:theme=\"@style/OppiaThemeWithoutActionBar\" />\n" +
+      "  </application>\n" +
+      "</manifest>\n"
+    val manifestFile = tempFolder.newFile("testfiles/AndroidManifest.xml")
+    manifestFile.writeText(manifestContent)
+
+    val exception = assertThrows(Exception::class) {
+      runScript("AndroidManifest.xml")
+    }
+
+    assertThat(exception).hasMessageThat().contains(
+      ScriptResultConstants.ACCESSIBILITY_LABEL_CHECK_FAILED
+    )
+    assertThat(outContent.toString().trim()).isEqualTo(
+      "Accessiblity labels missing for Activities:\n" +
+        "TempActivity1\n" +
+        "TempActivity2"
+    )
   }
 
-  fun expectScriptFailure() {
-    thrown.expect(java.lang.Exception::class.java)
-    thrown.expectMessage(ScriptResultConstants.ACCESSIBILITY_LABEL_CHECK_FAILED)
+  /** Helper function which executes the main method of the script. */
+  private fun runScript(manifestFile: String) {
+    AccessibilityLabelCheck.main(tempFolder.getRoot().toString() + "/testfiles", manifestFile)
   }
 }
-
