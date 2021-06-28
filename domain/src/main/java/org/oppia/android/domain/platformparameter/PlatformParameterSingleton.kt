64f098c5cc6da4009bb2f48df3edf656de7050dd
Arjun Gupta <arjupta.90@gmail.com>
2021-06-28 23:05:05 +0530

    Lint check

--- domain/src/main/java/org/oppia/android/domain/platformparameter/PlatformParameterSingleton.kt
+++ domain/src/main/java/org/oppia/android/domain/platformparameter/PlatformParameterSingleton.kt
@@ -1,9 +1,9 @@
 package org.oppia.android.domain.platformparameter
 
-import org.oppia.android.app.model.PlatformParameter as ParameterValue
 import org.oppia.android.util.platformparameter.PlatformParameter
 import javax.inject.Inject
 import javax.inject.Singleton
+import org.oppia.android.app.model.PlatformParameter as ParameterValue
 
 /** Singleton class which helps in storing and providing Platform Parameters at runtime. */
 @Singleton
