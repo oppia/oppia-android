3afc018349e7ce1e7e4500e7657948fb79b843e4
Sparsh1212 <sparshagrawal1212@gmail.com>
2021-06-28 12:11:22 +0530

    nit fix

--- scripts/src/java/org/oppia/android/scripts/SyntaxErrorHandler.kt
+++ scripts/src/java/org/oppia/android/scripts/SyntaxErrorHandler.kt
@@ -27,13 +27,13 @@ class SyntaxErrorHandler(val repoPath: String) : ErrorHandler {
    * @param repoPath the path of the repo to be analyzed
    * @return log the failure
    */
-  private fun logCheckFailure(e: SAXParseException, repoPath: String) {
+  private fun logCheckFailure(parseException: SAXParseException, repoPath: String) {
     val failureMessage =
       """
-          XML syntax error: ${e.message}
-          lineNumber: ${e.getLineNumber()}
-          columnNumber: ${e.getColumnNumber()}
-          ${e.getSystemId().replace("file:$repoPath", "File: [ROOT]/")}
+          XML syntax error: ${parseException.message}
+          lineNumber: ${parseException.getLineNumber()}
+          columnNumber: ${parseException.getColumnNumber()}
+          ${parseException.getSystemId().replace("file:$repoPath", "File: [ROOT]/")}
       """.trimIndent()
     println(failureMessage)
     println()
