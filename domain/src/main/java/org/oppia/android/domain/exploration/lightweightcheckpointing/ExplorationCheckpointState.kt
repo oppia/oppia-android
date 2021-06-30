79f430b43c09fd1c83cb3e79e8733576e5237a02
MaskedCarrot <maskedcarrot01@gmail.com>
2021-06-30 16:30:20 +0530

    fixed comments

--- domain/src/main/java/org/oppia/android/domain/exploration/lightweightcheckpointing/ExplorationCheckpointState.kt
+++ domain/src/main/java/org/oppia/android/domain/exploration/lightweightcheckpointing/ExplorationCheckpointState.kt
@@ -3,14 +3,14 @@ package org.oppia.android.domain.exploration.lightweightcheckpointing
 /** Different states in which checkpoint saving exploration progress can exist. */
 enum class ExplorationCheckpointState {
   /**
-   *  Progress made in the exploration is saved and the size of the checkpoint database has
-   * not exceeded limit.
+   * Progress made in the exploration is saved and the size of the checkpoint database has
+   * not exceeded the allocated limit.
    */
   CHECKPOINT_SAVED_DATABASE_NOT_EXCEEDED_LIMIT,
 
   /**
    * Progress made in the exploration is saved and the size of the checkpoint database has
-   * exceeded limit.
+   * exceeded the allocated limit.
    */
   CHECKPOINT_SAVED_DATABASE_EXCEEDED_LIMIT,
 
