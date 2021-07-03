52d6ca2ff53be0125a0494c964b2885434571301
Ben Henning <henning.benmax@gmail.com>
2021-07-03 00:06:51 -0700

    Fix part of #2691: Migrate compute affected tests script to Kotlin (#3374)
    * Migrate compute affected tests script to Kotlin.
    
    No tests for the script yet.
    
    * Add BUILD.bazel docstrings.
    
    * Enable linters for scripts code. Fix ktlint issues.
    
    * Add tests for the compute affected tests script.
    
    Fix some typos & realized bug in the script.
    
    * Check out develop first so that exists in CI.
    
    See comment in PR for context.
    
    * Reorganize & refactor, and improve tests.
    
    This splits up the test & script into multiple utility packages (for
    common & testing utilities), reorganizes the test to be in a proper
    javatests/ & java/ arrangement, and adds tests for checking the error
    conditions of the test.
    
    * Add tests for executeCommand().
    
    This also refactors that function to be part of a class so that its
    operating conditions are configurable (namely the process timeout).
    
    * Add tests for BazelClient.
    
    * Add tests for GitClient.
    
    * Add tests for TestBazelWorkspace & documentation.
    
    Also, fix a few small issues in the utility.
    
    * Add tests git TestGitRepository.
    
    * Add support for changing relative develop branch.
    
    Also, fix some changed behaviors that broke the affected tests script.
    
    * Fix linter error found in CI.
    
    * Address reviewer comments.
    
    * Remove unnecessary extra checkout from CI.
    
    * Disable test that can't pass in CI.
    
    See #2691 for context.

--- scripts/ktlint_lint_check.sh
+++ scripts/ktlint_lint_check.sh
@@ -14,7 +14,7 @@ else
     jar_file_path="$github_actions_path/oppia-android-tools/ktlint"
 fi
 
-java -jar $jar_file_path --android app/src/**/*.kt data/src/**/*.kt domain/src/**/*.kt testing/src/**/*.kt utility/src/**/*.kt
+java -jar $jar_file_path --android app/src/**/*.kt data/src/**/*.kt domain/src/**/*.kt testing/src/**/*.kt utility/src/**/*.kt scripts/src/**/*.kt
 
 status=$?
 
@@ -25,7 +25,7 @@ else
   echo "********************************"
   echo "Ktlint issue found."
   echo "Please fix the above issues.
-  You can also use the ktlint -F --android domain/src/**/*.kt utility/src/**/*.kt data/src/**/*.kt app/src/**/*.kt testing/src/**/*.kt
+  You can also use the java -jar $jar_file_path -F --android domain/src/**/*.kt utility/src/**/*.kt data/src/**/*.kt app/src/**/*.kt testing/src/**/*.kt scripts/src/**/*.kt
   command to fix the most common issues."
   echo "Please note, there might be a possibility where the above command will not fix the issue.
   In that case, you will have to fix it yourself."
---
+++ scripts/src/java/org/oppia/android/scripts/common/CommandExecutorImpl.kt
@@ -0,0 +1,41 @@
+package org.oppia.android.scripts.common
+
+import java.io.File
+import java.util.concurrent.TimeUnit
+
+/**
+ * The default amount of time that should be waited before considering a process as 'hung', in
+ * milliseconds.
+ */
+const val WAIT_PROCESS_TIMEOUT_MS = 60_000L
+
+/** Default implementation of [CommandExecutor]. */
+class CommandExecutorImpl(
+  private val processTimeout: Long = WAIT_PROCESS_TIMEOUT_MS,
+  private val processTimeoutUnit: TimeUnit = TimeUnit.MILLISECONDS
+) : CommandExecutor {
+  override fun executeCommand(
+    workingDir: File,
+    command: String,
+    vararg arguments: String,
+    includeErrorOutput: Boolean
+  ): CommandResult {
+    check(workingDir.isDirectory) {
+      "Expected working directory to be an actual directory: $workingDir"
+    }
+    val assembledCommand = listOf(command) + arguments.toList()
+    val process =
+      ProcessBuilder(assembledCommand)
+        .directory(workingDir)
+        .redirectErrorStream(includeErrorOutput)
+        .start()
+    val finished = process.waitFor(processTimeout, processTimeoutUnit)
+    check(finished) { "Process did not finish within the expected timeout" }
+    return CommandResult(
+      process.exitValue(),
+      process.inputStream.bufferedReader().readLines(),
+      if (!includeErrorOutput) process.errorStream.bufferedReader().readLines() else listOf(),
+      assembledCommand,
+    )
+  }
+}
