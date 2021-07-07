73a3b30d4746085484a0e203e8dfe12afdf6448f
Arjun Gupta <arjupta.90@gmail.com>
2021-07-07 11:46:46 +0530

    Further KDoc changes

--- app/src/main/java/org/oppia/android/app/testing/SplashTestActivity.kt
+++ app/src/main/java/org/oppia/android/app/testing/SplashTestActivity.kt
@@ -2,9 +2,14 @@ package org.oppia.android.app.testing
 
 import android.os.Bundle
 import org.oppia.android.app.activity.InjectableAppCompatActivity
+import org.oppia.android.app.splash.SplashActivity
+import org.oppia.android.util.platformparameter.PlatformParameterValue
 import javax.inject.Inject
 
-/** This is a dummy activity to test the injection of Platform Parameters. */
+/**
+ * A test activity to verify the injection of [PlatformParameterValue] in the [SplashActivity].
+ * This test activity is used in integration tests for platform parameters.
+ */
 class SplashTestActivity : InjectableAppCompatActivity() {
 
   @Inject
