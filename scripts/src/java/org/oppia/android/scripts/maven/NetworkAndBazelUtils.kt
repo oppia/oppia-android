2236a0adef01ed666cba3d741ec2d05d3248401c
prayutsu <gargabhay2000@gmail.com>
2021-07-17 12:10:49 +0530

    Delete unwanted files

--- scripts/src/java/org/oppia/android/scripts/maven/NetworkAndBazelUtils.kt
+++
@@ -1,7 +0,0 @@
-package org.oppia.android.scripts.maven
-
-interface NetworkAndBazelUtils {
-  fun scrapeText(link: String): String
-
-  fun retrieveThirdPartyMavenDependenciesList(rootPath: String): List<String>
-}
