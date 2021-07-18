b2aa5536e5e4ba2338807d7299bcc230149ef642
Arjun Gupta <arjupta.90@gmail.com>
2021-07-19 01:01:44 +0530

    Bazel changes for TestConstants alomng with moving these constants into new file

---
+++ testing/src/main/java/org/oppia/android/testing/platformparameter/TestPlatformParameterConstants.kt
@@ -0,0 +1,25 @@
+package org.oppia.android.testing.platformparameter
+
+import javax.inject.Qualifier
+
+// These constants are only used for testing purpose.
+@Qualifier
+annotation class TestStringParam
+
+val TEST_STRING_PARAM_NAME = "test_string_param_name"
+val TEST_STRING_PARAM_DEFAULT_VALUE = "test_string_param_default_value"
+val TEST_STRING_PARAM_VALUE = "test_string_param_value"
+
+@Qualifier
+annotation class TestIntegerParam
+
+val TEST_INTEGER_PARAM_NAME = "test_integer_param_name"
+val TEST_INTEGER_PARAM_DEFAULT_VALUE = 0
+val TEST_INTEGER_PARAM_VALUE = 1
+
+@Qualifier
+annotation class TestBooleanParam
+
+val TEST_BOOLEAN_PARAM_NAME = "test_boolean_param_name"
+val TEST_BOOLEAN_PARAM_DEFAULT_VALUE = false
+val TEST_BOOLEAN_PARAM_VALUE = true
