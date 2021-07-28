f6e1ce431a4e0119f7c8c59ef78713e52257f5fa
Jacque Li <jacqueli@google.com>
2021-07-27 17:13:00 -0700

    Update service URL to match backend

--- data/src/main/java/org/oppia/android/data/backends/gae/api/FeedbackReportingService.kt
+++ data/src/main/java/org/oppia/android/data/backends/gae/api/FeedbackReportingService.kt
@@ -7,7 +7,7 @@ import retrofit2.http.POST
 
 /** Service that posts feedback reports to remote storage. */
 interface FeedbackReportingService {
-  @POST("app_feedback_report/incoming_android_report")
+  @POST("appfeedbackreporthandler/incoming_android_report")
   // TODO(#76): Update return payload for handling storage failures once retry policy is defined.
   fun postFeedbackReport(@Body report: GaeFeedbackReport): Call<Unit>
 }
