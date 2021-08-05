b217d1b3ac16d06430c7c6dc10468eff11cafccf
yashraj-01 <yashrajprime@gmail.com>
2021-08-05 22:33:37 +0530

    Moved activity and fragment tests to forcenetworktype package

--- app/src/sharedTest/java/org/oppia/android/app/devoptions/forcenetworktype/ForceNetworkTypeActivityTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/devoptions/forcenetworktype/ForceNetworkTypeActivityTest.kt
@@ -1,4 +1,4 @@
-package org.oppia.android.app.devoptions
+package org.oppia.android.app.devoptions.forcenetworktype
 
 import android.app.Application
 import android.content.Context
@@ -27,7 +27,8 @@ import org.oppia.android.app.application.ApplicationInjector
 import org.oppia.android.app.application.ApplicationInjectorProvider
 import org.oppia.android.app.application.ApplicationModule
 import org.oppia.android.app.application.ApplicationStartupListenerModule
-import org.oppia.android.app.devoptions.forcenetworktype.ForceNetworkTypeActivity
+import org.oppia.android.app.devoptions.DeveloperOptionsModule
+import org.oppia.android.app.devoptions.DeveloperOptionsStarterModule
 import org.oppia.android.app.player.state.hintsandsolution.HintsAndSolutionConfigModule
 import org.oppia.android.app.shim.ViewBindingShimModule
 import org.oppia.android.app.topic.PracticeTabModule
