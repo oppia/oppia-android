926c116db1d21516286cdb89fe5653a15c07d420
Sparsh1212 <sparshagrawal1212@gmail.com>
2021-08-05 22:41:54 +0530

    add tests

--- scripts/src/java/org/oppia/android/scripts/todo/CommentCheck.kt
+++ scripts/src/java/org/oppia/android/scripts/todo/CommentCheck.kt
@@ -1,8 +1,27 @@
 package org.oppia.android.scripts.todo
 
 import java.io.File
-import kotlin.system.exitProcess
 
+/**
+ * Script for ensuring that the [ClosedIssueCheck] failure comment is not the same as the latest
+ * comment of the closed issue.
+ *
+ * Usage:
+ *   bazel run //scripts:closed_issue_check -- <path_to_directory_root>
+ *   <path_to_latest_comment_file> <path_to_script_failure_comment_file>
+ *
+ * Arguments:
+ * - path_to_directory_root: directory path to the root of the Oppia Android repository.
+ * - path_to_latest_comment_file: file path to the latest comment body.
+ * - path_to_script_failure_comment_file: file path to the script failure comment body.
+ *
+ * Example:
+ *   bazel run //scripts:comment_check -- $(pwd) latest_comment.txt todo_list.txt
+ *
+ * NOTE TO DEVELOPERS: The script is executed in the CI enviornment. The CI workflow provides the
+ * file path to the latest comment body of the closed issue and the file path to the script failure
+ * comment body.
+ */
 fun main(vararg args: String) {
   val repoPath = args[0]
 
@@ -14,15 +33,17 @@ fun main(vararg args: String) {
 
   val failureCommentContentList = File(repoPath, failureCommentFilePath).readText().trim().lines()
 
-  if (latestCommentContentList.size != failureCommentContentList.size) exitProcess(0)
+  if (latestCommentContentList.size != failureCommentContentList.size) return
 
-  if (latestCommentContentList.first() != failureCommentContentList.first()) exitProcess(0)
+  if (latestCommentContentList.first() != failureCommentContentList.first()) return
 
   for (index in 1 until latestCommentContentList.size) {
+    // The commit SHA can vary from workflow to workflow. This can make the permalinks different.
+    // Hence, we are comparing by the relative file path.
     val latestCommentLineContent = latestCommentContentList[index].substring(85)
     val failureCommentLineContent = failureCommentContentList[index].substring(85)
-    if (latestCommentLineContent != failureCommentLineContent) exitProcess(0)
+    if (latestCommentLineContent != failureCommentLineContent) return
   }
 
-  throw Exception("COMMENT ALREADY EXISTS")
+  throw Exception("LATEST COMMENT IS SAME AS THE FAILURE COMMENT")
 }
