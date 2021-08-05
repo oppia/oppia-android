7d86287bc640823f7396cccdef5ea1554c9a5d05
MaskedCarrot <maskedcarrot01@gmail.com>
2021-08-06 03:13:30 +0530

    Revert "added tests"
    This reverts commit 4df426d4d44c04af8e4550b991f94ad1e9a92d21.

--- app/src/sharedTest/java/org/oppia/android/app/resumelesson/ResumeLessonFragmentTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/resumelesson/ResumeLessonFragmentTest.kt
@@ -122,34 +122,22 @@ class ResumeLessonFragmentTest {
     ApplicationProvider.getApplicationContext<TestApplication>().inject(this)
   }
 
-  @Config(qualifiers = "port")
+  @Test
+  fun testResumeLessonFragment_configurationChange_lessonThumbnailIsDisplayed() {
+    launch<ResumeLessonActivity>(createResumeLessonActivityIntent()).use {
+      testCoroutineDispatchers.runCurrent()
+      onView(isRoot()).perform(orientationLandscape())
+      testCoroutineDispatchers.runCurrent()
+      onView(withId(R.id.resume_lesson_chapter_thumbnail_image_view)).check(
+        matches(withDrawable(R.drawable.lesson_thumbnail_graphic_child_with_fractions_homework))
+      )
+    }
+  }
+
   @Test
   fun testResumeLessonFragment_lessonThumbnailIsDisplayed() {
     launch<ResumeLessonActivity>(createResumeLessonActivityIntent()).use {
       testCoroutineDispatchers.runCurrent()
-      onView(withId(R.id.resume_lesson_chapter_thumbnail_image_view)).check(
-        matches(withDrawable(R.drawable.lesson_thumbnail_graphic_child_with_fractions_homework))
-      )
-    }
-  }
-
-  @Config(qualifiers = "sw600dp-port")
-  @Test
-  fun testResumeLessonFragment_onTablet_lessonThumbnailIsDisplayed() {
-    launch<ResumeLessonActivity>(createResumeLessonActivityIntent()).use {
-      testCoroutineDispatchers.runCurrent()
-      onView(withId(R.id.resume_lesson_chapter_thumbnail_image_view)).check(
-        matches(withDrawable(R.drawable.lesson_thumbnail_graphic_child_with_fractions_homework))
-      )
-    }
-  }
-
-  @Config(qualifiers = "sw600dp-port")
-  @Test
-  fun testResumeLessonFragment_onTablet_configurationChange_lessonThumbnailIsDisplayed() {
-    launch<ResumeLessonActivity>(createResumeLessonActivityIntent()).use {
-      testCoroutineDispatchers.runCurrent()
-      onView(isRoot()).perform(orientationLandscape())
       onView(withId(R.id.resume_lesson_chapter_thumbnail_image_view)).check(
         matches(withDrawable(R.drawable.lesson_thumbnail_graphic_child_with_fractions_homework))
       )
