51758ab46713734a7024c723de5bc8c3ce51d00b
veena14cs <veena14cs@gmail.com>
2021-08-07 01:15:42 +0530

    Fixed rotation animation for icon in Topic lesson and Hints and Solution

--- app/src/main/java/org/oppia/android/app/activity/ActivityComponent.kt
+++ app/src/main/java/org/oppia/android/app/activity/ActivityComponent.kt
@@ -60,6 +60,7 @@ import org.oppia.android.app.testing.TestFontScaleConfigurationUtilActivity
 import org.oppia.android.app.testing.TopicRevisionTestActivity
 import org.oppia.android.app.testing.TopicTestActivity
 import org.oppia.android.app.testing.TopicTestActivityForStory
+import org.oppia.android.app.testing.ViewBindingAdaptersTestActivity
 import org.oppia.android.app.topic.TopicActivity
 import org.oppia.android.app.topic.questionplayer.QuestionPlayerActivity
 import org.oppia.android.app.topic.revisioncard.RevisionCardActivity
@@ -137,6 +138,7 @@ interface ActivityComponent {
   fun inject(topicRevisionTestActivity: TopicRevisionTestActivity)
   fun inject(topicTestActivity: TopicTestActivity)
   fun inject(topicTestActivityForStory: TopicTestActivityForStory)
+  fun inject(viewBindingAdaptersTestActivity: ViewBindingAdaptersTestActivity)
   fun inject(viewEventLogsActivity: ViewEventLogsActivity)
   fun inject(viewEventLogsTestActivity: ViewEventLogsTestActivity)
   fun inject(walkthroughActivity: WalkthroughActivity)
