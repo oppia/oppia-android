f29e66ac0648e50aa3ec9ae23e864eaef91a7604
veena14cs <veena14cs@gmail.com>
2021-08-13 16:59:10 +0530

    moved tests

--- app/src/sharedTest/java/org/oppia/android/app/parser/HtmlParserTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/parser/HtmlParserTest.kt
@@ -6,16 +6,20 @@ import android.content.Context
 import android.content.Intent
 import android.text.Html
 import android.text.Spannable
+import android.text.SpannableString
 import android.text.SpannableStringBuilder
+import android.text.Spanned
 import android.text.style.BulletSpan
 import android.text.style.ClickableSpan
 import android.text.style.ImageSpan
 import android.text.style.LeadingMarginSpan
+import android.text.style.UnderlineSpan
 import android.view.View
 import android.widget.TextView
 import androidx.annotation.DimenRes
 import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.getSpans
 import androidx.core.view.ViewCompat
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
@@ -99,7 +103,6 @@ import org.oppia.android.util.parser.image.TestGlideImageLoader
 import org.robolectric.annotation.Config
 import org.robolectric.annotation.LooperMode
 import javax.inject.Inject
-import javax.inject.Qualifier
 import javax.inject.Singleton
 import kotlin.reflect.KClass
 
@@ -121,8 +124,7 @@ class HtmlParserTest {
 
   @Captor lateinit var stringCaptor: ArgumentCaptor<String>
 
-  @Inject
-  lateinit var context: Context
+  private var context: Context = ApplicationProvider.getApplicationContext<TestApplication>()
 
   @Inject
   lateinit var htmlParserFactory: HtmlParser.Factory
@@ -149,6 +151,27 @@ class HtmlParserTest {
   @After
   fun tearDown() {
     Intents.release()
+  }
+
+  private val testStringWithoutBulletSpan = SpannableString("Text Without BulletSpan")
+  private val testStringWithBulletSpan = SpannableString("Text With \nBullet Point").apply {
+    val spacingBeforeBullet = context.resources.getDimensionPixelSize(org.oppia.android.util.R.dimen.spacing_before_bullet)
+    setSpan(
+      LeadingMarginSpan.Standard(spacingBeforeBullet),
+      10,
+      22,
+      Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+    )
+    setSpan(BulletSpan(), 10, 22, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+  }
+  private val testStringWithMultipleBulletSpan = SpannableString(
+    "Text With \nfirst \nsecond \nthird \nfour \nfive"
+  ).apply {
+    setSpan(BulletSpan(), 10, 18, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+    setSpan(BulletSpan(), 18, 27, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+    setSpan(BulletSpan(), 27, 35, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+    setSpan(BulletSpan(), 35, 42, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+    setSpan(UnderlineSpan(), 42, 43, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
   }
 
   @Test
@@ -290,8 +313,8 @@ class HtmlParserTest {
     assertThat(bulletSpan1).isNotNull()
     assertThat(htmlResult.toString())
       .isEqualTo(
-        "You should know the following before going on:\n\n" + "The counting numbers" +
-          " (1, 2, 3, 4, 5 ….)\n\n" + "How to tell whether one counting number is bigger or " +
+        "You should know the following before going on:\n\nThe counting numbers" +
+          " (1, 2, 3, 4, 5 ….)\n\nHow to tell whether one counting number is bigger or " +
           "smaller than another"
       )
   }
@@ -628,6 +651,94 @@ class HtmlParserTest {
     assertThat(loadedInlineImages).hasSize(1)
     assertThat(loadedInlineImages.first()).contains("math_image1.svg")
   }
+
+  @Test
+  fun testBulletSpan_spannableStringWithoutBulletSpanRemainSame() {
+    val spannableString1 = testStringWithoutBulletSpan
+    assertThat(getBulletSpanCount(spannableString1)).isEqualTo(0)
+  }
+
+  @Test
+  fun testBulletSpan_spannableStringWithBulletSpan_isNotSame() {
+    val spannableString1 = testStringWithBulletSpan
+    assertThat(getBulletSpanCount(spannableString1)).isEqualTo(1)
+  }
+
+  @Test
+  fun testBulletSpan_includingUnderlineSpan_underlineSpanRemainsSame() {
+    val spannableString1 = testStringWithMultipleBulletSpan
+
+    assertThat(getBulletSpanCount(spannableString1)).isEqualTo(4)
+    assertThat(getUnderlineSpanCount(spannableString1)).isEqualTo(1)
+  }
+
+  @Test
+  fun testLeadMargin_isComputedToProperlyIndentText() {
+    val spannableString = SpannableStringBuilder(testStringWithBulletSpan)
+    val bulletSpans = spannableString.getSpans(
+      0, spannableString.length,
+      BulletSpan::class.java
+    )
+    assertThat(bulletSpans).hasLength(1)
+    assertSpanLocation(bulletSpans[0], spannableString, 10, 22)
+
+    val leadingMarginSpans = spannableString.getSpans(
+      0, spannableString.length,
+      LeadingMarginSpan.Standard::class.java
+    )
+    assertThat(leadingMarginSpans).hasLength(1)
+    assertSpanLocation(
+      leadingMarginSpans[0],
+      spannableString,
+      10,
+      22
+    )
+  }
+
+  @Test
+  fun testMultipleBulletSpan_spannableStringWithBulletSpan_isComputedToProperlyIndentText() {
+    val spannableString = SpannableStringBuilder(testStringWithMultipleBulletSpan)
+
+    val bulletSpans = spannableString.getSpans(
+      0, spannableString.length,
+      BulletSpan::class.java
+    )
+    assertThat(bulletSpans).hasLength(4)
+    assertSpanLocation(bulletSpans[0], spannableString, 10, 18)
+    assertSpanLocation(bulletSpans[1], spannableString, 18, 27)
+    assertSpanLocation(bulletSpans[2], spannableString, 27, 35)
+    assertSpanLocation(bulletSpans[3], spannableString, 35, 42)
+  }
+
+  private fun assertSpanLocation(
+    span: Any, text: Spanned, expectedStartIndex: Int, expectedEndIndex: Int
+  ) {
+    assertThat(text.getSpanStart(span)).isEqualTo(expectedStartIndex)
+    assertThat(text.getSpanEnd(span)).isEqualTo(expectedEndIndex)
+  }
+
+  private fun getBulletSpans(spannableString: SpannableString): Array<out BulletSpan> {
+    return spannableString.getSpans<BulletSpan>(
+      0,
+      spannableString.length
+    )
+  }
+
+  private fun getUnderlineSpans(spannableString: SpannableString): Array<out UnderlineSpan> {
+    return spannableString.getSpans<UnderlineSpan>(
+      0,
+      spannableString.length
+    )
+  }
+
+  private fun getBulletSpanCount(spannableString: SpannableString): Int {
+    return getBulletSpans(spannableString).size
+  }
+
+  private fun getUnderlineSpanCount(spannableString: SpannableString): Int {
+    return getUnderlineSpans(spannableString).size
+  }
+
 
   private fun arrangeTextViewWithLayoutDirection(
     htmlParser: HtmlParser,
