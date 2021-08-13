f5bceb8236ac0a11293e5dd7fbeb8022c644e6b8
prayutsu <gargabhay2000@gmail.com>
2021-08-13 21:40:25 +0530

    Update branch

--- app/src/main/java/org/oppia/android/app/help/HelpActivity.kt
+++ app/src/main/java/org/oppia/android/app/help/HelpActivity.kt
@@ -10,18 +10,14 @@ import org.oppia.android.app.drawer.NAVIGATION_PROFILE_ID_ARGUMENT_KEY
 import org.oppia.android.app.help.faq.FAQListActivity
 import org.oppia.android.app.help.faq.RouteToFAQSingleListener
 import org.oppia.android.app.help.faq.faqsingle.FAQSingleActivity
-import org.oppia.android.app.help.thirdparty.LicenseListActivity
-import org.oppia.android.app.help.thirdparty.LicenseTextViewerActivity
-import org.oppia.android.app.help.thirdparty.RouteToLicenseListListener
-import org.oppia.android.app.help.thirdparty.RouteToLicenseTextListener
 import org.oppia.android.app.help.thirdparty.ThirdPartyDependencyListActivity
 import javax.inject.Inject
 import kotlin.properties.Delegates
 
-private const val MULTIPANE_TITLE_KEY = "HelpActivity.help_options_title"
-private const val SELECTED_FRAGMENT_KEY = "HelpActivity.selected_fragment"
-private const val THIRD_PARTY_DEPENDENCY_INDEX_KEY = "HelpActivity.third_party_dependency_index"
-private const val LICENSE_INDEX_KEY = "HelpActivity.license_index"
+private const val HELP_OPTIONS_TITLE_SAVED_KEY = "HelpActivity.help_options_title"
+private const val SELECTED_FRAGMENT_SAVED_KEY = "HelpActivity.selected_fragment"
+private const val THIRD_PARTY_DEPENDENCY_INDEX_SAVED_KEY = "HelpActivity.third_party_dependency_index"
+private const val LICENSE_INDEX_SAVED_KEY = "HelpActivity.license_index"
 const val FAQ_LIST_FRAGMENT = "FAQListFragment"
 const val THIRD_PARTY_DEPENDENCY_LIST_FRAGMENT = "ThirdPartyDependencyListFragment"
 const val LICENSE_LIST_FRAGMENT = "LicenseListFragment"
@@ -41,10 +37,10 @@ class HelpActivity :
   @Inject
   lateinit var helpActivityPresenter: HelpActivityPresenter
 
-  private lateinit var selectedMultipaneFragment: String
-  private lateinit var selectedMultipaneFragmentTitle: String
-  private var savedDependencyIndex by Delegates.notNull<Int>()
-  private var savedLicenseIndex by Delegates.notNull<Int>()
+  private lateinit var selectedFragment: String
+  private lateinit var selectedHelpOptionsTitle: String
+  private var selectedDependencyIndex by Delegates.notNull<Int>()
+  private var selectedLicenseIndex by Delegates.notNull<Int>()
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -53,16 +49,16 @@ class HelpActivity :
       BOOL_IS_FROM_NAVIGATION_DRAWER_EXTRA_KEY,
       /* defaultValue= */ false
     )
-    selectedMultipaneFragment = savedInstanceState?.getString(SELECTED_FRAGMENT_KEY) ?: FAQ_LIST_FRAGMENT
-    savedDependencyIndex = savedInstanceState?.getInt(THIRD_PARTY_DEPENDENCY_INDEX_KEY) ?: 0
-    savedLicenseIndex = savedInstanceState?.getInt(LICENSE_INDEX_KEY) ?: 0
-    val extraHelpOptionsTitle = savedInstanceState?.getString(MULTIPANE_TITLE_KEY)
+    selectedFragment = savedInstanceState?.getString(SELECTED_FRAGMENT_SAVED_KEY) ?: FAQ_LIST_FRAGMENT
+    selectedDependencyIndex = savedInstanceState?.getInt(THIRD_PARTY_DEPENDENCY_INDEX_SAVED_KEY) ?: 0
+    selectedLicenseIndex = savedInstanceState?.getInt(LICENSE_INDEX_SAVED_KEY) ?: 0
+    val extraHelpOptionsTitle = savedInstanceState?.getString(HELP_OPTIONS_TITLE_SAVED_KEY)
     helpActivityPresenter.handleOnCreate(
       extraHelpOptionsTitle,
       isFromNavigationDrawer,
-      selectedMultipaneFragment,
-      savedDependencyIndex,
-      savedLicenseIndex
+      selectedFragment,
+      selectedDependencyIndex,
+      selectedLicenseIndex
     )
     title = getString(R.string.menu_help)
   }
@@ -95,25 +91,25 @@ class HelpActivity :
   }
 
   override fun loadFAQListFragment() {
-    selectedMultipaneFragment = FAQ_LIST_FRAGMENT
+    selectedFragment = FAQ_LIST_FRAGMENT
     helpActivityPresenter.handleLoadFAQListFragment()
   }
 
   override fun loadThirdPartyDependencyListFragment() {
-    selectedMultipaneFragment = THIRD_PARTY_DEPENDENCY_LIST_FRAGMENT
+    selectedFragment = THIRD_PARTY_DEPENDENCY_LIST_FRAGMENT
     helpActivityPresenter.handleLoadThirdPartyDependencyListFragment()
   }
 
   override fun loadLicenseListFragment(dependencyIndex: Int) {
-    selectedMultipaneFragment = LICENSE_LIST_FRAGMENT
-    savedDependencyIndex = dependencyIndex
+    selectedFragment = LICENSE_LIST_FRAGMENT
+    selectedDependencyIndex = dependencyIndex
     helpActivityPresenter.handleLoadLicenseListFragment(dependencyIndex)
   }
 
   override fun loadLicenseTextViewerFragment(dependencyIndex: Int, licenseIndex: Int) {
-    selectedMultipaneFragment = LICENSE_TEXT_FRAGMENT
-    savedDependencyIndex = dependencyIndex
-    savedLicenseIndex = licenseIndex
+    selectedFragment = LICENSE_TEXT_FRAGMENT
+    selectedDependencyIndex = dependencyIndex
+    selectedLicenseIndex = licenseIndex
     helpActivityPresenter.handleLoadLicenseTextViewerFragment(dependencyIndex, licenseIndex)
   }
 
@@ -121,19 +117,19 @@ class HelpActivity :
     super.onSaveInstanceState(outState)
     val titleTextView = findViewById<TextView>(R.id.help_multipane_options_title_textview)
     if (titleTextView != null) {
-      outState.putString(MULTIPANE_TITLE_KEY, titleTextView.text.toString())
+      outState.putString(HELP_OPTIONS_TITLE_SAVED_KEY, titleTextView.text.toString())
     }
-    outState.putString(SELECTED_FRAGMENT_KEY, selectedMultipaneFragment)
-    outState.putInt(THIRD_PARTY_DEPENDENCY_INDEX_KEY, savedDependencyIndex)
-    outState.putInt(LICENSE_INDEX_KEY, savedLicenseIndex)
+    outState.putString(SELECTED_FRAGMENT_SAVED_KEY, selectedFragment)
+    outState.putInt(THIRD_PARTY_DEPENDENCY_INDEX_SAVED_KEY, selectedDependencyIndex)
+    outState.putInt(LICENSE_INDEX_SAVED_KEY, selectedLicenseIndex)
   }
 
   override fun onRestoreInstanceState(savedInstanceState: Bundle) {
     super.onRestoreInstanceState(savedInstanceState)
-    selectedMultipaneFragment = savedInstanceState.getString(SELECTED_FRAGMENT_KEY) ?: FAQ_LIST_FRAGMENT
-    savedDependencyIndex = savedInstanceState.getInt(THIRD_PARTY_DEPENDENCY_INDEX_KEY)
-    savedLicenseIndex = savedInstanceState.getInt(LICENSE_INDEX_KEY)
-    selectedMultipaneFragmentTitle = savedInstanceState.getString(MULTIPANE_TITLE_KEY) ?: ""
+    selectedFragment = savedInstanceState.getString(SELECTED_FRAGMENT_SAVED_KEY) ?: FAQ_LIST_FRAGMENT
+    selectedDependencyIndex = savedInstanceState.getInt(THIRD_PARTY_DEPENDENCY_INDEX_SAVED_KEY)
+    selectedLicenseIndex = savedInstanceState.getInt(LICENSE_INDEX_SAVED_KEY)
+    selectedHelpOptionsTitle = savedInstanceState.getString(HELP_OPTIONS_TITLE_SAVED_KEY) ?: ""
   }
 
   override fun onRouteToFAQSingle(question: String, answer: String) {
