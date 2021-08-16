cd8361fb5db0f63c988191ab43149c0349de92b7
farees <fareezzhussain@gmail.com>
2021-08-16 16:36:32 +0530

    Added kdoc

--- data/src/test/java/org/oppia/android/data/backends/gae/NetworkConfigProdModuleTest.kt
+++ data/src/test/java/org/oppia/android/data/backends/gae/NetworkConfigProdModuleTest.kt
@@ -17,6 +17,7 @@ import org.robolectric.annotation.LooperMode
 import javax.inject.Inject
 import javax.inject.Singleton
 
+/** Tests for [NetworkConfigProdModule] */
 @RunWith(AndroidJUnit4::class)
 @LooperMode(LooperMode.Mode.PAUSED)
 @Config(application = NetworkConfigProdModuleTest.TestApplication::class)
