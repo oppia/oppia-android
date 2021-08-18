5d71407dcb84f06ed7725bd7713799b53548f753
Yash Raj <63469151+yashraj-01@users.noreply.github.com>
2021-08-18 13:42:25 +0530

    Fix #3309 and #3311: Implement Force Network Type Feature (#3502)
    * Introduced initial UI for force network type screen
    
    * Introduced prod and debug NetworkConnectionUtil
    
    * Fixed bazel build failure
    
    * Fixed app layer tests
    
    * Fixed domain layer tests
    
    * Fixed testing layer tests
    
    * Fixed util layer tests
    
    * Introduced prod and debug tests for NetworkConnectionUtil
    
    * Nit fixes
    
    * Renamed files
    
    * Fixed failing domain layer tests on bazel
    
    * Fixed failing utility layer tests on Bazel
    
    * Added named arguments and other fixes
    
    * Introduced a separate NetworkConnectionTestUtil
    
    * Fixed failing tests
    
    * Moved setNetworkInfo to companion object
    
    * Added test for NetworkConnectionTestUtil
    
    * Changes to BUILD.bazel files and tests. Mostly nits.
    
    * Moved NetworkConnectionTestUtil and its Test to networking package
    
    * Renamed debug and prod util
    
    * Added tests for modules
    
    * Nit fixes
    
    * Added missing KDocs
    
    * Removed redundant kdoc exemption
    
    * Implemented force network type feature
    
    * Added tests for DeveloperOptionsFragmentTest
    
    * Introduced ForceNetworkTypeActivityTest and ForceNetworkTypeTestActivity
    
    * Introduced ForceNetworkTypeFragmentTest
    
    * Added test file exemptions
    
    * Added accessibility label exemption
    
    * Added missing KDocs
    
    * Bazel nit
    
    * Created dedicated local test targets for test/networking
    
    * Removed extra checks and updated test cases accordingly
    
    * Migrated tests to networking package
    
    * Made test util inject
    
    * Updated test util test
    
    * Removed kdocs from test files
    
    * Nit fix
    
    * Nit fix
    
    * bazel nit fix
    
    * Nit fix
    
    * fixed bazel lint
    
    * Removed common test deps
    
    * moved networking test util and its test to testing module
    
    * bazel nit fix
    
    * Moved activity and fragment tests to forcenetworktype package
    
    * Nit fixes
    
    * Used string instead of enum names
    
    * Fixed failing static checks
    
    * Fixed failing test
    
    * nit fix
    
    * Removed redundant kdoc exemption
    
    * Made NetworkConnectionDebugUtil optional
    
    * Fixed tests
    
    * fixed bazel build and added TODOs
    
    * Added Kdocs and test file exemptions
    
    * Added network util module in test file
    
    * Added debug util module
    
    * fixed bazel failure
    
    * renamed connection status
    
    * Nit fix
    
    * introduced oppia_debug_visibility
    
    * added TODO
    
    * fixed incorrect connection status
    
    * used Guava's optional instead of java8

--- app/src/sharedTest/java/org/oppia/android/app/options/OptionsFragmentTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/options/OptionsFragmentTest.kt
@@ -82,6 +82,7 @@ import org.oppia.android.util.logging.GlobalLogLevel
 import org.oppia.android.util.logging.LogLevel
 import org.oppia.android.util.logging.LoggerModule
 import org.oppia.android.util.logging.firebase.FirebaseLogUploaderModule
+import org.oppia.android.util.networking.NetworkConnectionDebugUtilModule
 import org.oppia.android.util.networking.NetworkConnectionUtilDebugModule
 import org.oppia.android.util.parser.html.HtmlParserEntityTypeModule
 import org.oppia.android.util.parser.image.GlideImageLoaderModule
@@ -626,7 +627,8 @@ class OptionsFragmentTest {
       WorkManagerConfigurationModule::class, HintsAndSolutionConfigModule::class,
       FirebaseLogUploaderModule::class, FakeOppiaClockModule::class, PracticeTabModule::class,
       DeveloperOptionsStarterModule::class, DeveloperOptionsModule::class,
-      ExplorationStorageModule::class, NetworkConnectionUtilDebugModule::class
+      ExplorationStorageModule::class, NetworkConnectionUtilDebugModule::class,
+      NetworkConnectionDebugUtilModule::class
     ]
   )
   interface TestApplicationComponent : ApplicationComponent {
--- app/src/sharedTest/java/org/oppia/android/app/thirdparty/LicenseListFragmentTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/thirdparty/LicenseListFragmentTest.kt
@@ -74,6 +74,7 @@ import org.oppia.android.util.caching.testing.CachingTestModule
 import org.oppia.android.util.gcsresource.GcsResourceModule
 import org.oppia.android.util.logging.LoggerModule
 import org.oppia.android.util.logging.firebase.FirebaseLogUploaderModule
+import org.oppia.android.util.networking.NetworkConnectionDebugUtilModule
 import org.oppia.android.util.networking.NetworkConnectionUtilDebugModule
 import org.oppia.android.util.parser.html.HtmlParserEntityTypeModule
 import org.oppia.android.util.parser.image.GlideImageLoaderModule
@@ -351,7 +352,8 @@ class LicenseListFragmentTest {
       WorkManagerConfigurationModule::class, HintsAndSolutionConfigModule::class,
       FirebaseLogUploaderModule::class, FakeOppiaClockModule::class, PracticeTabModule::class,
       DeveloperOptionsStarterModule::class, DeveloperOptionsModule::class,
-      ExplorationStorageModule::class, NetworkConnectionUtilDebugModule::class
+      ExplorationStorageModule::class, NetworkConnectionUtilDebugModule::class,
+      NetworkConnectionDebugUtilModule::class
     ]
   )
 
