b1a597972a675f6b8c2054075c74114a21ebef30
Arjun Gupta <arjupta.90@gmail.com>
2021-08-21 03:13:58 +0530

    Fix #3605: Introduce Platform Parameter WorkManager classes (#3606)
    * Added GaeModels + PlatformParameterService and its Mock
    
    * Trying to fix bazel check by adding dependency over data module
    
    * Changed the location of MockService to testing/network and of ServiceTest to data
    
    * Bazel changes for TestConstants alomng with moving these constants into new file
    
    * Nit change in the bazel package name
    
    * Adding platform parameter BUILD.bazel as a dependency in testing
    
    * Adding test_constants in testing/network BUILD.bazel and utility in testing/platformparameter BUILD.bazel
    
    * Suggested changes related to exemption file, renaming test constants and Gae models also adding test cases in Service test and documentation over Gae models
    
    * Nit change related to TODO issue number
    
    * Reverting to the original response structure from backend. Removed GaeModels and used a Map<String,Any> instead
    
    * Removed GaeModels from exemptions file & platform parameter from TEST_DEPS of testing module
    
    * Nit change for renaming test function
    
    * Nit change for adding named arguments
    
    * Suggested change for testing request query, new files for test constants
    and KDoc changes
    
    * Static checks and updating exemption file
    
    * Basic commit for introducing a Worker, WorkerFactory and WorkerInitializer
    
    * Including all the files related to Platform Parameter Worker.
    
    * SyncUp worker tests are running fine. Commented out Prod Platform Parameter Service Impl in Network Module
    
    * Added the tests for SyncUpWorkManagerInitializer
    
    * Introduced TestNetworkModule in the tests to remove the dependency from NetworkModule
    
    * Modified exemption file to exclude Worker Factory and Module
    
    * Added bazel dependency over PlatformParameterService and ContextExtensions
    
    * Added NetworkModule in all the tests under where WorkManagerConfigurationModule is used
    
    * Suggested nit changes
    
    * Added tests for MockPlatformParameterService
    
    * Added test files in the kdoc-exemption
    
    * Corrected the import statement for WorkManagerConfigurationModule
    
    * KDoc exemption related changes
    
    * Changed the worker test to include exception checking
    
    * Nit changes related to KDoc and BUILD.bazel
    
    * Nit change of Reference Url in PlatformParameterService
    
    * Nit changes related to KDoc, BUILD.bazel and WorkMangerConfigurationModule import
    
    * Added NetworkModule in MarkTopicsCompletedFragmentTest
    
    * Failing checks related to WorkManager imports and Bazel dependency over testing/network
    
    * Trying to fix Bazel failure
    
    * Nit changes in KDoc exemption file, BUILD.bazel and WorkManagerImport
    
    * Suggested Changes for new Param,TestCases and KDoc
    
    - New SyncUpTimePeriod Parameter
    - New Test Case for Empty Response in WorkerTests
    - Shifted SplashScreenWlecomeMsgServerValue to IntegrationTest
    - Few KDoc changes
    
    * Nit KDoc change + NetworkModule in TestComponent
    
    * Bazel reformating for lint check
    
    * Suggested change for KDoc, Refactor & DataProvider
    
    - Changed the Time Period Parameter Name
    - Made updatePlaformParameter to return a dataprovider
    - KDoc changes
    
    * Correcting the work manager imports for new files
    
    * Suggested Nit changes by Ben
    
    * More Nit changes
    
    * Adding the tests for DataProvider we get from update operation for platform parameter database
    
    * Nit KDoc changes
    
    * KDoc changes and deleting commented out code
    
    * Suggested changes for KDoc and VisibleForTesting functions
    
    * Added NetworkModule in ForceNetworkTypeTests
    
    * Add NetworkConnectionDebugUtilModule in domain/BUILD.bazel
    
    * Suggested Nit changes
    
    * Bazel check failure due to NetworkConfigProdModule
    
    * Adding NetworkConfigProdModule into domain tests
    
    * Adding NetworkConfigProdModule in StateFragmentTest
    
    * Added WorkManagerConfigurationModule in OppiaTestApplication
    
    * Injected baseUrl into SyncuUpWorkerTests
    
    * Correcting the imports in TestApplicationComponent

--- app/src/sharedTest/java/org/oppia/android/app/testing/TestFontScaleConfigurationUtilActivityTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/testing/TestFontScaleConfigurationUtilActivityTest.kt
@@ -30,6 +30,8 @@ import org.oppia.android.app.model.ReadingTextSize
 import org.oppia.android.app.shim.ViewBindingShimModule
 import org.oppia.android.app.topic.PracticeTabModule
 import org.oppia.android.app.utility.FontSizeMatcher.Companion.withFontSize
+import org.oppia.android.data.backends.gae.NetworkConfigProdModule
+import org.oppia.android.data.backends.gae.NetworkModule
 import org.oppia.android.domain.classify.InteractionsModule
 import org.oppia.android.domain.classify.rules.continueinteraction.ContinueModule
 import org.oppia.android.domain.classify.rules.dragAndDropSortInput.DragDropSortInputModule
@@ -47,10 +49,10 @@ import org.oppia.android.domain.hintsandsolution.HintsAndSolutionProdModule
 import org.oppia.android.domain.onboarding.ExpirationMetaDataRetrieverModule
 import org.oppia.android.domain.oppialogger.LogStorageModule
 import org.oppia.android.domain.oppialogger.loguploader.LogUploadWorkerModule
-import org.oppia.android.domain.oppialogger.loguploader.WorkManagerConfigurationModule
 import org.oppia.android.domain.platformparameter.PlatformParameterModule
 import org.oppia.android.domain.question.QuestionModule
 import org.oppia.android.domain.topic.PrimeTopicAssetsControllerModule
+import org.oppia.android.domain.workmanager.WorkManagerConfigurationModule
 import org.oppia.android.testing.TestLogReportingModule
 import org.oppia.android.testing.robolectric.RobolectricModule
 import org.oppia.android.testing.threading.TestDispatcherModule
@@ -177,8 +179,8 @@ class TestFontScaleConfigurationUtilActivityTest {
       HintsAndSolutionConfigModule::class, HintsAndSolutionProdModule::class,
       FirebaseLogUploaderModule::class, FakeOppiaClockModule::class, PracticeTabModule::class,
       DeveloperOptionsStarterModule::class, DeveloperOptionsModule::class,
-      ExplorationStorageModule::class, NetworkConnectionUtilDebugModule::class,
-      NetworkConnectionDebugUtilModule::class
+      ExplorationStorageModule::class, NetworkModule::class, NetworkConfigProdModule::class,
+      NetworkConnectionUtilDebugModule::class, NetworkConnectionDebugUtilModule::class
     ]
   )
   interface TestApplicationComponent : ApplicationComponent {
---
+++ domain/src/main/java/org/oppia/android/domain/platformparameter/syncup/PlatformParameterSyncUpWorkManagerInitializer.kt
@@ -0,0 +1,88 @@
+package org.oppia.android.domain.platformparameter.syncup
+
+import android.annotation.SuppressLint
+import android.content.Context
+import androidx.annotation.VisibleForTesting
+import androidx.work.Constraints
+import androidx.work.Data
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkManager
+import org.oppia.android.domain.oppialogger.ApplicationStartupListener
+import org.oppia.android.util.platformparameter.PlatformParameterValue
+import org.oppia.android.util.platformparameter.SyncUpWorkerTimePeriodHours
+import java.util.UUID
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
+
+/**
+ * Enqueues unique periodic work requests for fetching and caching latest platform parameter values
+ * from the remote service on application creation.
+ */
+class PlatformParameterSyncUpWorkManagerInitializer @Inject constructor(
+  private val context: Context,
+  @SyncUpWorkerTimePeriodHours private val workRequestRepeatInterval: PlatformParameterValue<Int>
+) : ApplicationStartupListener {
+
+  private val OPPIA_PLATFORM_PARAMETER_WORK_REQUEST_NAME = "OPPIA_PLATFORM_PARAMETER_WORK_REQUEST"
+
+  /** [Constraints] for platform parameter sync up work request. */
+  private val platformParameterSyncUpWorkerConstraints = Constraints.Builder()
+    .setRequiredNetworkType(NetworkType.CONNECTED)
+    .setRequiresBatteryNotLow(true)
+    .build()
+
+  /** [Data] for platform parameter sync up work request. */
+  private val workerTypeForSyncingPlatformParameters: Data = Data.Builder()
+    .putString(
+      PlatformParameterSyncUpWorker.WORKER_TYPE_KEY,
+      PlatformParameterSyncUpWorker.PLATFORM_PARAMETER_WORKER
+    )
+    .build()
+
+  /** [PeriodicWorkRequest] for platform parameter sync up worker. */
+  private val workRequestForSyncingPlatformParameters =
+    PeriodicWorkRequest.Builder(
+      PlatformParameterSyncUpWorker::class.java,
+      workRequestRepeatInterval.value.toLong(),
+      TimeUnit.HOURS
+    )
+      .addTag(PlatformParameterSyncUpWorker.TAG)
+      .setInputData(workerTypeForSyncingPlatformParameters)
+      .setConstraints(platformParameterSyncUpWorkerConstraints)
+      .build()
+
+  override fun onCreate() {
+    val workManager = WorkManager.getInstance(context)
+    workManager.enqueueUniquePeriodicWork(
+      OPPIA_PLATFORM_PARAMETER_WORK_REQUEST_NAME,
+      ExistingPeriodicWorkPolicy.KEEP,
+      workRequestForSyncingPlatformParameters
+    )
+  }
+
+  /** Returns the [UUID] of the work request that is enqueued to sync-up platform parameters. */
+  @VisibleForTesting
+  fun getSyncUpWorkRequestId(): UUID {
+    return workRequestForSyncingPlatformParameters.id
+  }
+
+  /** Returns the [Data] that goes into the work request enqueued to sync-up platform parameters. */
+  @VisibleForTesting
+  fun getSyncUpWorkRequestData(): Data {
+    return workerTypeForSyncingPlatformParameters
+  }
+
+  /** Returns the time interval of periodic work request enqueued to sync-up platform parameters. */
+  @SuppressLint("RestrictedApi") // getWorkSpec is restricted; suppression is fine for tests.
+  @VisibleForTesting
+  fun getSyncUpWorkerTimePeriod(): Long {
+    return workRequestForSyncingPlatformParameters.workSpec.intervalDuration
+  }
+
+  /** Returns the Worker [Constraints] set for the platform parameter sync-up work requests. */
+  fun getSyncUpWorkerConstraints(): Constraints {
+    return platformParameterSyncUpWorkerConstraints
+  }
+}
