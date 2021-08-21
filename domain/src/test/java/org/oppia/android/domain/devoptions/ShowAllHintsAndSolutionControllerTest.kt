4a6270189ba7331a682b1daf090b443cd2d943b9
yashraj-01 <yashrajprime@gmail.com>
2021-08-21 08:33:52 +0530

    Added tests for showAllHintsAndSolutionController

---
+++ domain/src/test/java/org/oppia/android/domain/devoptions/ShowAllHintsAndSolutionControllerTest.kt
@@ -0,0 +1,88 @@
+package org.oppia.android.domain.devoptions
+
+import android.app.Application
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth.assertThat
+import dagger.BindsInstance
+import dagger.Component
+import dagger.Module
+import dagger.Provides
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.oppia.android.testing.robolectric.RobolectricModule
+import org.robolectric.annotation.Config
+import org.robolectric.annotation.LooperMode
+import javax.inject.Inject
+import javax.inject.Singleton
+
+/** Tests for [ShowAllHintsAndSolutionController]. */
+@RunWith(AndroidJUnit4::class)
+@LooperMode(LooperMode.Mode.PAUSED)
+@Config(application = ShowAllHintsAndSolutionControllerTest.TestApplication::class)
+class ShowAllHintsAndSolutionControllerTest {
+
+  @Inject
+  lateinit var showAllHintsAndSolutionController: ShowAllHintsAndSolutionController
+
+  @Before
+  fun setUp() {
+    setUpTestApplicationComponent()
+  }
+
+  @Test
+  fun testGetShowAllHintsAndSolution_returnsFalse() {
+    val showAllHintsAndSolution = showAllHintsAndSolutionController.getShowAllHintsAndSolution()
+    assertThat(showAllHintsAndSolution).isFalse()
+  }
+
+  @Test
+  fun testSetShowAllHintsAndSolution_true_returnsTrue() {
+    showAllHintsAndSolutionController.setShowAllHintsAndSolution(isEnabled = true)
+    val showAllHintsAndSolution = showAllHintsAndSolutionController.getShowAllHintsAndSolution()
+    assertThat(showAllHintsAndSolution).isTrue()
+  }
+
+  private fun setUpTestApplicationComponent() {
+    ApplicationProvider.getApplicationContext<TestApplication>().inject(this)
+  }
+
+  // TODO(#89): Move this to a common test application component.
+  @Module
+  class TestModule {
+    @Provides
+    fun provideContext(application: Application): Context = application
+  }
+
+  @Singleton
+  @Component(
+    modules = [
+      TestModule::class, RobolectricModule::class
+    ]
+  )
+  interface TestApplicationComponent {
+    @Component.Builder
+    interface Builder {
+      @BindsInstance
+      fun setApplication(application: Application): Builder
+
+      fun build(): TestApplicationComponent
+    }
+
+    fun inject(showAllHintsAndSolutionControllerTest: ShowAllHintsAndSolutionControllerTest)
+  }
+
+  class TestApplication : Application() {
+    private val component: TestApplicationComponent by lazy {
+      DaggerShowAllHintsAndSolutionControllerTest_TestApplicationComponent.builder()
+        .setApplication(this)
+        .build()
+    }
+
+    fun inject(showAllHintsAndSolutionControllerTest: ShowAllHintsAndSolutionControllerTest) {
+      component.inject(showAllHintsAndSolutionControllerTest)
+    }
+  }
+}
