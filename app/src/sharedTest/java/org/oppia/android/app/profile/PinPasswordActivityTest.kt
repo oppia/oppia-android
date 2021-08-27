a29f0c29f3eae11e4fc31caad30ad41b0bc56a1a
veena14cs <veena14cs@gmail.com>
2021-08-27 23:36:28 +0530

    Update PinPasswordActivityTest.kt

--- app/src/sharedTest/java/org/oppia/android/app/profile/PinPasswordActivityTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/profile/PinPasswordActivityTest.kt
@@ -113,8 +113,7 @@ class PinPasswordActivityTest {
     PinPasswordActivity::class.java, /* initialTouchMode= */ true, /* launchActivity= */ false
   )
 
-  @Inject
-  lateinit var context: Context
+  @Inject lateinit var context: Context
 
   @Inject
   lateinit var profileTestHelper: ProfileTestHelper
