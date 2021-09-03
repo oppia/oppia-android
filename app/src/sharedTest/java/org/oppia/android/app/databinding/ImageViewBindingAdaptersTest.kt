8fccdfaa02caf537fc3456dadf171b7f9dcacf81
yash10019coder <lit2020066@iiitl.ac.in>
2021-09-03 23:55:15 +0530

    added test activity file and writeen some starter code on it

---
+++ app/src/sharedTest/java/org/oppia/android/app/databinding/ImageViewBindingAdaptersTest.kt
@@ -0,0 +1,17 @@
+package org.oppia.android.app.databinding;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class ImageViewBindingAdaptersTest {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+}
