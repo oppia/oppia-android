9f3754d4d5be61432e42bb95e38f77305cbb7204
yash10019coder <lit2020066@iiitl.ac.in>
2021-09-08 10:37:07 +0530

    Added test activity for ImageViewBindingAdapters

---
+++ app/src/main/java/org/oppia/android/app/testing/ImageViewBindingAdaptersTestActivity.kt
@@ -0,0 +1,12 @@
+package org.oppia.android.app.testing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.oppia.android.R
+
+class ImageViewBindingAdaptersTestActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_image_view_binding_adapters_test)
+  }
+}
