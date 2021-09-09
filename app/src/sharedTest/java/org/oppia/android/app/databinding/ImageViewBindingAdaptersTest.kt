746363a0743926965f47581ac04140542dd4952e
yash10019coder <lit2020066@iiitl.ac.in>
2021-09-09 17:07:13 +0530

    added first test case

--- app/src/sharedTest/java/org/oppia/android/app/databinding/ImageViewBindingAdaptersTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/databinding/ImageViewBindingAdaptersTest.kt
@@ -7,9 +7,12 @@ import android.content.Intent
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.databinding.adapters.ImageViewBindingAdapter
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
@@ -63,6 +66,7 @@ import org.oppia.android.testing.TestImageLoaderModule
 import org.oppia.android.testing.TestLogReportingModule
 import org.oppia.android.testing.robolectric.RobolectricModule
 import org.oppia.android.testing.threading.TestDispatcherModule
+import org.oppia.android.app.databinding.ImageViewBindingAdapters
 import org.oppia.android.testing.time.FakeOppiaClockModule
 import org.oppia.android.util.accessibility.AccessibilityTestModule
 import org.oppia.android.util.caching.testing.CachingTestModule
@@ -73,10 +77,10 @@ import org.oppia.android.util.networking.NetworkConnectionDebugUtilModule
 import org.oppia.android.util.networking.NetworkConnectionUtilDebugModule
 import org.oppia.android.util.parser.html.HtmlParserEntityTypeModule
 import org.oppia.android.util.parser.image.ImageParsingModule
-import org.robolectric.annotation.Config
 import org.robolectric.annotation.LooperMode
 import javax.inject.Inject
 import javax.inject.Singleton
+import kotlinx.android.synthetic.main.activity_image_view_binding_adapters_test.*
 import org.oppia.android.app.testing.ImageViewBindingAdaptersTestActivity
 
 /** Default value for float comparison. */
@@ -107,5 +111,93 @@ class ImageViewBindingAdaptersTest {
   @After
   fun tearDown() {
     Intents.release()
+  }
+
+  @Test
+  fun setImageDrawableWithGlide() {
+    val imageView = activityRule.scenario.runWithActivity {
+      val imageView = it.findViewById<ImageView>(R.id.imageView)
+      ImageViewBindingAdapters.setImageDrawable(imageView, R.drawable.bg_blue_card)
+      return@runWithActivity imageView
+    }
+    assertThat(imageView.drawable.isVisible)
+    assertThat(imageView.drawable.isStateful)
+  }
+
+  private inline fun <reified V, A : Activity> ActivityScenario<A>.runWithActivity(
+    crossinline action: (A) -> V
+  ): V {
+    // Use Mockito to ensure the routine is actually executed before returning the result.
+    @Suppress("UNCHECKED_CAST") // The unsafe cast is necessary to make the routine generic.
+    val fakeMock: Consumer<V> = mock(Consumer::class.java) as Consumer<V>
+    val valueCaptor = ArgumentCaptor.forClass(V::class.java)
+    onActivity { fakeMock.consume(action(it)) }
+    verify(fakeMock).consume(valueCaptor.capture())
+    return valueCaptor.value
+  }
+
+  private fun setUpTestApplicationComponent() {
+    ApplicationProvider.getApplicationContext<TestApplication>().inject(this)
+  }// TODO(#59): Figure out a way to reuse modules instead of needing to re-declare them.
+
+  @Singleton
+  @Component(
+    modules = [
+      RobolectricModule::class,
+      PlatformParameterModule::class,
+      TestDispatcherModule::class, ApplicationModule::class,
+      LoggerModule::class, ContinueModule::class, FractionInputModule::class,
+      ItemSelectionInputModule::class, MultipleChoiceInputModule::class,
+      NumberWithUnitsRuleModule::class, NumericInputRuleModule::class, TextInputRuleModule::class,
+      DragDropSortInputModule::class, ImageClickInputModule::class, InteractionsModule::class,
+      GcsResourceModule::class, TestImageLoaderModule::class, ImageParsingModule::class,
+      HtmlParserEntityTypeModule::class, QuestionModule::class, TestLogReportingModule::class,
+      AccessibilityTestModule::class, LogStorageModule::class, CachingTestModule::class,
+      PrimeTopicAssetsControllerModule::class, ExpirationMetaDataRetrieverModule::class,
+      ViewBindingShimModule::class, RatioInputModule::class, WorkManagerConfigurationModule::class,
+      ApplicationStartupListenerModule::class, LogUploadWorkerModule::class,
+      HintsAndSolutionConfigModule::class, HintsAndSolutionProdModule::class,
+      FirebaseLogUploaderModule::class, FakeOppiaClockModule::class, PracticeTabModule::class,
+      DeveloperOptionsStarterModule::class, DeveloperOptionsModule::class,
+      ExplorationStorageModule::class, NetworkModule::class, NetworkConfigProdModule::class,
+      NetworkConnectionUtilDebugModule::class, NetworkConnectionDebugUtilModule::class
+    ]
+  )
+  /** Create a TestApplicationComponent. */
+  interface TestApplicationComponent : ApplicationComponent {
+    /** Build the TestApplicationComponent. */
+    @Component.Builder
+    interface Builder : ApplicationComponent.Builder
+
+    /** Inject [MarginBindingAdaptersTest] in TestApplicationComponent . */
+    fun inject(imageViewBindingAdaptersTest: ImageViewBindingAdaptersTest)
+  }
+
+  /**
+   * Class to override a dependency throughout the test application, instead of overriding the
+   * dependencies in every test class, we can just do it once by extending the Application class.
+   */
+  class TestApplication : Application(), ActivityComponentFactory, ApplicationInjectorProvider {
+    private val component: TestApplicationComponent by lazy {
+      DaggerMarginBindingAdaptersTest_TestApplicationComponent.builder()
+        .setApplication(this)
+        .build() as TestApplicationComponent
+    }
+
+    /** Inject [MarginBindingAdaptersTest] in TestApplicationComponent . */
+    fun inject(imageViewBindingAdaptersTest: ImageViewBindingAdaptersTest) {
+      component.inject(imageViewBindingAdaptersTest)
+    }
+
+    override fun createActivityComponent(activity: AppCompatActivity): ActivityComponent {
+      return component.getActivityComponentBuilderProvider().get().setActivity(activity).build()
+    }
+
+    override fun getApplicationInjector(): ApplicationInjector = component
+  }
+
+  private interface Consumer<T> {
+    /** Represents an operation that accepts a single input argument and returns no result. */
+    fun consume(value: T)
   }
 }
