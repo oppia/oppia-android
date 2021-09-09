6a39383f963c5e93f30afb4a37f09b273081730f
yash10019coder <lit2020066@iiitl.ac.in>
2021-09-09 17:52:16 +0530

    added comment in ImageViewBindingAdaptersTestActivity

--- app/src/main/java/org/oppia/android/app/testing/ImageViewBindingAdaptersTestActivity.kt
+++ app/src/main/java/org/oppia/android/app/testing/ImageViewBindingAdaptersTestActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import org.oppia.android.R
 
+/** Test Activity for ImageViewBindingAdapters. */
 class ImageViewBindingAdaptersTestActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
