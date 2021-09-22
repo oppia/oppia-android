1025bf538027c07825bce4a645adaf6af61f39db
darkmat13r <darkmat13r@gmail.com>
2021-09-22 20:59:46 +0530

    correct chapter summary tests

--- domain/src/test/java/org/oppia/android/domain/topic/TopicControllerTest.kt
+++ domain/src/test/java/org/oppia/android/domain/topic/TopicControllerTest.kt
@@ -423,7 +423,7 @@ class TopicControllerTest {
     val chapterSummary = chapterSummaryResultCaptor.value.getOrThrow()
     assertThat(chapterSummary.name).isEqualTo("What is a Fraction?")
     assertThat(chapterSummary.summary)
-      .isEqualTo("This is outline/summary for <b>What is a Fraction?</b>")
+      .isEqualTo("Matthew learns about fractions.")
   }
 
   @Test
