229f1efa950a6d895f0847b7581bf1e9b59278de
viktoriia <viktoriia@gmail.com>
2021-09-27 21:48:56 -0700

    New test extras

--- app/src/sharedTest/java/org/oppia/android/app/databinding/ImageViewBindingAdaptersTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/databinding/ImageViewBindingAdaptersTest.kt
@@ -9,10 +9,13 @@ import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import dagger.Component
+import org.junit.After
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -95,6 +98,17 @@ class ImageViewBindingAdaptersTest {
         ImageViewBindingAdaptersTestActivity::class.java
       )
     )
+
+  @Before
+  fun setUp() {
+    ApplicationProvider.getApplicationContext<TestApplication>().inject(this)
+    Intents.init()
+  }
+
+  @After
+  fun tearDown() {
+    Intents.release()
+  }
 
   @Config(qualifiers = "port")
   @Test
