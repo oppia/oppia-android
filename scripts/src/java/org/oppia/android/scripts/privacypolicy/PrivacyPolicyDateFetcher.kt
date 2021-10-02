6c9019cf0e61261b1515f2e91a1e87db6655bd10
Veena <veena14cs@gmail.com>
2021-10-02 15:06:09 -0700

    removed scripts files

--- scripts/src/java/org/oppia/android/scripts/privacypolicy/PrivacyPolicyDateFetcher.kt
+++
@@ -1,7 +0,0 @@
-package org.oppia.android.scripts.license
-
-/** Utility class to extract the privacy policy date text from a given URL. */
-interface PrivacyPolicyDateFetcher {
-  /** Scrapes and returns the text from a given URL. */
-  fun scrapeText(link: String): String
-}
