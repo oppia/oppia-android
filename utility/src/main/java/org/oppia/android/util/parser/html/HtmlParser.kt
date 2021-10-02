d06a03d0fe5749c1fad1c0e5c2b52eb3f77db7e1
Veena <veena14cs@gmail.com>
2021-10-02 14:58:14 -0700

    added func to handle url clicks

--- utility/src/main/java/org/oppia/android/util/parser/html/HtmlParser.kt
+++ utility/src/main/java/org/oppia/android/util/parser/html/HtmlParser.kt
@@ -2,7 +2,10 @@ package org.oppia.android.util.parser.html
 
 import android.text.Spannable
 import android.text.SpannableStringBuilder
+import android.text.Spanned
 import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.text.style.URLSpan
 import android.view.View
 import android.widget.TextView
 import androidx.core.view.ViewCompat
@@ -75,6 +78,8 @@ class HtmlParser private constructor(
         .replace("</li>", "</$CUSTOM_BULLET_LIST_TAG>")
     }
 
+    htmlContentTextView.handleUrlClicks {}
+
     // https://stackoverflow.com/a/8662457
     if (supportsLinks) {
       htmlContentTextView.movementMethod = LinkMovementMethod.getInstance()
@@ -96,6 +101,34 @@ class HtmlParser private constructor(
 
   private fun getLayoutDirection(view: View): Int {
     return ViewCompat.getLayoutDirection(view)
+  }
+
+  /**
+   * Searches for all URLSpans in current text replaces them with our own ClickableSpans
+   * forwards clicks to provided function.
+   */
+  fun TextView.handleUrlClicks(onClicked: ((String) -> Unit)? = null) {
+    //create span builder and replaces current text with it
+    text = SpannableStringBuilder.valueOf(text).apply {
+      //search for all URL spans and replace all spans with our own clickable spans
+      getSpans(0, length, URLSpan::class.java).forEach {
+        //add new clickable span at the same position
+        setSpan(
+          object : ClickableSpan() {
+            override fun onClick(widget: View) {
+              onClicked?.invoke(it.url)
+            }
+          },
+          getSpanStart(it),
+          getSpanEnd(it),
+          Spanned.SPAN_INCLUSIVE_EXCLUSIVE
+        )
+        //remove old URLSpan
+        removeSpan(it)
+      }
+    }
+    //make sure movement method is set
+    movementMethod = LinkMovementMethod.getInstance()
   }
 
   private fun computeCustomTagHandlers(
