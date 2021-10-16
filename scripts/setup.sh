926ccd16126b87801abf881def2b733bfadf9ed8
farees <fareezzhussain@gmail.com>
2021-10-17 00:35:15 +0530

    Fixed command only for darwin

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -28,9 +28,9 @@ bash ../oppia-android/scripts/buf_download.sh
 
 # Add protobuf platform for M1 Mac
 arch_name="$(uname -m)"
-if [ "${arch_name}" = "x86_64" ]; then
+if [ "${arch_name}" = "x86_64" ] && [[ "$OSTYPE" == "darwin"* ]]; then
     if [ "$(sysctl -in sysctl.proc_translated)" = "1" ]; then
-        echo '\nprotobuf_platform=osx-x86_64' >> ../oppia-android/local.properties
+        echo 'protobuf_platform=osx-x86_64' >> ../oppia-android/local.properties
         echo 'Added protobuf platform to local properties.'
     fi
 fi
