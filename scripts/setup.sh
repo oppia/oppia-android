80dd7aa1c5a60d87c0cad289e5e66b454ce73603
farees <fareezzhussain@gmail.com>
2021-10-23 11:35:41 +0530

    seperated the script, removed misc and run configurations

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -27,10 +27,4 @@ bash ../oppia-android/scripts/checkstyle_download.sh
 bash ../oppia-android/scripts/buf_download.sh
 
 # Add protobuf platform for M1 Mac
-arch_name="$(uname -m)"
-if [ "${arch_name}" = "x86_64" ] && [[ "$OSTYPE" == "darwin"* ]]; then
-    if [ "$(sysctl -in sysctl.proc_translated)" = "1" ]; then
-        echo 'protobuf_platform=osx-x86_64' >> ../oppia-android/local.properties
-        echo 'Added protobuf platform to local properties.'
-    fi
-fi
+bash ../oppia-android/scripts/buf_m1_mac_setup.sh
