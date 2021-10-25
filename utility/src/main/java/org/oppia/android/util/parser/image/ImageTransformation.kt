f97e13fb67d22776ef51342ff4c691c3077d67b5
yash10019coder <lit2020066@iiitl.ac.in>
2021-10-25 19:45:57 +0530

    documented the ImageTransformation.kt according to the Kdoc

--- utility/src/main/java/org/oppia/android/util/parser/image/ImageTransformation.kt
+++ utility/src/main/java/org/oppia/android/util/parser/image/ImageTransformation.kt
@@ -1,6 +1,6 @@
 package org.oppia.android.util.parser.image
 
+/** Represents Blur Transformation on an [ImageTarget]. */
 enum class ImageTransformation {
-  /** Represents Blur Transformation on an [ImageTarget]. */
   BLUR
 }
