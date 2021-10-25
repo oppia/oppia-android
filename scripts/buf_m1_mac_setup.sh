de07972c8cb6c3e28ef3e763239b22d966ee4a6a
Farees Hussain <fareezzhussain@gmail.com>
2021-10-26 00:31:56 +0530

    Update scripts/buf_m1_mac_setup.sh
    Co-authored-by: Akshay Nandwana <akshaynandwana001@gmail.com>

--- scripts/buf_m1_mac_setup.sh
+++ scripts/buf_m1_mac_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Add protobuf platform for M1 Mac
+# Setup protobuf platform for M1 Mac
 arch_name="$(uname -m)"
 if [ "${arch_name}" = "x86_64" ] && [[ "$OSTYPE" == "darwin"* ]]; then
     if [ "$(sysctl -in sysctl.proc_translated)" = "1" ]; then
