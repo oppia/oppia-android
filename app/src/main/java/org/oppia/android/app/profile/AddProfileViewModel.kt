33bc621a855aa3fa160b6ea5f63f062c4a23ec24
rishidyno <rishiraj727909@gmail.com>
2021-10-26 21:27:48 +0530

    used-different-approach-to-get-string-resources

--- app/src/main/java/org/oppia/android/app/profile/AddProfileViewModel.kt
+++ app/src/main/java/org/oppia/android/app/profile/AddProfileViewModel.kt
@@ -3,13 +3,14 @@ package org.oppia.android.app.profile
 import android.content.Context
 import androidx.databinding.ObservableField
 import org.oppia.android.app.activity.ActivityScope
+import org.oppia.android.app.translation.AppLanguageResourceHandler
 import org.oppia.android.app.viewmodel.ObservableViewModel
 import org.oppia.android.R
 import javax.inject.Inject
 
 /** The ViewModel for [AddProfileActivity]. */
 @ActivityScope
-class AddProfileViewModel @Inject constructor(context: Context) : ObservableViewModel() {
+class AddProfileViewModel @Inject constructor(resourceHandler: AppLanguageResourceHandler, context: Context) : ObservableViewModel() {
   val validPin = ObservableField(false)
   val pinErrorMsg = ObservableField("")
   val confirmPinErrorMsg = ObservableField("")
@@ -20,7 +21,7 @@ class AddProfileViewModel @Inject constructor(context: Context) : ObservableView
   val createPin = ObservableField(false)
   val isButtonActive = ObservableField(false)
   val showInfoAlertPopup = ObservableField<Boolean>(false)
-  val starRequired: String = "*" + context.resources.getString(R.string.add_profile_required)
+  val starRequired: String = "*" + resourceHandler.getStringInLocale(R.string.add_profile_required)
 
   fun clearAllErrorMessages() {
     pinErrorMsg.set("")
