08036d7487da23a940eb3c861afcdbb39624ec26
Veena <veena14cs@gmail.com>
2021-10-30 17:42:06 -0700

    fixed naming

--- app/src/main/java/org/oppia/android/app/help/LoadPoliciesFragmentListener.kt
+++ app/src/main/java/org/oppia/android/app/help/LoadPoliciesFragmentListener.kt
@@ -5,5 +5,5 @@ import org.oppia.android.app.policies.Policies
 /** Listener for when a selection should result in opening the policies on tablet. */
 interface LoadPoliciesFragmentListener {
   /**  Called when the user wants to open the policies in tablet devices. */
-  fun loadPoliciesFragment(privacyPolicy: Policies)
+  fun loadPoliciesFragment(policies: Policies)
 }
