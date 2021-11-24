fd46b82806542a4c9254a21ca86cb7354bd9831c
Veena <veena14cs@gmail.com>
2021-11-24 23:48:16 +0530

    Update OnboardingFragmentTest.kt

--- app/src/sharedTest/java/org/oppia/android/app/onboarding/OnboardingFragmentTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/onboarding/OnboardingFragmentTest.kt
@@ -1,10 +1,17 @@
 package org.oppia.android.app.onboarding
 
+import android.app.Activity
 import android.app.Application
 import android.content.Context
 import android.content.res.Resources
+import android.text.Spannable
+import android.text.style.ClickableSpan
 import android.view.View
+import android.widget.TextView
+import androidx.annotation.DimenRes
+import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ActivityScenario.launch
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
@@ -29,6 +36,7 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.viewpager2.widget.ViewPager2
+import com.google.common.truth.Truth.assertThat
 import dagger.Component
 import org.hamcrest.CoreMatchers.allOf
 import org.hamcrest.CoreMatchers.not
@@ -38,6 +46,14 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.ArgumentCaptor
+import org.mockito.Captor
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.times
+import org.mockito.Mockito.verify
+import org.mockito.junit.MockitoJUnit
+import org.mockito.junit.MockitoRule
 import org.oppia.android.R
 import org.oppia.android.app.activity.ActivityComponent
 import org.oppia.android.app.activity.ActivityComponentFactory
@@ -48,6 +64,7 @@ import org.oppia.android.app.application.ApplicationModule
 import org.oppia.android.app.application.ApplicationStartupListenerModule
 import org.oppia.android.app.devoptions.DeveloperOptionsModule
 import org.oppia.android.app.devoptions.DeveloperOptionsStarterModule
+import org.oppia.android.app.model.PolicyPage
 import org.oppia.android.app.profile.ProfileChooserActivity
 import org.oppia.android.app.shim.ViewBindingShimModule
 import org.oppia.android.app.topic.PracticeTabModule
@@ -80,6 +97,7 @@ import org.oppia.android.domain.workmanager.WorkManagerConfigurationModule
 import org.oppia.android.testing.AccessibilityTestRule
 import org.oppia.android.testing.TestLogReportingModule
 import org.oppia.android.testing.junit.InitializeDefaultLocaleRule
+import org.oppia.android.testing.mockito.capture
 import org.oppia.android.testing.robolectric.RobolectricModule
 import org.oppia.android.testing.threading.TestCoroutineDispatchers
 import org.oppia.android.testing.threading.TestDispatcherModule
@@ -87,12 +105,14 @@ import org.oppia.android.testing.time.FakeOppiaClockModule
 import org.oppia.android.util.accessibility.AccessibilityTestModule
 import org.oppia.android.util.caching.AssetModule
 import org.oppia.android.util.caching.testing.CachingTestModule
+import org.oppia.android.util.gcsresource.DefaultResourceBucketName
 import org.oppia.android.util.gcsresource.GcsResourceModule
 import org.oppia.android.util.locale.LocaleProdModule
 import org.oppia.android.util.logging.LoggerModule
 import org.oppia.android.util.logging.firebase.FirebaseLogUploaderModule
 import org.oppia.android.util.networking.NetworkConnectionDebugUtilModule
 import org.oppia.android.util.networking.NetworkConnectionUtilDebugModule
+import org.oppia.android.util.parser.html.HtmlParser
 import org.oppia.android.util.parser.html.HtmlParserEntityTypeModule
 import org.oppia.android.util.parser.image.GlideImageLoaderModule
 import org.oppia.android.util.parser.image.ImageParsingModule
@@ -100,6 +120,7 @@ import org.robolectric.annotation.Config
 import org.robolectric.annotation.LooperMode
 import javax.inject.Inject
 import javax.inject.Singleton
+import kotlin.reflect.KClass
 
 /** Tests for [OnboardingFragment]. */
 @RunWith(AndroidJUnit4::class)
@@ -119,7 +140,24 @@ class OnboardingFragmentTest {
   lateinit var testCoroutineDispatchers: TestCoroutineDispatchers
 
   @Inject
+  lateinit var htmlParserFactory: HtmlParser.Factory
+
+  @Inject
   lateinit var context: Context
+
+  @Inject
+  @field:DefaultResourceBucketName
+  lateinit var resourceBucketName: String
+
+  @Captor
+  lateinit var policyPageCaptor: ArgumentCaptor<PolicyPage>
+
+  @Mock
+  lateinit var mockPolicyOppiaTagActionListener: HtmlParser.PolicyOppiaTagActionListener
+
+  @Rule
+  @JvmField
+  val mockitoRule: MockitoRule = MockitoJUnit.rule()
 
   @Before
   fun setUp() {
@@ -636,6 +674,73 @@ class OnboardingFragmentTest {
     }
   }
 
+  @Test
+  fun testOnboardingFragment_checkSlide3_policiesLinkIsVisible() {
+    launch(OnboardingActivity::class.java).use {
+      onView(withId(R.id.skip_text_view)).perform(click())
+      testCoroutineDispatchers.runCurrent()
+      onView(withId(R.id.slide_terms_of_service_and_privacy_policy_links_text_view)).perform(
+        scrollTo()
+      )
+      onView(withId(R.id.slide_terms_of_service_and_privacy_policy_links_text_view)).check(
+        matches(
+          isDisplayed()
+        )
+      )
+    }
+  }
+
+  @Test
+  fun testHtmlContent_withPolicies_linkSupport_clickSpan_callsTagListener() {
+    val htmlParser = htmlParserFactory.create(
+      resourceBucketName,
+      entityType = "",
+      entityId = "",
+      imageCenterAlign = true,
+      policyOppiaTagActionListener = mockPolicyOppiaTagActionListener
+    )
+    testCoroutineDispatchers.runCurrent()
+
+    launch(OnboardingActivity::class.java).use {
+      onView(withId(R.id.skip_text_view)).perform(click())
+      testCoroutineDispatchers.runCurrent()
+
+      val textView: TextView =
+        it.findViewById(R.id.slide_terms_of_service_and_privacy_policy_links_text_view)
+
+      val htmlResult: Spannable = htmlParser.parseOppiaHtml(
+        "By using %s, you agree to our <br> <oppia-noninteractive-policy " +
+          "link=\"Terms of Service\"> Terms of Service</oppia-noninteractive-policy> and" +
+          " <oppia-noninteractive-policy link=\"Privacy Policy\">Privacy Policy" +
+          "</oppia-noninteractive-policy>.",
+        textView,
+        supportsLinks = true,
+        supportsConceptCards = false
+      )
+      textView.text = htmlResult
+
+      // Verify the displayed text is correct & has a clickable span.
+      val clickableSpans = htmlResult.getSpansFromWholeString(ClickableSpan::class)
+      assertThat(htmlResult.toString()).isEqualTo(
+        "By using %s, you agree to our \n" +
+          "Terms of Service and Privacy Policy."
+      )
+      assertThat(clickableSpans).hasLength(2)
+      clickableSpans[0].onClick(textView)
+      clickableSpans[1].onClick(textView)
+
+      // Verify that the tag listener is called.
+      verify(mockPolicyOppiaTagActionListener, times(2)).onPolicyPageLinkClicked(
+        capture(policyPageCaptor)
+      )
+
+      assertThat(policyPageCaptor.allValues).containsExactly(
+        PolicyPage.TERMS_OF_SERVICE,
+        PolicyPage.PRIVACY_POLICY
+      ).inOrder()
+    }
+  }
+
   private fun getResources(): Resources =
     ApplicationProvider.getApplicationContext<Context>().resources
 
@@ -659,6 +764,33 @@ class OnboardingFragmentTest {
       }
     }
   }
+
+  private fun <A : Activity> ActivityScenario<A>.getDimensionPixelSize(
+    @DimenRes dimenResId: Int
+  ): Int {
+    return runWithActivity { it.resources.getDimensionPixelSize(dimenResId) }
+  }
+
+  private inline fun <A : Activity, reified V : View> ActivityScenario<A>.findViewById(
+    @IdRes viewResId: Int
+  ): V {
+    return runWithActivity { it.findViewById(viewResId) }
+  }
+
+  private inline fun <reified V, A : Activity> ActivityScenario<A>.runWithActivity(
+    crossinline action: (A) -> V
+  ): V {
+    // Use Mockito to ensure the routine is actually executed before returning the result.
+    @Suppress("UNCHECKED_CAST") // The unsafe cast is necessary to make the routine generic.
+    val fakeMock: Consumer<V> = Mockito.mock(Consumer::class.java) as Consumer<V>
+    val valueCaptor = ArgumentCaptor.forClass(V::class.java)
+    onActivity { fakeMock.consume(action(it)) }
+    Mockito.verify(fakeMock).consume(valueCaptor.capture())
+    return valueCaptor.value
+  }
+
+  private fun <T : Any> Spannable.getSpansFromWholeString(spanClass: KClass<T>): Array<T> =
+    getSpans(/* start= */ 0, /* end= */ length, spanClass.javaObjectType)
 
   // TODO(#59): Figure out a way to reuse modules instead of needing to re-declare them.
   @Singleton
@@ -708,5 +840,9 @@ class OnboardingFragmentTest {
     }
 
     override fun getApplicationInjector(): ApplicationInjector = component
+  }
+
+  private interface Consumer<T> {
+    fun consume(value: T)
   }
 }
