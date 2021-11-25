4f1c4843e1c7760f1350c2cbf9fa3b2962866ea2
Veena <veena14cs@gmail.com>
2021-11-26 00:26:55 +0530

    add kdoc

--- app/src/main/java/org/oppia/android/app/policies/PoliciesActivity.kt
+++ app/src/main/java/org/oppia/android/app/policies/PoliciesActivity.kt
@@ -30,6 +30,7 @@ class PoliciesActivity : InjectableAppCompatActivity() {
   }
 
   companion object {
+    // TODO(#1655): Re-restrict access to fields in tests post-Gradle.
     const val POLICIES_ACTIVITY_POLICY_PAGE_ARGUMENT_PROTO = "PoliciesActivity.policy_page"
 
     /** Returns the [Intent] for opening [PoliciesActivity]. */
