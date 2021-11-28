45303b493a99cac9c8f1acc732e44e876e1a3489
yash10019coder <lit2020066@iiitl.ac.in>
2021-11-28 12:15:20 +0530

    renamed the key properly

--- app/src/main/java/org/oppia/android/app/settings/profile/ProfileRenameFragment.kt
+++ app/src/main/java/org/oppia/android/app/settings/profile/ProfileRenameFragment.kt
@@ -15,13 +15,13 @@ class ProfileRenameFragment : InjectableFragment() {
   lateinit var profileRenameFragmentPresenter: ProfileRenameFragmentPresenter
 
   companion object {
-    private const val PROFILE_ID = "Profile_Id"
+    private const val PROFILE_ID_ARGUMENT_KEY = "ProfileRenameFragment.profile_id"
 
     /** Returns the instance of [ProfileRenameFragment]. */
     fun newInstance(profileId: Int): ProfileRenameFragment {
       val profileRenameFragment = ProfileRenameFragment()
       val args = Bundle()
-      args.putInt(PROFILE_ID, profileId)
+      args.putInt(PROFILE_ID_ARGUMENT_KEY, profileId)
       profileRenameFragment.arguments = args
       return profileRenameFragment
     }
@@ -39,7 +39,7 @@ class ProfileRenameFragment : InjectableFragment() {
   ): View? {
     val args =
       checkNotNull(arguments) { "Expected arguments to be passed to ProfileRenameFragment" }
-    val profileId = args.getInt(PROFILE_ID)
+    val profileId = args.getInt(PROFILE_ID_ARGUMENT_KEY)
     return profileRenameFragmentPresenter.handleCreateView(
       inflater,
       container,
