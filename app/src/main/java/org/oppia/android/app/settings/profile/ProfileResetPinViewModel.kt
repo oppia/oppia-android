7949438099bcd70f3f11a88519b4b07679818211
yash10019coder <lit2020066@iiitl.ac.in>
2021-12-02 12:38:09 +0530

    nit fixes.

--- app/src/main/java/org/oppia/android/app/settings/profile/ProfileResetPinViewModel.kt
+++ app/src/main/java/org/oppia/android/app/settings/profile/ProfileResetPinViewModel.kt
@@ -8,21 +8,21 @@ import javax.inject.Inject
 /** The ViewModel for [ProfileResetPinActivity]. */
 @ActivityScope
 class ProfileResetPinViewModel @Inject constructor() : ObservableViewModel() {
-  /** variable for storing error message in first text input layout. */
+  /** Variable for storing error message in first text input layout. */
   val pinErrorMsg = ObservableField("")
 
-  /** variable for storing error message in second text input layout. */
+  /** Variable for storing error message in second text input layout. */
   val confirmErrorMsg = ObservableField("")
 
-  /** for confirming if the profile is admin or not. */
+  /** For confirming if the profile is admin or not. */
   val isAdmin = ObservableField(false)
 
-  /** for storing input pin entered by user in first text input layout. */
+  /** For storing input pin entered by user in first text input layout. */
   val inputPin = ObservableField("")
 
-  /** for storing input pin entered by user in second text input layout. */
+  /** For storing input pin entered by user in second text input layout. */
   val inputConfirmPin = ObservableField("")
 
-  /** for verification if both pins are same then only enable the button. */
+  /** For verification if both pins are same then only enable the button. */
   val isButtonActive = ObservableField(false)
 }
