2f020bf86d8aa60af2796b301b339d574e4fbe67
Rohit Kumar <rohit@nobleeden.in>
2021-12-06 02:08:58 +0530

    nit changes

--- app/src/main/java/org/oppia/android/app/settings/profile/ProfileRenameActivityPresenter.kt
+++ app/src/main/java/org/oppia/android/app/settings/profile/ProfileRenameActivityPresenter.kt
@@ -7,7 +7,6 @@ import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
-import javax.inject.Inject
 import org.oppia.android.R
 import org.oppia.android.app.activity.ActivityScope
 import org.oppia.android.app.model.ProfileId
@@ -18,6 +17,7 @@ import org.oppia.android.databinding.ProfileRenameActivityBinding
 import org.oppia.android.domain.profile.ProfileManagementController
 import org.oppia.android.util.data.AsyncResult
 import org.oppia.android.util.data.DataProviders.Companion.toLiveData
+import javax.inject.Inject
 
 /** The presenter for [ProfileRenameActivity]. */
 @ActivityScope
