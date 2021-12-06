bc42fde1f9c574bdc338aaa68b740f13e84850ce
yash10019coder <lit2020066@iiitl.ac.in>
2021-12-07 01:04:03 +0530

    nit fixes.

--- app/src/main/java/org/oppia/android/app/settings/profile/ProfileListFragmentPresenter.kt
+++ app/src/main/java/org/oppia/android/app/settings/profile/ProfileListFragmentPresenter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import org.oppia.android.app.administratorcontrols.AdministratorControlsActivityPresenter
+import org.oppia.android.app.administratorcontrols.LoadProfileEditListener
 import org.oppia.android.app.fragment.FragmentScope
 import org.oppia.android.app.model.Profile
 import org.oppia.android.app.recyclerview.BindableAdapter
@@ -13,7 +14,6 @@ import org.oppia.android.app.viewmodel.ViewModelProvider
 import org.oppia.android.databinding.ProfileListFragmentBinding
 import org.oppia.android.databinding.ProfileListProfileViewBinding
 import javax.inject.Inject
-import org.oppia.android.app.administratorcontrols.LoadProfileEditListener
 
 /** The presenter for [ProfileListFragment]. */
 @FragmentScope
