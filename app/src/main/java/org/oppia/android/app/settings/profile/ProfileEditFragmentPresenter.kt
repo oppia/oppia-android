e6be9b9308ffa41fa0a816a7f1faa6268ec9649b
yash10019coder <lit2020066@iiitl.ac.in>
2021-12-14 15:43:56 +0530

    line wrapped the comment in ProfileEditFragmentPresenter.kt

--- app/src/main/java/org/oppia/android/app/settings/profile/ProfileEditFragmentPresenter.kt
+++ app/src/main/java/org/oppia/android/app/settings/profile/ProfileEditFragmentPresenter.kt
@@ -115,7 +115,11 @@ class ProfileEditFragmentPresenter @Inject constructor(
     dialogFragment.showNow(fragment.childFragmentManager, TAG_PROFILE_DELETION_DIALOG)
   }
 
-  /** Requests the specific profile to be deleted from the app and then routes the user either to administrator controls activity or profile list activity depending on whether they are currently using a tablet device. */
+  /** Requests the specific profile to be deleted from the app
+   * and then routes the user either to administrator controls
+   * activity or profile list activity depending on whether
+   * they are currently using a tablet device.
+   */
   fun deleteProfile(internalProfileId: Int) {
     profileManagementController
       .deleteProfile(ProfileId.newBuilder().setInternalId(internalProfileId).build()).toLiveData()
