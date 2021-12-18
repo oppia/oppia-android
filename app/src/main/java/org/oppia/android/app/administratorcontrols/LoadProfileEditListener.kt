0f3e3a52c56b9d9467519623b8d31e9889da158a
yash10019coder <lit2020066@iiitl.ac.in>
2021-12-18 21:58:21 +0530

    added kdocs for methods.

--- app/src/main/java/org/oppia/android/app/administratorcontrols/LoadProfileEditListener.kt
+++ app/src/main/java/org/oppia/android/app/administratorcontrols/LoadProfileEditListener.kt
@@ -2,5 +2,6 @@ package org.oppia.android.app.administratorcontrols
 
 /** Listener for when an activity should load [ProfileEditFragment]. */
 interface LoadProfileEditListener {
+  /** This method is called when the fragment of [ProfileEditFragment] is inflated. */
   fun loadProfileEdit(proifleId: Int, profileName: String)
 }
