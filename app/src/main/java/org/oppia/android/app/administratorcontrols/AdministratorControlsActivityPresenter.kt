de347792e12498657b35a7b3efafe894f0e0f0d8
yash10019coder <lit2020066@iiitl.ac.in>
2021-12-18 20:46:45 +0530

    nit fixes.

--- app/src/main/java/org/oppia/android/app/administratorcontrols/AdministratorControlsActivityPresenter.kt
+++ app/src/main/java/org/oppia/android/app/administratorcontrols/AdministratorControlsActivityPresenter.kt
@@ -122,6 +122,10 @@ class AdministratorControlsActivityPresenter @Inject constructor(
         TAG,
         "handleOnResume:\n **** back stack changed\n *******"
       )
+      if (activity.supportFragmentManager.backStackEntryCount == 0)
+        binding.administratorControlsMultipaneOptionsBackButton!!.visibility = View.GONE
+      if (activity.supportFragmentManager.findFragmentById(R.id.administrator_controls_fragment_multipane_placeholder) is ProfileListFragment)
+        setExtraControlsTitle("Edit profiles")
     }
     if (isMultipane) {
       binding.administratorControlsMultipaneOptionsBackButton!!.setOnClickListener {
