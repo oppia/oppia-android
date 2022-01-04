b26d4484644ddf8c819e8b0ba620527515c72b1f
Rishi Raj <rishiraj727909@gmail.com>
2022-01-04 15:23:32 +0530

    fixed tests

--- app/src/sharedTest/java/org/oppia/android/app/databinding/TextViewBindingAdaptersTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/databinding/TextViewBindingAdaptersTest.kt
@@ -83,7 +83,6 @@ import org.oppia.android.util.parser.html.HtmlParserEntityTypeModule
 import org.oppia.android.util.parser.image.ImageParsingModule
 import org.robolectric.annotation.Config
 import org.robolectric.annotation.LooperMode
-import javax.inject.Inject
 import javax.inject.Singleton
 
 /** Tests for [TextViewBindingAdapters]. */
@@ -117,10 +116,7 @@ class TextViewBindingAdaptersTest {
     Intents.release()
   }
 
-  @Inject
   lateinit var resourceHandler: AppLanguageResourceHandler
-
-  @Inject
   lateinit var fakeOppiaClock: FakeOppiaClock
 
   @Test
