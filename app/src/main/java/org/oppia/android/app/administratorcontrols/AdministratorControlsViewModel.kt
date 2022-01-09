55381200d392433b62b1bde0ceb71738b28f4064
JishnuGoyal <64526117+JishnuGoyal@users.noreply.github.com>
2022-01-09 19:30:25 +0530

    optimise code

--- app/src/main/java/org/oppia/android/app/administratorcontrols/AdministratorControlsViewModel.kt
+++ app/src/main/java/org/oppia/android/app/administratorcontrols/AdministratorControlsViewModel.kt
@@ -17,7 +17,6 @@ import org.oppia.android.app.administratorcontrols.administratorcontrolsitemview
 import org.oppia.android.app.fragment.FragmentScope
 import org.oppia.android.app.model.DeviceSettings
 import org.oppia.android.app.model.ProfileId
-import org.oppia.android.app.shim.IntentFactoryShim
 import org.oppia.android.domain.oppialogger.OppiaLogger
 import org.oppia.android.domain.profile.ProfileManagementController
 import org.oppia.android.util.data.AsyncResult
@@ -32,7 +31,6 @@ class AdministratorControlsViewModel @Inject constructor(
   private val fragment: Fragment,
   private val oppiaLogger: OppiaLogger,
   private val profileManagementController: ProfileManagementController,
-  private val IntentFactoryShim: IntentFactoryShim,
   @EnableEditAccountsOptionsUi private val enableEditAccountsOptionsUi:
   PlatformParameterValue<Boolean>
 ) {
