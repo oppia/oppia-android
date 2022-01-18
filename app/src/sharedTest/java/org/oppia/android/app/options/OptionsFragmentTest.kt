73911008f08411a8990d0167b7ae71b2480ba4d5
JishnuGoyal <64526117+JishnuGoyal@users.noreply.github.com>
2022-01-19 03:32:09 +0530

    lint

--- app/src/sharedTest/java/org/oppia/android/app/options/OptionsFragmentTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/options/OptionsFragmentTest.kt
@@ -91,6 +91,7 @@ import org.oppia.android.util.networking.NetworkConnectionUtilDebugModule
 import org.oppia.android.util.parser.html.HtmlParserEntityTypeModule
 import org.oppia.android.util.parser.image.GlideImageLoaderModule
 import org.oppia.android.util.parser.image.ImageParsingModule
+import org.oppia.android.util.platformparameter.EnableEditAccountsOptionsUi
 import org.oppia.android.util.platformparameter.EnableLanguageSelectionUi
 import org.oppia.android.util.platformparameter.PlatformParameterValue
 import org.oppia.android.util.platformparameter.SPLASH_SCREEN_WELCOME_MSG_DEFAULT_VALUE
@@ -101,8 +102,6 @@ import org.robolectric.annotation.Config
 import org.robolectric.annotation.LooperMode
 import javax.inject.Inject
 import javax.inject.Singleton
-import org.oppia.android.app.administratorcontrols.AdministratorControlsActivityTest
-import org.oppia.android.util.platformparameter.EnableEditAccountsOptionsUi
 
 /** Tests for [OptionsFragment]. */
 @RunWith(AndroidJUnit4::class)
