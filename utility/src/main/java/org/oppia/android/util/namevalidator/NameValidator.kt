717aa8b2f2f889c77859e9b3f3d610d7dc7d1378
JishnuGoyal <64526117+JishnuGoyal@users.noreply.github.com>
2022-01-19 21:24:18 +0530

    create name validator class

---
+++ utility/src/main/java/org/oppia/android/util/namevalidator/NameValidator.kt
@@ -0,0 +1,39 @@
+package org.oppia.android.util.namevalidator
+
+class NameValidator(name: String) {
+  companion object {
+    private const val regContainsLettersAndNoWhiteSpace =
+      "^.[\\w\\u00BF-\\u1FFF\\u2C00-\\uD7FF.'\\-]+\$"
+
+    private const val regContainsNoSymbols = "^[^#*!@\$%^&()_+=\\\\|\\]\\[\":;?/><,`~{}]*\$"
+
+    private const val regContainsRepeatedAllowedSymbols = "[\\.'-]{2}"
+
+    fun nameAllowed(name: String): Boolean {
+      return (
+        notEmptyNoSpacesAndContainsLetters(name) &&
+          noNumbers(name) &&
+          noSymbols(name) &&
+          noRepeatedUseOfAllowedSymbols(name)
+        )
+    }
+
+    private fun notEmptyNoSpacesAndContainsLetters(name: String): Boolean {
+      return name.matches(Regex(regContainsLettersAndNoWhiteSpace))
+    }
+
+    private fun noNumbers(name: String): Boolean {
+      return name.none {
+        it.isDigit()
+      }
+    }
+
+    private fun noSymbols(name: String): Boolean {
+      return name.matches(Regex(regContainsNoSymbols))
+    }
+
+    private fun noRepeatedUseOfAllowedSymbols(name: String): Boolean {
+      return !name.contains(Regex(regContainsRepeatedAllowedSymbols))
+    }
+  }
+}
