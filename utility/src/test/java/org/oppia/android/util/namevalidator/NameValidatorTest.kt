22d1fd27927fe53c5055b55bcc13e3f123e83563
JishnuGoyal <64526117+JishnuGoyal@users.noreply.github.com>
2022-01-19 22:59:27 +0530

    add nameValidatorTest

---
+++ utility/src/test/java/org/oppia/android/util/namevalidator/NameValidatorTest.kt
@@ -0,0 +1,27 @@
+package org.oppia.android.util.namevalidator
+
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class NameValidatorTest {
+
+  private val allowedNamesList = listOf<String>("जिष्णु", "Ben-Henning", "Rajat.T", "جيشنو")
+
+  private val unAllowedNamesList =
+    listOf<String>("जिष्णु7", "Ben_Henning", "Rajat..T", "جيشنو^&&", " ", ".")
+
+  @Test
+  fun testNameValidator_addUnAllowedName_returnFalse() {
+    unAllowedNamesList.forEach {
+      assertFalse(NameValidator.nameAllowed(it))
+    }
+  }
+
+  @Test
+  fun testNameValidator_addAllowedName_returnTrue() {
+    allowedNamesList.forEach {
+      assertTrue(NameValidator.nameAllowed(it))
+    }
+  }
+}
