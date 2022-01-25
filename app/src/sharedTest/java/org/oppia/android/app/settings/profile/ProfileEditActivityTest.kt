3d2a5e33ec12dd1891603be2d000087cfa90b29e
shankarpriyank <shankarpriyank312002@gmail.com>
2022-01-25 20:48:25 +0530

    Made Changes

--- app/src/sharedTest/java/org/oppia/android/app/settings/profile/ProfileEditActivityTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/settings/profile/ProfileEditActivityTest.kt
@@ -26,8 +26,6 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
 import com.google.common.truth.Truth.assertThat
 import dagger.Component
-import javax.inject.Inject
-import javax.inject.Singleton
 import org.hamcrest.Matchers.not
 import org.junit.After
 import org.junit.Before
@@ -98,6 +96,8 @@ import org.oppia.android.util.parser.image.GlideImageLoaderModule
 import org.oppia.android.util.parser.image.ImageParsingModule
 import org.robolectric.annotation.Config
 import org.robolectric.annotation.LooperMode
+import javax.inject.Inject
+import javax.inject.Singleton
 
 /** Tests for [ProfileEditActivity]. */
 @RunWith(AndroidJUnit4::class)
@@ -155,6 +155,7 @@ class ProfileEditActivityTest {
   @Test
   fun testProfileEditActivity_hasCorrectActivityLabel() {
     launch(
+      // Verify that the activity label is correct as a proxy to verify TalkBack will announce the
       ProfileEditActivity::class.java
     ).use { scenario ->
       scenario.onActivity { activity ->
