78a6583bf7aaf513e9b286c0baf6c10167af0225
Rishi Raj <rishiraj727909@gmail.com>
2022-01-26 19:21:58 +0530

    commit

--- app/src/main/java/org/oppia/android/app/testing/DrawableBindingAdaptersTestActivity.kt
+++ app/src/main/java/org/oppia/android/app/testing/DrawableBindingAdaptersTestActivity.kt
@@ -4,8 +4,9 @@ import android.os.Bundle
 import org.oppia.android.R
 import org.oppia.android.app.activity.ActivityComponentImpl
 import org.oppia.android.app.activity.InjectableAppCompatActivity
+import org.oppia.android.app.databinding.DrawableBindingAdapters
 
-/** Test activity for DrawableBindingAdapters. */
+/** Test activity for [DrawableBindingAdapters]. */
 class DrawableBindingAdaptersTestActivity : InjectableAppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
--- app/src/main/java/org/oppia/android/app/testing/ImageViewBindingAdaptersTestActivity.kt
+++ app/src/main/java/org/oppia/android/app/testing/ImageViewBindingAdaptersTestActivity.kt
@@ -4,8 +4,9 @@ import android.os.Bundle
 import org.oppia.android.R
 import org.oppia.android.app.activity.ActivityComponentImpl
 import org.oppia.android.app.activity.InjectableAppCompatActivity
+import org.oppia.android.app.databinding.ImageViewBindingAdapters
 
-/** Test activity for ImageViewBindingAdapters. */
+/** Test activity for [ImageViewBindingAdapters]. */
 class ImageViewBindingAdaptersTestActivity : InjectableAppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
