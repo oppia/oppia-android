119fc275f73773f89e8a4d2eacd7a064f99c7b61
yash10019coder <76404817+yash10019coder@users.noreply.github.com>
2022-02-02 01:03:46 +0530

    Update app/src/main/java/org/oppia/android/app/administratorcontrols/LoadProfileEditListener.kt
    Co-authored-by: Ben Henning <henning.benmax@gmail.com>

--- app/src/main/java/org/oppia/android/app/administratorcontrols/LoadProfileEditListener.kt
+++ app/src/main/java/org/oppia/android/app/administratorcontrols/LoadProfileEditListener.kt
@@ -2,6 +2,6 @@ package org.oppia.android.app.administratorcontrols
 
 /** Listener for when an activity should load [ProfileEditFragment]. */
 interface LoadProfileEditListener {
-  /** Used for inflating [ProfileEditFragment] in a multipane fragment in tablet mode. */
+  /** Inflates [ProfileEditFragment] as part of a tablet mode a multipane fragment. */
   fun loadProfileEdit(proifleId: Int, profileName: String)
 }
