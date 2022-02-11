41905b6365484a1c83cf52bb854e0becab965b57
yash10019coder <yashverma10019@gmail.com>
2022-02-11 22:05:06 +0530

    fixed typo for LoadProfileEditListener.kt

--- app/src/main/java/org/oppia/android/app/administratorcontrols/LoadProfileEditListener.kt
+++ app/src/main/java/org/oppia/android/app/administratorcontrols/LoadProfileEditListener.kt
@@ -3,5 +3,5 @@ package org.oppia.android.app.administratorcontrols
 /** Listener for when an activity should load [ProfileEditFragment]. */
 interface LoadProfileEditListener {
   /** Inflates [ProfileEditFragment] as part of a tablet mode a multipane fragment. */
-  fun loadProfileEdit(proifleId: Int, profileName: String)
+  fun loadProfileEdit(profileId: Int, profileName: String)
 }
