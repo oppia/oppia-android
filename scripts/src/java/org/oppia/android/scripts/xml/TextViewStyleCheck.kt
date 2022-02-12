9df8ea9df7e37863a61dbcfbcf2ccc17fdb2dce1
yash10019coder <yashverma10019@gmail.com>
2022-02-12 15:52:34 +0530

    fixed the bazel target added a short script
    Signed-off-by: yash10019coder <yashverma10019@gmail.com>

--- scripts/src/java/org/oppia/android/scripts/xml/TextViewStyleCheck.kt
+++ scripts/src/java/org/oppia/android/scripts/xml/TextViewStyleCheck.kt
@@ -2,6 +2,7 @@ package org.oppia.android.scripts.xml
 
 import org.oppia.android.scripts.common.RepositoryFile
 import javax.xml.parsers.DocumentBuilderFactory
+import org.w3c.dom.Node
 
 fun main(vararg args: String) {
   // Path of the repo to be analyzed.
@@ -9,17 +10,36 @@ fun main(vararg args: String) {
 
   // A list of all XML files in the repo to be analyzed.
   val searchFiles = RepositoryFile.collectSearchFiles(
-    repoPath = repoPath,
-    expectedExtension = ".xml"
+    repoPath = repoPath, expectedExtension = ".xml"
   )
-
+  val profileEditFragmentFile = searchFiles[452]
   val builderFactory = DocumentBuilderFactory.newInstance()
+  val builder = builderFactory.newDocumentBuilder()
+  val doc = builder.parse(profileEditFragmentFile)
+  val root = doc.getElementsByTagName("TextView")
+  for (i in 0 until root.length) {
+    val node = root.item(i)
+    textViewRTL(node as Node)
+  }
+}
 
-  val allErrorsList = searchFiles.flatMap { file ->
-    val docBuilder = builderFactory.newDocumentBuilder()
-    val xmlSyntaxErrorHandler = XmlSyntaxErrorHandler()
-    docBuilder.setErrorHandler(xmlSyntaxErrorHandler)
-    val xmlDocument = docBuilder.parse(file.toFile())
-    val list = xmlDocument.fileErrorList.map
+private fun textViewRTL(node: Node) {
+  val attributes = node.attributes
+  for (i in 0 until attributes.length) {
+    val attribute = attributes.item(i)
+    when (attribute.nodeName) {
+      "android:layout_marginLeft" -> {
+        throw Exception("TextView has layout_marginLeft")
+      }
+      "android:layout_marginRight" -> {
+        throw Exception("TextView has layout_marginRight")
+      }
+      "android:layout_paddingLeft" -> {
+        throw Exception("TextView has layout_paddingLeft")
+      }
+      "android:layout_paddingRight" -> {
+        throw Exception("TextView has layout_paddingRight")
+      }
+    }
   }
 }
