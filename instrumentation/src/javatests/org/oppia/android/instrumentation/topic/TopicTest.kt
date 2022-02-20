03598f9c3bf02ddd769754957a70ba0842dbeb27
ayush0402 <lit2020024@iiitl.ac.in>
2022-02-21 03:36:09 +0530

    added assertions and renamed class

--- instrumentation/src/javatests/org/oppia/android/instrumentation/topic/TopicTest.kt
+++ instrumentation/src/javatests/org/oppia/android/instrumentation/topic/TopicTest.kt
@@ -2,6 +2,7 @@ package org.oppia.android.instrumentation.topic
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.uiautomator.UiDevice
+import com.google.common.truth.Truth.assertThat
 import org.junit.Before
 import org.junit.Test
 import org.oppia.android.instrumentation.testing.EndToEndTestHelper.findObjectByRes
@@ -12,7 +13,7 @@ import org.oppia.android.instrumentation.testing.EndToEndTestHelper.waitForRes
 import org.oppia.android.instrumentation.testing.EndToEndTestHelper.waitForText
 
 /** Tests for TopicActivity. */
-class TopicActivityTest {
+class TopicTest {
   private lateinit var device: UiDevice
 
   @Before
@@ -33,24 +34,36 @@ class TopicActivityTest {
     device.findObjectByRes("expand_list_icon").click()
     device.waitForRes("chapter_list_container")
     device.findObjectByText("What is a Fraction?").click()
-    device.waitForText("CONTINUE")
+    device.waitForRes("continue_navigation_button")
     device.pressBack()
+    // Asserting that lessons fragment is open
+    assertThat(device.findObjectByText("Stories You Can Play")).isNotNull()
+    assertThat(device.findObjectByRes("chapter_play_state_icon")).isNotNull()
+
     device.waitForRes("expand_list_icon")
     device.findObjectByRes("expand_list_icon").click()
     device.findObjectByRes("story_name_text_view").click()
     device.waitForRes("story_toolbar_title")
     device.pressBack()
+    // Asserting that lessons fragment is open
+    assertThat(device.findObjectByText("Stories You Can Play")).isNotNull()
 
     device.findObjectByText("PRACTICE").click()
+    assertThat(device.findObjectByText("START").isEnabled()).isFalse()
+
     checkSkillsForPracticeFragment()
     device.findObjectByText("START").click()
     device.waitForText("PRACTICE MODE")
     device.pressBack()
     device.findObjectByText("LEAVE").click()
+    // Asserting that practice fragment is open
+    assertThat(device.findObjectByText("Master These Skills")).isNotNull()
 
     device.findObjectByText("REVISION").click()
     device.findObjectByText("Fractions of a group").click()
     device.findObjectByText("RETURN TO TOPIC").click()
+    // Asserting that revision fragment is open
+    assertThat(device.findObjectByRes("subtopic_title")).isNotNull()
   }
 
   private fun checkSkillsForPracticeFragment() {
