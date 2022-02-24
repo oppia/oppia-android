07b92c401545d06f2f964c8a0e3eea81fa5addf6
Rishi Raj <rishiraj727909@gmail.com>
2022-02-24 20:24:09 +0530

    nitfix

--- app/src/main/java/org/oppia/android/app/activity/ActivityIntentFactories.kt
+++ app/src/main/java/org/oppia/android/app/activity/ActivityIntentFactories.kt
@@ -1,6 +1,7 @@
 package org.oppia.android.app.activity
 
 import android.content.Intent
+import org.oppia.android.app.model.ProfileId
 import org.oppia.android.app.model.RecentlyPlayedActivityIntentExtras
 
 // TODO(#59): Split this up into separate interfaces & move them to the corresponding activities.
