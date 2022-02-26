8691d1b3b994193e3bc678f897e03c28d61f4d3b
JishnuGoyal <64526117+JishnuGoyal@users.noreply.github.com>
2022-02-27 03:01:52 +0530

    add JavaDoc

--- utility/src/main/java/org/oppia/android/util/profile/ProfileNameValidatorUtil.java
+++ utility/src/main/java/org/oppia/android/util/profile/ProfileNameValidatorUtil.java
@@ -1,15 +1,18 @@
 package org.oppia.android.util.profile;
 
+/** Utility for [ProfileNameValidatorImpl] containing isAlphabetic method, only available in Java.*/
 public class ProfileNameValidatorUtil {
+  /**
+   * Validates if the character in the name is an alphabet or an allowed symbol or not.
+   *
+   * @param name is the input.
+   * @return true if the char is one of the allowed symbol or a letter character.
+   * */
   public boolean isNameValid(Character name) {
-    return (
-        Character.isAlphabetic(name) || isSymbolAllowed(name)
-    );
+    return (Character.isAlphabetic(name) || isSymbolAllowed(name));
   }
 
-  public boolean isSymbolAllowed(Character name) {
-    return (
-        name.equals('.') || name.equals('-') || name.equals('\'')
-    );
+  private boolean isSymbolAllowed(Character name) {
+    return (name.equals('.') || name.equals('-') || name.equals('\''));
   }
 }
