c358720042e65135c366957160a89b58e7ec8511
Yash Verma <lit2020066@iiitl.ac.in>
2022-03-01 23:29:20 +0530

    Update app/src/main/java/org/oppia/android/app/settings/profile/LoadProfileEditDeletionDialogListener.kt
    Co-authored-by: Ben Henning <henning.benmax@gmail.com>

--- app/src/main/java/org/oppia/android/app/settings/profile/LoadProfileEditDeletionDialogListener.kt
+++ app/src/main/java/org/oppia/android/app/settings/profile/LoadProfileEditDeletionDialogListener.kt
@@ -4,7 +4,7 @@ package org.oppia.android.app.settings.profile
 interface LoadProfileEditDeletionDialogListener {
   /**
    * Inflates [ProfileEditDeletionDialogFragment] for the configuration changes, i.e. rotating the device
-   *  from landscape to portrait and saves the state of the dialog.
+   * from landscape to portrait, and saves the state of the dialog.
    */
   fun loadProfileEditDeletionDialog(internalProfileId: Int)
 }
