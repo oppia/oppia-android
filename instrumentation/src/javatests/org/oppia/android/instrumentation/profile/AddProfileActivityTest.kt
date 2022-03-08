16fcfac990e846ec00cb6beb5bae365d873a2db3
Rishi Raj <rishiraj727909@gmail.com>
2022-03-09 00:25:01 +0530

    added-bazel-file

--- instrumentation/src/javatests/org/oppia/android/instrumentation/profile/AddProfileActivityTest.kt
+++ instrumentation/src/javatests/org/oppia/android/instrumentation/profile/AddProfileActivityTest.kt
@@ -1,1 +1,13 @@
 package org.oppia.android.instrumentation.profile
+
+/** Tests for Profile. */
+class AddProfileActivityTest {
+  private lateinit var device: UiDevice
+
+  @Before
+  fun setUp() {
+    // Initialize UiDevice instance
+    device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+    device.startOppiaFromScratch()
+  }
+}
