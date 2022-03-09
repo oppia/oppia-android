baa4ae8b37d017541e2fb119b271d2e976d6cfc3
Veena <veena14cs@gmail.com>
2022-03-09 13:30:56 +0530

    Update HtmlParserTest.kt

--- app/src/sharedTest/java/org/oppia/android/app/parser/HtmlParserTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/parser/HtmlParserTest.kt
@@ -244,8 +244,10 @@ class HtmlParserTest {
       textView.text = htmlResult
       return@runWithActivity textView to htmlResult
     }
-    assertThat(htmlResult.toString()).isEqualTo("The counting numbers (1, 2, 3, 4, 5 ….)" +
-      "\nHow to tell whether one counting number is bigger or smaller than another")
+    assertThat(htmlResult.toString()).isEqualTo(
+      "The counting numbers (1, 2, 3, 4, 5 ….)" +
+        "\nHow to tell whether one counting number is bigger or smaller than another"
+    )
   }
 
   @Test
