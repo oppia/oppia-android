a5dd610ad1ad1744a8795c7c0da178efb68cb00f
Veena <veena14cs@gmail.com>
2022-03-24 14:51:24 +0530

    Update utility/src/main/java/org/oppia/android/util/parser/html/PolicyType.kt
    Co-authored-by: Ben Henning <henning.benmax@gmail.com>

--- utility/src/main/java/org/oppia/android/util/parser/html/PolicyType.kt
+++ utility/src/main/java/org/oppia/android/util/parser/html/PolicyType.kt
@@ -2,7 +2,7 @@ package org.oppia.android.util.parser.html
 
 /** the type of policy. */
 enum class PolicyType {
-  /** Corresponds to the Privacy Policy type. */
+  /** Corresponds to the Privacy Policy. */
   PRIVACY_POLICY,
   /** Corresponds to the Terms of Service type. */
   TERMS_OF_SERVICE
