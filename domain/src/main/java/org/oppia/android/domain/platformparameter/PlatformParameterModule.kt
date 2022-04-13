c68aece1b61fb10733512c286cbdf47332f44946
JishnuGoyal <64526117+JishnuGoyal@users.noreply.github.com>
2022-04-14 00:36:58 +0530

    move companion object to TestPlatformParameterModule.kt

--- domain/src/main/java/org/oppia/android/domain/platformparameter/PlatformParameterModule.kt
+++ domain/src/main/java/org/oppia/android/domain/platformparameter/PlatformParameterModule.kt
@@ -24,10 +24,6 @@ import org.oppia.android.util.platformparameter.SyncUpWorkerTimePeriodHours
 /** Dagger module that provides bindings for platform parameters. */
 @Module
 class PlatformParameterModule {
-
-  /** Indicates that force mode was not specified before forcing platform parameter value. */
-  class FailedToProvideForceModeException(msg: String) : Exception(msg)
-
   @Provides
   @SplashScreenWelcomeMsg
   fun provideSplashScreenWelcomeMsgParam(
@@ -53,7 +49,7 @@ class PlatformParameterModule {
   @EnableLanguageSelectionUi
   fun provideEnableLanguageSelectionUi(): PlatformParameterValue<Boolean> {
     return PlatformParameterValue.createDefaultParameter(
-      enableLanguageSelectionUi
+      ENABLE_LANGUAGE_SELECTION_UI_DEFAULT_VALUE
     )
   }
 
@@ -61,7 +57,7 @@ class PlatformParameterModule {
   @EnableEditAccountsOptionsUi
   fun provideEnableEditAccountsOptionsUi(): PlatformParameterValue<Boolean> {
     return PlatformParameterValue.createDefaultParameter(
-      enableEditAccountsOptionsUi
+      ENABLE_EDIT_ACCOUNTS_OPTIONS_UI_DEFAULT_VALUE
     )
   }
 
@@ -82,46 +78,5 @@ class PlatformParameterModule {
     return platformParameterSingleton.getBooleanPlatformParameter(CACHE_LATEX_RENDERING)
       ?: PlatformParameterValue.createDefaultParameter(CACHE_LATEX_RENDERING_DEFAULT_VALUE)
   }
-
-  companion object {
-    private var enableLanguageSelectionUi = ENABLE_LANGUAGE_SELECTION_UI_DEFAULT_VALUE
-    private var enableEditAccountsOptionsUi = ENABLE_EDIT_ACCOUNTS_OPTIONS_UI_DEFAULT_VALUE
-
-    /**
-     * @param value force value needed during test
-     * @param forceMode explicitly declare whether forcing is being done from a test or not
-     * @throws FailedToProvideForceModeException if forceMode is not provided
-     */
-    @Throws(FailedToProvideForceModeException::class)
-    fun forceEnableLanguageSelectionUi(value: Boolean, forceMode: PlatformParameterForceMode) {
-      if (forceMode is PlatformParameterForceMode.TESTING) {
-        enableLanguageSelectionUi = value
-      } else if (forceMode is PlatformParameterForceMode.DEFAULT) {
-        enableLanguageSelectionUi = ENABLE_LANGUAGE_SELECTION_UI_DEFAULT_VALUE
-      } else {
-        throw
-        FailedToProvideForceModeException("Failed to provide mode while forcing platform parameter")
-      }
-    }
-
-    @Throws(FailedToProvideForceModeException::class)
-    fun forceEnableEditAccountsOptionsUi(value: Boolean, forceMode: PlatformParameterForceMode) {
-      if (forceMode is PlatformParameterForceMode.TESTING) {
-        enableEditAccountsOptionsUi = value
-      } else if (forceMode is PlatformParameterForceMode.DEFAULT) {
-        enableEditAccountsOptionsUi = ENABLE_EDIT_ACCOUNTS_OPTIONS_UI_DEFAULT_VALUE
-      } else {
-        throw
-        FailedToProvideForceModeException("Failed to provide mode while forcing platform parameter")
-      }
-    }
-  }
 }
-sealed class PlatformParameterForceMode {
-  /** To be used only from tests */
-  object TESTING : PlatformParameterForceMode()
 
-  /** Default force method should always provide default parameter values
-   * defined in [PlatformParameterConstants] for production */
-  object DEFAULT : PlatformParameterForceMode()
-}
