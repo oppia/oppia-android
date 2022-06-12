1917ec255a0e42cc32c999784385f0caf757713a
Sarthak Agarwal <agarwal.sarthak262012@gmail.com>
2022-06-12 09:12:12 +0530

    nits.

--- domain/src/main/java/org/oppia/android/domain/oppialogger/loguploader/LogUploadWorkManagerInitializer.kt
+++ domain/src/main/java/org/oppia/android/domain/oppialogger/loguploader/LogUploadWorkManagerInitializer.kt
@@ -10,15 +10,13 @@ import org.oppia.android.domain.oppialogger.ApplicationStartupListener
 import org.oppia.android.domain.oppialogger.loggenerator.LogGenerationWorker
 import org.oppia.android.util.logging.LogGenerator
 import org.oppia.android.util.logging.LogUploader
-import org.oppia.android.util.platformparameter.PerformanceMetricsCollectionHighFrequentRecordTimeIntervalInMinutes
-import org.oppia.android.util.platformparameter.PerformanceMetricsCollectionLessFrequentRecordTimeIntervalInMinutes
+import org.oppia.android.util.platformparameter.PerformanceMetricsCollectionHighFrequencyTimeIntervalInMinutes
+import org.oppia.android.util.platformparameter.PerformanceMetricsCollectionLowFrequencyTimeIntervalInMinutes
 import org.oppia.android.util.platformparameter.PlatformParameterValue
 import java.util.UUID
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 import javax.inject.Singleton
-import org.oppia.android.util.platformparameter.PerformanceMetricsCollectionHighFrequencyTimeIntervalInMinutes
-import org.oppia.android.util.platformparameter.PerformanceMetricsCollectionLowFrequencyTimeIntervalInMinutes
 
 /** Enqueues unique periodic work requests for uploading events and exceptions to the remote service on application creation. */
 @Singleton
