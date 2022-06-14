3e9986397a2afcef84fe8482a91aa8023c895381
kevingitonga <kevinjones4199@gmail.com>
2022-06-14 09:49:11 +0300

    Revert "Fix #4186 Undo changes on line wrap as advised by code reviewer."
    This reverts commit ca5bca5e93a96d6dee5ea7142716eedb00d6bc2d.

--- app/src/main/java/org/oppia/android/app/devoptions/markchapterscompleted/MarkChaptersCompletedFragmentPresenter.kt
+++ app/src/main/java/org/oppia/android/app/devoptions/markchapterscompleted/MarkChaptersCompletedFragmentPresenter.kt
@@ -72,8 +72,7 @@ class MarkChaptersCompletedFragmentPresenter @Inject constructor(
           if (viewModel is ChapterSummaryViewModel) {
             if (!viewModel.checkIfChapterIsCompleted())
               chapterSelected(
-                viewModel.chapterIndex,
-                viewModel.nextStoryIndex,
+                viewModel.chapterIndex, viewModel.nextStoryIndex,
                 viewModel.chapterSummary.explorationId
               )
           }
