0062ce35c2c8c7052b88c75c66b237786a7e1685
Sarthak Agarwal <agarwal.sarthak262012@gmail.com>
2022-06-22 03:22:47 +0530

    performance metric event logger nits.

--- utility/src/main/java/org/oppia/android/util/logging/performancemetrics/PerformanceMetricLogger.kt
+++
@@ -1,7 +0,0 @@
-package org.oppia.android.util.logging.performancemetrics
-
-import org.oppia.android.app.model.OppiaMetricLog
-
-interface PerformanceMetricLogger {
-  fun logPerformanceMetric(oppiaMetricLog: OppiaMetricLog)
-}
