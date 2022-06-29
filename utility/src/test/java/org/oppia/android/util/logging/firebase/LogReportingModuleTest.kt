8c773f92f767688d79287eae26714b15288ee836
Sarthak Agarwal <agarwal.sarthak262012@gmail.com>
2022-06-30 01:16:27 +0530

    nits

--- utility/src/test/java/org/oppia/android/util/logging/firebase/LogReportingModuleTest.kt
+++ utility/src/test/java/org/oppia/android/util/logging/firebase/LogReportingModuleTest.kt
@@ -10,8 +10,6 @@ import dagger.BindsInstance
 import dagger.Component
 import dagger.Module
 import dagger.Provides
-import javax.inject.Inject
-import javax.inject.Singleton
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -36,6 +34,8 @@ import org.oppia.android.util.platformparameter.SplashScreenWelcomeMsg
 import org.oppia.android.util.platformparameter.SyncUpWorkerTimePeriodHours
 import org.robolectric.annotation.Config
 import org.robolectric.annotation.LooperMode
+import javax.inject.Inject
+import javax.inject.Singleton
 
 /** Tests for [LogReportingModule]. */
 // FunctionName: test names are conventionally named with underscores.
