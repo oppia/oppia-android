e814bc0d11b0b3a41dc150e98851938f839a064b
vrajdesai78 <vrajdesai78@gmail.com>
2022-07-22 13:04:17 +0530

    import optimized

--- app/src/main/java/org/oppia/android/app/resumelesson/ResumeLessonViewModel.kt
+++ app/src/main/java/org/oppia/android/app/resumelesson/ResumeLessonViewModel.kt
@@ -1,12 +1,12 @@
 package org.oppia.android.app.resumelesson
 
 import androidx.databinding.ObservableField
-import javax.inject.Inject
 import org.oppia.android.app.fragment.FragmentScope
 import org.oppia.android.app.model.ChapterSummary
 import org.oppia.android.app.model.ExplorationCheckpoint
 import org.oppia.android.app.viewmodel.ObservableViewModel
 import org.oppia.android.util.parser.html.ExplorationHtmlParserEntityType
+import javax.inject.Inject
 
 /**
  * The viewModel that provides a [ChapterSummary] and [ExplorationCheckpoint] for the exploration
