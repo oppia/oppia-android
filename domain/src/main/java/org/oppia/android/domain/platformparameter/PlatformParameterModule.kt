dcefb48913ebfc2a151224e47ab9148895dacc35
JishnuGoyal <64526117+JishnuGoyal@users.noreply.github.com>
2022-07-24 22:12:54 +0530

    add enableSpotlightsUi platform parameter

--- domain/src/main/java/org/oppia/android/domain/platformparameter/PlatformParameterModule.kt
+++ domain/src/main/java/org/oppia/android/domain/platformparameter/PlatformParameterModule.kt
@@ -10,8 +10,10 @@ import org.oppia.android.util.platformparameter.ENABLE_LANGUAGE_SELECTION_UI_DEF
 import org.oppia.android.util.platformparameter.ENABLE_PERFORMANCE_METRICS_COLLECTION
 import org.oppia.android.util.platformparameter.ENABLE_PERFORMANCE_METRICS_COLLECTION_DEFAULT_VALUE
 import org.oppia.android.util.platformparameter.EnableEditAccountsOptionsUi
+import org.oppia.android.util.platformparameter.ENABLE_SPOTLIGHT_UI_DEFAULT_VALUE
 import org.oppia.android.util.platformparameter.EnableLanguageSelectionUi
 import org.oppia.android.util.platformparameter.EnablePerformanceMetricsCollection
+import org.oppia.android.util.platformparameter.EnableSpotlightUi
 import org.oppia.android.util.platformparameter.LEARNER_STUDY_ANALYTICS
 import org.oppia.android.util.platformparameter.LEARNER_STUDY_ANALYTICS_DEFAULT_VALUE
 import org.oppia.android.util.platformparameter.LearnerStudyAnalytics
@@ -136,6 +138,14 @@ class PlatformParameterModule {
       PERFORMANCE_METRICS_COLLECTION_LOW_FREQUENCY_TIME_INTERVAL_IN_MINUTES
     ) ?: PlatformParameterValue.createDefaultParameter(
       PERFORMANCE_METRICS_COLLECTION_LOW_FREQUENCY_TIME_INTERVAL_IN_MINUTES_DEFAULT_VAL
+    )
+  }
+
+  @Provides
+  @EnableSpotlightUi
+  fun provideEnableSpotlightUi(): PlatformParameterValue<Boolean> {
+    return PlatformParameterValue.createDefaultParameter(
+      ENABLE_SPOTLIGHT_UI_DEFAULT_VALUE
     )
   }
 }
