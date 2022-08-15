762e212c801e815e10a9ede53c02d217ee8cdba1
Zerg <rajattalesra4914@gmail.com>
2022-08-16 04:57:37 +0530

    Lint fixes

--- app/src/main/java/org/oppia/android/app/player/state/StateFragmentPresenter.kt
+++ app/src/main/java/org/oppia/android/app/player/state/StateFragmentPresenter.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.view.inputmethod.InputMethodManager
 import android.widget.TextView
