6431be051b9e81d39e041087ae7eef7bca99ff1b
JishnuGoyal <64526117+JishnuGoyal@users.noreply.github.com>
2022-08-26 03:15:27 +0530

    remove unused resource - position with three tabs

--- app/src/main/java/org/oppia/android/app/topic/TopicTab.kt
+++ app/src/main/java/org/oppia/android/app/topic/TopicTab.kt
@@ -8,35 +8,30 @@ import org.oppia.android.R
 enum class TopicTab(
   private val positionWithTwoTabs: Int,
   private val positionWithFourTabs: Int,
-  private val positionWithThreeTabs: Int,
   @StringRes val tabLabelResId: Int,
   @DrawableRes val tabIconResId: Int
 ) {
   INFO(
     positionWithTwoTabs = -1,
     positionWithFourTabs = 0,
-    positionWithThreeTabs = 0,
     tabLabelResId = R.string.info,
     tabIconResId = R.drawable.ic_info_icon_24dp
   ),
   LESSONS(
     positionWithTwoTabs = 0,
     positionWithFourTabs = 1,
-    positionWithThreeTabs = 1,
     tabLabelResId = R.string.lessons,
     tabIconResId = R.drawable.ic_lessons_icon_24dp
   ),
   PRACTICE(
     positionWithTwoTabs = -1,
     positionWithFourTabs = 2,
-    positionWithThreeTabs = -1,
     tabLabelResId = R.string.practice,
     tabIconResId = R.drawable.ic_practice_icon_24dp
   ),
   REVISION(
     positionWithTwoTabs = 1,
     positionWithFourTabs = 3,
-    positionWithThreeTabs = 2,
     tabLabelResId = R.string.revision,
     tabIconResId = R.drawable.ic_revision_icon_24dp
   );
