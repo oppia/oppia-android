ea443c68a8ece1dd63147ce265432f441a81ef7c
JishnuGoyal <64526117+JishnuGoyal@users.noreply.github.com>
2022-09-07 22:00:33 +0530

    add custom view

---
+++ app/src/main/java/org/oppia/android/app/customview/ContinueButtonView.kt
@@ -0,0 +1,37 @@
+package org.oppia.android.app.customview
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.animation.AnimationUtils
+import androidx.appcompat.widget.AppCompatButton
+import org.oppia.android.R
+import org.oppia.android.app.player.state.itemviewmodel.ContinueInteractionViewModel
+
+class ContinueButtonView @JvmOverloads constructor(
+  context: Context,
+  attrs: AttributeSet? = null,
+  defStyleAttr: Int = R.style.StateButtonActive
+) : AppCompatButton(context, attrs, defStyleAttr) {
+
+  private lateinit var viewModel: ContinueInteractionViewModel
+
+//  @Inject
+//  private lateinit var lifecycleSafeTimerFactory: LifecycleSafeTimerFactory
+
+   fun setViewModel(viewModel: ContinueInteractionViewModel) {
+    this.viewModel = viewModel
+     startAnimating()
+  }
+
+  fun startAnimating() {
+    val animation = AnimationUtils.loadAnimation(context, R.anim.fade_in)
+    animation.repeatCount = Int.MAX_VALUE
+    this.startAnimation(animation)
+  }
+
+
+  override fun onAttachedToWindow() {
+    super.onAttachedToWindow()
+  }
+
+}
