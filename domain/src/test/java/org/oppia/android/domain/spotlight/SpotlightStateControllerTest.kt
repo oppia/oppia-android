a8a1aa86f86afc8305004d8816294f545d716269
JishnuGoyal <64526117+JishnuGoyal@users.noreply.github.com>
2022-09-10 23:11:33 +0530

    lint

--- domain/src/test/java/org/oppia/android/domain/spotlight/SpotlightStateControllerTest.kt
+++ domain/src/test/java/org/oppia/android/domain/spotlight/SpotlightStateControllerTest.kt
@@ -9,8 +9,6 @@ import dagger.BindsInstance
 import dagger.Component
 import dagger.Module
 import dagger.Provides
-import javax.inject.Inject
-import javax.inject.Singleton
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -68,6 +66,8 @@ import org.oppia.android.util.logging.SyncStatusModule
 import org.oppia.android.util.networking.NetworkConnectionUtilDebugModule
 import org.robolectric.annotation.Config
 import org.robolectric.annotation.LooperMode
+import javax.inject.Inject
+import javax.inject.Singleton
 
 @Suppress("SameParameterValue", "FunctionName")
 @RunWith(AndroidJUnit4::class)
