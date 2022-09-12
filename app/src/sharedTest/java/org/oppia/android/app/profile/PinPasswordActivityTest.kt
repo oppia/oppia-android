42508155dfefa868cdad3a053f5537a8e0b47875
pratyaksh1610 <pratyakshkhuranaofficial@gmail.com>
2022-09-12 23:03:21 +0530

    fixed CI checks

--- app/src/sharedTest/java/org/oppia/android/app/profile/PinPasswordActivityTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/profile/PinPasswordActivityTest.kt
@@ -1150,7 +1150,7 @@ class PinPasswordActivityTest {
             context.resources.getString(R.string.reset_pin_enter_dialog_message, "Ben")
           )
         )
-      ).check(matches(isDisplayed()))
+      ).inRoot(isDialog()).check(matches(isDisplayed()))
     }
   }
 
