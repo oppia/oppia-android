152e100a003b40dd9821772780d614dd6911e070
vrajdesai78 <vrajdesai78@gmail.com>
2022-09-19 00:36:26 +0530

    Optimized code

--- app/src/main/java/org/oppia/android/app/player/exploration/testing/BottomSheetOptionsMenuTestActivity.kt
+++ app/src/main/java/org/oppia/android/app/player/exploration/testing/BottomSheetOptionsMenuTestActivity.kt
@@ -5,8 +5,14 @@ import org.oppia.android.app.player.exploration.BottomSheetOptionsMenu
 import org.oppia.android.app.player.exploration.BottomSheetOptionsMenuItemClickListener
 import org.oppia.android.app.testing.activity.TestActivity
 
+/** [TestActivity] for setting up a test environment for testing the BottomSheetOptionsMenu. */
 class BottomSheetOptionsMenuTestActivity : TestActivity(), BottomSheetOptionsMenuItemClickListener {
-
+  /**
+   * [BottomSheetOptionsMenuItemClickListener] that must be initialized by the test,
+   * and is presumed to be a Mockito mock (though this is not, strictly speaking, required).
+   *
+   * This listener will be used as the callback for the dialog in response to UI operations.
+   */
   lateinit var mockCallbacklistner: BottomSheetOptionsMenuItemClickListener
 
   override fun onCreate(savedInstanceState: Bundle?) {
