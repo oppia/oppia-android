819adbceef76c71109e31f670c09be74f9afdc15
Veena <veena14cs@gmail.com>
2022-09-20 12:51:30 +0530

    Update PoliciesFragmentTest.kt

--- app/src/sharedTest/java/org/oppia/android/app/policies/PoliciesFragmentTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/policies/PoliciesFragmentTest.kt
@@ -26,8 +26,6 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
 import dagger.Component
-import javax.inject.Inject
-import javax.inject.Singleton
 import org.hamcrest.CoreMatchers.allOf
 import org.hamcrest.Matchers
 import org.junit.After
@@ -117,6 +115,8 @@ import org.oppia.android.util.parser.html.PolicyType
 import org.oppia.android.util.parser.image.ImageParsingModule
 import org.robolectric.annotation.Config
 import org.robolectric.annotation.LooperMode
+import javax.inject.Inject
+import javax.inject.Singleton
 
 /** Tests for [PoliciesFragment]. */
 @RunWith(AndroidJUnit4::class)
