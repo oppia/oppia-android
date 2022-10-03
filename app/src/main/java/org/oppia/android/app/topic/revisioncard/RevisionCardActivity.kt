f096b1d3260319bb0599692b29e9beb330841d92
Jishnu <64526117+JishnuGoyal@users.noreply.github.com>
2022-10-03 05:38:04 +0530

    Fix #4450: Add next and previous card options on revision screen (#4554)
    * add functionality for next/prev navigation for revision cards
    
    * update according to changes from develop
    
    * fix bad merge with develop
    
    * make toolbar orange
    
    * refactor code + add tests
    
    * lint/shorten test name
    
    * lint/shorten test name
    
    * add all refactor jobs
    
    * requested changes
    
    * add 'scroll to' to tests
    
    * add min height and width
    
    * remove accidentally added code that led to test failure
    
    * pre-load subtopic list size
    
    * lint
    
    * piping was not correct
    
    * lint
    
    * lint
    
    * disable accessibility checks
    
    * finish up small left bits
    
    * lint
    
    * commit before checkout
    
    * partial: Add changes requested in review.
    
    * lint
    
    * partial: suggested changes
    
    * kt to binding
    
    * kt to xml binding
    
    * change colors of status bar and toolbar color without using logic
    
    * lint
    
    * fix todo spacing
    
    * wrap up with review comments and fix CI
    
    * lint
    
    * text alignment should be right when only next card is visible
    
    * add kdocs
    
    * finish up kdocs

--- app/src/main/java/org/oppia/android/app/topic/revisioncard/RevisionCardActivity.kt
+++ app/src/main/java/org/oppia/android/app/topic/revisioncard/RevisionCardActivity.kt
@@ -7,6 +7,7 @@ import org.oppia.android.app.activity.ActivityComponentImpl
 import org.oppia.android.app.activity.InjectableAppCompatActivity
 import org.oppia.android.app.model.ScreenName.REVISION_CARD_ACTIVITY
 import org.oppia.android.app.player.exploration.BottomSheetOptionsMenuItemClickListener
+import org.oppia.android.app.topic.RouteToRevisionCardListener
 import org.oppia.android.app.topic.conceptcard.ConceptCardListener
 import org.oppia.android.util.logging.CurrentAppScreenNameIntentDecorator.decorateWithScreenName
 import javax.inject.Inject
@@ -16,6 +17,7 @@ class RevisionCardActivity :
   InjectableAppCompatActivity(),
   ReturnToTopicClickListener,
   ConceptCardListener,
+  RouteToRevisionCardListener,
   BottomSheetOptionsMenuItemClickListener {
 
   @Inject
@@ -30,7 +32,13 @@ class RevisionCardActivity :
         "Expected topic ID to be included in intent for RevisionCardActivity."
       }
       val subtopicId = intent.getIntExtra(SUBTOPIC_ID_EXTRA_KEY, -1)
-      revisionCardActivityPresenter.handleOnCreate(internalProfileId, topicId, subtopicId)
+      val subtopicListSize = intent.getIntExtra(SUBTOPIC_LIST_SIZE_EXTRA_KEY, -1)
+      revisionCardActivityPresenter.handleOnCreate(
+        internalProfileId,
+        topicId,
+        subtopicId,
+        subtopicListSize
+      )
     }
   }
 
@@ -42,21 +50,42 @@ class RevisionCardActivity :
     internal const val INTERNAL_PROFILE_ID_EXTRA_KEY = "RevisionCardActivity.internal_profile_id"
     internal const val TOPIC_ID_EXTRA_KEY = "RevisionCardActivity.topic_id"
     internal const val SUBTOPIC_ID_EXTRA_KEY = "RevisionCardActivity.subtopic_id"
+    internal const val SUBTOPIC_LIST_SIZE_EXTRA_KEY = "RevisionCardActivity.subtopic_list_size"
 
     /** Returns a new [Intent] to route to [RevisionCardActivity]. */
     fun createRevisionCardActivityIntent(
       context: Context,
       internalProfileId: Int,
       topicId: String,
-      subtopicId: Int
+      subtopicId: Int,
+      subtopicListSize: Int
     ): Intent {
       return Intent(context, RevisionCardActivity::class.java).apply {
         putExtra(INTERNAL_PROFILE_ID_EXTRA_KEY, internalProfileId)
         putExtra(TOPIC_ID_EXTRA_KEY, topicId)
         putExtra(SUBTOPIC_ID_EXTRA_KEY, subtopicId)
+        putExtra(SUBTOPIC_LIST_SIZE_EXTRA_KEY, subtopicListSize)
         decorateWithScreenName(REVISION_CARD_ACTIVITY)
       }
     }
+  }
+
+  override fun routeToRevisionCard(
+    internalProfileId: Int,
+    topicId: String,
+    subtopicId: Int,
+    subtopicListSize: Int
+  ) {
+    startActivity(
+      createRevisionCardActivityIntent(
+        this,
+        internalProfileId,
+        topicId,
+        subtopicId,
+        subtopicListSize
+      )
+    )
+    this.finish()
   }
 
   override fun onReturnToTopicClicked() {
