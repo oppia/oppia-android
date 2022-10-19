aba11b5613135ae057961f916f29c4dae0a845f6
Veena <veena14cs@gmail.com>
2022-10-19 17:21:13 +0530

    Update PoliciesActivityTest.kt

--- app/src/sharedTest/java/org/oppia/android/app/policies/PoliciesActivityTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/policies/PoliciesActivityTest.kt
@@ -10,8 +10,6 @@ import androidx.test.espresso.intent.Intents
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
 import dagger.Component
-import javax.inject.Inject
-import javax.inject.Singleton
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -90,6 +88,8 @@ import org.oppia.android.util.parser.html.HtmlParserEntityTypeModule
 import org.oppia.android.util.parser.image.ImageParsingModule
 import org.robolectric.annotation.Config
 import org.robolectric.annotation.LooperMode
+import javax.inject.Inject
+import javax.inject.Singleton
 
 /** Tests for [PoliciesActivity]. */
 @RunWith(AndroidJUnit4::class)
