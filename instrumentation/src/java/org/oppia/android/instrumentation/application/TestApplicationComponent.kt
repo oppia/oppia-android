b5e420dbeca10a070fa80ce12deed8fa2e3ecf83
Sarthak Agarwal <agarwal.sarthak262012@gmail.com>
2022-10-21 02:58:31 +0530

    bazel build fix

--- instrumentation/src/java/org/oppia/android/instrumentation/application/TestApplicationComponent.kt
+++ instrumentation/src/java/org/oppia/android/instrumentation/application/TestApplicationComponent.kt
@@ -33,7 +33,6 @@ import org.oppia.android.domain.hintsandsolution.HintsAndSolutionProdModule
 import org.oppia.android.domain.onboarding.ExpirationMetaDataRetrieverModule
 import org.oppia.android.domain.oppialogger.LogStorageModule
 import org.oppia.android.domain.oppialogger.LoggingIdentifierModule
-import org.oppia.android.domain.oppialogger.analytics.ActivityLifecycleObserverModule
 import org.oppia.android.domain.oppialogger.analytics.ApplicationLifecycleModule
 import org.oppia.android.domain.oppialogger.exceptions.UncaughtExceptionLoggerModule
 import org.oppia.android.domain.oppialogger.logscheduler.MetricLogSchedulerModule
@@ -97,10 +96,9 @@ import javax.inject.Singleton
     MathEquationInputModule::class, SplitScreenInteractionModule::class,
     LoggingIdentifierModule::class, ApplicationLifecycleModule::class,
     SyncStatusModule::class, NetworkConnectionDebugUtilModule::class,
-    MetricLogSchedulerModule::class, ActivityLifecycleObserverModule::class,
+    MetricLogSchedulerModule::class, ActivityRouterModule::class
     PerformanceMetricsAssessorModule::class, PerformanceMetricsConfigurationsModule::class,
     TestingBuildFlavorModule::class, EventLoggingConfigurationModule::class,
-    ActivityRouterModule::class
   ]
 )
 interface TestApplicationComponent : ApplicationComponent {
