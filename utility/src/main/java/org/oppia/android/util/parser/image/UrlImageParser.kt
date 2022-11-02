19ff9216c18de6873f6bfa166d04b9650d5f7a86
vrajdesai78 <vrajdesai78@gmail.com>
2022-11-02 12:40:38 +0530

    Optimized import

--- utility/src/main/java/org/oppia/android/util/parser/image/UrlImageParser.kt
+++ utility/src/main/java/org/oppia/android/util/parser/image/UrlImageParser.kt
@@ -9,7 +9,6 @@ import android.graphics.Rect
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.text.Html
-import android.util.Log
 import android.util.TypedValue
 import android.view.View
 import android.view.ViewGroup
