7d9d672e7ca0b88281d3151657189b8f0fb3da6b
pratyaksh1610 <pratyakshkhuranaofficial@gmail.com>
2022-11-26 19:26:12 +0530

    fix imports order

--- utility/src/test/java/org/oppia/android/util/logging/KenyaAlphaEventBundleCreatorTest.kt
+++ utility/src/test/java/org/oppia/android/util/logging/KenyaAlphaEventBundleCreatorTest.kt
@@ -14,6 +14,8 @@ import dagger.BindsInstance
 import dagger.Component
 import dagger.Module
 import dagger.Provides
+import javax.inject.Inject
+import javax.inject.Singleton
 import org.junit.After
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -65,8 +67,6 @@ import org.oppia.android.util.platformparameter.PlatformParameterValue
 import org.robolectric.Shadows
 import org.robolectric.annotation.Config
 import org.robolectric.annotation.LooperMode
-import javax.inject.Inject
-import javax.inject.Singleton
 
 private const val TEST_ANDROID_SDK_VERSION = 30
 
