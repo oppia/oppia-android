c86a48d76673496134b449f94f7ce0eeffd37a28
pratyaksh1610 <pratyakshkhuranaofficial@gmail.com>
2022-11-26 19:06:22 +0530

    fix imports order

--- utility/src/test/java/org/oppia/android/util/logging/SyncStatusManagerImplTest.kt
+++ utility/src/test/java/org/oppia/android/util/logging/SyncStatusManagerImplTest.kt
@@ -9,8 +9,6 @@ import dagger.BindsInstance
 import dagger.Component
 import dagger.Module
 import dagger.Provides
-import javax.inject.Inject
-import javax.inject.Singleton
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -38,6 +36,8 @@ import org.oppia.android.util.platformparameter.SplashScreenWelcomeMsg
 import org.oppia.android.util.platformparameter.SyncUpWorkerTimePeriodHours
 import org.robolectric.annotation.Config
 import org.robolectric.annotation.LooperMode
+import javax.inject.Inject
+import javax.inject.Singleton
 
 /** Tests for [SyncStatusManagerImpl]. */
 // FunctionName: test names are conventionally named with underscores.
