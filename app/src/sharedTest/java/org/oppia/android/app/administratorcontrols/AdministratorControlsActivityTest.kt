4e4e8b6322df187d46aa4286fe92f3c3ed9def98
MohitGupta121 <guptamm.22@gmail.com>
2022-12-20 23:19:46 +0530

    Lint Fixed Imports

--- app/src/sharedTest/java/org/oppia/android/app/administratorcontrols/AdministratorControlsActivityTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/administratorcontrols/AdministratorControlsActivityTest.kt
@@ -40,6 +40,8 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
 import com.google.common.truth.Truth.assertThat
 import dagger.Component
+import javax.inject.Inject
+import javax.inject.Singleton
 import org.hamcrest.Matchers
 import org.hamcrest.Matchers.not
 import org.junit.After
@@ -49,9 +51,9 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.oppia.android.R
 import org.oppia.android.R.string.administrator_controls_activity_general_view_administrator_edit_account
-import org.oppia.android.R.string.administrator_controls_activity_profile_view_administrator_edit_profiles
 import org.oppia.android.R.string.administrator_controls_activity_log_out_dialog_fragment_cancel_button
 import org.oppia.android.R.string.administrator_controls_activity_log_out_dialog_fragment_okay_button
+import org.oppia.android.R.string.administrator_controls_activity_profile_view_administrator_edit_profiles
 import org.oppia.android.app.activity.ActivityComponent
 import org.oppia.android.app.activity.ActivityComponentFactory
 import org.oppia.android.app.activity.route.ActivityRouterModule
@@ -128,8 +130,6 @@ import org.oppia.android.util.parser.image.GlideImageLoaderModule
 import org.oppia.android.util.parser.image.ImageParsingModule
 import org.robolectric.annotation.Config
 import org.robolectric.annotation.LooperMode
-import javax.inject.Inject
-import javax.inject.Singleton
 
 /** Tests for [AdministratorControlsActivity]. */
 @RunWith(AndroidJUnit4::class)
