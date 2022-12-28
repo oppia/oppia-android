28a46b566b8e44dcee925a71feb9e76934e2865b
Shubhadeep_Karmakar <shubhadeepkarmakar1@gmail.com>
2022-12-28 09:11:29 +0530

    Resolve #4806

--- app/src/main/java/org/oppia/android/app/profile/AddProfileActivityPresenter.kt
+++ app/src/main/java/org/oppia/android/app/profile/AddProfileActivityPresenter.kt
@@ -236,8 +236,8 @@ class AddProfileActivityPresenter @Inject constructor(
     }
   }
 
-  private fun openGalleryIntent() {
-    val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+  private fun openGalleryIntent() {val galleryIntent = Intent(Intent.ACTION_GET_CONTENT)
+    galleryIntent.type="image/*"
     activity.startActivityForResult(galleryIntent, GALLERY_INTENT_RESULT_CODE)
   }
 
