33942922f2aa39e29ac7976e77e5063b6b959596
MohitGupta121 <guptamm.22@gmail.com>
2023-02-28 09:19:03 +0530

    Testing Activity and Fragments Test

--- app/src/main/java/org/oppia/android/app/onboarding/OnboardingActivity.kt
+++ app/src/main/java/org/oppia/android/app/onboarding/OnboardingActivity.kt
@@ -3,6 +3,8 @@ package org.oppia.android.app.onboarding
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import javax.inject.Inject
+import org.oppia.android.R
 import org.oppia.android.app.activity.ActivityComponentImpl
 import org.oppia.android.app.activity.InjectableAppCompatActivity
 import org.oppia.android.app.model.PolicyPage
@@ -11,8 +13,6 @@ import org.oppia.android.app.policies.PoliciesActivity
 import org.oppia.android.app.policies.RouteToPoliciesListener
 import org.oppia.android.app.profile.ProfileChooserActivity
 import org.oppia.android.util.logging.CurrentAppScreenNameIntentDecorator.decorateWithScreenName
-import javax.inject.Inject
-import org.oppia.android.R
 
 /** Activity that contains the onboarding flow for learners. */
 class OnboardingActivity :
