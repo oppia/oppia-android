01f555e5ba12ff11f75ffe54b24811b090452797
kkmurerwa <xwaxes@gmail.com>
2023-03-22 12:58:28 +0300

    fix: Try fix for the lint checks error

--- app/src/sharedTest/java/org/oppia/android/app/settings/profile/ProfileEditActivityTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/settings/profile/ProfileEditActivityTest.kt
@@ -168,8 +168,9 @@ class ProfileEditActivityTest {
     ).use { activityScenario ->
       activityScenario.onActivity {
         val titleToolbar = it.findViewById<Toolbar>(R.id.profile_edit_toolbar)
+        val titleToolbarText = titleToolbar.title
 
-        assertThat(titleToolbar.title).isEqualTo(context.getString(R.string.profile_edit_activity_title))
+        assertThat(titleToolbarText).isEqualTo(context.getString(R.string.profile_edit_activity_title))
       }
     }
   }
