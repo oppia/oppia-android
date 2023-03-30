7fd290d68f0440d926f2d443dbd7bfb28ab20547
masclot <103062089+masclot@users.noreply.github.com>
2023-03-30 07:12:11 +0200

    Fix part of #632: Move PromotedStoryListAdapter to BindableAdater (#4874)
    <!-- READ ME FIRST: Please fill in the explanation section below and
    check off every point from the Essential Checklist! -->
    ## Explanation
    <!--
    - Explain what your PR does. If this PR fixes an existing bug, please
    include
    - "Fixes #bugnum:" in the explanation so that GitHub can auto-close the
    issue
      - when this PR is merged.
      -->
    
    Fixes part of #632: Reimplementation of PromotedStoryListAdapter as
    BindableAdater
    Espresso tests are failing, but they were already failing. The errors
    with the new adapter are the same. The same tests pass when using
    gradle.
    
    There was an error in MarginBindingAdapter which I'm also fixing in this
    cl: MarginBindingAdapter was replacing start/end margin changes for
    left/right margin changes. However, this replacement relied on getting
    the layout direction from the View being changed. When the direction is
    inherited and the View is not attached to a parent (recursively until
    top level View), the direction defaults to LTR, no matter the
    Locale/language. This situation happens for example in item Views within
    RecyclerView. The change does not replace start/end any more, but sets
    them through MarginLayoutParamsCompat so that it is compatible with api
    < 17.
    
    ## Essential Checklist
    <!-- Please tick the relevant boxes by putting an "x" in them. -->
    - [x] The PR title and explanation each start with "Fix #bugnum: " (If
    this PR fixes part of an issue, prefix the title with "Fix part of
    #bugnum: ...".)
    - [X] Any changes to
    [scripts/assets](https://github.com/oppia/oppia-android/tree/develop/scripts/assets)
    files have their rationale included in the PR explanation.
    - [X] The PR follows the [style
    guide](https://github.com/oppia/oppia-android/wiki/Coding-style-guide).
    - [X] The PR does not contain any unnecessary code changes from Android
    Studio
    ([reference](https://github.com/oppia/oppia-android/wiki/Guidance-on-submitting-a-PR#undo-unnecessary-changes)).
    - [X] The PR is made from a branch that's **not** called "develop" and
    is up-to-date with "develop".
    - [x] The PR is **assigned** to the appropriate reviewers
    ([reference](https://github.com/oppia/oppia-android/wiki/Guidance-on-submitting-a-PR#clarification-regarding-assignees-and-reviewers-section)).
    
    | Current | new |
    |-|-|
    |
    ![bindable_adapter_current](https://user-images.githubusercontent.com/103062089/217546850-30d59ac3-d996-4856-9a55-43a61e533eb2.png)
    |
    ![bindable_adapter_new](https://user-images.githubusercontent.com/103062089/217546857-224a287d-b55a-4af9-8b05-e4855d78bd44.png)
    |
    
    Unit tests:
    
    ![recently_played_span_test](https://user-images.githubusercontent.com/103062089/217547368-f86fa7c4-c915-4257-948c-90a8e73c3ab6.png)
    
    Espresso Tests. The run for the new version has an extra error, but it
    is due to timings. In other runs it passes.
    
    ![recently_played_espresso_tests_current_vs_new](https://user-images.githubusercontent.com/103062089/217548889-21e0599e-e8b0-463d-a3f6-eeaaa93e674a.png)
    
    @rt4914 PTAL
    
    ---------
    
    Co-authored-by: Ben Henning <ben@oppia.org>

---
+++ app/src/main/java/org/oppia/android/app/home/recentlyplayed/RecentlyPlayedViewModel.kt
@@ -0,0 +1,172 @@
+package org.oppia.android.app.home.recentlyplayed
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import org.oppia.android.R
+import org.oppia.android.app.model.ProfileId
+import org.oppia.android.app.model.PromotedActivityList
+import org.oppia.android.app.model.PromotedStory
+import org.oppia.android.app.translation.AppLanguageResourceHandler
+import org.oppia.android.domain.topic.TopicListController
+import org.oppia.android.domain.translation.TranslationController
+import org.oppia.android.util.data.AsyncResult
+import org.oppia.android.util.data.DataProviders.Companion.toLiveData
+import org.oppia.android.util.parser.html.StoryHtmlParserEntityType
+import javax.inject.Inject
+
+/** View model for [RecentlyPlayedFragment]. */
+class RecentlyPlayedViewModel private constructor(
+  private val activity: AppCompatActivity,
+  private val topicListController: TopicListController,
+  @StoryHtmlParserEntityType private val entityType: String,
+  private val resourceHandler: AppLanguageResourceHandler,
+  private val translationController: TranslationController,
+  private val promotedStoryClickListener: PromotedStoryClickListener,
+  private val profileId: ProfileId,
+) {
+
+  /** Factory of RecentlyPlayedViewModel. */
+  class Factory @Inject constructor(
+    private val activity: AppCompatActivity,
+    private val topicListController: TopicListController,
+    @StoryHtmlParserEntityType private val entityType: String,
+    private val resourceHandler: AppLanguageResourceHandler,
+    private val translationController: TranslationController,
+  ) {
+
+    /** Creates an instance of [RecentlyPlayedViewModel] */
+    fun create(
+      promotedStoryClickListener: PromotedStoryClickListener,
+      profileId: ProfileId
+    ): RecentlyPlayedViewModel {
+      return RecentlyPlayedViewModel(
+        activity,
+        topicListController,
+        entityType,
+        resourceHandler,
+        translationController,
+        promotedStoryClickListener,
+        profileId,
+      )
+    }
+  }
+
+  /**
+   * [LiveData] with the list of recently played items for a ProfileId, organized in sections.
+   */
+  val recentlyPlayedItems: LiveData<List<RecentlyPlayedItemViewModel>> by lazy {
+    Transformations.map(promotedActivityListLiveData, ::processPromotedStoryList)
+  }
+
+  private val promotedActivityListLiveData: LiveData<PromotedActivityList> by lazy {
+    getAssumedSuccessfulPromotedActivityList()
+  }
+
+  private val promotedStoryListSummaryResultLiveData:
+    LiveData<AsyncResult<PromotedActivityList>>
+    by lazy {
+      topicListController.getPromotedActivityList(profileId).toLiveData()
+    }
+
+  private fun getAssumedSuccessfulPromotedActivityList(): LiveData<PromotedActivityList> {
+    return Transformations.map(promotedStoryListSummaryResultLiveData) {
+      when (it) {
+        // If there's an error loading the data, assume the default.
+        is AsyncResult.Failure, is AsyncResult.Pending -> PromotedActivityList.getDefaultInstance()
+        is AsyncResult.Success -> it.value
+      }
+    }
+  }
+
+  private fun processPromotedStoryList(
+    promotedActivityList: PromotedActivityList
+  ): List<RecentlyPlayedItemViewModel> {
+    val itemList: MutableList<RecentlyPlayedItemViewModel> = mutableListOf()
+    if (promotedActivityList.promotedStoryList.recentlyPlayedStoryList.isNotEmpty()) {
+      addRecentlyPlayedStoryListSection(
+        promotedActivityList.promotedStoryList.recentlyPlayedStoryList,
+        itemList
+      )
+    }
+
+    if (promotedActivityList.promotedStoryList.olderPlayedStoryList.isNotEmpty()) {
+      addOlderStoryListSection(
+        promotedActivityList.promotedStoryList.olderPlayedStoryList,
+        itemList
+      )
+    }
+
+    if (promotedActivityList.promotedStoryList.suggestedStoryList.isNotEmpty()) {
+      addRecommendedStoryListSection(
+        promotedActivityList.promotedStoryList.suggestedStoryList,
+        itemList
+      )
+    }
+    return itemList
+  }
+
+  private fun addRecentlyPlayedStoryListSection(
+    recentlyPlayedStoryList: MutableList<PromotedStory>,
+    itemList: MutableList<RecentlyPlayedItemViewModel>
+  ) {
+    val recentSectionTitleViewModel =
+      SectionTitleViewModel(
+        resourceHandler.getStringInLocale(R.string.ongoing_story_last_week), false
+      )
+    itemList.add(recentSectionTitleViewModel)
+    recentlyPlayedStoryList.forEachIndexed { index, promotedStory ->
+      val ongoingStoryViewModel = createOngoingStoryViewModel(promotedStory, index)
+      itemList.add(ongoingStoryViewModel)
+    }
+  }
+
+  private fun addOlderStoryListSection(
+    olderPlayedStoryList: List<PromotedStory>,
+    itemList: MutableList<RecentlyPlayedItemViewModel>
+  ) {
+    val showDivider = itemList.isNotEmpty()
+    val olderSectionTitleViewModel =
+      SectionTitleViewModel(
+        resourceHandler.getStringInLocale(R.string.ongoing_story_last_month),
+        showDivider
+      )
+    itemList.add(olderSectionTitleViewModel)
+    olderPlayedStoryList.forEachIndexed { index, promotedStory ->
+      val ongoingStoryViewModel = createOngoingStoryViewModel(promotedStory, index)
+      itemList.add(ongoingStoryViewModel)
+    }
+  }
+
+  private fun addRecommendedStoryListSection(
+    suggestedStoryList: List<PromotedStory>,
+    itemList: MutableList<RecentlyPlayedItemViewModel>
+  ) {
+    val showDivider = itemList.isNotEmpty()
+    val recommendedSectionTitleViewModel =
+      SectionTitleViewModel(
+        resourceHandler.getStringInLocale(R.string.recommended_stories),
+        showDivider
+      )
+    itemList.add(recommendedSectionTitleViewModel)
+    suggestedStoryList.forEachIndexed { index, suggestedStory ->
+      val ongoingStoryViewModel = createOngoingStoryViewModel(suggestedStory, index)
+      itemList.add(ongoingStoryViewModel)
+    }
+  }
+
+  private fun createOngoingStoryViewModel(
+    promotedStory: PromotedStory,
+    index: Int
+  ): RecentlyPlayedItemViewModel {
+    return PromotedStoryViewModel(
+      activity,
+      promotedStory,
+      entityType,
+      promotedStoryClickListener,
+      index,
+      resourceHandler,
+      translationController
+    )
+  }
+}
