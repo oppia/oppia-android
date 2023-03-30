cad268b0a980af788230483764fc175bef2de309
masclot <103062089+masclot@users.noreply.github.com>
2023-03-30 22:31:09 +0200

    Fix #3755: Administration controls invisible but actionable (#4901)
    <!-- READ ME FIRST: Please fill in the explanation section below and
    check off every point from the Essential Checklist! -->
    ## Explanation
    Fix #3755: AdministratorControlsActivity tablet has Fragment management
    issues
    
    The "App Version" fragment was being added on top of the previous
    fragment, instead of replacing it.
    
    ## Essential Checklist
    <!-- Please tick the relevant boxes by putting an "x" in them. -->
    - [x] The PR title and explanation each start with "Fix #bugnum: " (If
    this PR fixes part of an issue, prefix the title with "Fix part of
    #bugnum: ...".)
    - [x] Any changes to
    [scripts/assets](https://github.com/oppia/oppia-android/tree/develop/scripts/assets)
    files have their rationale included in the PR explanation.
    - [x] The PR follows the [style
    guide](https://github.com/oppia/oppia-android/wiki/Coding-style-guide).
    - [x] The PR does not contain any unnecessary code changes from Android
    Studio
    ([reference](https://github.com/oppia/oppia-android/wiki/Guidance-on-submitting-a-PR#undo-unnecessary-changes)).
    - [x] The PR is made from a branch that's **not** called "develop" and
    is up-to-date with "develop".
    - [x] The PR is **assigned** to the appropriate reviewers
    ([reference](https://github.com/oppia/oppia-android/wiki/Guidance-on-submitting-a-PR#clarification-regarding-assignees-and-reviewers-section)).
    
    ## For UI-specific PRs only
    <!-- Delete these section if this PR does not include UI-related
    changes. -->
    If your PR includes UI-related changes, then:
    - Add screenshots for portrait/landscape for both a tablet & phone of
    the before & after UI changes
    - For the screenshots above, include both English and pseudo-localized
    (RTL) screenshots (see [RTL
    guide](https://github.com/oppia/oppia-android/wiki/RTL-Guidelines))
    - Add a video showing the full UX flow with a screen reader enabled (see
    [accessibility
    guide](https://github.com/oppia/oppia-android/wiki/Accessibility-(A11y)-Guide))
    - Add a screenshot demonstrating that you ran affected Espresso tests
    locally & that they're passing
    
    
    The following videos show how you can click on a hidden ui control in
    the current version, but you cannot after the changes in this cl.
    |current|new|
    |-|-|
    
    |[current_administration_controls_invisible_but_actionable.webm](https://user-images.githubusercontent.com/103062089/225941610-d41ed412-625c-45de-b18c-3279bd2d2401.webm)|[new_administration_controls_invisible_but_actionable.webm](https://user-images.githubusercontent.com/103062089/225941644-7b3598ba-89c5-4a8b-9611-ecc751ea1b25.webm)|
    
    Screenshot with test failure when app-version goes back to use add
    fragment, instead of replace fragment.
    
    ![Test failure when app-version adds fragment, instead of replacing
    it](https://user-images.githubusercontent.com/103062089/228270103-04a9224e-5ca2-4150-9a8e-abc717972290.png)
    
    ---------
    
    Co-authored-by: Ben Henning <ben@oppia.org>

--- app/src/sharedTest/java/org/oppia/android/app/administratorcontrols/AdministratorControlsActivityTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/administratorcontrols/AdministratorControlsActivityTest.kt
@@ -29,7 +29,9 @@ import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.espresso.matcher.RootMatchers.isDialog
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.hasChildCount
 import androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.isRoot
 import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
@@ -41,6 +43,7 @@ import androidx.test.rule.ActivityTestRule
 import com.google.common.truth.Truth.assertThat
 import dagger.Component
 import org.hamcrest.Matchers
+import org.hamcrest.Matchers.allOf
 import org.hamcrest.Matchers.not
 import org.junit.After
 import org.junit.Before
@@ -371,6 +374,56 @@ class AdministratorControlsActivityTest {
       verifyTextInDialog(textInDialogId = R.string.log_out_dialog_message)
       onView(withText(R.string.log_out_dialog_cancel_button)).perform(click())
       onView(withId(R.id.log_out_text_view)).check(matches(isDisplayed()))
+    }
+  }
+
+  @Test
+  @Config(qualifiers = "sw600dp")
+  fun testAdministratorControls_defaultTabletConfig_openAppVersion_replacesPreviousFragment() {
+    launch<AdministratorControlsActivity>(
+      createAdministratorControlsActivityIntent(
+        profileId = internalProfileId
+      )
+    ).use {
+      testCoroutineDispatchers.runCurrent()
+
+      // Check that the multipane container has only one child and it is the edit profiles fragment
+      onView(
+        allOf(
+          withId(R.id.profile_list_recycler_view),
+          isDescendantOfA(withId(R.id.administrator_controls_fragment_multipane_placeholder))
+        )
+      ).check(
+        matches(
+          isDisplayed()
+        )
+      )
+      onView(withId(R.id.administrator_controls_fragment_multipane_placeholder)).check(
+        matches(
+          hasChildCount(1)
+        )
+      )
+
+      // Open the app version fragment
+      scrollToPosition(position = 3)
+      onView(withId(R.id.app_version_text_view)).perform(click())
+
+      // Check that the multipane container has only one child and it is the app version fragment
+      onView(
+        allOf(
+          withId(R.id.app_version_text_view),
+          isDescendantOfA(withId(R.id.administrator_controls_fragment_multipane_placeholder))
+        )
+      ).check(
+        matches(
+          isDisplayed()
+        )
+      )
+      onView(withId(R.id.administrator_controls_fragment_multipane_placeholder)).check(
+        matches(
+          hasChildCount(1)
+        )
+      )
     }
   }
 
