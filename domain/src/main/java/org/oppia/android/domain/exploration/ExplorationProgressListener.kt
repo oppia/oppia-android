d87b51e1137866789584fc6d7dfeacc66cfee668
Adhiambo Peres <adhiambooyier@gmail.com>
2023-04-17 03:36:40 +0300

    Refactor the implementation of ExplorationProgressListener
    onExplorationSessionPaused receives the correct instances of topic ID and profile ID

--- domain/src/main/java/org/oppia/android/domain/exploration/ExplorationProgressListener.kt
+++ domain/src/main/java/org/oppia/android/domain/exploration/ExplorationProgressListener.kt
@@ -1,10 +1,12 @@
 package org.oppia.android.domain.exploration
 
+import org.oppia.android.app.model.ProfileId
+
 /** Listener for when an exploration is started or paused. */
 interface ExplorationProgressListener {
-  /** Called when an exploration is started. */
+  /** Called when an exploration session is started. */
   fun onExplorationSessionStarted()
 
-  /** Called when an exploration is finished. */
-  fun onExplorationSessionPaused()
+  /** Called when an exploration session is finished. */
+  fun onExplorationSessionPaused(profileId: ProfileId, topicId: String)
 }
--- domain/src/main/java/org/oppia/android/domain/exploration/ExplorationProgressListenerImpl.kt
+++ domain/src/main/java/org/oppia/android/domain/exploration/ExplorationProgressListenerImpl.kt
@@ -1,39 +1,20 @@
 package org.oppia.android.domain.exploration
 
 import org.oppia.android.app.model.ProfileId
-import org.oppia.android.domain.oppialogger.analytics.ApplicationLifecycleListener
-import org.oppia.android.util.logging.ConsoleLogger
 import javax.inject.Inject
+import javax.inject.Singleton
 
 /** Implementation of [ExplorationProgressListener] */
+@Singleton
 class ExplorationProgressListenerImpl @Inject constructor(
-  private val explorationActiveTimeController: ExplorationActiveTimeController,
-  private val consoleLogger: ConsoleLogger
-) : ExplorationProgressListener, ApplicationLifecycleListener {
-  val profileId = ProfileId.getDefaultInstance()
-  val topicId = "testTopicIdImpl"
+  private val explorationActiveTimeController: ExplorationActiveTimeController
+) : ExplorationProgressListener {
 
   override fun onExplorationSessionStarted() {
-
-    startTimer()
-  }
-
-  override fun onExplorationSessionPaused() {
-    stopTimerAndSave()
-  }
-
-  override fun onAppInForeground() {
-  }
-
-  override fun onAppInBackground() {
-    stopTimerAndSave()
-  }
-
-  private fun startTimer() {
     explorationActiveTimeController.setExplorationSessionStarted()
   }
 
-  private fun stopTimerAndSave() {
+  override fun onExplorationSessionPaused(profileId: ProfileId, topicId: String) {
     explorationActiveTimeController.setExplorationSessionPaused(profileId, topicId)
   }
 }
