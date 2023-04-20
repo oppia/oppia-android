2bc06d688a9639f68cc0a94bddc4c5be2f1c8815
Kevin Gitonga <kevinjones4199@gmail.com>
2023-04-20 15:21:09 +0300

    Revert "Merge branch 'develop' into profile_id_provision_feature"
    This reverts commit f28d12ac006b87b957c23fddb1f83b764688c876, reversing
    changes made to 3773844e8c87fc43d67039f875a377d89a4054ad.

--- app/src/main/java/org/oppia/android/app/topic/lessons/StorySummaryViewModel.kt
+++ app/src/main/java/org/oppia/android/app/topic/lessons/StorySummaryViewModel.kt
@@ -41,6 +41,17 @@ class StorySummaryViewModel(
     storyProgressPercentageText.set(computeStoryProgressPercentageText(storyPercentage))
   }
 
+  fun computeStoryNameChapterCountContainerContentDescription(): String {
+    // TODO(#3844): Combine these strings together.
+    val chapterCountText =
+      resourceHandler.getQuantityStringInLocaleWithWrapping(
+        R.plurals.chapter_count, storySummary.chapterCount, storySummary.chapterCount.toString()
+      )
+    return resourceHandler.getStringInLocaleWithWrapping(
+      R.string.chapter_count_with_story_name, chapterCountText, storyTitle
+    )
+  }
+
   fun computeChapterCountText(): String {
     return resourceHandler.getQuantityStringInLocaleWithWrapping(
       R.plurals.chapter_count, storySummary.chapterCount, storySummary.chapterCount.toString()
