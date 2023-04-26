5b222d9eb6261b6706b22766e24b4250b4a83e5b
Adhiambo Peres <adhiambooyier@gmail.com>
2023-04-26 19:34:23 +0300

    Refactor time of day gating logic to not use calendar directly
    Make use of MachineLocale time of day function.

--- domain/src/main/java/org/oppia/android/domain/survey/SurveyGatingController.kt
+++ domain/src/main/java/org/oppia/android/domain/survey/SurveyGatingController.kt
@@ -3,106 +3,86 @@ package org.oppia.android.domain.survey
 import org.oppia.android.app.model.ProfileId
 import org.oppia.android.domain.exploration.ExplorationActiveTimeController
 import org.oppia.android.domain.profile.ProfileManagementController
+import org.oppia.android.util.data.AsyncResult
 import org.oppia.android.util.data.DataProvider
-import org.oppia.android.util.data.DataProviders
-import org.oppia.android.util.data.DataProviders.Companion.combineWith
 import org.oppia.android.util.data.DataProviders.Companion.transform
+import org.oppia.android.util.locale.OppiaLocale
 import org.oppia.android.util.system.OppiaClock
-import java.util.Calendar
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
-private const val SURVEY_LAST_SHOWN_DATE_LIMIT_DAYS = 30
 private const val SURVEY_MINIMUM_TOPIC_LEARNING_TIME_MINUTES = 5
 
-private const val SURVEY_LAST_SHOWN_DATE_GATING_PROVIDER_ID =
-  "survey_last_shown_date_gating_provider_id"
-private const val SURVEY_TIME_OF_DAY_GATING_PROVIDER_ID = "survey_time_of_day_gating_provider_id"
-private const val SURVEY_TIME_AND_DATE_COMBINED_GATING_PROVIDER_ID =
-  "survey_time_and_date_gating_provider_id"
-private const val SURVEY_TOPIC_LEARNING_TIME_GATING_PROVIDER_ID =
-  "survey_topic_learning_time_gating_provider_id"
-private const val SURVEY_COMBINED_GATING_PROVIDER_ID =
-  "survey_topic_learning_time_provider_id"
+private const val GET_SURVEY_LAST_SHOWN_TIMESTAMP_PROVIDER_ID =
+  "get_survey_last_shown_timestamp_provider_id"
+private const val SURVEY_LAST_SHOWN_TIMESTAMP_PROVIDER_ID =
+  "survey_last_shown_timestamp_provider_id"
+private const val GET_TOPIC_LEARNING_TIME_PROVIDER_ID =
+  "get_topic_learning_time_provider_id"
+private const val TOPIC_LEARNING_TIME_PROVIDER_ID =
+  "topic_learning_time_provider_id"
 
 /**
  * Controller for retrieving survey gating criteria and deciding if a survey should be shown.
  */
 class SurveyGatingController @Inject constructor(
-  private val dataProviders: DataProviders,
   private val profileManagementController: ProfileManagementController,
   private val oppiaClock: OppiaClock,
-  private val explorationActiveTimeController: ExplorationActiveTimeController
+  private val explorationActiveTimeController: ExplorationActiveTimeController,
+  private val machineLocale: OppiaLocale.MachineLocale
 ) {
+
+  private val surveyGracePeriodMillis = TimeUnit.DAYS.toMillis(30)
 
   /**
    * Returns a boolean indicating whether a survey can be shown.
    */
-  fun shouldShowSurvey(
-    profileId: ProfileId,
-    topicId: String
-  ): DataProvider<Boolean> {
-    val timeAndDateGatingProvider = isSurveyTimeOfDayWindowOpen().combineWith(
-      getSurveyLastShownDateMs(profileId),
-      SURVEY_TIME_AND_DATE_COMBINED_GATING_PROVIDER_ID
-    ) { isSurveyWindowOpen, isSurveyDateLimitPassed ->
-      isSurveyWindowOpen && isSurveyLastShownDateLimitPassed(isSurveyDateLimitPassed)
-    }
-    return timeAndDateGatingProvider.combineWith(
-      getAggregateLearningTime(profileId, topicId),
-      SURVEY_COMBINED_GATING_PROVIDER_ID
-    ) { isTimeAndDateAllowed, aggregateLearningTime ->
-      isTimeAndDateAllowed && isMinimumAggregateTopicLearningTime(aggregateLearningTime)
-    }
+  fun shouldShowSurvey(profileId: ProfileId, topicId: String) {}
+
+  private fun isSurveyTimeOfDayWindowOpen(): Boolean = when (machineLocale.getCurrentTimeOfDay()) {
+    OppiaLocale.MachineLocale.TimeOfDay.MORNING,
+    OppiaLocale.MachineLocale.TimeOfDay.AFTERNOON,
+    OppiaLocale.MachineLocale.TimeOfDay.EVENING -> true
+    OppiaLocale.MachineLocale.TimeOfDay.EARLY_MORNING,
+    OppiaLocale.MachineLocale.TimeOfDay.NIGHT,
+    OppiaLocale.MachineLocale.TimeOfDay.UNKNOWN -> false
   }
 
-  private val morningLimit = Calendar.getInstance().apply {
-    set(Calendar.HOUR_OF_DAY, 9)
-    set(Calendar.MINUTE, 0)
-    set(Calendar.SECOND, 0)
-  }
+  private suspend fun isSurveyLastShownDateLimitPassed(profileId: ProfileId): AsyncResult<Boolean> {
+    return getSurveyLastShownDateMs(profileId)
+      .transform(SURVEY_LAST_SHOWN_TIMESTAMP_PROVIDER_ID) { lastShownTimestampMs ->
+        val surveyLastShownCalendar = oppiaClock.getCurrentCalendar()
+        surveyLastShownCalendar.timeInMillis = lastShownTimestampMs
 
-  private val eveningLimit = Calendar.getInstance().apply {
-    set(Calendar.HOUR_OF_DAY, 22)
-    set(Calendar.MINUTE, 0)
-    set(Calendar.SECOND, 0)
-  }
+        val currentCalendar = oppiaClock.getCurrentCalendar()
+        val showNextCalendar = currentCalendar.timeInMillis.plus(surveyGracePeriodMillis)
 
-  private fun isSurveyTimeOfDayWindowOpen(): DataProvider<Boolean> {
-    return dataProviders.createInMemoryDataProvider(SURVEY_TIME_OF_DAY_GATING_PROVIDER_ID) {
-      oppiaClock.getCurrentCalendar().after(morningLimit) &&
-        oppiaClock.getCurrentCalendar().before(eveningLimit)
-    }
+        currentCalendar.after(showNextCalendar) ||
+          currentCalendar.equals(showNextCalendar)
+      }.retrieveData()
   }
 
   private fun getSurveyLastShownDateMs(profileId: ProfileId): DataProvider<Long> {
     return profileManagementController.fetchSurveyLastShownTimestamp(profileId)
-      .transform(SURVEY_LAST_SHOWN_DATE_GATING_PROVIDER_ID) { lastShownTimestampMs ->
+      .transform(GET_SURVEY_LAST_SHOWN_TIMESTAMP_PROVIDER_ID) { lastShownTimestampMs ->
         lastShownTimestampMs
       }
   }
 
-  private fun isSurveyLastShownDateLimitPassed(timestamp: Long): Boolean {
-    val surveyLastShownDateCalendar = oppiaClock.getCurrentCalendar()
-    surveyLastShownDateCalendar.timeInMillis = timestamp
-
-    // Add the grace period allowed before a survey can be shown again.
-    surveyLastShownDateCalendar.add(Calendar.DAY_OF_YEAR, SURVEY_LAST_SHOWN_DATE_LIMIT_DAYS)
-
-    val currentDateCalendar = oppiaClock.getCurrentCalendar()
-    return currentDateCalendar.after(surveyLastShownDateCalendar) ||
-      currentDateCalendar == surveyLastShownDateCalendar
-  }
-
-  private fun isMinimumAggregateTopicLearningTime(
-    learningTime: Long
-  ): Boolean {
-    return learningTime >= SURVEY_MINIMUM_TOPIC_LEARNING_TIME_MINUTES
+  private suspend fun isThereMinimumAggregateTopicLearningTime(
+    profileId: ProfileId,
+    topicId: String
+  ): AsyncResult<Boolean> {
+    return getAggregateLearningTime(profileId, topicId)
+      .transform(TOPIC_LEARNING_TIME_PROVIDER_ID) { topicLearningTimeMs ->
+        topicLearningTimeMs >= SURVEY_MINIMUM_TOPIC_LEARNING_TIME_MINUTES
+      }.retrieveData()
   }
 
   private fun getAggregateLearningTime(profileId: ProfileId, topicId: String): DataProvider<Long> {
     return explorationActiveTimeController.retrieveAggregateTopicLearningTimeDataProvider(
       profileId, topicId
-    ).transform(SURVEY_TOPIC_LEARNING_TIME_GATING_PROVIDER_ID) { topicLearningTime ->
+    ).transform(GET_TOPIC_LEARNING_TIME_PROVIDER_ID) { topicLearningTime ->
       topicLearningTime.topicLearningTimeMs
     }
   }
