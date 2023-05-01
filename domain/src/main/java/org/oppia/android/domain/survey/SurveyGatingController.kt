33fa19ad535eaa0aa3cbc73f71e794145adc5100
Adhiambo Peres <adhiambooyier@gmail.com>
2023-05-01 03:12:44 +0300

    Refactor gating API.
    Use combineWith operations to transform the gating criteria and return a gating result as a boolean

--- domain/src/main/java/org/oppia/android/domain/survey/SurveyGatingController.kt
+++ domain/src/main/java/org/oppia/android/domain/survey/SurveyGatingController.kt
@@ -1,26 +1,23 @@
 package org.oppia.android.domain.survey
 
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
 import org.oppia.android.app.model.ProfileId
 import org.oppia.android.domain.exploration.ExplorationActiveTimeController
 import org.oppia.android.domain.profile.ProfileManagementController
 import org.oppia.android.util.data.AsyncResult
 import org.oppia.android.util.data.DataProvider
-import org.oppia.android.util.data.DataProviders.Companion.transform
+import org.oppia.android.util.data.DataProviders.Companion.combineWith
+import org.oppia.android.util.data.DataProviders.Companion.transformAsync
 import org.oppia.android.util.locale.OppiaLocale
 import org.oppia.android.util.system.OppiaClock
-import java.util.concurrent.TimeUnit
-import javax.inject.Inject
-
-private const val SURVEY_MINIMUM_TOPIC_LEARNING_TIME_MINUTES = 5
 
 private const val GET_SURVEY_LAST_SHOWN_TIMESTAMP_PROVIDER_ID =
   "get_survey_last_shown_timestamp_provider_id"
-private const val SURVEY_LAST_SHOWN_TIMESTAMP_PROVIDER_ID =
-  "survey_last_shown_timestamp_provider_id"
 private const val GET_TOPIC_LEARNING_TIME_PROVIDER_ID =
   "get_topic_learning_time_provider_id"
-private const val TOPIC_LEARNING_TIME_PROVIDER_ID =
-  "topic_learning_time_provider_id"
+private const val GATING_RESULT_PROVIDER_ID =
+  "gating_result_provider_id"
 
 /**
  * Controller for retrieving survey gating criteria and deciding if a survey should be shown.
@@ -32,14 +29,25 @@ class SurveyGatingController @Inject constructor(
   private val machineLocale: OppiaLocale.MachineLocale
 ) {
 
-  private val surveyGracePeriodMillis = TimeUnit.DAYS.toMillis(30)
+  private val gracePeriodMillis = TimeUnit.DAYS.toMillis(30)
+  private val minimumLearningTimeForGatingMillis = TimeUnit.MINUTES.toMillis(5)
 
   /**
    * Returns a boolean indicating whether a survey can be shown.
    */
-  fun shouldShowSurvey(profileId: ProfileId, topicId: String) {}
+  fun maybeShowSurvey(profileId: ProfileId, topicId: String): DataProvider<Any?> {
+    val lastShownDateProvider = retrieveSurveyLastShownDate(profileId)
+    val learningTimeProvider = retrieveAggregateLearningTime(profileId, topicId)
+    return lastShownDateProvider.combineWith(
+      learningTimeProvider, GATING_RESULT_PROVIDER_ID
+    ) { lastShownTimestampMs, learningTimeMs ->
+      isSurveyGracePeriodExpired(lastShownTimestampMs) &&
+        hasReachedMinimumTopicLearningThreshold(learningTimeMs) &&
+        isWithinSurveyTimeWindow()
+    }
+  }
 
-  private fun isSurveyTimeOfDayWindowOpen(): Boolean = when (machineLocale.getCurrentTimeOfDay()) {
+  private fun isWithinSurveyTimeWindow(): Boolean = when (machineLocale.getCurrentTimeOfDay()) {
     OppiaLocale.MachineLocale.TimeOfDay.MID_MORNING,
     OppiaLocale.MachineLocale.TimeOfDay.AFTERNOON,
     OppiaLocale.MachineLocale.TimeOfDay.EVENING -> true
@@ -48,42 +56,37 @@ class SurveyGatingController @Inject constructor(
     OppiaLocale.MachineLocale.TimeOfDay.UNKNOWN -> false
   }
 
-  private suspend fun isSurveyLastShownDateLimitPassed(profileId: ProfileId): AsyncResult<Boolean> {
-    return getSurveyLastShownDateMs(profileId)
-      .transform(SURVEY_LAST_SHOWN_TIMESTAMP_PROVIDER_ID) { lastShownTimestampMs ->
-        val surveyLastShownCalendar = oppiaClock.getCurrentCalendar()
-        surveyLastShownCalendar.timeInMillis = lastShownTimestampMs
+  private fun isSurveyGracePeriodExpired(lastShownTimestampMs: Long): Boolean {
+    val surveyLastShownCalendar = oppiaClock.getCurrentCalendar()
+    surveyLastShownCalendar.timeInMillis = lastShownTimestampMs
 
-        val currentCalendar = oppiaClock.getCurrentCalendar()
-        val showNextCalendar = currentCalendar.timeInMillis.plus(surveyGracePeriodMillis)
+    val currentCalendar = oppiaClock.getCurrentCalendar()
+    val showNextCalendar = currentCalendar.timeInMillis.plus(gracePeriodMillis)
 
-        currentCalendar.after(showNextCalendar) ||
-          currentCalendar.equals(showNextCalendar)
-      }.retrieveData()
+    return currentCalendar.after(showNextCalendar) ||
+      currentCalendar.equals(showNextCalendar)
   }
 
-  private fun getSurveyLastShownDateMs(profileId: ProfileId): DataProvider<Long> {
-    return profileManagementController.fetchSurveyLastShownTimestamp(profileId)
-      .transform(GET_SURVEY_LAST_SHOWN_TIMESTAMP_PROVIDER_ID) { lastShownTimestampMs ->
-        lastShownTimestampMs
+  private fun retrieveSurveyLastShownDate(profileId: ProfileId): DataProvider<Long> {
+    return profileManagementController.retrieveSurveyLastShownTimestamp(profileId)
+      .transformAsync(GET_SURVEY_LAST_SHOWN_TIMESTAMP_PROVIDER_ID) { lastShownTimestampMs ->
+        AsyncResult.Success(lastShownTimestampMs)
       }
   }
 
-  private suspend fun isThereMinimumAggregateTopicLearningTime(
-    profileId: ProfileId,
-    topicId: String
-  ): AsyncResult<Boolean> {
-    return getAggregateLearningTime(profileId, topicId)
-      .transform(TOPIC_LEARNING_TIME_PROVIDER_ID) { topicLearningTimeMs ->
-        topicLearningTimeMs >= SURVEY_MINIMUM_TOPIC_LEARNING_TIME_MINUTES
-      }.retrieveData()
+  private fun hasReachedMinimumTopicLearningThreshold(topicLearningTimeMs: Long): Boolean {
+    return topicLearningTimeMs >= minimumLearningTimeForGatingMillis
   }
 
-  private fun getAggregateLearningTime(profileId: ProfileId, topicId: String): DataProvider<Long> {
+  private fun retrieveAggregateLearningTime(
+    profileId: ProfileId,
+    topicId: String
+  ): DataProvider<Long> {
     return explorationActiveTimeController.retrieveAggregateTopicLearningTimeDataProvider(
       profileId, topicId
-    ).transform(GET_TOPIC_LEARNING_TIME_PROVIDER_ID) { topicLearningTime ->
-      topicLearningTime.topicLearningTimeMs
+    ).transformAsync(GET_TOPIC_LEARNING_TIME_PROVIDER_ID) { topicLearningTime ->
+      val aggregateLearningTimeMs = topicLearningTime.topicLearningTimeMs
+      AsyncResult.Success(aggregateLearningTimeMs)
     }
   }
 }
