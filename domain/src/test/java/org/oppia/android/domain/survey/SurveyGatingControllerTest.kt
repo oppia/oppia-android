73dd2405e10e5d4496c305460ad4482bf4d8da44
Adhiambo Peres <adhiambooyier@gmail.com>
2023-05-17 01:43:59 +0300

    Fix gating logic error found in tests
    Correctly compute whether the Survey Grace Period has Ended

--- domain/src/test/java/org/oppia/android/domain/survey/SurveyGatingControllerTest.kt
+++ domain/src/test/java/org/oppia/android/domain/survey/SurveyGatingControllerTest.kt
@@ -9,9 +9,6 @@ import dagger.BindsInstance
 import dagger.Component
 import dagger.Module
 import dagger.Provides
-import java.util.concurrent.TimeUnit
-import javax.inject.Inject
-import javax.inject.Singleton
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -46,6 +43,9 @@ import org.oppia.android.util.networking.NetworkConnectionUtilDebugModule
 import org.oppia.android.util.platformparameter.LEARNER_STUDY_ANALYTICS_DEFAULT_VALUE
 import org.robolectric.annotation.Config
 import org.robolectric.annotation.LooperMode
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
+import javax.inject.Singleton
 
 private const val SESSION_LENGTH_SHORT = 120000L
 private const val SESSION_LENGTH_LONG = 360000L
--- testing/src/main/java/org/oppia/android/testing/platformparameter/TestPlatformParameterModule.kt
+++ testing/src/main/java/org/oppia/android/testing/platformparameter/TestPlatformParameterModule.kt
@@ -3,7 +3,6 @@ package org.oppia.android.testing.platformparameter
 import androidx.annotation.VisibleForTesting
 import dagger.Module
 import dagger.Provides
-import javax.inject.Singleton
 import org.oppia.android.util.platformparameter.CACHE_LATEX_RENDERING
 import org.oppia.android.util.platformparameter.CACHE_LATEX_RENDERING_DEFAULT_VALUE
 import org.oppia.android.util.platformparameter.CacheLatexRendering
@@ -45,6 +44,7 @@ import org.oppia.android.util.platformparameter.SYNC_UP_WORKER_TIME_PERIOD_IN_HO
 import org.oppia.android.util.platformparameter.SYNC_UP_WORKER_TIME_PERIOD_IN_HOURS_DEFAULT_VALUE
 import org.oppia.android.util.platformparameter.SplashScreenWelcomeMsg
 import org.oppia.android.util.platformparameter.SyncUpWorkerTimePeriodHours
+import javax.inject.Singleton
 
 /* Fake Platform Parameter Module that provides individual Platform Parameters for testing. */
 @Module
