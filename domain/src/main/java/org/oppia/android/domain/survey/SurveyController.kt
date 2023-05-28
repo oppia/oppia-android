fe4a55bb2be13a6fd526d2295d25e1ec7c408916
Adhiambo Peres <adhiambooyier@gmail.com>
2023-05-28 19:14:41 +0300

    Refactor SurveyController to only contain survey and question creation logic

--- domain/src/main/java/org/oppia/android/domain/survey/SurveyController.kt
+++ domain/src/main/java/org/oppia/android/domain/survey/SurveyController.kt
@@ -1,21 +1,17 @@
 package org.oppia.android.domain.survey
 
-import java.util.*
-import javax.inject.Inject
-import javax.inject.Singleton
-import org.oppia.android.app.model.MarketFitAnswer
 import org.oppia.android.app.model.OppiaLanguage
 import org.oppia.android.app.model.Survey
 import org.oppia.android.app.model.SurveyQuestion
 import org.oppia.android.app.model.SurveyQuestionName
-import org.oppia.android.app.model.SurveyQuestionOption
-import org.oppia.android.app.model.SurveyQuestionOptionList
-import org.oppia.android.app.model.UserTypeAnswer
 import org.oppia.android.domain.oppialogger.exceptions.ExceptionsController
 import org.oppia.android.util.data.AsyncResult
 import org.oppia.android.util.data.DataProvider
 import org.oppia.android.util.data.DataProviders
 import org.oppia.android.util.data.DataProviders.Companion.combineWith
+import java.util.UUID
+import javax.inject.Inject
+import javax.inject.Singleton
 
 private const val CREATE_SURVEY_PROVIDER_ID = "create_survey_provider_id"
 private const val START_SURVEY_SESSION_PROVIDER_ID = "start_survey_session_provider_id"
@@ -74,90 +70,22 @@ class SurveyController @Inject constructor(
       .map { questionName ->
         createSurveyQuestion(
           questionName,
-          OppiaLanguage.ENGLISH,
-          SurveyQuestion.QuestionTypeCase.MULTIPLE_CHOICE_WITH_OTHER
+          OppiaLanguage.ENGLISH // todo pass app language
         )
       }
   }
 
   private fun createSurveyQuestion(
     questionName: SurveyQuestionName,
-    language: OppiaLanguage,
-    questionType: SurveyQuestion.QuestionTypeCase
+    language: OppiaLanguage
   ): SurveyQuestion {
-    val surveyQuestionBuilder = SurveyQuestion.newBuilder()
+    return SurveyQuestion.newBuilder()
       .setQuestionName(questionName)
       .setLanguage(language)
-
-    return when (questionType) {
-      SurveyQuestion.QuestionTypeCase.MULTIPLE_CHOICE_WITH_OTHER -> {
-        surveyQuestionBuilder.setMultipleChoice(createUserTypeAnswerOptions()).build()
-      }
-      SurveyQuestion.QuestionTypeCase.MULTIPLE_CHOICE -> {
-        surveyQuestionBuilder.setMultipleChoice(createMarketFitAnswerOptions()).build()
-      }
-      SurveyQuestion.QuestionTypeCase.FREE_FORM_TEXT -> {
-        surveyQuestionBuilder.setFreeFormText(true).build()
-      }
-      else -> SurveyQuestion.getDefaultInstance()
-    }
-  }
-
-  // todo maybe add question_option_id
-  private fun createUserTypeAnswerOptions(): SurveyQuestionOptionList {
-    val userTypeOptions = UserTypeAnswer.values()
-      .filter { it.isValid() }
-      .map { userType ->
-        SurveyQuestionOption.newBuilder()
-          .setUserType(userType)
-          .build()
-      }
-    return SurveyQuestionOptionList.newBuilder()
-      .addAllOptions(userTypeOptions)
-      .build()
-  }
-
-  private fun createMarketFitAnswerOptions(): SurveyQuestionOptionList {
-    val marketFitOptions = MarketFitAnswer.values()
-      .filter { it.isValid() }
-      .map { marketFitAnswer ->
-        SurveyQuestionOption.newBuilder()
-          .setMarketFit(marketFitAnswer)
-          .build()
-      }
-    return SurveyQuestionOptionList.newBuilder()
-      .addAllOptions(marketFitOptions)
-      .build()
-  }
-
-  private fun createNpsScoreAnswerOptions(): SurveyQuestionOptionList {
-    val npsOptions = (0..10).map { npsScore ->
-      SurveyQuestionOption.newBuilder()
-        .setNpsScore(npsScore)
-        .build()
-    }
-    return SurveyQuestionOptionList.newBuilder()
-      .addAllOptions(npsOptions)
       .build()
   }
 
   companion object {
-    /** Returns whether a [MarketFitAnswer] is valid. */
-    fun MarketFitAnswer.isValid(): Boolean {
-      return when (this) {
-        MarketFitAnswer.UNRECOGNIZED, MarketFitAnswer.MARKET_FIT_ANSWER_UNSPECIFIED -> false
-        else -> true
-      }
-    }
-
-    /** Returns whether a [UserTypeAnswer] is valid. */
-    fun UserTypeAnswer.isValid(): Boolean {
-      return when (this) {
-        UserTypeAnswer.UNRECOGNIZED, UserTypeAnswer.USER_TYPE_UNSPECIFIED -> false
-        else -> true
-      }
-    }
-
     /** Returns whether a [SurveyQuestionName] is valid. */
     fun SurveyQuestionName.isValid(): Boolean {
       return when (this) {
