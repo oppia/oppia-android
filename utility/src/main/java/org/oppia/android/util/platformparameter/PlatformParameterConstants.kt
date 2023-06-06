bf87df52726f905fffee07dbd9dd3bf2d3e9ed1a
Aayushi Mathur <aayushimathur6@gmail.com>
2023-06-06 17:49:18 +0530

    updated with top constraint for profile deletion button

--- utility/src/main/java/org/oppia/android/util/platformparameter/PlatformParameterConstants.kt
+++ utility/src/main/java/org/oppia/android/util/platformparameter/PlatformParameterConstants.kt
@@ -109,7 +109,7 @@ const val LEARNER_STUDY_ANALYTICS = "learner_study_analytics"
  * Default value of the platform parameter that controls the visibility of [ProfileAndDeviceIdActivity]
  * and working of learner study related analytics logging.
  */
-const val LEARNER_STUDY_ANALYTICS_DEFAULT_VALUE = false
+const val LEARNER_STUDY_ANALYTICS_DEFAULT_VALUE = true
 
 /**
  * Qualifier for the platform parameter that controls whether to cache LaTeX rendering using Glide.
