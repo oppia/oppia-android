8a4cbfcf51a31e9abb9fedf3fd3a9ceaf912386d
Adhiambo Peres <adhiambooyier@gmail.com>
2023-06-07 01:21:50 +0300

    Add caution for accessing ExplorationProgressListener on multiple threads
    Fixes https://github.com/oppia/oppia-android/pull/4916#discussion_r1214058001

--- domain/src/main/java/org/oppia/android/domain/exploration/ExplorationProgressListener.kt
+++ domain/src/main/java/org/oppia/android/domain/exploration/ExplorationProgressListener.kt
@@ -2,7 +2,13 @@ package org.oppia.android.domain.exploration
 
 import org.oppia.android.app.model.ProfileId
 
-/** Listener for when an exploration has started or ended. */
+/** Listener for when an exploration has started or ended.
+ *
+ * This listener is not safe to use across multiple threads and should only be accessed via the main
+ * thread.
+ *
+ * These methods are invoked via the [ExplorationProgressController].
+ */
 interface ExplorationProgressListener {
   /** Called when an exploration has started. */
   fun onExplorationStarted(profileId: ProfileId, topicId: String)
