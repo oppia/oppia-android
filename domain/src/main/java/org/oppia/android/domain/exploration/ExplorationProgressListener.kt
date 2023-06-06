9d8e70371c3de12d6a1938b8b1ccb398156865d7
Adhiambo Peres <adhiambooyier@gmail.com>
2023-06-07 00:41:29 +0300

    Fix nit on verb use in ExplorationProgressListener
    Fixes: https://github.com/oppia/oppia-android/pull/4916#discussion_r1213871142

--- domain/src/main/java/org/oppia/android/domain/exploration/ExplorationProgressListener.kt
+++ domain/src/main/java/org/oppia/android/domain/exploration/ExplorationProgressListener.kt
@@ -2,11 +2,11 @@ package org.oppia.android.domain.exploration
 
 import org.oppia.android.app.model.ProfileId
 
-/** Listener for when an exploration is started or ended. */
+/** Listener for when an exploration has started or ended. */
 interface ExplorationProgressListener {
-  /** Called when an exploration is started. */
+  /** Called when an exploration has started. */
   fun onExplorationStarted(profileId: ProfileId, topicId: String)
 
-  /** Called when an exploration is stopped. */
+  /** Called when an exploration has ended. */
   fun onExplorationEnded(profileId: ProfileId, topicId: String)
 }
