a5cc2cfe1117422e0f573c1d6cd33aa06c2b8171
Adhiambo Peres <adhiambooyier@gmail.com>
2023-07-17 13:38:02 +0300

    Fix failing static checks

--- domain/src/main/java/org/oppia/android/domain/oppialogger/firestoreuploader/FirestoreUploadWorkManagerInitializer.kt
+++ domain/src/main/java/org/oppia/android/domain/oppialogger/firestoreuploader/FirestoreUploadWorkManagerInitializer.kt
@@ -7,12 +7,12 @@ import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.NetworkType
 import androidx.work.PeriodicWorkRequest
 import androidx.work.WorkManager
+import java.util.*
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
 import org.oppia.android.domain.oppialogger.analytics.AnalyticsStartupListener
 import org.oppia.android.domain.oppialogger.firestoreuploader.FirestoreUploadWorker.Companion.FIRESTORE_WORKER
 import org.oppia.android.domain.oppialogger.firestoreuploader.FirestoreUploadWorker.Companion.WORKER_CASE_KEY
-import java.util.UUID
-import java.util.concurrent.TimeUnit
-import javax.inject.Inject
 
 /**
  * Enqueues unique periodic work requests for uploading events to Firestore on application creation.
@@ -32,9 +32,8 @@ class FirestoreUploadWorkManagerInitializer @Inject constructor() : AnalyticsSta
     )
     .build()
 
-  // TODO change interval to 6 hours
   private val workRequestForUploadingFireStoreData: PeriodicWorkRequest =
-    PeriodicWorkRequest.Builder(FirestoreUploadWorker::class.java, 15, TimeUnit.MINUTES)
+    PeriodicWorkRequest.Builder(FirestoreUploadWorker::class.java, 6, TimeUnit.HOURS)
       .setInputData(workerCaseForUploadingFirestoreData)
       .setConstraints(firestoreUploadWorkerConstraints)
       .build()
--- utility/src/main/java/org/oppia/android/util/firestore/FirestoreDocumentCreator.kt
+++ utility/src/main/java/org/oppia/android/util/firestore/FirestoreDocumentCreator.kt
@@ -4,5 +4,10 @@ import org.oppia.android.app.model.EventLog
 
 /** Utility for creating Firestore Documents from [EventLog] objects. */
 interface FirestoreDocumentCreator {
+  /**
+   * Converts an event log to a a hash map that is to be uploaded as a Firestore Document.
+   *
+   * @param eventLog refers to the log object which contains all the relevant data to be reported
+   */
   fun createDocument(eventLog: EventLog): HashMap<String, Any?>
 }
