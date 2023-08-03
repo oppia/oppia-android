4176ab7632d8ba155be8047fbe29a4171c14a984
Akshat Kamboj <akukamboj@gmail.com>
2023-08-04 00:15:27 +0530

    corrected k-doc

--- app/src/sharedTest/java/org/oppia/android/app/settings/profile/SnackbarManagerTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/settings/profile/SnackbarManagerTest.kt
@@ -93,7 +93,7 @@ import org.robolectric.annotation.LooperMode
 import javax.inject.Inject
 import javax.inject.Singleton
 
-/** Tests for [SnackbarManager]*/
+/** Test for [SnackbarManager]. */
 @RunWith(AndroidJUnit4::class)
 @LooperMode(LooperMode.Mode.PAUSED)
 @Config(application = SnackbarManagerTest.TestApplication::class, qualifiers = "port-xxhdpi")
