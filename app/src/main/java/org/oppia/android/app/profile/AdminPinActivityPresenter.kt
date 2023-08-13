0e31d2a01c428bb95824a81762dc9825f93d9c24
Vishwajith-Shettigar <zekromvishwa56789@gmail.com>
2023-08-13 10:15:31 +0530

    toolbarspfix

--- app/src/main/java/org/oppia/android/app/profile/AdminPinActivityPresenter.kt
+++ app/src/main/java/org/oppia/android/app/profile/AdminPinActivityPresenter.kt
@@ -38,17 +38,21 @@ class AdminPinActivityPresenter @Inject constructor(
 
   /** Binds ViewModel and sets up text and button listeners. */
   fun handleOnCreate() {
-    activity.supportActionBar?.setDisplayHomeAsUpEnabled(true)
-    activity.supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_close_white_24dp)
-    activity.supportActionBar?.setHomeActionContentDescription(R.string.admin_auth_close)
+
+//    activity.supportActionBar?.setDisplayHomeAsUpEnabled(true)
+//    activity.supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_close_white_24dp)
+//    activity.supportActionBar?.setHomeActionContentDescription(R.string.admin_auth_close)
 
     val binding =
       DataBindingUtil.setContentView<AdminPinActivityBinding>(activity, R.layout.admin_pin_activity)
+
 
     binding.apply {
       lifecycleOwner = activity
       viewModel = adminViewModel
     }
+
+    binding.adminPinToolbar.title= resourceHandler.getStringInLocale(R.string.admin_auth_activity_add_profiles_title)
 
     // [onTextChanged] is a extension function defined at [TextInputEditTextHelper]
     binding.adminPinInputPinEditText.onTextChanged { pin ->
