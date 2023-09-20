42a6fd7f4b1a15eb835b98038d2190201414aa89
Vishwajith-Shettigar <zekromvishwa56789@gmail.com>
2023-09-20 20:08:15 +0530

    talkbackisssuecommit10

--- app/src/main/java/org/oppia/android/app/profile/PinPasswordActivityPresenter.kt
+++ app/src/main/java/org/oppia/android/app/profile/PinPasswordActivityPresenter.kt
@@ -57,6 +57,7 @@ class PinPasswordActivityPresenter @Inject constructor(
 
     binding.pinPasswordInputPinEditText.setOnClickListener { pinEditText ->
       pinEditText.isFocusable = true
+      pinEditText.isFocusableInTouchMode=true
       pinEditText.requestFocus()
     }
 
