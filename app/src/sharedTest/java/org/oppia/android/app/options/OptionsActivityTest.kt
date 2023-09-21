89a5bdf538f52a422938319233afdd2a9b834cab
Adhiambo Peres <59600948+adhiamboperes@users.noreply.github.com>
2023-09-21 16:12:08 +0300

    Add newline after imports in OptionsActivityTest.kt

--- app/src/sharedTest/java/org/oppia/android/app/options/OptionsActivityTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/options/OptionsActivityTest.kt
@@ -87,6 +87,7 @@ import org.robolectric.annotation.Config
 import org.robolectric.annotation.LooperMode
 import javax.inject.Inject
 import javax.inject.Singleton
+
 /** Tests for [OptionsActivity]. */
 @RunWith(AndroidJUnit4::class)
 @LooperMode(LooperMode.Mode.PAUSED)
