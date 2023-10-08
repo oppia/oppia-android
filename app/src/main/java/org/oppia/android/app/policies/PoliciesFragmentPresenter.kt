d2f7bb39e59788226e2a82b999c02141b33a61cd
Shubhadeep_Karmakar <shubhadeepkarmakar1@gmail.com>
2023-10-08 19:28:36 +0530

    .

--- app/src/main/java/org/oppia/android/app/policies/PoliciesFragmentPresenter.kt
+++ app/src/main/java/org/oppia/android/app/policies/PoliciesFragmentPresenter.kt
@@ -1,7 +1,7 @@
 package org.oppia.android.app.policies
 
-import android.view.LayoutInflater
 import android.view.View
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
