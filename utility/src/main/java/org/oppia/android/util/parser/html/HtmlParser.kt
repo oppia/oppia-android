e2c39f8cfc936f68ec1d2d026ceb234bd1f346c4
Vishwajith-Shettigar <zekromvishwa56789@gmail.com>
2023-10-28 16:12:55 +0530

    centered image in RTL mode

--- utility/src/main/java/org/oppia/android/util/parser/html/HtmlParser.kt
+++ utility/src/main/java/org/oppia/android/util/parser/html/HtmlParser.kt
@@ -75,25 +75,25 @@ class HtmlParser private constructor(
     supportsLinks: Boolean = false,
     supportsConceptCards: Boolean = false
   ): Spannable {
+
+    var htmlContent = rawString
+
     // Canvas does not support RTL, it always starts from left to right in RTL due to which compound drawables are
     // not center aligned. To avoid this situation check if RTL is enabled and set the textDirection.
     if (isRtl) {
       htmlContentTextView.textDirection = View.TEXT_DIRECTION_RTL
-    } else {
-      htmlContentTextView.textDirection = View.TEXT_DIRECTION_LTR
-    }
-    htmlContentTextView.invalidate()
 
-    var htmlContent = rawString
-
-    if (isRtl) {
       val regex = Regex("""<oppia-noninteractive-image [^>]*>.*?</oppia-noninteractive-image>""")
       val modifiedHtmlContent = rawString.replace(regex) {
         val oppiaImageTag = it.value
         """<div style="text-align: center;">$oppiaImageTag</div>"""
       }
       htmlContent = modifiedHtmlContent
+    } else {
+      htmlContentTextView.textDirection = View.TEXT_DIRECTION_LTR
     }
+
+    htmlContentTextView.invalidate()
 
     if ("\n\t" in htmlContent) {
       htmlContent = htmlContent.replace("\n\t", "")
