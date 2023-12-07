40c44dfa31aaa206ba1d850000c58e690a56a011
XichengSpencer <74568012+XichengSpencer@users.noreply.github.com>
2023-12-07 12:06:33 -0500

    Add comment to explain the negating logic

--- app/src/main/java/org/oppia/android/app/profile/AddProfileActivityPresenter.kt
+++ app/src/main/java/org/oppia/android/app/profile/AddProfileActivityPresenter.kt
@@ -65,6 +65,7 @@ class AddProfileActivityPresenter @Inject constructor(
     }
     if (enableDownloadsSupport.value) {
       binding.addProfileActivityAllowDownloadConstraintLayout.setOnClickListener {
+        // Negating logic to sync with the switch component state
         allowDownloadAccess = !allowDownloadAccess
         binding.addProfileActivityAllowDownloadSwitch.isChecked = allowDownloadAccess
       }
