9a865462fa9d132fb2ff5b897cd965af82a42dac
kmanikanta335 <118070186+kmanikanta335@users.noreply.github.com>
2023-12-11 21:22:42 +0530

    Fix #5225: Remove the EnableLanguageSelectionUi Feature Flag

--- app/src/test/java/org/oppia/android/app/testing/options/OptionsFragmentTest.kt
+++ app/src/test/java/org/oppia/android/app/testing/options/OptionsFragmentTest.kt
@@ -7,10 +7,6 @@ import androidx.test.core.app.ActivityScenario.launch
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
 import dagger.Component
