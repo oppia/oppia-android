37ad54707106f31dc4942e05061b14c0eb450058
Adhiambo Peres <59600948+adhiamboperes@users.noreply.github.com>
2023-12-21 21:11:18 +0300

    Fix static analysis checks

--- testing/src/main/java/org/oppia/android/testing/FakeFirebaseWrapperImpl.kt
+++ testing/src/main/java/org/oppia/android/testing/FakeFirebaseWrapperImpl.kt
@@ -1,19 +1,22 @@
 package org.oppia.android.testing
 
-import org.oppia.android.domain.auth.FirebaseAuthWrapper
-import org.oppia.android.domain.auth.FirebaseUserWrapper
 import java.util.UUID
 import javax.inject.Inject
 import javax.inject.Singleton
+import org.oppia.android.domain.auth.FirebaseAuthWrapper
+import org.oppia.android.domain.auth.FirebaseUserWrapper
 
+/** A test specific fake for the [FirebaseAuthWrapper]. */
 @Singleton
 class FakeFirebaseWrapperImpl @Inject constructor() : FirebaseAuthWrapper {
   private var simulateSuccess: Boolean = true
 
+  /** Fake a successful auth response. */
   fun simulateSignInSuccess() {
     simulateSuccess = true
   }
 
+  /** Fake a failed auth response. */
   fun simulateSignInFailure() {
     simulateSuccess = false
   }
