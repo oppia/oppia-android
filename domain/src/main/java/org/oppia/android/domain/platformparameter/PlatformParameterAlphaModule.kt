082c1b65b68a80c54f0d923ffd8a186e050f788a
kmanikanta335 <118070186+kmanikanta335@users.noreply.github.com>
2024-01-04 02:56:43 +0530

    Fix #5225: Remove the EnableLanguageSelectionUi Feature Flag (#5239)
    <!-- READ ME FIRST: Please fill in the explanation section below and
    check off every point from the Essential Checklist! -->
    ## Explanation
    <!--
    - Explain what your PR does. If this PR fixes an existing bug, please
    include
    - "Fixes #bugnum:" in the explanation so that GitHub can auto-close the
    issue
      - when this PR is merged.
      -->
    Fix https://github.com/oppia/oppia-android/issues/5225
    
    Remove declarations and usages of the EnableLanguageSelectionUi
    PlatformParameter.
    
    ## Essential Checklist
    <!-- Please tick the relevant boxes by putting an "x" in them. -->
    - The PR title and explanation each start with "Fix #bugnum: " (If this
    PR fixes part of an issue, prefix the title with "Fix part of #bugnum:
    ...".)
    - [ ] Any changes to
    [scripts/assets](https://github.com/oppia/oppia-android/tree/develop/scripts/assets)
    files have their rationale included in the PR explanation.
    - [ ] The PR follows the [style
    guide](https://github.com/oppia/oppia-android/wiki/Coding-style-guide).
    - [ ] The PR does not contain any unnecessary code changes from Android
    Studio
    ([reference](https://github.com/oppia/oppia-android/wiki/Guidance-on-submitting-a-PR#undo-unnecessary-changes)).
    - [ ] The PR is made from a branch that's **not** called "develop" and
    is up-to-date with "develop".
    - [ ] The PR is **assigned** to the appropriate reviewers
    ([reference](https://github.com/oppia/oppia-android/wiki/Guidance-on-submitting-a-PR#clarification-regarding-assignees-and-reviewers-section)).
    
    -

--- domain/src/main/java/org/oppia/android/domain/platformparameter/PlatformParameterAlphaModule.kt
+++ domain/src/main/java/org/oppia/android/domain/platformparameter/PlatformParameterAlphaModule.kt
@@ -15,7 +15,6 @@ import org.oppia.android.util.platformparameter.ENABLE_DOWNLOADS_SUPPORT_DEFAULT
 import org.oppia.android.util.platformparameter.ENABLE_EDIT_ACCOUNTS_OPTIONS_UI_DEFAULT_VALUE
 import org.oppia.android.util.platformparameter.ENABLE_EXTRA_TOPIC_TABS_UI_DEFAULT_VALUE
 import org.oppia.android.util.platformparameter.ENABLE_INTERACTION_CONFIG_CHANGE_STATE_RETENTION_DEFAULT_VALUE
-import org.oppia.android.util.platformparameter.ENABLE_LANGUAGE_SELECTION_UI_DEFAULT_VALUE
 import org.oppia.android.util.platformparameter.ENABLE_PERFORMANCE_METRICS_COLLECTION
 import org.oppia.android.util.platformparameter.ENABLE_PERFORMANCE_METRICS_COLLECTION_DEFAULT_VALUE
 import org.oppia.android.util.platformparameter.EXTRA_TOPIC_TABS_UI
@@ -25,7 +24,6 @@ import org.oppia.android.util.platformparameter.EnableEditAccountsOptionsUi
 import org.oppia.android.util.platformparameter.EnableExtraTopicTabsUi
 import org.oppia.android.util.platformparameter.EnableFastLanguageSwitchingInLesson
 import org.oppia.android.util.platformparameter.EnableInteractionConfigChangeStateRetention
-import org.oppia.android.util.platformparameter.EnableLanguageSelectionUi
 import org.oppia.android.util.platformparameter.EnableLearnerStudyAnalytics
 import org.oppia.android.util.platformparameter.EnableLoggingLearnerStudyIds
 import org.oppia.android.util.platformparameter.EnablePerformanceMetricsCollection
@@ -98,14 +96,6 @@ class PlatformParameterAlphaModule {
       SYNC_UP_WORKER_TIME_PERIOD_IN_HOURS
     ) ?: PlatformParameterValue.createDefaultParameter(
       SYNC_UP_WORKER_TIME_PERIOD_IN_HOURS_DEFAULT_VALUE
-    )
-  }
-
-  @Provides
-  @EnableLanguageSelectionUi
-  fun provideEnableLanguageSelectionUi(): PlatformParameterValue<Boolean> {
-    return PlatformParameterValue.createDefaultParameter(
-      ENABLE_LANGUAGE_SELECTION_UI_DEFAULT_VALUE
     )
   }
 
