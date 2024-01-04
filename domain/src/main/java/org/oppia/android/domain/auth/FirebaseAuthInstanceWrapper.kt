215793e214cdc5ec3fe54cb504a4e65e2a649386
Adhiambo Peres <59600948+adhiamboperes@users.noreply.github.com>
2024-01-04 15:12:53 +0300

    Fix lint errors

--- domain/src/main/java/org/oppia/android/domain/auth/FirebaseAuthInstanceWrapper.kt
+++ domain/src/main/java/org/oppia/android/domain/auth/FirebaseAuthInstanceWrapper.kt
@@ -2,5 +2,6 @@ package org.oppia.android.domain.auth
 
 /** Interface for providing an implementation of [FirebaseAuthInstance]. */
 interface FirebaseAuthInstanceWrapper {
+  /** Returns a wrapped instance of FirebaseAuth. */
   val firebaseAuthInstance: FirebaseAuthInstance
 }
