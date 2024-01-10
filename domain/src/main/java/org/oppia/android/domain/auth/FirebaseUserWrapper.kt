4197aa6b6742ffc7be0df2c92b8ead863162c30c
Adhiambo Peres <59600948+adhiamboperes@users.noreply.github.com>
2024-01-10 04:56:32 +0300

    Fix failing lint checks

--- domain/src/main/java/org/oppia/android/domain/auth/FirebaseUserWrapper.kt
+++ domain/src/main/java/org/oppia/android/domain/auth/FirebaseUserWrapper.kt
@@ -2,7 +2,7 @@ package org.oppia.android.domain.auth
 
 import com.google.firebase.auth.FirebaseUser
 
-/** Wrapper for [FirebaseUser]. **/
+/** Wrapper for [FirebaseUser]. */
 data class FirebaseUserWrapper(
   val uid: String,
 )
