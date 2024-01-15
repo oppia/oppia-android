06a999c7aeb7962563e4a294e7d6efef5e2f124f
Adhiambo Peres <59600948+adhiamboperes@users.noreply.github.com>
2024-01-15 16:57:34 +0300

    Remove singleton annotation from TestAuthenticationModule

--- testing/src/main/java/org/oppia/android/testing/TestAuthenticationModule.kt
+++ testing/src/main/java/org/oppia/android/testing/TestAuthenticationModule.kt
@@ -3,13 +3,11 @@ package org.oppia.android.testing
 import dagger.Module
 import dagger.Provides
 import org.oppia.android.domain.auth.FirebaseAuthWrapper
-import javax.inject.Singleton
 
 /** Provides test authentication dependencies. */
 @Module
 class TestAuthenticationModule {
   @Provides
-  @Singleton
   fun provideFakeFirebaseAuthWrapper(fakeFirebaseWrapperImpl: FakeFirebaseAuthWrapperImpl):
     FirebaseAuthWrapper = fakeFirebaseWrapperImpl
 }
