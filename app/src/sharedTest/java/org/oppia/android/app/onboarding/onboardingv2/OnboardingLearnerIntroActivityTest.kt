8a271fa40920a24042f1cf46e559b77600bee892
Adhiambo Peres <59600948+adhiamboperes@users.noreply.github.com>
2024-02-20 19:38:05 +0300

    Fix missing test configs

--- app/src/sharedTest/java/org/oppia/android/app/onboarding/onboardingv2/OnboardingLearnerIntroActivityTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/onboarding/onboardingv2/OnboardingLearnerIntroActivityTest.kt
@@ -6,12 +6,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.intent.Intents
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth
 import dagger.Component
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 import org.oppia.android.R
 import org.oppia.android.app.activity.ActivityComponent
 import org.oppia.android.app.activity.ActivityComponentFactory
@@ -83,10 +85,19 @@ import org.oppia.android.util.networking.NetworkConnectionUtilDebugModule
 import org.oppia.android.util.parser.html.HtmlParserEntityTypeModule
 import org.oppia.android.util.parser.image.GlideImageLoaderModule
 import org.oppia.android.util.parser.image.ImageParsingModule
+import org.robolectric.annotation.Config
+import org.robolectric.annotation.LooperMode
 import javax.inject.Inject
 import javax.inject.Singleton
 
 /** Tests for [OnboardingLearnerIntroActivity]. */
+@RunWith(AndroidJUnit4::class)
+@LooperMode(LooperMode.Mode.PAUSED)
+@Config(
+  application = OnboardingLearnerIntroActivityTest.TestApplication::class,
+  qualifiers = "port-xxhdpi"
+)
+
 class OnboardingLearnerIntroActivityTest {
   @get:Rule
   val initializeDefaultLocaleRule = InitializeDefaultLocaleRule()
