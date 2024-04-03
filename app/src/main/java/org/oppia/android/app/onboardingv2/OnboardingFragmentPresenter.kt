5a4125634305faa355af5d175c1fefa1338f7987
Adhiambo Peres <59600948+adhiamboperes@users.noreply.github.com>
2024-04-03 04:47:32 +0300

    Add profile type selection UI with navigation.

--- app/src/main/java/org/oppia/android/app/onboardingv2/OnboardingFragmentPresenter.kt
+++ app/src/main/java/org/oppia/android/app/onboardingv2/OnboardingFragmentPresenter.kt
@@ -3,6 +3,7 @@ package org.oppia.android.app.onboardingv2
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import org.oppia.android.R
 import org.oppia.android.app.fragment.FragmentScope
@@ -13,6 +14,7 @@ import javax.inject.Inject
 /** The presenter for [OnboardingFragment] V2. */
 @FragmentScope
 class OnboardingFragmentPresenter @Inject constructor(
+  private val activity: AppCompatActivity,
   private val fragment: Fragment,
   private val appLanguageResourceHandler: AppLanguageResourceHandler
 ) {
@@ -34,6 +36,11 @@ class OnboardingFragmentPresenter @Inject constructor(
       R.string.onboarding_language_activity_title,
       appLanguageResourceHandler.getStringInLocale(R.string.app_name)
     )
+
+    binding.onboardingLanguageLetsGoButton.setOnClickListener {
+      val intent = OnboardingProfileTypeActivity.createOnboardingProfileTypeActivityIntent(activity)
+      fragment.startActivity(intent)
+    }
 
     return binding.root
   }
