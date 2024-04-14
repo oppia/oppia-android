46d54844fbef20a51950a11b3f20501ce60f8f19
Kenneth Murerwa <xwaxes@gmail.com>
2024-04-14 09:47:47 +0300

    test: Add parameterized test for FeatureFlagsLoggerTest

--- domain/src/test/java/org/oppia/android/domain/oppialogger/analytics/FeatureFlagsLoggerTest.kt
+++ domain/src/test/java/org/oppia/android/domain/oppialogger/analytics/FeatureFlagsLoggerTest.kt
@@ -3,7 +3,6 @@ package org.oppia.android.domain.oppialogger.analytics
 import android.app.Application
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import dagger.BindsInstance
 import dagger.Component
 import dagger.Module
@@ -18,6 +17,12 @@ import org.oppia.android.domain.oppialogger.LoggingIdentifierModule
 import org.oppia.android.domain.platformparameter.PlatformParameterSingletonModule
 import org.oppia.android.testing.FakeAnalyticsEventLogger
 import org.oppia.android.testing.TestLogReportingModule
+import org.oppia.android.testing.junit.OppiaParameterizedTestRunner
+import org.oppia.android.testing.junit.OppiaParameterizedTestRunner.Iteration
+import org.oppia.android.testing.junit.OppiaParameterizedTestRunner.Parameter
+import org.oppia.android.testing.junit.OppiaParameterizedTestRunner.RunParameterized
+import org.oppia.android.testing.junit.OppiaParameterizedTestRunner.SelectRunnerPlatform
+import org.oppia.android.testing.junit.ParameterizedRobolectricTestRunner
 import org.oppia.android.testing.logging.EventLogSubject.Companion.assertThat
 import org.oppia.android.testing.logging.SyncStatusTestModule
 import org.oppia.android.testing.platformparameter.EnableTestFeatureFlag
@@ -38,7 +43,19 @@ import org.oppia.android.util.logging.EnableFileLog
 import org.oppia.android.util.logging.GlobalLogLevel
 import org.oppia.android.util.logging.LogLevel
 import org.oppia.android.util.networking.NetworkConnectionUtilDebugModule
+import org.oppia.android.util.platformparameter.APP_AND_OS_DEPRECATION
+import org.oppia.android.util.platformparameter.DOWNLOADS_SUPPORT
+import org.oppia.android.util.platformparameter.EDIT_ACCOUNTS_OPTIONS_UI
+import org.oppia.android.util.platformparameter.ENABLE_NPS_SURVEY
+import org.oppia.android.util.platformparameter.ENABLE_ONBOARDING_FLOW_V2
+import org.oppia.android.util.platformparameter.ENABLE_PERFORMANCE_METRICS_COLLECTION
+import org.oppia.android.util.platformparameter.EXTRA_TOPIC_TABS_UI
+import org.oppia.android.util.platformparameter.FAST_LANGUAGE_SWITCHING_IN_LESSON
+import org.oppia.android.util.platformparameter.INTERACTION_CONFIG_CHANGE_STATE_RETENTION
+import org.oppia.android.util.platformparameter.LEARNER_STUDY_ANALYTICS
+import org.oppia.android.util.platformparameter.LOGGING_LEARNER_STUDY_IDS
 import org.oppia.android.util.platformparameter.PlatformParameterValue
+import org.oppia.android.util.platformparameter.SPOTLIGHT_UI
 import org.robolectric.annotation.Config
 import org.robolectric.annotation.LooperMode
 import javax.inject.Inject
@@ -47,7 +64,8 @@ import javax.inject.Singleton
 /** Tests for [FeatureFlagsLogger]. */
 // FunctionName: test names are conventionally named with underscores.
 @Suppress("FunctionName")
-@RunWith(AndroidJUnit4::class)
+@RunWith(OppiaParameterizedTestRunner::class)
+@SelectRunnerPlatform(ParameterizedRobolectricTestRunner::class)
 @LooperMode(LooperMode.Mode.PAUSED)
 @Config(application = FeatureFlagsLoggerTest.TestApplication::class)
 class FeatureFlagsLoggerTest {
@@ -59,6 +77,9 @@ class FeatureFlagsLoggerTest {
   lateinit var testFeatureFlag: PlatformParameterValue<Boolean>
   @field:[Inject EnableTestFeatureFlagWithEnabledDefault]
   lateinit var testFeatureFlagWithEnabledDefault: PlatformParameterValue<Boolean>
+
+  @Parameter var index: Int = Int.MIN_VALUE
+  @Parameter lateinit var flagName: String
 
   @Before
   fun setup() {
@@ -135,6 +156,64 @@ class FeatureFlagsLoggerTest {
     val eventLog = fakeAnalyticsEventLogger.getMostRecentEvent()
     assertThat(eventLog).hasFeatureFlagContextThat {
       hasFeatureFlagItemCountThat().isEqualTo(expectedClassNames)
+    }
+  }
+
+  @Test
+  @RunParameterized(
+    Iteration("downloads_support", "index=0", "flagName=$DOWNLOADS_SUPPORT"),
+    Iteration("extra_topics_tabs_ui", "index=1", "flagName=$EXTRA_TOPIC_TABS_UI"),
+    Iteration(
+      "learner_study_analytics", "index=2",
+      "flagName=$LEARNER_STUDY_ANALYTICS"
+    ),
+    Iteration(
+      "fast_language_switching_in_lesson", "index=3",
+      "flagName=$FAST_LANGUAGE_SWITCHING_IN_LESSON"
+    ),
+    Iteration(
+      "logging_learner_study_ids", "index=4",
+      "flagName=$LOGGING_LEARNER_STUDY_IDS"
+    ),
+    Iteration(
+      "edit_accounts_options_ui", "index=5",
+      "flagName=$EDIT_ACCOUNTS_OPTIONS_UI"
+    ),
+    Iteration(
+      "enable_performance_metrics_collection", "index=6",
+      "flagName=$ENABLE_PERFORMANCE_METRICS_COLLECTION"
+    ),
+    Iteration(
+      "spotlight_ui", "index=7",
+      "flagName=$SPOTLIGHT_UI"
+    ),
+    Iteration(
+      "interaction_config_change_state_retention", "index=8",
+      "flagName=$INTERACTION_CONFIG_CHANGE_STATE_RETENTION"
+    ),
+    Iteration(
+      "app_and_os_deprecation", "index=9",
+      "flagName=$APP_AND_OS_DEPRECATION"
+    ),
+    Iteration(
+      "enable_nps_survey", "index=10",
+      "flagName=$ENABLE_NPS_SURVEY"
+    ),
+    Iteration(
+      "enable_onboarding_flow_v2", "index=11",
+      "flagName=$ENABLE_ONBOARDING_FLOW_V2"
+    ),
+  )
+  fun testLogFeatureFlags_logsFeatureFlagsWithDifferentValues() {
+    featureFlagsLogger.logAllFeatureFlags(TEST_SESSION_ID)
+
+    testCoroutineDispatchers.runCurrent()
+
+    val eventLog = fakeAnalyticsEventLogger.getMostRecentEvent()
+    assertThat(eventLog).hasFeatureFlagContextThat {
+      hasFeatureFlagItemContextThatAtIndex(index) {
+        hasFeatureFlagNameThat().isEqualTo(flagName)
+      }
     }
   }
 
