c743b35f9be26837c6173f03fd809d0397b8ae3e
XichengSpencer <74568012+XichengSpencer@users.noreply.github.com>
2024-05-27 18:54:46 -0400

    Remove #4419 TODOs Remove application\alphakenya package Remove codes in bazel files,test_file_exemptions.textproto that is related with alphakenya Remove AlphaKenya related modules and corresponding tests

--- app/src/main/java/org/oppia/android/app/application/alphakenya/AlphaKenyaOppiaApplication.kt
+++
@@ -1,9 +0,0 @@
-package org.oppia.android.app.application.alphakenya
-
-import org.oppia.android.app.application.AbstractOppiaApplication
-
-// TODO(#4419): Remove this application class & broader Kenya-specific alpha package.
-/** The root [AbstractOppiaApplication] for the Kenya-specific alpha build of the Oppia app. */
-class AlphaKenyaOppiaApplication : AbstractOppiaApplication(
-  DaggerAlphaKenyaApplicationComponent::builder
-)
