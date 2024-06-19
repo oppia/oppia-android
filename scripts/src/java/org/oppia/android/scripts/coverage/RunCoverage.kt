7f1be37be1910d8d90da990029b2e9b8a0262722
Rd <rd4ramadevi@gmail.com>
2024-06-19 20:14:20 +0530

    Running Multiple test targets using hardcoded test target names

--- scripts/src/java/org/oppia/android/scripts/coverage/RunCoverage.kt
+++ scripts/src/java/org/oppia/android/scripts/coverage/RunCoverage.kt
@@ -1,5 +1,6 @@
 package org.oppia.android.scripts.coverage
 
+import org.oppia.android.scripts.common.BazelClient
 import org.oppia.android.scripts.common.CommandExecutor
 import org.oppia.android.scripts.common.CommandExecutorImpl
 import org.oppia.android.scripts.common.ScriptBackgroundCoroutineDispatcher
@@ -10,30 +11,58 @@ import java.io.FileInputStream
 
 fun main(vararg args: String) {
   val repoRoot = args[0]
+  val rootDirectory = File(repoRoot).absoluteFile
   val targetPath = args[1]
   val filePath = args[2]
 
-  println("Repo root: $repoRoot")
-  println("Targetpath: $targetPath")
-  println("Filepath: $filePath")
+  ScriptBackgroundCoroutineDispatcher().use { scriptBgDispatcher ->
+    val commandExecutor: CommandExecutor = CommandExecutorImpl(
+      scriptBgDispatcher, processTimeout = 5, processTimeoutUnit = TimeUnit.MINUTES
+    )
 
-  val testFileExemptiontextProto = "scripts/assets/test_file_exemptions"
+    val bazelClient = BazelClient(rootDirectory, commandExecutor)
 
-  // A list of all the files to be exempted for this check.
-  // TODO(#3436): Develop a mechanism for permanently exempting files which do not ever need tests.
-  val testFileExemptionList = loadTestFileExemptionsProto(testFileExemptiontextProto)
-    .getExemptedFilePathList()
+    println("Repo root: $repoRoot")
+    println("Targetpath: $targetPath")
+    println("Filepath: $filePath")
+
+    val testFileExemptiontextProto = "scripts/assets/test_file_exemptions"
+
+    // A list of all the files to be exempted for this check.
+    // TODO(#3436): Develop a mechanism for permanently exempting files which do not ever need tests.
+    val testFileExemptionList = loadTestFileExemptionsProto(testFileExemptiontextProto)
+      .getExemptedFilePathList()
 
 //  println("test file exemption list: $testFileExemptionList")
 
-  val isExempted = testFileExemptionList.contains(filePath)
-  if(isExempted) {
-    println("This file is exempted from having a test file. Hence No coverage!")
-    return
+    val isExempted = testFileExemptionList.contains(filePath)
+    if (isExempted) {
+      println("This file is exempted from having a test file. Hence No coverage!")
+      return
+    }
+
+    val testFilePath = findTestFile(repoRoot, filePath)
+    println("Test File paths list: $testFilePath")
+
+    val result = bazelClient.retrieveBazelTargets(testFilePath)
+    println("Result from Retrieve Bazel Target; $result")
+
+    val testResults = listOf(
+      "//utility/src/test/java/org/oppia/android/util/parser/math:MathModelTest",
+      "//utility/src/test/java/org/oppia/android/util/math:FloatExtensionsTest")
+
+    //.substringBeforeLast(".kt")
+
+    for (r in testResults) {
+      RunCoverageForTestTarget(
+        rootDirectory,
+        r,
+        commandExecutor,
+        scriptBgDispatcher
+      ).runCoverage()
+    }
   }
 
-  val testFilePath = findTestFile(repoRoot, filePath)
-  println("Test File path: $testFilePath")
 
 //  ScriptBackgroundCoroutineDispatcher().use { scriptBgDispatcher ->
 //    val commandExecutor: CommandExecutor = CommandExecutorImpl(
@@ -63,6 +92,10 @@ private fun findTestFile(repoRoot: String, filePath: String): List<String> {
     val testFilePath = filePath.replace("/main/", "/test/").replace(".kt", "Test.kt")
     val localTestFilePath = filePath.replace("/main/", "/test/").replace(".kt", "LocalTest.kt")
 
+    println("Shared: $sharedTestFilePath")
+    println("Test: $testFilePath")
+    println("LocalTest: $localTestFilePath")
+
     if (File(repoRoot, sharedTestFilePath).exists()) {
       testFiles.add(sharedTestFilePath)
     }
@@ -74,7 +107,7 @@ private fun findTestFile(repoRoot: String, filePath: String): List<String> {
     }
   } else {
     val defaultTestFilePath = filePath.replace("/main/", "/test/").replace(".kt", "Test.kt")
-    if (File(defaultTestFilePath).exists()) {
+    if (File(repoRoot, defaultTestFilePath).exists()) {
       testFiles.add(defaultTestFilePath)
     }
   }
