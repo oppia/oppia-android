9e78d1a9fbb29f7d4be748a54e78d1ea7e6ac328
Rd <rd4ramadevi@gmail.com>
2024-06-19 21:16:00 +0530

    Added Kdoc and processTimeout custom argument with basic clean up

--- scripts/src/java/org/oppia/android/scripts/coverage/RunCoverage.kt
+++ scripts/src/java/org/oppia/android/scripts/coverage/RunCoverage.kt
@@ -9,72 +9,65 @@ import java.util.concurrent.TimeUnit
 import java.io.File
 import java.io.FileInputStream
 
+/**
+ * Entry point function for running coverage analysis for a source file.
+ *
+ * Usage:
+ *   bazel run //scripts:run_coverage_for_test_target -- <path_to_root> <relative_path_to_file>
+ *
+ * Arguments:
+ * - path_to_root: directory path to the root of the Oppia Android repository.
+ * - relative_path_to_file: the relative path to the file to analyse coverage
+ *
+ * Example:
+ *     bazel run //scripts:run_coverage -- $(pwd)
+ *     utility/src/main/java/org/oppia/android/util/parser/math/MathModel.kt
+ * Example with custom process timeout:
+ *     bazel run //scripts:run_coverage -- $(pwd)
+ *     utility/src/main/java/org/oppia/android/util/parser/math/MathModel.kt processTimeout=10
+ *
+ */
 fun main(vararg args: String) {
   val repoRoot = args[0]
+  val filePath = args[1]
+
   val rootDirectory = File(repoRoot).absoluteFile
-  val targetPath = args[1]
-  val filePath = args[2]
+
+  val testFileExemptiontextProto = "scripts/assets/test_file_exemptions"
+
+  // A list of all the files to be exempted for this check.
+  val testFileExemptionList = loadTestFileExemptionsProto(testFileExemptiontextProto)
+    .getExemptedFilePathList()
+
+  val isExempted = testFileExemptionList.contains(filePath)
+  if (isExempted) {
+    println("This file is exempted from having a test file. Hence No coverage!")
+    return
+  }
 
   ScriptBackgroundCoroutineDispatcher().use { scriptBgDispatcher ->
+    val processTimeout: Long = args.find { it.startsWith("processTimeout=") }
+      ?.substringAfter("=")
+      ?.toLongOrNull() ?: 5
+
     val commandExecutor: CommandExecutor = CommandExecutorImpl(
-      scriptBgDispatcher, processTimeout = 5, processTimeoutUnit = TimeUnit.MINUTES
+      scriptBgDispatcher, processTimeout = processTimeout, processTimeoutUnit = TimeUnit.MINUTES
     )
 
     val bazelClient = BazelClient(rootDirectory, commandExecutor)
 
-    println("Repo root: $repoRoot")
-    println("Targetpath: $targetPath")
-    println("Filepath: $filePath")
-
-    val testFileExemptiontextProto = "scripts/assets/test_file_exemptions"
-
-    // A list of all the files to be exempted for this check.
-    // TODO(#3436): Develop a mechanism for permanently exempting files which do not ever need tests.
-    val testFileExemptionList = loadTestFileExemptionsProto(testFileExemptiontextProto)
-      .getExemptedFilePathList()
-
-//  println("test file exemption list: $testFileExemptionList")
-
-    val isExempted = testFileExemptionList.contains(filePath)
-    if (isExempted) {
-      println("This file is exempted from having a test file. Hence No coverage!")
-      return
-    }
-
     val testFilePath = findTestFile(repoRoot, filePath)
-    println("Test File paths list: $testFilePath")
+    val testTargets = bazelClient.retrieveBazelTargets(testFilePath)
 
-    val result = bazelClient.retrieveBazelTargets(testFilePath)
-    println("Result from Retrieve Bazel Target; $result")
-
-    val testResults = listOf(
-      "//utility/src/test/java/org/oppia/android/util/parser/math:MathModelTest",
-      "//utility/src/test/java/org/oppia/android/util/math:FloatExtensionsTest")
-
-    //.substringBeforeLast(".kt")
-
-    for (r in testResults) {
+    for (testTarget in testTargets) {
       RunCoverageForTestTarget(
         rootDirectory,
-        r,
+        testTarget.substringBeforeLast(".kt"),
         commandExecutor,
         scriptBgDispatcher
       ).runCoverage()
     }
   }
-
-
-//  ScriptBackgroundCoroutineDispatcher().use { scriptBgDispatcher ->
-//    val commandExecutor: CommandExecutor = CommandExecutorImpl(
-//      scriptBgDispatcher, processTimeout = 5, processTimeoutUnit = TimeUnit.MINUTES
-//    )
-    /*RunCoverageForTestTarget(
-      repoRoot,
-      targetPath,
-      commandExecutor,
-      scriptBgDispatcher
-    ).runCoverage()*/
-//  }
 }
 
 private fun findTestFile(repoRoot: String, filePath: String): List<String> {
@@ -91,10 +84,6 @@ private fun findTestFile(repoRoot: String, filePath: String): List<String> {
     val sharedTestFilePath = filePath.replace("/main/", "/sharedTest/").replace(".kt", "Test.kt")
     val testFilePath = filePath.replace("/main/", "/test/").replace(".kt", "Test.kt")
     val localTestFilePath = filePath.replace("/main/", "/test/").replace(".kt", "LocalTest.kt")
-
-    println("Shared: $sharedTestFilePath")
-    println("Test: $testFilePath")
-    println("LocalTest: $localTestFilePath")
 
     if (File(repoRoot, sharedTestFilePath).exists()) {
       testFiles.add(sharedTestFilePath)
