9390eef3679af9aaa9eab214881225e83faa1f64
Tejas-67 <tejasjha3210@gmail.com>
2024-06-21 00:20:02 +0530

    refractor: ProfileAndDeviceActivity.kt, AppVersionActivity.kt

--- app/src/main/java/org/oppia/android/app/administratorcontrols/appversion/AppVersionActivity.kt
+++ app/src/main/java/org/oppia/android/app/administratorcontrols/appversion/AppVersionActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.activity.OnBackPressedCallback
 import org.oppia.android.app.activity.ActivityComponentImpl
 import org.oppia.android.app.activity.InjectableAutoLocalizedAppCompatActivity
 import org.oppia.android.app.model.ScreenName.APP_VERSION_ACTIVITY
@@ -19,14 +20,27 @@ class AppVersionActivity : InjectableAutoLocalizedAppCompatActivity() {
     super.onCreate(savedInstanceState)
     (activityComponent as ActivityComponentImpl).inject(this)
     appVersionActivityPresenter.handleOnCreate()
+    handleBackPress()
   }
 
   override fun onOptionsItemSelected(item: MenuItem): Boolean {
     if (item.itemId == android.R.id.home) {
-      @Suppress("DEPRECATION") // TODO(#5404): Migrate to a back pressed dispatcher.
-      onBackPressed()
+      onBackPressedDispatcher.onBackPressed()
     }
     return super.onOptionsItemSelected(item)
+  }
+
+  private fun handleBackPress() {
+    onBackPressedDispatcher.addCallback(
+      this@AppVersionActivity,
+        object: OnBackPressedCallback(true){
+          override fun handleOnBackPressed() {
+            isEnabled = false
+            onBackPressedDispatcher.onBackPressed()
+            isEnabled = true
+          }
+        }
+      )
   }
 
   companion object {
