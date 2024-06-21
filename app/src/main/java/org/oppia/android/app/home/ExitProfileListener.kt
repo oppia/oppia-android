2cb0469b4731e6cda360eb2948f247e7d0d33753
Adhiambo Peres <59600948+adhiamboperes@users.noreply.github.com>
2024-06-21 22:38:04 +0300

    Fix static check failures

--- app/src/main/java/org/oppia/android/app/home/ExitProfileListener.kt
+++ app/src/main/java/org/oppia/android/app/home/ExitProfileListener.kt
@@ -3,9 +3,10 @@ package org.oppia.android.app.home
 import org.oppia.android.app.model.ProfileType
 /** Listener for when a user wishes to exit their profile. */
 interface ExitProfileListener {
-  /** Called when back press is clicked on the HomeScreen.
+  /**
+   * Called when back press is clicked on the HomeScreen.
    *
-   * A SOLE_LEARNER exits the pp completely while other [ProfileType]s are routed to the
+   * A SOLE_LEARNER exits the app completely while other [ProfileType]s are routed to the
    * [ProfileChooserActivity].
    */
   fun exitProfile(profileType: ProfileType)
