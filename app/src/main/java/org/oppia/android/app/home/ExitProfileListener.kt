7304449a28ebf2188b9c7ac057d0f6f3031793a6
Adhiambo Peres <59600948+adhiamboperes@users.noreply.github.com>
2024-06-21 07:54:43 +0300

    Add kdoc

--- app/src/main/java/org/oppia/android/app/home/ExitProfileListener.kt
+++ app/src/main/java/org/oppia/android/app/home/ExitProfileListener.kt
@@ -1,9 +1,9 @@
 package org.oppia.android.app.home
 
 import org.oppia.android.app.model.ProfileType
-
+/** Listener for when a user wishes to exit their profile. */
 interface ExitProfileListener {
-  /** Listener for when a user wishes to exit their profile.
+  /** Called when back press is clicked on the HomeScreen.
    *
    * A SOLE_LEARNER exits the pp completely while other [ProfileType]s are routed to the
    * [ProfileChooserActivity].
