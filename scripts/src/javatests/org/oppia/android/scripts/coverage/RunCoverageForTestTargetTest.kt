25a064e555894a21b18a0c5aa759e9f8e28b3a19
Rd <rd4ramadevi@gmail.com>
2024-06-22 06:07:54 +0530

    Updated the tests to now check with the CoverageReport proto result

--- scripts/src/javatests/org/oppia/android/scripts/coverage/RunCoverageForTestTargetTest.kt
+++ scripts/src/javatests/org/oppia/android/scripts/coverage/RunCoverageForTestTargetTest.kt
@@ -9,6 +9,11 @@ import org.junit.rules.TemporaryFolder
 import org.oppia.android.scripts.common.CommandExecutorImpl
 import org.oppia.android.scripts.common.ScriptBackgroundCoroutineDispatcher
 import org.oppia.android.scripts.testing.TestBazelWorkspace
+import org.oppia.android.scripts.proto.BranchCoverage
+import org.oppia.android.scripts.proto.CoverageReport
+import org.oppia.android.scripts.proto.CoveredFile
+import org.oppia.android.scripts.proto.CoveredLine
+import org.oppia.android.scripts.proto.FunctionCoverage
 import org.oppia.android.testing.assertThrows
 import java.util.concurrent.TimeUnit
 
@@ -119,28 +124,60 @@ class RunCoverageForTestTargetTest {
       scriptBgDispatcher
     ).runCoverage()
 
-    val expectedResult = listOf(
-      "SF:coverage/main/java/com/example/TwoSum.kt",
-      "FN:7,com/example/TwoSum${'$'}Companion::sumNumbers (II)Ljava/lang/Object;",
-      "FN:3,com/example/TwoSum::<init> ()V",
-      "FNDA:1,com/example/TwoSum${'$'}Companion::sumNumbers (II)Ljava/lang/Object;",
-      "FNDA:0,com/example/TwoSum::<init> ()V",
-      "FNF:2",
-      "FNH:1",
-      "BRDA:7,0,0,1",
-      "BRDA:7,0,1,1",
-      "BRDA:7,0,2,1",
-      "BRDA:7,0,3,1",
-      "BRF:4",
-      "BRH:4",
-      "DA:3,0",
-      "DA:7,1",
-      "DA:8,1",
-      "DA:10,1",
-      "LH:3",
-      "LF:4",
-      "end_of_record"
-    )
+
+    val expectedCoveredFile = CoveredFile.newBuilder()
+      .setFilePath("coverage/main/java/com/example/TwoSum.kt")
+      .setFileSha1Hash("f6fb075e115775f6729615a79f0e7e34fe9735b5")
+      .addCoveredLine(CoveredLine.newBuilder().setLineNumber(3).setCoverage(CoveredLine.Coverage.NONE).build())
+      .addCoveredLine(CoveredLine.newBuilder().setLineNumber(7).setCoverage(CoveredLine.Coverage.FULL).build())
+      .addCoveredLine(CoveredLine.newBuilder().setLineNumber(8).setCoverage(CoveredLine.Coverage.FULL).build())
+      .addCoveredLine(CoveredLine.newBuilder().setLineNumber(10).setCoverage(CoveredLine.Coverage.FULL).build())
+      .setLinesFound(4)
+      .setLinesHit(3)
+      .addFunctionCoverage(FunctionCoverage.newBuilder()
+        .setLineNumber(7)
+        .setFunctionName("com/example/TwoSum\$Companion::sumNumbers (II)Ljava/lang/Object;")
+        .setExecutionCount(1)
+        .setCoverage(FunctionCoverage.Coverage.FULL).build())
+      .addFunctionCoverage(FunctionCoverage.newBuilder()
+        .setLineNumber(3)
+        .setFunctionName("com/example/TwoSum::<init> ()V")
+        .setExecutionCount(0)
+        .setCoverage(FunctionCoverage.Coverage.NONE).build())
+      .setFunctionsFound(2)
+      .setFunctionsHit(1)
+      .addBranchCoverage(BranchCoverage.newBuilder()
+        .setLineNumber(7)
+        .setBlockNumber(0)
+        .setBranchNumber(0)
+        .setHitCount(1)
+        .setCoverage(BranchCoverage.Coverage.FULL).build())
+      .addBranchCoverage(BranchCoverage.newBuilder()
+        .setLineNumber(7)
+        .setBlockNumber(0)
+        .setBranchNumber(1)
+        .setHitCount(1)
+        .setCoverage(BranchCoverage.Coverage.FULL).build())
+      .addBranchCoverage(BranchCoverage.newBuilder()
+        .setLineNumber(7)
+        .setBlockNumber(0)
+        .setBranchNumber(2)
+        .setHitCount(1)
+        .setCoverage(BranchCoverage.Coverage.FULL).build())
+      .addBranchCoverage(BranchCoverage.newBuilder()
+        .setLineNumber(7)
+        .setBlockNumber(0)
+        .setBranchNumber(3)
+        .setHitCount(1)
+        .setCoverage(BranchCoverage.Coverage.FULL).build())
+      .setBranchesFound(4)
+      .setBranchesHit(4)
+      .build()
+
+    val expectedResult = CoverageReport.newBuilder()
+      .setBazelTestTarget("//coverage/test/java/com/example:TwoSumTest")
+      .addCoveredFile(expectedCoveredFile)
+      .build()
 
     assertThat(result).isEqualTo(expectedResult)
   }
