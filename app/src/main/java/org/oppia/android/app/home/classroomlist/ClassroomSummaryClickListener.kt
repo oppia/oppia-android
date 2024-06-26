018b0a0b58074e81431a78d8352637a295683f1d
Saptak Manna <saptakmanna100@gmail.com>
2024-06-26 22:14:45 +0530

    Update classroom click listener kdoc

--- app/src/main/java/org/oppia/android/app/home/classroomlist/ClassroomSummaryClickListener.kt
+++ app/src/main/java/org/oppia/android/app/home/classroomlist/ClassroomSummaryClickListener.kt
@@ -2,7 +2,7 @@ package org.oppia.android.app.home.classroomlist
 
 import org.oppia.android.app.model.ClassroomSummary
 
-/** Listener interface for when classroom summaries are clicked in the UI. */
+/** Listener interface for when a classroom card is clicked. */
 interface ClassroomSummaryClickListener {
   /** Called when a classroom card is clicked by the user. */
   fun onClassroomSummaryClicked(classroomSummary: ClassroomSummary)
