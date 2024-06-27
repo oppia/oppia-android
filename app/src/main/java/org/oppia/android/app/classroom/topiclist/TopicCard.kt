876803099d5340573ef81f74357087d77a0f45ad
Saptak Manna <saptakmanna100@gmail.com>
2024-06-27 19:18:09 +0530

    Update TopicCard composable

--- app/src/main/java/org/oppia/android/app/classroom/topiclist/TopicCard.kt
+++ app/src/main/java/org/oppia/android/app/classroom/topiclist/TopicCard.kt
@@ -33,8 +33,8 @@ fun TopicCard(topicSummaryViewModel: TopicSummaryViewModel) {
   Card(
     modifier = Modifier
       .padding(
-        start = dimensionResource(R.dimen.promoted_story_card_layout_margin_start),
-        end = dimensionResource(R.dimen.promoted_story_card_layout_margin_end),
+        start = dimensionResource(R.dimen.topic_card_margin_start),
+        end = dimensionResource(R.dimen.topic_card_margin_end),
       )
       .clickable { topicSummaryViewModel.clickOnSummaryTile() },
     elevation = 4.dp,
@@ -46,7 +46,9 @@ fun TopicCard(topicSummaryViewModel: TopicSummaryViewModel) {
         painter = painterResource(
           id = topicSummaryViewModel.topicSummary.topicThumbnail.getDrawableResource()
         ),
-        contentDescription = "${topicSummaryViewModel.title} Card",
+        contentDescription = "Picture of a ${
+          topicSummaryViewModel.topicSummary.topicThumbnail.thumbnailGraphic.name
+        }.",
         modifier = Modifier
           .aspectRatio(4f / 3f)
           .background(
@@ -58,41 +60,46 @@ fun TopicCard(topicSummaryViewModel: TopicSummaryViewModel) {
             )
           )
       )
-      Column(
-        modifier = Modifier
-          .fillMaxWidth()
-          .background(
-            color = colorResource(
-              id = R.color.component_color_shared_topic_card_item_background_color
-            )
-          ),
-        verticalArrangement = Arrangement.SpaceBetween,
-      ) {
-        Text(
-          text = topicSummaryViewModel.title,
-          modifier = Modifier
-            .padding(start = 8.dp, top = 8.dp, end = 8.dp),
-          color = colorResource(id = R.color.component_color_shared_secondary_4_text_color),
-          fontFamily = FontFamily.SansSerif,
-          fontSize = 14.sp,
-          textAlign = TextAlign.Start,
-          maxLines = 1,
-          overflow = TextOverflow.Ellipsis,
-        )
-        Text(
-          text = topicSummaryViewModel.computeLessonCountText(),
-          modifier = Modifier
-            .padding(start = 8.dp, top = 8.dp, end = 8.dp, bottom = 8.dp),
-          color = colorResource(id = R.color.component_color_shared_secondary_4_text_color),
-          fontFamily = FontFamily.SansSerif,
-          fontWeight = FontWeight.Light,
-          fontSize = 14.sp,
-          fontStyle = FontStyle.Italic,
-          textAlign = TextAlign.Start,
-          maxLines = 1,
-          overflow = TextOverflow.Ellipsis,
-        )
-      }
+      TopicCardTextSection(topicSummaryViewModel)
     }
+  }
+}
+
+@Composable
+fun TopicCardTextSection(topicSummaryViewModel: TopicSummaryViewModel) {
+  Column(
+    modifier = Modifier
+      .fillMaxWidth()
+      .background(
+        color = colorResource(
+          id = R.color.component_color_shared_topic_card_item_background_color
+        )
+      ),
+    verticalArrangement = Arrangement.SpaceBetween,
+  ) {
+    Text(
+      text = topicSummaryViewModel.title,
+      modifier = Modifier
+        .padding(start = 8.dp, top = 8.dp, end = 8.dp),
+      color = colorResource(id = R.color.component_color_shared_secondary_4_text_color),
+      fontFamily = FontFamily.SansSerif,
+      fontSize = 14.sp,
+      textAlign = TextAlign.Start,
+      maxLines = 1,
+      overflow = TextOverflow.Ellipsis,
+    )
+    Text(
+      text = topicSummaryViewModel.computeLessonCountText(),
+      modifier = Modifier
+        .padding(start = 8.dp, top = 8.dp, end = 8.dp, bottom = 8.dp),
+      color = colorResource(id = R.color.component_color_shared_secondary_4_text_color),
+      fontFamily = FontFamily.SansSerif,
+      fontWeight = FontWeight.Light,
+      fontSize = 14.sp,
+      fontStyle = FontStyle.Italic,
+      textAlign = TextAlign.Start,
+      maxLines = 1,
+      overflow = TextOverflow.Ellipsis,
+    )
   }
 }
