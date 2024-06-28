3cf5410c1abf2ae227080f91c1c7dde44f5511fd
Saptak Manna <saptakmanna100@gmail.com>
2024-06-28 17:40:01 +0530

    Fix ktlint check

--- app/src/main/java/org/oppia/android/app/classroom/topiclist/TopicCard.kt
+++ app/src/main/java/org/oppia/android/app/classroom/topiclist/TopicCard.kt
@@ -21,7 +21,6 @@ import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.oppia.android.R
 import org.oppia.android.app.classroom.getDrawableResource
