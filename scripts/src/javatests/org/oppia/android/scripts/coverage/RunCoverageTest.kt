4f5d47c8b9827a7a3c12f1bd33ae8b64d955919b
Rd <rd4ramadevi@gmail.com>
2024-06-29 17:46:25 +0530

    Import CommandExecutor statement added

--- scripts/src/javatests/org/oppia/android/scripts/coverage/RunCoverageTest.kt
+++ scripts/src/javatests/org/oppia/android/scripts/coverage/RunCoverageTest.kt
@@ -6,6 +6,7 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TemporaryFolder
+import org.oppia.android.scripts.common.CommandExecutor
 import org.oppia.android.scripts.common.CommandExecutorImpl
 import org.oppia.android.scripts.common.ScriptBackgroundCoroutineDispatcher
 import org.oppia.android.scripts.testing.TestBazelWorkspace
