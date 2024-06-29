718b065ca74655d5f5e9cffcb7e06595c561cb64
Rd <rd4ramadevi@gmail.com>
2024-06-29 10:12:50 +0530

    Re-introducing the test changes with the passed ci check with ignore case removal

--- scripts/src/javatests/org/oppia/android/scripts/coverage/RunCoverageTest.kt
+++ scripts/src/javatests/org/oppia/android/scripts/coverage/RunCoverageTest.kt
@@ -68,7 +68,7 @@ class RunCoverageTest {
     assertThat(exception).hasMessageThat().contains("No appropriate test file found")
   }
 
-  /*@Test
+  @Test
   fun testRunCoverage_invalidFormat_throwsException() {
     testBazelWorkspace.initEmptyWorkspace()
     val exception = assertThrows<IllegalStateException>() {
@@ -265,7 +265,7 @@ class RunCoverageTest {
     )
 
     assertThat(outputReport.exists()).isTrue()
-  }*/
+  }
 
   @Test
   fun testRunCoverage_testFileExempted_noCoverage() {
