6d8f71432bad90a3945d9ce93b17d2b75e0649f1
Vishwajith-Shettigar <zekromvishwa56789@gmail.com>
2024-07-02 09:04:43 +0530

    Add kdoc and remove file from kdoc_validity_excemptions

--- app/src/sharedTest/java/org/oppia/android/app/utility/ProtoExtraMatcher.kt
+++ app/src/sharedTest/java/org/oppia/android/app/utility/ProtoExtraMatcher.kt
@@ -10,18 +10,24 @@ import org.oppia.android.util.extensions.getProtoExtra
 /** A custom matcher to check if an Intent has a specific proto extra. */
 class ProtoExtraMatcher {
   companion object {
-    fun <T : MessageLite> hasProtoExtraCheck(keyName: String, expectedProto: T):
-      Matcher<Intent> {
-        val defaultProto = expectedProto.newBuilderForType().build()
-        return object : TypeSafeMatcher<Intent>() {
-          override fun describeTo(description: Description) {
-            description.appendText("Intent with extra: $keyName and proto value: $expectedProto")
-          }
-          override fun matchesSafely(intent: Intent): Boolean {
-            return intent.hasExtra(keyName) &&
-              intent.getProtoExtra(keyName, defaultProto) == expectedProto
-          }
+    /**
+     * Returns a matcher that verifies if an Intent contains a specific proto extra.
+     *
+     * @param keyName The key name of the proto extra in the Intent.
+     * @param expectedProto The expected proto message to be matched.
+     */
+    fun <T : MessageLite> hasProtoExtraCheck(keyName: String, expectedProto: T): Matcher<Intent> {
+      val defaultProto = expectedProto.newBuilderForType().build()
+      return object : TypeSafeMatcher<Intent>() {
+        override fun describeTo(description: Description) {
+          description.appendText("Intent with extra: $keyName and proto value: $expectedProto")
+        }
+
+        override fun matchesSafely(intent: Intent): Boolean {
+          return intent.hasExtra(keyName) &&
+            intent.getProtoExtra(keyName, defaultProto) == expectedProto
         }
       }
+    }
   }
 }
