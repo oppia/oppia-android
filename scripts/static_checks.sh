2a3aa8ac86af89e8071f36bee8871243ee887ad7
Kenneth Murerwa <kkmurerwa@gmail.com>
2024-07-17 22:31:55 +0300

    Fix failing to-do check

--- scripts/static_checks.sh
+++ scripts/static_checks.sh
@@ -111,7 +111,7 @@ echo "********************************"
 bazel run //scripts:license_texts_check -- $(pwd)/app/src/main/res/values/third_party_dependencies.xml
 echo ""
 
-# TODO checks.
+# TO-DO checks.
 echo "********************************"
 echo "Running TODO correctness checks"
 echo "********************************"
