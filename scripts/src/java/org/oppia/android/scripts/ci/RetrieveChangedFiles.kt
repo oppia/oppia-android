f73037c5e1588a840cd7bd570a1c6a279541c268
Rd <rd4ramadevi@gmail.com>
2024-07-25 11:37:37 +0530

    Added missing import TimeUnit

--- scripts/src/java/org/oppia/android/scripts/ci/RetrieveChangedFiles.kt
+++ scripts/src/java/org/oppia/android/scripts/ci/RetrieveChangedFiles.kt
@@ -8,6 +8,7 @@ import org.oppia.android.scripts.common.ScriptBackgroundCoroutineDispatcher
 import org.oppia.android.scripts.proto.ChangedFilesBucket
 import org.oppia.android.scripts.proto.TestFileExemptions
 import java.io.File
+import java.util.concurrent.TimeUnit
 import kotlin.system.exitProcess
 
 /**
