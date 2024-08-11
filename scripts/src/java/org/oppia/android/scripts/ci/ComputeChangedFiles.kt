d6fe94ee937772361c107512af2e8211248bde90
Rd <rd4ramadevi@gmail.com>
2024-08-11 15:28:16 +0530

    Reverting format lets see if we can use the existing ones

--- scripts/src/java/org/oppia/android/scripts/ci/ComputeChangedFiles.kt
+++ scripts/src/java/org/oppia/android/scripts/ci/ComputeChangedFiles.kt
@@ -129,7 +129,7 @@ class ComputeChangedFiles(
 
     File(pathToOutputFile).printWriter().use { writer ->
       encodedFileBucketEntries.forEachIndexed { index, (encoded, bucket) ->
-        writer.println("${bucket.cacheBucketName}_shard$index-$encoded")
+        writer.println("${bucket.cacheBucketName}-shard$index;$encoded")
       }
     }
   }
