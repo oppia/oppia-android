f8bcbb5b631681d870f874c69dadfaa34156f22a
Rd <rd4ramadevi@gmail.com>
2024-08-11 08:10:10 +0530

    Workaround to atleast get comment out

--- scripts/src/java/org/oppia/android/scripts/coverage/CoverageReporter.kt
+++ scripts/src/java/org/oppia/android/scripts/coverage/CoverageReporter.kt
@@ -11,13 +11,13 @@ const val MIN_THRESHOLD = 70
 
 /** ANSI escape codes for colors. */
 /** Green text. */
-const val GREEN = "\u001B[32m"
+const val GREEN2 = "\u001B[32m"
 /** Red text. */
-const val RED = "\u001B[31m"
+const val RED2 = "\u001B[31m"
 /** Default text. */
-const val RESET = "\u001B[0m"
+const val RESET2 = "\u001B[0m"
 /** Bold text. */
-const val BOLD = "\u001B[1m"
+const val BOLD2 = "\u001B[1m"
 
 fun main(vararg args: String) {
   val repoRoot = args[0]
@@ -46,8 +46,8 @@ fun main(vararg args: String) {
   ).generateRichTextReport()
 
   when (coverageStatus) {
-    CoverageCheck.PASS -> println("Coverage Analysis$BOLD$GREEN PASSED$RESET")
-    CoverageCheck.FAIL -> error("Coverage Analysis$BOLD$RED FAILED$RESET")
+    CoverageCheck.PASS -> println("Coverage Analysis$BOLD2$GREEN2 PASSED$RESET2")
+    CoverageCheck.FAIL -> error("Coverage Analysis$BOLD2$RED2 FAILED$RESET2")
   }
 }
 
