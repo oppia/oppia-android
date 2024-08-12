a7cd6853a8f74dd47841cc222d98120d446e20a9
Rd <rd4ramadevi@gmail.com>
2024-08-12 07:27:12 +0530

    Renamed step name for coverage check

--- scripts/src/java/org/oppia/android/scripts/coverage/CoverageReporter.kt
+++ scripts/src/java/org/oppia/android/scripts/coverage/CoverageReporter.kt
@@ -9,7 +9,8 @@ import java.io.File
 /** Minimum coverage percentage required. */
 const val MIN_THRESHOLD = 70
 
-/** ANSI escape codes for colors. */
+/* ANSI escape codes for colors. */
+
 /** Green text. */
 const val GREEN = "\u001B[32m"
 /** Red text. */
