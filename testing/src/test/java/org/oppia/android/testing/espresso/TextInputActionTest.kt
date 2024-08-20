264b579a5600994e3b0de598124051b2777df591
Kenneth Murerwa <kkmurerwa@gmail.com>
2024-08-20 23:09:42 +0300

    Test workflow with TextInputActionTest changes

--- testing/src/test/java/org/oppia/android/testing/espresso/TextInputActionTest.kt
+++ testing/src/test/java/org/oppia/android/testing/espresso/TextInputActionTest.kt
@@ -5,18 +5,34 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.android.material.textfield.TextInputLayout
 import com.google.common.truth.Truth.assertThat
-import org.hamcrest.Description
-import org.hamcrest.StringDescription
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.oppia.android.testing.TextInputActionTestActivity
 import org.oppia.android.testing.espresso.TextInputAction.Companion.hasErrorText
-import org.oppia.android.testing.espresso.TextInputAction.Companion.hasNoErrorText
 import org.robolectric.annotation.LooperMode
 
 @RunWith(AndroidJUnit4::class)
 @LooperMode(LooperMode.Mode.PAUSED)
 class TextInputActionTest {
 
+  @get:Rule
+  var activityRule =
+    ActivityScenarioRule<TextInputActionTestActivity>(
+      TextInputActionTestActivity.createIntent(ApplicationProvider.getApplicationContext())
+    )
+
+  @Test
+  fun testTextExistsMatcher_errorMatchesExpectedText_matchesSafelyReturnsTrue() {
+    activityRule.scenario.onActivity { activity ->
+      val expectedErrorText = "Incorrect Administrator PIN. Please try again."
+      val textInputLayout = TextInputLayout(activity)
+
+      textInputLayout.error = "Incorrect Administrator PIN. Please try again."
+
+      val errorTextExisted = hasErrorText(expectedErrorText)
+      val result: Boolean = errorTextExisted.matches(textInputLayout)
+      assertThat(result).isTrue()
+    }
+  }
 }
