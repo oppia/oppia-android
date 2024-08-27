94b2909cb5870f93a9fe88f8359964452fbd4e3a
Sneha Datta <snehadatta431@gmail.com>
2024-08-27 10:25:03 +0530

    Migrate from onBackPressed in AppVersionActivity

--- app/src/main/java/org/oppia/android/app/administratorcontrols/appversion/AppVersionActivity.kt
+++ app/src/main/java/org/oppia/android/app/administratorcontrols/appversion/AppVersionActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.activity.OnBackPressedCallback
 import org.oppia.android.app.activity.ActivityComponentImpl
 import org.oppia.android.app.activity.InjectableAutoLocalizedAppCompatActivity
 import org.oppia.android.app.model.ScreenName.APP_VERSION_ACTIVITY
@@ -19,12 +20,17 @@ class AppVersionActivity : InjectableAutoLocalizedAppCompatActivity() {
     super.onCreate(savedInstanceState)
     (activityComponent as ActivityComponentImpl).inject(this)
     appVersionActivityPresenter.handleOnCreate()
+
+    onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(/* enabled = */ true) {
+      override fun handleOnBackPressed() {
+        finish()
+      }
+    })
   }
 
   override fun onOptionsItemSelected(item: MenuItem): Boolean {
     if (item.itemId == android.R.id.home) {
-      @Suppress("DEPRECATION") // TODO(#5404): Migrate to a back pressed dispatcher.
-      onBackPressed()
+      onBackPressedDispatcher.onBackPressed()
     }
     return super.onOptionsItemSelected(item)
   }
