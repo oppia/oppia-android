680dd4f4d592bc0a3825b6469d3e876711c3a77e
manas-yu <img_2022037@iiitm.ac.in>
2024-10-28 19:07:48 +0530

    script integration

--- scripts/ktlint_lint_check.sh
+++ scripts/ktlint_lint_check.sh
@@ -6,29 +6,28 @@ echo "********************************"
 
 github_actions_path=$1
 
-jar_file_path=$?
-
 if [ $# -eq 0 ]; then
     jar_file_path="../oppia-android-tools/ktlint"
+    custom_rules_path="../oppia-android-tools/custom-ktlint-rules.jar"
 else
     jar_file_path="$github_actions_path/oppia-android-tools/ktlint"
+    custom_rules_path="$github_actions_path/oppia-android-tools/custom-ktlint-rules.jar"
 fi
 
-java -jar $jar_file_path --android app/src/**/*.kt data/src/**/*.kt domain/src/**/*.kt testing/src/**/*.kt utility/src/**/*.kt scripts/src/**/*.kt instrumentation/src/**/*.kt
-
+java -jar "$custom_rules_path" --android app/src/**/*.kt data/src/**/*.kt
 status=$?
 
-if [ "$status" = 0 ] ; then
+if [ "$status" = 0 ]; then
   echo "Lint completed successfully."
   exit 0
 else
   echo "********************************"
   echo "Ktlint issue found."
-  echo "Please fix the above issues.
-  You can also use the java -jar $jar_file_path -F --android domain/src/**/*.kt utility/src/**/*.kt data/src/**/*.kt app/src/**/*.kt testing/src/**/*.kt scripts/src/**/*.kt  instrumentation/src/**/*.kt
-  command to fix the most common issues."
-  echo "Please note, there might be a possibility where the above command will not fix the issue.
-  In that case, you will have to fix it yourself."
+  echo "Please fix the above issues."
+  echo "You can also use the following command to fix the most common issues:"
+  echo "java -cp \"$jar_file_path:$custom_rules_path\" com.pinterest.ktlint.Main -F --android domain/src/**/*.kt utility/src/**/*.kt data/src/**/*.kt app/src/**/*.kt testing/src/**/*.kt scripts/src/**/*.kt instrumentation/src/**/*.kt"
+  echo "Please note, there might be cases where this command does not fix the issue."
+  echo "In that case, you will need to fix it manually."
   echo "********************************"
   exit 1
 fi
