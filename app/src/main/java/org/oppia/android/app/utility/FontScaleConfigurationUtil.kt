9734fc995fe8e1b08229141745ecf105b1a1b087
Saptak Manna <saptakmanna100@gmail.com>
2024-12-18 17:03:41 +0530

    Update builds to target sdk 34

--- app/src/main/java/org/oppia/android/app/utility/FontScaleConfigurationUtil.kt
+++ app/src/main/java/org/oppia/android/app/utility/FontScaleConfigurationUtil.kt
@@ -21,6 +21,7 @@ class FontScaleConfigurationUtil @Inject constructor() {
     // TODO(#3616): Migrate to the proper SDK 30+ APIs.
     @Suppress("DEPRECATION") // The code is correct for targeted versions of Android.
     windowManager!!.defaultDisplay.getMetrics(metrics)
+    @Suppress("DEPRECATION")
     metrics.scaledDensity = configuration.fontScale * metrics.density
     context.createConfigurationContext(configuration)
     context.resources.displayMetrics.setTo(metrics)
