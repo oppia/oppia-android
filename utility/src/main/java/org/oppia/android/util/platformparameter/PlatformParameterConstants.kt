f99069258f5a1005b7d44e598500dcccb5300750
Rd <rd4ramadevi@gmail.com>
2025-01-05 15:00:51 +0530

    Reverting latex platform parameter constant
    Set to false to test override of parameter value

--- utility/src/main/java/org/oppia/android/util/platformparameter/PlatformParameterConstants.kt
+++ utility/src/main/java/org/oppia/android/util/platformparameter/PlatformParameterConstants.kt
@@ -90,7 +90,7 @@ annotation class CacheLatexRendering
 const val CACHE_LATEX_RENDERING = "cache_latex_rendering"
 
 /** Default value for whether to cache LaTeX rendering using Glide. */
-const val CACHE_LATEX_RENDERING_DEFAULT_VALUE = false
+const val CACHE_LATEX_RENDERING_DEFAULT_VALUE = true
 
 /**
  * Qualifier for the platform parameter that controls the time interval in minutes of uploading
