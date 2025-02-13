810c080099d6d3bced788a1189e0024e163a76e3
TanishMoral11 <ishantmoral@gmail.com>
2025-02-14 01:31:14 +0530

    Solve Regex

--- app/src/main/java/org/oppia/android/app/player/state/StateFragmentPresenter.kt
+++ app/src/main/java/org/oppia/android/app/player/state/StateFragmentPresenter.kt
@@ -619,12 +619,12 @@ class StateFragmentPresenter @Inject constructor(
     )
   }
 
-  /** Save the state of the header */
+  /** Saves the state of the header. */
   fun saveExpandedState(outState: Bundle) {
     recyclerViewAssembler.saveExpandedState(outState)
   }
 
-  /** Restore the state of the header */
+  /** Restore the state of the header. */
   fun restoreExpandedState(savedInstanceState: Bundle?) {
     recyclerViewAssembler.restoreExpandedState(savedInstanceState)
   }
