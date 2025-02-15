991c88bb351cde442c5aca909b4d3576f7767559
manas-yu <img_2022037@iiitm.ac.in>
2025-02-16 01:04:46 +0530

    ktlint

--- app/src/main/java/org/oppia/android/app/player/state/itemviewmodel/ContentViewModel.kt
+++ app/src/main/java/org/oppia/android/app/player/state/itemviewmodel/ContentViewModel.kt
@@ -1,6 +1,5 @@
 package org.oppia.android.app.player.state.itemviewmodel
 
-import android.text.SpannableStringBuilder
 import org.oppia.android.util.parser.html.CustomHtmlContentHandler
 
 /** [StateItemViewModel] for content-card state. */
