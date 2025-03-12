48fb35b58525d30a8e11838c88c8db81ffde80ac
Ben Henning <bhenning@google.com>
2025-03-12 19:10:45 +0000

    Post-merge lint fix.

--- app/src/main/java/org/oppia/android/app/utility/ClickableAreasImage.kt
+++ app/src/main/java/org/oppia/android/app/utility/ClickableAreasImage.kt
@@ -12,8 +12,8 @@ import org.oppia.android.app.model.ImageWithRegions.LabeledRegion
 import org.oppia.android.app.model.UserAnswerState
 import org.oppia.android.app.player.state.ImageRegionSelectionInteractionView
 import org.oppia.android.app.shim.ViewBindingShim
-import org.oppia.android.app.views.R
 import org.oppia.android.app.translation.AppLanguageResourceHandler
+import org.oppia.android.app.views.R
 import kotlin.math.roundToInt
 
 /** Helper class to handle clicks on an image along with highlighting the selected region. */
