05240054e62fb137c262756ff6c9db7c32df0100
adhiamboperes <adhiambooyier@gmail.com>
2025-05-23 02:27:41 +0300

    Move dialogs to own files

---
+++ app/src/main/java/org/oppia/android/app/profile/ConfirmDataResetDialog.kt
@@ -0,0 +1,60 @@
+package org.oppia.android.app.profile
+
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.window.DialogProperties
+import org.oppia.android.app.ui.R
+
+/** Test tag for the confirm reset pin dialog. */
+const val ADMIN_RESET_PIN_DIALOG_TEST_TAG = "TEST_TAG.admin_reset_pin"
+
+@Composable
+fun ConfirmDataResetDialog(
+  onDismissRequest: () -> Unit,
+  deleteAppData: () -> Unit
+) {
+  val appName = stringResource(R.string.app_name)
+
+  AlertDialog(
+    title = {
+      Text(
+        stringResource(R.string.admin_confirm_app_wipe_title, appName))
+    },
+    text = {
+      Text(stringResource(R.string.admin_confirm_app_wipe_message, appName))
+    },
+    properties = DialogProperties(
+      dismissOnClickOutside = false,
+      dismissOnBackPress = false
+    ),
+    onDismissRequest = {
+      onDismissRequest()
+    },
+    dismissButton = {
+      TextButton(
+        onClick = {
+          onDismissRequest()
+        }
+      ) {
+        Text(stringResource(R.string.admin_confirm_app_wipe_negative_button_text))
+      }
+    },
+    confirmButton = {
+      TextButton(
+        onClick = { deleteAppData() }
+      ) {
+        Text(stringResource(
+          R.string.admin_confirm_app_wipe_positive_button_text,
+          appName
+        )
+        )
+      }
+    },
+    modifier = Modifier.testTag(ADMIN_RESET_PIN_DIALOG_TEST_TAG)
+  )
+}
---
+++ app/src/main/java/org/oppia/android/app/profile/ForgotAdminPinDialog.kt
@@ -0,0 +1,57 @@
+package org.oppia.android.app.profile
+
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.window.DialogProperties
+import org.oppia.android.app.ui.R
+
+/** Test tag for the admin's forgot pin dialog. */
+const val ADMIN_FORGOT_PIN_DIALOG_TEST_TAG = "TEST_TAG.admin_forgot_pin"
+
+@Composable
+fun ForgotAdminPinDialog(
+  onDismissRequest: () -> Unit,
+  onConfirmation: () -> Unit
+) {
+  val appName = stringResource(R.string.app_name)
+
+  AlertDialog(
+    title = {
+      Text(stringResource(R.string.profile_login_forgot_pin_dialog_title))
+    },
+    text = {
+      Text(stringResource(R.string.profile_login_forgot_pin_dialog_message, appName))
+    },
+    properties = DialogProperties(
+      dismissOnClickOutside = false,
+      dismissOnBackPress = false
+    ),
+    onDismissRequest = {
+      onDismissRequest()
+    },
+    dismissButton = {
+      TextButton(
+        onClick = {
+          onDismissRequest()
+        }
+      ) {
+        Text(stringResource(R.string.profile_login_forgot_pin_dialog_cancel_button))
+      }
+    },
+    confirmButton = {
+      TextButton(
+        onClick = {
+          onConfirmation()
+        }
+      ) {
+        Text(stringResource(R.string.profile_login_forgot_pin_dialog_reset_button, appName))
+      }
+    },
+    modifier = Modifier.testTag(ADMIN_FORGOT_PIN_DIALOG_TEST_TAG)
+  )
+}
