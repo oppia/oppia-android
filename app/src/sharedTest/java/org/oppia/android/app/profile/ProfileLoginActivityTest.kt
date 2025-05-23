c1f6619c2f21b6f33c5b1ac914444eecb21d8be9
adhiamboperes <adhiambooyier@gmail.com>
2025-05-23 23:12:47 +0300

    Fix missing dagger module

--- app/src/sharedTest/java/org/oppia/android/app/profile/ProfileLoginActivityTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/profile/ProfileLoginActivityTest.kt
@@ -67,6 +67,7 @@ import org.oppia.android.testing.OppiaTestRule
 import org.oppia.android.testing.TestLogReportingModule
 import org.oppia.android.testing.firebase.TestAuthenticationModule
 import org.oppia.android.testing.junit.InitializeDefaultLocaleRule
+import org.oppia.android.testing.platformparameter.TestPlatformParameterModule
 import org.oppia.android.testing.robolectric.RobolectricModule
 import org.oppia.android.testing.threading.TestCoroutineDispatchers
 import org.oppia.android.testing.threading.TestDispatcherModule
@@ -205,6 +206,7 @@ class ProfileLoginActivityTest {
       TestAuthenticationModule::class,
       TestDispatcherModule::class,
       TestLogReportingModule::class,
+      TestPlatformParameterModule::class,
       TestingBuildFlavorModule::class,
       TextInputRuleModule::class,
       ViewBindingShimModule::class,
