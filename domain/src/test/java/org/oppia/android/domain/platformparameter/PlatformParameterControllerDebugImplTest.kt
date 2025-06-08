868f6d4c8e47e23fceb02bb8831dd0ccb3be3f31
theayushyadav11 <your.email@example.com>
2025-06-09 01:45:47 +0530

    set up test

--- domain/src/test/java/org/oppia/android/domain/platformparameter/PlatformParameterControllerDebugImplTest.kt
+++ domain/src/test/java/org/oppia/android/domain/platformparameter/PlatformParameterControllerDebugImplTest.kt
@@ -2,11 +2,15 @@ package org.oppia.android.domain.platformparameter
 
 import android.app.Application
 import android.content.Context
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth.assertThat
 import dagger.BindsInstance
 import dagger.Component
 import dagger.Module
 import dagger.Provides
+import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.oppia.android.domain.oppialogger.LogStorageModule
 import org.oppia.android.testing.TestLogReportingModule
@@ -29,7 +33,25 @@ import javax.inject.Singleton
 @Config(manifest = Config.NONE)
 class PlatformParameterControllerDebugImplTest {
 
-  @Inject lateinit var platformParameterController: PlatformParameterController
+  @Inject
+  lateinit var platformParameterController: PlatformParameterController
+
+  @Before
+  fun setUp() {
+    setUpTestApplicationComponent()
+  }
+  @Test
+  fun justA() {
+    setUpTestApplicationComponent()
+    assertThat(platformParameterController).isNotNull()
+  }
+
+  private fun setUpTestApplicationComponent() {
+    DaggerPlatformParameterControllerDebugImplTest_TestApplicationComponent.builder()
+      .setApplication(ApplicationProvider.getApplicationContext())
+      .build()
+      .inject(this)
+  }
 
   @Module
   class TestModule {
--- testing/src/main/java/org/oppia/android/testing/platformparameter/TestPlatformParameterDebugModule.kt
+++ testing/src/main/java/org/oppia/android/testing/platformparameter/TestPlatformParameterDebugModule.kt
@@ -4,18 +4,26 @@ import dagger.Module
 import dagger.Provides
 import org.oppia.android.domain.platformparameter.PlatformParameterController
 import org.oppia.android.domain.platformparameter.PlatformParameterControllerDebugImpl
+import org.oppia.android.domain.platformparameter.PlatformParameterControllerProdImpl
 import org.oppia.android.domain.platformparameter.PlatformParameterDebugController
+import org.oppia.android.domain.platformparameter.PlatformParameterProcessState
 import javax.inject.Singleton
 
 @Module
 class TestPlatformParameterDebugModule {
 
+  private val processState by lazy { PlatformParameterProcessState() }
   @Provides
   @Singleton
   fun providePlatformParameterController(
     impl: PlatformParameterControllerDebugImpl,
   ): PlatformParameterController = impl
 
+  @Provides
+  @Singleton
+  fun providePlatformParameterControllerProdImpl(
+    factory: PlatformParameterControllerProdImpl.Factory
+  ): PlatformParameterControllerProdImpl = factory.create(processState)
   @Provides
   @Singleton
   fun providePlatformParameterDebugController(
