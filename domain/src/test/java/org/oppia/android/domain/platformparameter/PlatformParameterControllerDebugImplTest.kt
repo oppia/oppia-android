3f668a78460375887537e8a47ff97581d7ad6287
theayushyadav11 <your.email@example.com>
2025-06-09 18:29:56 +0530

    setting up test

---
+++ domain/src/test/java/org/oppia/android/domain/platformparameter/PlatformParameterControllerDebugImplTest.kt
@@ -0,0 +1,115 @@
+package org.oppia.android.domain.platformparameter
+
+import android.app.Application
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth.assertThat
+import dagger.BindsInstance
+import dagger.Component
+import dagger.Module
+import dagger.Provides
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.oppia.android.domain.oppialogger.LogStorageModule
+import org.oppia.android.testing.TestLogReportingModule
+import org.oppia.android.testing.platformparameter.TestPlatformParameterModule
+import org.oppia.android.testing.robolectric.RobolectricModule
+import org.oppia.android.testing.threading.TestDispatcherModule
+import org.oppia.android.testing.time.FakeOppiaClockModule
+import org.oppia.android.util.caching.AssetModule
+import org.oppia.android.util.data.DataProvidersInjector
+import org.oppia.android.util.data.DataProvidersInjectorProvider
+import org.oppia.android.util.locale.testing.LocaleTestModule
+import org.oppia.android.util.logging.LoggerModule
+import org.oppia.android.util.networking.NetworkConnectionUtilDebugModule
+import org.robolectric.annotation.Config
+import org.robolectric.annotation.LooperMode
+import javax.inject.Inject
+import javax.inject.Singleton
+
+/** Tests for [PlatformParameterController]. */
+// FunctionName: test names are conventionally named with underscores.
+@Suppress("FunctionName")
+@RunWith(AndroidJUnit4::class)
+@LooperMode(LooperMode.Mode.PAUSED)
+@Config(application = PlatformParameterControllerDebugImplTest.TestApplication::class)
+class PlatformParameterControllerDebugImplTest {
+  @Inject lateinit var platformParameterController: PlatformParameterController
+
+  @Test
+  fun testDemo() {
+    setUpTestApplicationComponent()
+    assertThat(platformParameterController).isNotNull()
+  }
+
+  private fun setUpTestApplicationComponent() {
+    ApplicationProvider.getApplicationContext<TestApplication>().inject(this)
+  }
+
+  // TODO(#89): Move this to a common test application component.
+  @Module
+  class TestModule {
+    @Provides
+    @Singleton
+    fun provideContext(application: Application): Context {
+      return application
+    }
+    @Provides
+    fun providePlatformParameterController(
+      impl: PlatformParameterControllerDebugImpl,
+    ): PlatformParameterController = impl
+
+    @Provides
+    @Singleton
+    fun providePlatformParameterDebugController(
+      impl: PlatformParameterControllerDebugImpl,
+    ): PlatformParameterDebugController = impl
+  }
+
+  // TODO(#89): Move this to a common test application component.
+  @Singleton
+  @Component(
+    modules = [
+      AssetModule::class,
+      FakeOppiaClockModule::class,
+      LocaleTestModule::class,
+      LogStorageModule::class,
+      LoggerModule::class,
+      NetworkConnectionUtilDebugModule::class,
+      RobolectricModule::class,
+      TestDispatcherModule::class,
+      TestLogReportingModule::class,
+      TestModule::class,
+      TestPlatformParameterModule::class
+    ]
+  )
+  interface TestApplicationComponent : DataProvidersInjector {
+    @Component.Builder
+    interface Builder {
+      @BindsInstance
+      fun setApplication(application: Application): Builder
+      fun build(): TestApplicationComponent
+    }
+
+    fun inject(platformParameterControllerTest: PlatformParameterControllerDebugImplTest)
+  }
+
+  class TestApplication : Application(), DataProvidersInjectorProvider {
+    private val component: TestApplicationComponent by lazy {
+      DaggerPlatformParameterControllerDebugImplTest_TestApplicationComponent.builder()
+        .setApplication(this)
+        .build()
+    }
+
+    fun inject(platformParameterControllerTest: PlatformParameterControllerDebugImplTest) {
+      component.inject(platformParameterControllerTest)
+    }
+
+    public override fun attachBaseContext(base: Context?) {
+      super.attachBaseContext(base)
+    }
+
+    override fun getDataProvidersInjector(): DataProvidersInjector = component
+  }
+}
