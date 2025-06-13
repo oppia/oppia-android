011416c856951d64cbf6cdf41847bb343e609ce5
theayushyadav11 <your.email@example.com>
2025-06-14 02:20:15 +0530

    changes

--- app/src/main/java/org/oppia/android/app/devoptions/featureflags/FeatureFlagItemViewModel.kt
+++ app/src/main/java/org/oppia/android/app/devoptions/featureflags/FeatureFlagItemViewModel.kt
@@ -3,6 +3,8 @@ package org.oppia.android.app.devoptions.featureflags
 import org.oppia.android.app.viewmodel.ObservableViewModel
 import javax.inject.Inject
 
+
+/** [ViewModel] for displaying a feature flag for the recycler view in [FeatureFlagFragment]. */
 class FeatureFlagItemViewModel @Inject constructor(
   val featureFlagName: String,
   val syncStatus: String,
