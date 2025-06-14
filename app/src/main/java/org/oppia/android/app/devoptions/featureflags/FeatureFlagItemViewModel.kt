1e7b269d83dd8828141371a13044c53604a2bbfe
theayushyadav11 <your.email@example.com>
2025-06-14 20:42:11 +0530

    added colors

--- app/src/main/java/org/oppia/android/app/devoptions/featureflags/FeatureFlagItemViewModel.kt
+++ app/src/main/java/org/oppia/android/app/devoptions/featureflags/FeatureFlagItemViewModel.kt
@@ -3,7 +3,6 @@ package org.oppia.android.app.devoptions.featureflags
 import org.oppia.android.app.viewmodel.ObservableViewModel
 import javax.inject.Inject
 
-
 /** [ViewModel] for displaying a feature flag for the recycler view in [FeatureFlagFragment]. */
 class FeatureFlagItemViewModel @Inject constructor(
   val featureFlagName: String,
--- domain/src/main/java/org/oppia/android/domain/platformparameter/PlatformParameterDebugControllerModule.kt
+++
@@ -1,4 +0,0 @@
-package org.oppia.android.domain.platformparameter
-
-class PlatformParameterDebugControllerModule {
-}
