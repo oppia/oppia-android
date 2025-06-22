f4fe427ab9df7626a92a1c2041a637c9c7affa60
theayushyadav11 <your.email@example.com>
2025-06-22 21:37:26 +0530

    used binding adapter

--- app/src/main/java/org/oppia/android/app/databinding/DrawableBindingAdapters.java
+++ app/src/main/java/org/oppia/android/app/databinding/DrawableBindingAdapters.java
@@ -36,4 +36,13 @@ public final class DrawableBindingAdapters {
     GradientDrawable background = (GradientDrawable) view.getBackground();
     background.setColor(0xff000000 | colorRgb);
   }
+
+  /** Used to set a rounded-rect background drawable with a data-bound color. */
+  @BindingAdapter("largeRoundedRectDrawableWithColor")
+  public static void setLargerBorderRadiusBackgroundDrawable(@NonNull View view, @ColorInt int colorRgb) {
+    view.setBackgroundResource(R.drawable.rounded_rect_large_sync_status_chip);
+    // The input color needs to have alpha channel prepended to it.
+    GradientDrawable background = (GradientDrawable) view.getBackground();
+    background.setColor(0xff000000 | colorRgb);
+  }
 }
