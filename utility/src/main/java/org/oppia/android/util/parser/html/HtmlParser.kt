99eeafb3ea89c52c3a1a05189cf255c8db59b239
adhiamboperes <adhiambooyier@gmail.com>
2025-07-01 14:26:54 +0300

    fix tests

--- utility/src/main/java/org/oppia/android/util/parser/html/HtmlParser.kt
+++ utility/src/main/java/org/oppia/android/util/parser/html/HtmlParser.kt
@@ -13,12 +13,12 @@ import android.view.View
 import android.widget.TextView
 import androidx.core.text.util.LinkifyCompat
 import androidx.core.view.ViewCompat
-import javax.inject.Inject
 import org.oppia.android.util.locale.OppiaLocale
 import org.oppia.android.util.logging.ConsoleLogger
 import org.oppia.android.util.parser.image.UrlImageParser
 import org.oppia.android.util.platformparameter.CacheLatexRendering
 import org.oppia.android.util.platformparameter.PlatformParameterValue
+import javax.inject.Inject
 
 /** Html Parser to parse custom Oppia tags with Android-compatible versions. */
 class HtmlParser private constructor(
