d3d64d295a14613e6055833c96b137cfe823d116
manas-yu <img_2022037@iiitm.ac.in>
2025-07-03 01:41:20 +0530

    test commit

--- scripts/src/java/org/oppia/android/scripts/lint/AndroidLintRunner.kt
+++ scripts/src/java/org/oppia/android/scripts/lint/AndroidLintRunner.kt
@@ -221,6 +221,10 @@ class AndroidLintRunner(
   private fun reportLintIssues() {
     val reporter = LintAnalysisReporter()
     val allIssues = reporter.parseLintReport(reportFile.absolutePath)
+    val unknown = reporter.getUnknownIssueIds(allIssues)
+    unknown.forEach { issue ->
+      println("Unknown issue ID found in lint report: $issue")
+    }
     val exemptions = reporter.loadExemptionsProto(exemptionProtoPath)
     val filteredIssues = reporter.filterExemptedIssues(
       issues = allIssues,
