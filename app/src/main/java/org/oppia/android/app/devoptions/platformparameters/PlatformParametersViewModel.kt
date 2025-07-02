9be393be7fec7e5d6744bd58f56688e80720772a
theayushyadav11 <your.email@example.com>
2025-07-03 00:17:59 +0530

    removed observeforever

--- app/src/main/java/org/oppia/android/app/devoptions/platformparameters/PlatformParametersViewModel.kt
+++ app/src/main/java/org/oppia/android/app/devoptions/platformparameters/PlatformParametersViewModel.kt
@@ -1,7 +1,7 @@
 package org.oppia.android.app.devoptions.platformparameters
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import org.oppia.android.app.fragment.FragmentScope
 import org.oppia.android.app.model.EphemeralPlatformParameter
 import org.oppia.android.app.translation.AppLanguageResourceHandler
@@ -14,7 +14,7 @@ import javax.inject.Inject
 
 /**
  * [ViewModel] for [PlatformParametersFragment]. It populates the recycler view with a list of
- * [PlatformParameterItemViewModel] which in turn display the available feature flags.
+ * [PlatformParameterItemViewModel] which in turn display the available Platform Parameters.
  */
 @FragmentScope
 class PlatformParametersViewModel @Inject constructor(
@@ -23,27 +23,27 @@ class PlatformParametersViewModel @Inject constructor(
   private val resourceHandler: AppLanguageResourceHandler
 ) : ObservableViewModel() {
 
-  private val _platformParameterList = MutableLiveData<List<PlatformParameterItemViewModel>>()
-
-  /**
-   * List of [PlatformParameterItemViewModel] used to populate recycler view of [PlatformParametersFragment]
-   * to display the available feature flags.
-   */
-  val platformParameterList: LiveData<List<PlatformParameterItemViewModel>> = _platformParameterList
-
-  init {
-    loadPlatformParameters()
+  private val ephemeralParametersLiveData: LiveData<List<EphemeralPlatformParameter>> by lazy {
+    Transformations.map(
+      platformParameterControllerDebugImpl.loadEphemeralPlatformParameters().toLiveData(),
+      ::processEphemeralParameterResult
+    )
   }
 
-  private fun loadPlatformParameters() {
-    val dataProvider = platformParameterControllerDebugImpl.loadEphemeralPlatformParameters()
-    dataProvider.toLiveData().observeForever { result ->
-      when (result) {
-        is AsyncResult.Success ->
-          _platformParameterList.value =
-            processPlatformParameterList(result.value)
-        else -> _platformParameterList.value = listOf()
-      }
+  /**
+   * LiveData that contains a list of [PlatformParameterItemViewModel] which is used to populate the
+   * recycler view in [PlatformParametersFragment].
+   */
+  val platformParameterList: LiveData<List<PlatformParameterItemViewModel>> by lazy {
+    Transformations.map(ephemeralParametersLiveData, ::processPlatformParameterList)
+  }
+
+  private fun processEphemeralParameterResult(
+    result: AsyncResult<List<EphemeralPlatformParameter>>
+  ): List<EphemeralPlatformParameter> {
+    return when (result) {
+      is AsyncResult.Success -> result.value
+      else -> emptyList()
     }
   }
 
