9369e6c12f1acb2b18a65b875f9676c4bf1cb3f1
manas-yu <img_2022037@iiitm.ac.in>
2025-07-03 02:40:52 +0530

    improt fix

--- app/src/main/java/org/oppia/android/app/databinding/ImageViewBindingAdapters.java
+++ app/src/main/java/org/oppia/android/app/databinding/ImageViewBindingAdapters.java
@@ -5,7 +5,6 @@ import android.graphics.drawable.Drawable;
 import android.widget.ImageView;
 import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
-import androidx.core.content.ContextCompat;
 import androidx.databinding.BindingAdapter;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.DataSource;
