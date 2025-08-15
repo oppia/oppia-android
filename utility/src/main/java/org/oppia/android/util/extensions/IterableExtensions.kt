5384cc5bc64416bb2bbb1f86e38d7093ab964f92
manas-yu <img_2022037@iiitm.ac.in>
2025-08-16 01:15:55 +0530

    fix comment

--- utility/src/main/java/org/oppia/android/util/extensions/IterableExtensions.kt
+++ utility/src/main/java/org/oppia/android/util/extensions/IterableExtensions.kt
@@ -1,11 +1,6 @@
 package org.oppia.android.util.extensions
 
-/**
- * Safe forEach extension to replace direct forEach usage and avoid lint false positives on API < 24.
- * This extension provide the same functionality as the standard forEach methods but use
- * traditional for loops internally to bypass lint issues.
- */
-// TODO(#5734): Remove this function once lint no longer falsely triggers on forEach.
+// TODO(#5734): Remove this function once lint no longer falsely triggers on Iterable#forEach.
 
 /** Executes the given [action] for each element in this iterable. */
 inline fun <T> Iterable<T>.safeForEach(action: (T) -> Unit) {
