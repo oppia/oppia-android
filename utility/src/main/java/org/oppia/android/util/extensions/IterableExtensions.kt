ab05079d94e5f23dc5ebe1428133ca0f37d78ece
manas-yu <img_2022037@iiitm.ac.in>
2025-08-16 21:55:16 +0530

    remove inline

--- utility/src/main/java/org/oppia/android/util/extensions/IterableExtensions.kt
+++ utility/src/main/java/org/oppia/android/util/extensions/IterableExtensions.kt
@@ -2,6 +2,6 @@ package org.oppia.android.util.extensions
 
 // TODO(#5734): Remove this function once lint no longer falsely triggers on Iterable#forEach.
 /** Executes the given [action] for each element in this iterable. */
-inline fun <T> Iterable<T>.safeForEach(action: (T) -> Unit) {
+fun <T> Iterable<T>.safeForEach(action: (T) -> Unit) {
   for (element in this) action(element)
 }
