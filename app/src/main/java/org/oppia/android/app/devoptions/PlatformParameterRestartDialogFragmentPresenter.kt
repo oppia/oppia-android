da4ab10f721979fab580cae2854fc151432a3701
theayushyadav11 <your.email@example.com>
2025-08-20 19:32:07 +0530

    added restart dialog implementation

---
+++ app/src/main/java/org/oppia/android/app/devoptions/PlatformParameterRestartDialogFragmentPresenter.kt
@@ -0,0 +1,49 @@
+package org.oppia.android.app.devoptions
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import javax.inject.Inject
+import org.oppia.android.app.ui.R
+import org.oppia.android.app.databinding.databinding.RestartDialogFragmentBinding
+import org.oppia.android.app.splash.SplashActivity
+import org.oppia.android.domain.platformparameter.PlatformParameterControllerDebugImpl
+
+/** Presenter for the [PlatformParameterRestartDialogFragment]. */
+class PlatformParameterRestartDialogFragmentPresenter @Inject constructor(
+  private val fragment: Fragment,
+  private val activity: AppCompatActivity,
+) {
+
+  /**
+   * Creates a dialog that prompts the user to restart the app when platform parameters are
+   * updated.
+   */
+  fun handleOnCreateDialog(): Dialog {
+    val binding = RestartDialogFragmentBinding.inflate(
+      activity.layoutInflater,
+      /* parent= */ null,
+      /* attachToRoot= */ false
+    )
+    binding.lifecycleOwner = fragment
+
+    val dialog = AlertDialog.Builder(activity, R.style.OppiaAlertDialogTheme)
+      .setView(binding.root)
+      .create()
+    dialog.setCanceledOnTouchOutside(true)
+
+    binding.restartButton.setOnClickListener {
+     val intent = Intent(activity, SplashActivity::class.java).also {
+        it.action = Intent.ACTION_MAIN
+        it.addCategory(Intent.CATEGORY_LAUNCHER)
+      }
+      dialog.dismiss()
+      activity.finishAffinity()
+      activity.startActivity(intent)
+    }
+    return dialog
+  }
+
+}
