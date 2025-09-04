366e0f0e074b0a153546146f77738fce18e5072d
manas-yu <img_2022037@iiitm.ac.in>
2025-09-04 12:39:21 +0530

    fix test

--- scripts/src/javatests/org/oppia/android/scripts/lint/AndroidLintRunnerTest.kt
+++ scripts/src/javatests/org/oppia/android/scripts/lint/AndroidLintRunnerTest.kt
@@ -793,22 +793,23 @@ class AndroidLintRunnerTest {
   fun testAndroidLintAnalyzer_withMissingClass_detectsIssue() {
     setupProjectWithMissingClass()
 
-    val exception = assertThrows<IllegalArgumentException> {
+    val exception = assertThrows<IllegalStateException> {
       androidLintAnalyzerWithFakeExecutor.runAnalysis()
     }
-    assertThat(exception.message)
-      .contains("Unknown lint issue ID 'MissingClass' found during analysis.")
-
-    val output = reportfile.readText()
-    assertThat(output).contains("MissingClass")
+    val output = outputStream.toString()
+    assertThat(output).contains("MISSING_CLASS")
     assertThat(output)
-      .contains(" &lt;foo.bar.Baz />")
-    assertThat(output).contains("line=\"5\"")
+      .contains("<foo.bar.Baz />")
+    assertThat(output).contains("Line: 5")
     assertThat(output)
       .contains(
-        "If a class is referenced in the manifest or" +
-          " in a layout file, it must also exist in the project"
+        "Class referenced in the layout file, `foo.bar.Baz`," +
+          " was not found in the project or the libraries"
       )
+
+    assertThat(exception.message)
+      .isEqualTo("${RED}ANDROID LINT CHECK ${BOLD}FAILED$RESET")
+
     val projectDescriptionContent = projectDescriptionFile.readText()
     assertThat(projectDescriptionContent)
       .contains("app/src/main/res")
@@ -820,34 +821,34 @@ class AndroidLintRunnerTest {
     createFileWithContent(
       "app/src/main/res/layout/customview.xml",
       """
-      <LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
-          android:id="@+id/newlinear"
-          android:layout_width="match_parent"
-          android:layout_height="match_parent" >
-          <foo.bar.Baz />
-          <test.pkg.MyView />
-          <test.pkg.NotView />
-      </LinearLayout>
+    <LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
+        android:id="@+id/newlinear"
+        android:layout_width="match_parent"
+        android:layout_height="match_parent" >
+        <foo.bar.Baz />
+        <test.pkg.MyView />
+        <test.pkg.NotView />
+    </LinearLayout>
     """
     )
 
     createFileWithContent(
-      "app/src/main/java/test/pkg/MyView.kt",
+      "app/src/test/java/test/pkg/MyView.kt",
       """
-      package test.pkg
+    package test.pkg
 
-      abstract class MyView : I, android.view.View(null)
+    abstract class MyView : I, android.view.View(null)
 
-      interface I
+    interface I
     """
     )
 
     createFileWithContent(
-      "app/src/main/java/test/pkg/NotView.kt",
+      "app/src/test/java/test/pkg/NotView.kt",
       """
-      package test.pkg
+    package test.pkg
 
-      abstract class NotView : android.app.Fragment()
+    abstract class NotView : android.app.Fragment()
     """
     )
   }
