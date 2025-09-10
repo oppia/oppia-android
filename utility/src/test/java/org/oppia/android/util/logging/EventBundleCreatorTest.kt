183908c4e511dd04b06657d8d297f20537419830
Saptak Manna <saptakmanna100@gmail.com>
2025-09-10 12:31:46 +0530

    Fix tests

--- utility/src/test/java/org/oppia/android/util/logging/EventBundleCreatorTest.kt
+++ utility/src/test/java/org/oppia/android/util/logging/EventBundleCreatorTest.kt
@@ -875,7 +875,7 @@ class EventBundleCreatorTest {
     assertThat(bundle).string("feature_flag_names")
       .isEqualTo("2,3,4,5,7,8,10,11,12,13,14,15,17,18,0")
     assertThat(bundle).string("feature_flag_enabled_states")
-      .isEqualTo("0,1,0,1,0,1,0,1,0,1,0,1,0,0")
+      .isEqualTo("0,1,0,1,1,0,1,0,1,0,1,0,0,0,0")
     assertThat(bundle).string("feature_flag_sync_statuses").isEqualTo("0,1,2,0,1,2,0,1,2,0,1,2,0,0")
   }
 
