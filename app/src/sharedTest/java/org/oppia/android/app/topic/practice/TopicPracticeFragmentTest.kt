e9270e12741d23533f9c272301fcf4a3802490f0
Saptak Manna <saptakmanna100@gmail.com>
2025-10-29 23:18:16 +0530

    fix tests

--- app/src/sharedTest/java/org/oppia/android/app/topic/practice/TopicPracticeFragmentTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/topic/practice/TopicPracticeFragmentTest.kt
@@ -442,6 +442,7 @@ class TopicPracticeFragmentTest {
       clickPracticeItem(position = 2, targetViewId = R.id.subtopic_check_box)
 
       scenario.recreate()
+      testCoroutineDispatchers.runCurrent()
 
       scrollToPosition(position = 1)
       onView(
