f59a020987240fbfb3e02ee52ece334d4537c5c2
Saptak Manna <saptakmanna100@gmail.com>
2025-11-25 13:20:00 +0530

    Remove unused import

--- app/src/sharedTest/java/org/oppia/android/app/topic/TopicActivityTest.kt
+++ app/src/sharedTest/java/org/oppia/android/app/topic/TopicActivityTest.kt
@@ -8,12 +8,10 @@ import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions.scrollToPosition
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
