f7b2b6c23677d1fd2dc758abb08ea5ce643d9f0d
shankhanil <shankhanilsaha@gmail.com>
2025-12-11 21:10:31 +0530

    adds exception type PROFILES_ALREADY_EXIST for the new createDefaultProfile() function and hardcodes name attribute value as user name is always an empty string

--- domain/src/main/java/org/oppia/android/domain/profile/ProfileManagementController.kt
+++ domain/src/main/java/org/oppia/android/domain/profile/ProfileManagementController.kt
@@ -143,6 +143,9 @@ class ProfileManagementController @Inject constructor(
   /** Indicates that the there is not device settings currently. */
   class DeviceSettingsNotFoundException(msg: String) : Exception(msg)
 
+  /** Indicates  that profiles already exist in the app. */
+  class ProfilesAlreadyExistException(msg: String) : Exception(msg)
+
   /**
    * These statuses correspond to the exceptions above such that if the deferred contains
    * PROFILE_NOT_FOUND, the [ProfileNotFoundException] will be passed to a failed AsyncResult.
@@ -189,6 +192,9 @@ class ProfileManagementController @Inject constructor(
 
     /** Indicates that the operation failed due to the profileType property not supplied. */
     PROFILE_TYPE_UNKNOWN,
+
+    /** Indicates that the operation failed due to profiles already existing in the app */
+    PROFILES_ALREADY_EXIST
   }
 
   // TODO(#272): Remove init block when storeDataAsync is fixed
@@ -272,7 +278,7 @@ class ProfileManagementController @Inject constructor(
       updateInMemoryCache = true
     ) {
       if (it.profilesCount > 0) {
-        return@storeDataWithCustomChannelAsync Pair(it, ProfileActionStatus.PROFILES_EXIST)
+        return@storeDataWithCustomChannelAsync Pair(it, ProfileActionStatus.PROFILES_ALREADY_EXIST)
       }
 
       val nextProfileId = it.nextProfileId
@@ -304,7 +310,7 @@ class ProfileManagementController @Inject constructor(
       Pair(profileDatabaseBuilder.build(), ProfileActionStatus.SUCCESS)
     }
     return dataProviders.createInMemoryDataProviderAsync(ADD_PROFILE_PROVIDER_ID) {
-      return@createInMemoryDataProviderAsync getDeferredResult(null, name, deferred)
+      return@createInMemoryDataProviderAsync getDeferredResult(null, "", deferred)
     }
   }
 
@@ -1241,6 +1247,9 @@ class ProfileManagementController @Inject constructor(
         )
       ProfileActionStatus.PROFILE_TYPE_UNKNOWN ->
         AsyncResult.Failure(UnknownProfileTypeException("ProfileType must be set."))
+
+      ProfileActionStatus.PROFILES_ALREADY_EXIST ->
+        AsyncResult.Failure(ProfilesAlreadyExistException("Profiles already exist."))
     }
   }
 
