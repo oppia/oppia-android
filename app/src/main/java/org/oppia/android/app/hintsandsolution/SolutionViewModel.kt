8661e15f91e84797df0889a9cda905a60eefc6b5
Sangeeta Yadav <sangeetayadav6826@gmail.com>
2026-01-20 18:23:36 +0000

    remove unneccessary newline and revert explanation

--- app/src/main/java/org/oppia/android/app/hintsandsolution/SolutionViewModel.kt
+++ app/src/main/java/org/oppia/android/app/hintsandsolution/SolutionViewModel.kt
@@ -67,19 +67,7 @@ class SolutionViewModel private constructor(
   val isFlashback: Boolean,
   val solutionBoxStrokeWidth: Int,
   private val consoleLogger: ConsoleLogger
-
 ) {
-
-  /**
-   * Overrides default HTML parsing to include custom concept card tags when generating
-   * TalkBack-readable text for Solution and Flashback dialogs.
-   *
-   * The previous implementation ignored custom tags.
-   * This caused TalkBack to skip concept card references.
-   *
-   * This fix ensures that TalkBack reads the concept card reference exactly once
-   * and consistently.
-   */
 
   val solutionSummaryContentDescription by lazy {
     CustomHtmlContentHandler.getContentDescription(
