2261a52ff6317e3c56af9d32b8b14664f23168ba
Ben Henning <bhenning@google.com>
2026-03-03 23:54:46 +0000

    Pull in changes from #4885.

--- scripts/src/java/org/oppia/android/scripts/gae/json/GaeStateWithoutRecordedVoiceovers.kt
+++ scripts/src/java/org/oppia/android/scripts/gae/json/GaeStateWithoutRecordedVoiceovers.kt
@@ -3,14 +3,14 @@ package org.oppia.android.scripts.gae.json
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
+// TODO: Remove.
 @JsonClass(generateAdapter = true)
-data class GaeState(
+data class GaeStateWithoutRecordedVoiceovers(
   @Json(name = "content") val content: GaeSubtitledHtml,
   @Json(name = "param_changes") val paramChanges: List<GaeParamChange>,
   @Json(name = "interaction") val interaction: GaeInteractionInstance?,
   @Json(name = "classifier_model_id") val classifierModelId: String?,
   @Json(name = "linked_skill_id") val linkedSkillId: String?,
-  @Json(name = "recorded_voiceovers") val recordedVoiceovers: GaeRecordedVoiceovers,
   @Json(name = "solicit_answer_details") val solicitAnswerDetails: Boolean,
   @Json(name = "card_is_checkpoint") val cardIsCheckpoint: Boolean,
   @Json(
