e9bbb0d98129537be896d66e0a78038b76e7aadd
nikhilkumarpanigrahi <nikhilkumarpanigrahi29@gmail.com>
2026-03-20 13:20:27 +0530

    Fix #6086: address reviewer follow-ups in GAE scripts

--- scripts/src/java/org/oppia/android/scripts/gae/proto/LocalizationTracker.kt
+++ scripts/src/java/org/oppia/android/scripts/gae/proto/LocalizationTracker.kt
@@ -671,17 +671,11 @@ class LocalizationTracker private constructor(
     private const val CUSTOM_IMG_FILE_PATH_ATTRIBUTE = "filepath-with-value"
     private const val CUSTOM_MATH_TAG = "oppia-noninteractive-math"
     private const val CUSTOM_MATH_SVG_PATH_ATTRIBUTE = "math_content-with-value"
-    private val customMathTagContentRegex by lazy {
-      Regex(
-        "<\\s*$CUSTOM_MATH_TAG\\b[^>]*?(?:/>|>.*?</\\s*$CUSTOM_MATH_TAG\\s*>)",
-        setOf(RegexOption.DOT_MATCHES_ALL)
-      )
-    }
     private val customImageTagRegex by lazy {
       Regex("<\\s*$CUSTOM_IMG_TAG.+?$CUSTOM_IMG_FILE_PATH_ATTRIBUTE\\s*=\\s*\"(.+?)\"")
     }
     private val customMathTagRegex by lazy {
-      Regex("$CUSTOM_MATH_SVG_PATH_ATTRIBUTE\\s*=\\s*\"(.+?)\"")
+      Regex("<\\s*$CUSTOM_MATH_TAG\\b[^>]*?$CUSTOM_MATH_SVG_PATH_ATTRIBUTE\\s*=\\s*\"(.+?)\"")
     }
     val VALID_LANGUAGE_TYPES = LanguageType.values().filter { it.isValid() }
 
@@ -741,24 +735,16 @@ class LocalizationTracker private constructor(
     }
 
     fun extractMathContentsFromHtml(html: String): List<MathContentValue?> {
-      return extractMathTagContentValuesFromHtml(html).map { mathContentJson ->
-        mathContentJson?.let {
+      return customMathTagRegex.findAll(html)
+        .map { it.destructured }
+        .map { (match) -> match.replace("&amp;quot;", "\"") }
+        .map { mathContentJson ->
           try {
-            MathContentValue.parseFromHtmlValue(it)
+            MathContentValue.parseFromHtmlValue(mathContentJson)
           } catch (_: Exception) {
             null
           }
-        }
-      }
-    }
-
-    fun extractMathTagContentValuesFromHtml(html: String): List<String?> {
-      return customMathTagContentRegex.findAll(html).map { tagMatch ->
-        val tagContent = tagMatch.value
-        customMathTagRegex.find(tagContent)
-          ?.destructured?.let { (match) -> match }
-          ?.replace("&amp;quot;", "\"")
-      }.toList()
+        }.toList()
     }
   }
 }
