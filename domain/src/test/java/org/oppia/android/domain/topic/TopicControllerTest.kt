7572f8a17ed9df8bd31c02a6f971e247626a6d26
harshsomankar123-tech <harshsomankar123@gmail.com>
2026-04-02 18:54:23 +0530

    Fix ktlint import ordering in TopicControllerTest.kt

--- domain/src/test/java/org/oppia/android/domain/topic/TopicControllerTest.kt
+++ domain/src/test/java/org/oppia/android/domain/topic/TopicControllerTest.kt
@@ -1,8 +1,5 @@
 package org.oppia.android.domain.topic
 
-import java.util.Locale
-import javax.inject.Inject
-import javax.inject.Singleton
 import android.app.Application
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
@@ -60,6 +57,9 @@ import org.oppia.android.util.logging.SyncStatusModule
 import org.oppia.android.util.networking.NetworkConnectionUtilDebugModule
 import org.robolectric.annotation.Config
 import org.robolectric.annotation.LooperMode
+import java.util.Locale
+import javax.inject.Inject
+import javax.inject.Singleton
 
 private const val INVALID_STORY_ID_1 = "INVALID_STORY_ID_1"
 private const val INVALID_TOPIC_ID_1 = "INVALID_TOPIC_ID_1"
