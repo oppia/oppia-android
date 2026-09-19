c41916c118086ea9c0ac56ceaca5f196274fccb0
Aryan Singh <aaryansssingh1938@gmail.com>
2026-09-19 19:39:57 +0530

    Fix HtmlParserTest compilation error

--- utility/src/test/java/org/oppia/android/util/parser/html/HtmlParserTest.kt
+++ utility/src/test/java/org/oppia/android/util/parser/html/HtmlParserTest.kt
@@ -504,18 +504,24 @@ class HtmlParserTest {
 
   @Test
   fun testHtmlParser_namespacedList_doesNotCreateFalseLinks() {
+    val htmlParser = htmlParserFactory.create(
+      resourceBucketName,
+      entityType = "",
+      entityId = "",
+      imageCenterAlign = true,
+      displayLocale = appLanguageLocaleHandler.getDisplayLocale()
+    )
     val html = "<ul xmlns=\"http://www.w3.org/1999/xhtml\"><li>Test</li></ul>"
 
-    val parsedHtml = htmlParserFactory.create(
-      gcsResourceName,
-      entityType,
-      entityId,
-      imageCenterAlign = true,
-      customStyle = customOppiaStyle
-    ).parseOppiaHtml(html, htmlContentTextView)
-    // Assert that zero link spans were accidentally generated
-    val linkSpans = parsedHtml.getSpans(0, parsedHtml.length, URLSpan::class.java)
-    assertThat(linkSpans).isEmpty()
+    runWithLaunchedActivity {
+      val htmlResult = onActivityWithResult {
+        val textView: TextView = it.findViewById(R.id.test_html_content_text_view)
+        htmlParser.parseOppiaHtml(html, textView)
+      }
+      // Assert that zero link spans were accidentally generated
+      val linkSpans = htmlResult.getSpans(0, htmlResult.length, URLSpan::class.java)
+      assertThat(linkSpans).isEmpty()
+    }
   }
 
   // TODO(#3840): Make this test work on Espresso.
